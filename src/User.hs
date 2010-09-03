8cc86db3ab5c69733c097e89d3912b9b7e4d4ce7
Gracjan Polak <gracjan@skrivapa.se>
2010-09-03 18:26:05 +0000

    Fixed Brians conflict, using skrivapa.rpxnow.com

--- src/User.hs
+++ src/User.hs
@@ -1,4 +1,6 @@
+{-# LANGUAGE CPP #-}
 {-# OPTIONS_GHC -F -pgmFtrhsx #-}
+
 module User 
     ( module UserState
     , withUser
@@ -96,6 +98,7 @@ withUser Nothing action = msum
                              -- this is not going to work right now, stop it!
                             ]
 
+
 userLogin :: (MonadIO m) => ServerPartT m (Maybe User)
 userLogin = do
   maybeuser <- withMSessionDataSP2 $ \maybeuserid -> do 
@@ -165,23 +168,25 @@ userLogin1 = do
 
               maybeuser <- query $ GetUserByEmail (Email verifiedEmail)
     
-              user <- case maybeuser of
-                        Just user -> do
-                          liftIO $ noticeM rootLoggerName $ "User: " ++ BS.toString nameFormatted ++ " <" ++ 
-                                 BS.toString verifiedEmail ++ "> logged in"
-                          return user
-                        Nothing -> do
-                          user <- update $ AddUser nameFormatted verifiedEmail BS.empty Nothing
-                          liftIO $ noticeM rootLoggerName $ "User: " ++ BS.toString nameFormatted ++ " <" ++ 
-                                 BS.toString verifiedEmail ++ "> logged in (new)"
-                          return user
-              sessionid <- update $ NewSession (userid user)
-              startSession sessionid
-              rq <- askRq
-              let link = rqUri rq
-              response <- webHSP (seeOtherXML link)
-              finishWith (redirect 303 link response)
-              return (Just user)
+              case maybeuser of
+                Just user -> do
+                  liftIO $ noticeM rootLoggerName $ "User: " ++ BS.toString nameFormatted ++ " <" ++ 
+                         BS.toString verifiedEmail ++ "> logged in"
+                  sessionid <- update $ NewSession (userid user)
+                  startSession sessionid
+                  rq <- askRq
+                  let link = rqUri rq
+                  response <- webHSP (seeOtherXML link)
+                  finishWith (redirect 303 link response)
+                  return (Just user)
+
+                Nothing -> do
+                  let link = "/login"
+                  response <- webHSP $ seeOtherXML link
+                  finishWith (redirect 303 link response)
+                  return Nothing
+
+
 
 provideRPXNowLink :: (MonadIO m) => ServerPartT m Response
 provideRPXNowLink = do -- FIXME it was guarded by method GET but it didn't help
