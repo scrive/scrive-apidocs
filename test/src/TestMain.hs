f109003ee69974eeeeca054f98002f870f1870e7
brian <brian@ollenberger.com>
2010-09-08 06:42:01 +0000

    Added test for remember me logic.

--- test/src/TestMain.hs
+++ test/src/TestMain.hs
@@ -2,7 +2,9 @@ module Main where
 
 import Test.Framework (defaultMain, testGroup)
 import UserStateTest
+import RememberMeTest
 
 main = defaultMain tests
 
-tests = [testGroup "UserState" userStateTests]
+tests = [testGroup "UserState" userStateTests,
+         testGroup "RememberMe" rememberMeTests]
