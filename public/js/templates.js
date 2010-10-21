aca13fb249621f63f8e41b441b4bcab811fed7ed
ericwnormand <ericwnormand@gmail.com>
2010-10-21 14:42:08 +0000

    can drag (but not drop!), add new signatories, remove signatories

--- public/js/templates.js
+++ public/js/templates.js
@@ -2,15 +2,20 @@
 
 // include this file to start the dragging functionality
 
-function newsignatory(el) {
+function newUUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+	    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+	    return v.toString(16);
+	}).toUpperCase();
+}
+
+function newsignatory() {
     return {name: "", company: "", number: "", email: "", 
 	    nameplacements: [],
 	    companyplacements: [],
 	    numberplacements: [],
 	    emailplacements: [],
-	    otherfields: [],
-
-	    el: el};
+	    otherfields: []};
 }
 
 function newplacement(x, y, page, w, h, el) {
@@ -18,8 +23,12 @@ function newplacement(x, y, page, w, h, el) {
 }
 
 function placementToHTML(label, value) {
+    var v = value;
+    if(!v) {
+	v = label;
+    }
     return $("<div class='placedfield'>"
-	     + (value == null)?label:value +
+	     + v +
 	     "</div>");
 }
 
@@ -32,12 +41,23 @@ function placePlacements(pls, label, value) {
 	});
 }
 
+function newHiddenValue(label, value) {
+    return $("<div style='display: none' class='" + label + "'>" + value + "</div>");
+}
+
 // this var will be kept in sync with the fields.
 
 // var docstate;
 
 function buildDraggableField(info, val) {
-    return $("<div><input type='text' infotext='" + info + "' value='" + val + "' /></div>");
+    var x = $("<div><span class='draghandle'>drag</span><input type='text' infotext='" + info + "' value='" + val + "' /></div>");
+    x.draggable({ handle: ".draghandle"
+		, helper: function (event) {
+		return placementToHTML(info, val);
+	    }
+		});
+
+    return x;
 }
 
 // destroy the HTML that's there to sync it with the docstate
@@ -82,51 +102,81 @@ function docstateToHTML(){
     sl.html("");
 
     $(signatories).each(function () {
-	    var sig = this;
-	    
-	    var d = $("<div class='sigentry'></div>");
-
-	    var aname = buildDraggableField("Namn på motpart", sig.name);
-	    var acomp = buildDraggableField("Titel, företag", sig.company);
-	    var anumb = buildDraggableField("Orgnr/Persnr", sig.number);
-	    var aemai = buildDraggableField("Personens e-mail", sig.email);
-
-	    d.append(aname);
-	    d.append(acomp);
-	    d.append(anumb);
-	    d.append(aemai);
-
-	    // other fields
-
-	    $(sig.otherfields).each(function (){
-		    var fd = this;
-		    var field = buildDraggableField(fd.label, fd.value);
-		    d.append(field);
-		});
-
-	    sl.append(d);
-
-	    var removeLink = $("<small><a href='#'>Ta bort</a></small>");
-	    sl.append(removeLink);
-	    
-
-	    placePlacements(sig.nameplacements, "Avsändare namn på motpart", sig.name);
-	    placePlacements(sig.companyplacements, "Avsändare  titel, företag", sig.company);
-	    placePlacements(sig.numberplacements, "Avsändare Orgnr/Persnr", sig.number);
-	    placePlacements(sig.emailplacements, "Avsändare personens e-mail", sig.email);
-	    
-	    $(sig.otherfields).each(function (){
-		    var fd = this;
-		    placePlacements(fd.placements, fd.label, fd.value);
-		});
+	    signatoryToHTML(this);
 	});
-	    
+   
+}
+
+function signatoryToHTML(sig) {
+    var sl = $("#signatorylist");
+    var sigid = newUUID();
+    sig.id = sigid;
     
+    var sigentry = $("<div class='sigentry'></div>");
+    
+    
+    var d = $("<div class='fields'></div>");
+    
+    var aname = buildDraggableField("Namn på motpart", sig.name);
+    var acomp = buildDraggableField("Titel, företag", sig.company);
+    var anumb = buildDraggableField("Orgnr/Persnr", sig.number);
+    var aemai = buildDraggableField("Personens e-mail", sig.email);
+    
+    d.append(aname);
+    d.append(acomp);
+    d.append(anumb);
+    d.append(aemai);
+    
+    // other fields
+    
+    $(sig.otherfields).each(function (){
+	    var fd = this;
+	    var field = buildDraggableField(fd.label, fd.value);
+	    d.append(field);
+	});
+    
+    sl.append(sigentry);
+    
+    var removeLink = $("<small><a href='#'>Ta bort</a></small>");
+    removeLink.find("a").click(function () {
+	    var ind = -1;
+	    for(i = 0; i < docstate.signatories.length; i++){
+		if(docstate.signatories[i].id = sigid) {
+		    ind = i;
+		    break;
+		}
+	    }
+	    
+	    docstate.signatories.splice(ind, 1);
+	    var link = $(this);
+	    link.parent().parent().detach();
+	    return false;
+	});
+    sigentry.append(d);
+    sigentry.append(removeLink);
+    
+    placePlacements(sig.nameplacements, "Namn på motpart", sig.name);
+    placePlacements(sig.companyplacements, "Titel, företag", sig.company);
+    placePlacements(sig.numberplacements, "Orgnr/Persnr", sig.number);
+    placePlacements(sig.emailplacements, "Personens e-mail", sig.email);
+    
+    $(sig.otherfields).each(function (){
+	    var fd = this;
+	    placePlacements(fd.placements, fd.label, fd.value);
+	});
+}
+
+signatoryadd = function() {
+    var sig = newsignatory();
+    docstate.signatories.push(sig);
+    signatoryToHTML(sig);
+    enableInfoText();
+    return false;
 }
 
 $(document).ready(function () {
 	docstateToHTML();
-
+	
 
 	enableInfoText();
     });
