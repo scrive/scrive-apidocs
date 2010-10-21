40e5d8d74e3b9b8442ddf879f7fb17c0e7a8e26f
Gracjan Polak <gracjan@skrivapa.se>
2010-10-21 12:44:06 +0000

    Layout change in settings

--- src/AppView.hs
+++ src/AppView.hs
@@ -358,7 +358,7 @@ pageFromBody ctx@(Context {ctxmaybeuser,ctxhostpart,ctxflashmessages})
         <div id="loginContainer"> {- new id -}
            <% case ctxmaybeuser of
              Just User{userfullname} -> 
-                 <span id="logout">Välkomen <a href="account"><% userfullname %></a> | <a href="/logout">Logga ut</a></span>
+                 <span id="logout"><a href="account">Välkomen <% userfullname %></a> | <a href="/logout">Logga ut</a></span>
              Nothing -> 
 	         <form action="/login" method="post"> 
 		    <div> 
