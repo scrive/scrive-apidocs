bec0b85c40cb1c032892e9d53ef372d9dd24dc53
ericwnormand <ericwnormand@gmail.com>
2010-10-21 13:20:14 +0000

    more template.js changes

--- public/js/templates.js
+++ public/js/templates.js
@@ -23,12 +23,21 @@ function placementToHTML(label, value) {
 	     "</div>");
 }
 
+function placePlacements(pls, label, value) {
+    $(pls).each(function () {
+	    var pl = this;
+	    var d = placementToHTML(label, value);
+	    d.position({left: pl.x, top: pl.y});
+	    $("#page" + pl.page).append(d);
+	});
+}
+
 // this var will be kept in sync with the fields.
 
 // var docstate;
 
-function buildDraggableField(val) {
-    return $("<div><input type='text' value='" + val + "' /></div>");
+function buildDraggableField(info, val) {
+    return $("<div><input type='text' infotext='" + info + "' value='" + val + "' /></div>");
 }
 
 // destroy the HTML that's there to sync it with the docstate
@@ -40,25 +49,84 @@ function docstateToHTML(){
 
     ad.html("");
 
-    /*
-    $(author.nameplacements).each(function () {
-	    var pl = this;
-	    var d = placementToHTML("Author Name", author.name);
-	    // set offset of d
-	    $("#page" + pl.page).append(d);
-	});
-    */
+    var aname = buildDraggableField("Namn på motpart", author.name);
+    var acomp = buildDraggableField("Titel, företag", author.company);
+    var anumb = buildDraggableField("Orgnr/Persnr", author.number);
+    var aemai = buildDraggableField("Personens e-mail", author.email);
 
+    ad.append(aname);
+    ad.append(acomp);
+    ad.append(anumb);
+    ad.append(aemai);
+
+    // other fields
+
+    $(author.otherfields).each(function (){
+	    var fd = this;
+	    var field = buildDraggableField(fd.label, fd.value);
+	    ad.append(field);
+	});
+
+    placePlacements(author.nameplacements, "Avsändare namn på motpart", author.name);
+    placePlacements(author.companyplacements, "Avsändare  titel, företag", author.company);
+    placePlacements(author.numberplacements, "Avsändare Orgnr/Persnr", author.number);
+    placePlacements(author.emailplacements, "Avsändare personens e-mail", author.email);
+
+    $(author.otherfields).each(function (){
+	    var fd = this;
+	    placePlacements(fd.placements, fd.label, fd.value);
+	});
 
     var signatories = docstate.signatories;
-    $("#signatorylist").html("");
+    var sl = $("#signatorylist");
+    sl.html("");
 
     $(signatories).each(function () {
 	    var sig = this;
+	    
+	    var d = $("<div class='sigentry'></div>");
+
+	    var aname = buildDraggableField("Namn på motpart", sig.name);
+	    var acomp = buildDraggableField("Titel, företag", sig.company);
+	    var anumb = buildDraggableField("Orgnr/Persnr", sig.number);
+	    var aemai = buildDraggableField("Personens e-mail", sig.email);
+
+	    d.append(aname);
+	    d.append(acomp);
+	    d.append(anumb);
+	    d.append(aemai);
+
+	    // other fields
+
+	    $(sig.otherfields).each(function (){
+		    var fd = this;
+		    var field = buildDraggableField(fd.label, fd.value);
+		    d.append(field);
+		});
+
+	    sl.append(d);
+
+	    var removeLink = $("<small><a href='#'>Ta bort</a></small>");
+	    sl.append(removeLink);
+	    
+
+	    placePlacements(sig.nameplacements, "Avsändare namn på motpart", sig.name);
+	    placePlacements(sig.companyplacements, "Avsändare  titel, företag", sig.company);
+	    placePlacements(sig.numberplacements, "Avsändare Orgnr/Persnr", sig.number);
+	    placePlacements(sig.emailplacements, "Avsändare personens e-mail", sig.email);
+	    
+	    $(sig.otherfields).each(function (){
+		    var fd = this;
+		    placePlacements(fd.placements, fd.label, fd.value);
+		});
+	});
 	    
     
 }
 
 $(document).ready(function () {
 	docstateToHTML();
+
+
+	enableInfoText();
     });
