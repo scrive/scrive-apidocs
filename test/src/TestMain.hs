7453a4042ffcc4bbbcaecfab37d1f5dab034887d
Gracjan Polak <gracjan@skrivapa.se>
2010-11-10 16:38:47 +0000

    Removed remnants of rememebr me

--- test/src/TestMain.hs
+++ test/src/TestMain.hs
@@ -2,11 +2,9 @@ module Main where
 
 import Test.Framework (defaultMain, testGroup)
 import UserStateTest
-import RememberMeTest
 import SchedulerTest
 
 main = defaultMain tests
 
 tests = [testGroup "UserState" userStateTests,
-         testGroup "RememberMe" rememberMeTests,
          testGroup "Scheduler" schedulerTests]
