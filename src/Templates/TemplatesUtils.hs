97a597d3bfdc92a5d06815345981a54f24953c1c
ericwnormand <ericwnormand@gmail.com>
2011-01-06 10:12:51 +0000

    TemplateUtils.hs documentation + whitespace

--- src/Templates/TemplatesUtils.hs
+++ src/Templates/TemplatesUtils.hs
@@ -13,25 +13,29 @@
 module Templates.TemplatesUtils
     (wrapHTML,renderActionButton,option,soption,Option(..)) where
 
-
+import Data.Data
+import Data.Typeable
 import KontraLink 
 import Templates.Templates
-import Data.Typeable
-import Data.Data
 
-{- Common templates - should be shared and it seams like a good place fo them -}
-wrapHTML::KontrakcjaTemplates  -> String->IO String
-wrapHTML templates body =  renderTemplate templates  "wrapHTML" [("body",body)]
+{- Common templates - should be shared and it seams like a good place for them -}
 
+{- |
+   Wrap an HTML string so that it becomes the body of an HTML document
+ -}
+wrapHTML :: KontrakcjaTemplates -> String -> IO String
+wrapHTML templates body =  renderTemplate templates "wrapHTML" [("body",body)]
 
-renderActionButton::KontrakcjaTemplates  -> KontraLink -> String -> IO String
-renderActionButton templates  action button = do
-                                        buttonname <- renderTemplate templates  button []
-                                        renderTemplate templates "actionButton" [("action",show action),("buttonname",buttonname)]
+{- |
+   Create an HTML button that links to action with text button
+ -}
+renderActionButton :: KontrakcjaTemplates -> KontraLink -> String -> IO String
+renderActionButton templates action button = do
+  buttonname <- renderTemplate templates button []
+  renderTemplate templates "actionButton" [("action", show action), ("buttonname", buttonname)]
 
-
-
-{- Option one of standard wrappers. It holds two strings and bool. 
+{- | 
+   Option one of standard wrappers. It holds two strings and bool. 
    Quick note on how to use wrappers:
     (1) Lets say we want to print select a user select box
     (2) We take users list and  map over it with option userid username  
@@ -44,10 +48,11 @@ data Option = Option {
               oText:: String,
               oSelected :: Bool
             } deriving (Data, Typeable)
-{- Unselected option easy generation-}
+
+{- | Unselected option easy generation-}
 option::(a->String)->(a->String)->a->Option
 option f g x = Option {oValue=f x, oText=g x, oSelected = False}
 
-{- Selected option easy generation-}
+{- | Selected option easy generation-}
 soption::(a->String)->(a->String)->a->Option
 soption f g x= Option {oValue=f x, oText=g x, oSelected = True}
