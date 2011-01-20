623e444d45e6de879cb94145e127cb6930f4b989
Gracjan Polak <gracjan@skrivapa.se>
2011-01-20 07:06:00 +0000

    Logging made much better than it was before

--- src/AppLogger.hs
+++ src/AppLogger.hs
@@ -1,64 +1,102 @@
-module AppLogger (setupLogger, withLogger, teardownLogger) where
+module AppLogger ( setupLogger
+                 , withLogger
+                 , teardownLogger
+                   
+                 , debug
+                 , mail
+                 , amazon    
+                 , trustWeaver  
+                 , server
+                 ) where
 
 import System.Log.Logger
-    ( Priority(..)
+  ( Priority(..)
     , rootLoggerName
     , setLevel
     , setHandlers
     , updateGlobalLogger
+      , noticeM
     )
 import Control.Exception.Extensible (bracket)
-import System.Log.Handler (close)
+import System.Log.Handler (close, setFormatter)
 import System.Log.Handler.Simple (fileHandler, streamHandler, GenericHandler(..))
+import System.Log.Formatter
 import System.IO (stdout,Handle,hSetEncoding,utf8,hClose)
+import System.Directory
+import Control.Monad.Trans
 
 -- | Opaque type covering all information needed to teardown the logger.
-data LoggerHandle = LoggerHandle { 
-      rootLogHandler   :: GenericHandler Handle
-    , accessLogHandler :: GenericHandler Handle
-    , serverLogHandler :: GenericHandler Handle
-    , mailLogHandler   :: GenericHandler Handle
-    }
+data LoggerHandle = LoggerHandle [GenericHandler Handle]
 
 
 setupLogger = do
-    appLog <- fileHandler "app.log" INFO
-    accessLog <- fileHandler "access.log" INFO
-    mailLog <- fileHandler "mail.log" INFO
+    createDirectoryIfMissing False "log"
+    
+    let fmt = simpleLogFormatter "[$time : $loggername : $prio] $msg"
+        
+    appLog <- fileHandler "log/app.log" INFO
+    accessLog <- fileHandler "log/access.log" INFO
+    mailLog <- fileHandler "log/mail.log" INFO >>= \lh -> return $ setFormatter lh fmt
+    debugLog <- fileHandler "log/debug.log" INFO >>= \lh -> return $ setFormatter lh fmt
+    amazonLog <- fileHandler "log/amazon.log" INFO >>= \lh -> return $ setFormatter lh fmt
+    trustWeaverLog <- fileHandler "log/trustweaver.log" INFO >>= \lh -> return $ setFormatter lh fmt
     stdoutLog <- streamHandler stdout NOTICE
 
     hSetEncoding (privData appLog) utf8
     hSetEncoding (privData accessLog) utf8
     hSetEncoding (privData mailLog) utf8
+    hSetEncoding (privData debugLog) utf8
+    hSetEncoding (privData amazonLog) utf8
+    hSetEncoding (privData trustWeaverLog) utf8
 
     -- Root Log
     updateGlobalLogger
         rootLoggerName
-        (setLevel DEBUG . setHandlers [appLog])
+        (setLevel NOTICE . setHandlers [appLog])
 
     -- Access Log
     updateGlobalLogger
         "Happstack.Server.AccessLog.Combined"
-        (setLevel INFO . setHandlers [accessLog])
+        (setLevel NOTICE . setHandlers [accessLog])
 
     -- Mail Log
     updateGlobalLogger
         "Kontrakcja.Mail"
-        (setLevel INFO . setHandlers [mailLog])
+        (setLevel NOTICE . setHandlers [mailLog])
+
+    -- Amazon Log
+    updateGlobalLogger
+        "Kontrakcja.Amazon"
+        (setLevel NOTICE . setHandlers [amazonLog])
+    
+    -- TrustWeaver Log
+    updateGlobalLogger
+        "Kontrakcja.TrustWeaver"
+        (setLevel NOTICE . setHandlers [trustWeaverLog])
+
+    -- Debug Log
+    updateGlobalLogger
+        "Kontrakcja.Debug"
+        (setLevel NOTICE . setHandlers [debugLog])
 
     -- Server Log
     updateGlobalLogger
         "Happstack.Server"
         (setLevel NOTICE . setHandlers [stdoutLog])
-    return $ LoggerHandle appLog accessLog stdoutLog mailLog
+        
+    return $ LoggerHandle [ appLog
+                          , accessLog
+                          , stdoutLog
+                          , mailLog
+                          , debugLog
+                          , trustWeaverLog
+                          , amazonLog
+                          ]
 
 -- | Tear down the application logger; i.e. close all associated log handlers.
 teardownLogger :: LoggerHandle -> IO ()
-teardownLogger handle = do
-    close $ serverLogHandler handle
-    close $ accessLogHandler handle
-    close $ rootLogHandler   handle
-    close $ mailLogHandler   handle
+teardownLogger (LoggerHandle loggers) = do
+    mapM_ close loggers
 
 -- | Bracket an IO action which denotes the whole scope where the loggers of
 -- the application are needed to installed. Sets them up before running the action
@@ -66,5 +104,17 @@ teardownLogger handle = do
 withLogger :: IO a -> IO a
 withLogger = bracket setupLogger teardownLogger . const
 
-  
-  
+debug :: (MonadIO m) => String -> m ()
+debug msg = liftIO $ noticeM "Kontrakcja.Debug" msg
+
+mail :: (MonadIO m) => String -> m ()
+mail msg = liftIO $ noticeM "Kontrakcja.Mail" msg
+
+amazon :: (MonadIO m) => String -> m ()
+amazon msg = liftIO $ noticeM "Kontrakcja.Amazon" msg
+
+trustWeaver :: (MonadIO m) => String -> m ()
+trustWeaver msg = liftIO $ noticeM "Kontrakcja.TrustWeaver" msg
+
+server :: (MonadIO m) => String -> m ()
+server msg = liftIO $ noticeM "Happstack.Server" msg
