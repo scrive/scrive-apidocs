f27ffdeb9a4997b2461e3b9e3120b2295c9426b7
Gracjan Polak <gracjan@skrivapa.se>
2011-02-16 16:44:08 +0000

    Upgrade to MemCache to use max size limit argument

--- src/MemCache.hs
+++ src/MemCache.hs
@@ -6,31 +6,41 @@ import qualified Data.Map as Map
 import Control.Concurrent.MVar
 import System.Random
 
-
-data MemCache' k v = MemCache' (v -> Int) Int (Map.Map k v)
+data MemCache' k v = MemCache' (v -> Int) Int Int (Map.Map k v)
 
 newtype MemCache k v = MemCache (MVar (MemCache' k v))
 
 {- | Create new memory cache. Supply a memory limit (in bytes)
    and a sizing function. Key type should have 'Ord' and 'Eq' instances
  -}
-new :: (v -> Int) -> IO (MemCache k v)
-new sizefun = do
-  fmap MemCache $ newMVar (MemCache' sizefun 0 Map.empty)
+new :: (v -> Int) -> Int -> IO (MemCache k v)
+new sizefun sizelimit = do
+  fmap MemCache $ newMVar (MemCache' sizefun sizelimit 0 Map.empty)
 
 {- | Put a key value pair into cache. Cache will take care of its own size
    and never cross total size limit of values.
  -}
-put :: (Ord k) => k -> v -> MemCache k v -> IO Bool
+put :: (Ord k) => k -> v -> MemCache k v -> IO ()
 put k v (MemCache mc) = do
-  modifyMVar mc $ \(MemCache' sizefun size mmap) -> do
-               return ((MemCache' sizefun (size + sizefun v) (Map.insert k v mmap)), True)
+  modifyMVar mc $ \(MemCache' sizefun sizelimit size mmap) -> 
+      do
+        let newsize = size + sizefun v
+        mmap' <- if not (Map.null mmap) && newsize > sizelimit
+                 then do
+                   -- now we need to kill one random thing in the cache
+                   r <- randomRIO (0,Map.size mmap - 1)
+                   return $ Map.deleteAt r mmap
+                 else return mmap
+        let mmap'' = Map.insert k v mmap
+        let mc' = MemCache' sizefun sizelimit newsize mmap''
+        
+        return (mc', ())
 
 
 {- | Get a value under a key in cache. This might return 'Nothing'.
  -}
 get :: (Ord k) => k -> MemCache k v -> IO (Maybe v)
 get k (MemCache mc) = do
-  withMVar mc $ \mc@(MemCache' sizefun size mmap) -> do
+  withMVar mc $ \mc@(MemCache' sizefun sizelimit size mmap) -> do
              return $ Map.lookup k mmap
 
