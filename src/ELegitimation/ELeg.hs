be874bb2262d27323e080f089341a1c9404f5209
Gracjan Polak <gracjan@skrivapa.se>
2011-03-04 22:10:24 +0000

    ELeg uses simpler deriving mechanism

--- src/ELegitimation/ELeg.hs
+++ src/ELegitimation/ELeg.hs
@@ -6,10 +6,9 @@ import Happstack.State
 import Doc.DocState
 import MinutesTime
 import Misc
+import Data.Data
 
-$(deriveAll [''Eq, ''Ord, ''Default]
-  [d|
-      data ELegTransaction = ELegTransaction { transactiontransactionid :: String
+data ELegTransaction = ELegTransaction { transactiontransactionid :: String
                                              , transactionnonce :: String
                                              , transactiontbs :: String
                                              , transactionencodedtbs :: String
@@ -18,9 +17,7 @@ $(deriveAll [''Eq, ''Ord, ''Default]
                                              , transactiondocumentid :: DocumentID
                                              , transactionmagichash :: Maybe MagicHash
                                              }
-  |])
+                     deriving (Eq, Ord, Show, Typeable, Data)
 
 $(deriveSerialize ''ELegTransaction)
 instance Version (ELegTransaction)
-
-deriving instance Show ELegTransaction
