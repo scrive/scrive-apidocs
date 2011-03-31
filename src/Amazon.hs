613ed41527e61d907a2a2d41df8a9e53ce04e1ac
andrzej <andrzej@skrivapa.se>
2011-03-31 18:07:21 +0000

    make Amazon -Wall -Werror compliant and beautify its code a bit

--- src/Amazon.hs
+++ src/Amazon.hs
@@ -1,83 +1,84 @@
+{-# OPTIONS_GHC -Wall -Werror #-}
+
 {-
-
 All function related to Amazon Web Services
-
 -}
 
-module Amazon 
-    ( module Network.AWS.Authentication
+module Amazon (
+      module Network.AWS.Authentication
     , urlFromFile
     , uploadFile
     , getFileContents
-    )
-where
+    ) where
 
-import Doc.DocState
+import Happstack.State (update)
 import Network.AWS.Authentication
-import qualified Network.AWS.Authentication as AWS
-import qualified Network.HTTP as HTTP
+import System.FilePath ((</>))
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as BSC
 import qualified Data.ByteString.Lazy as BSL
-import qualified Data.ByteString.Lazy.UTF8 as BSL hiding (length)
 import qualified Data.ByteString.UTF8 as BS hiding (length)
+import qualified Network.AWS.Authentication as AWS
+import qualified Network.HTTP as HTTP
+
+import Doc.DocState
 import Misc (concatChunks)
-import Happstack.State (update)
-import System.FilePath ((</>))
+
 
 urlFromFile :: File -> String
-urlFromFile File{filename,fileid} =
+urlFromFile File{filename, fileid} =
     -- here we use BSC.unpack, as HTTP.urlEncode
     -- does only %-escaping for 8bit values
     "file/" ++ show fileid ++ "/" ++ HTTP.urlEncode (BSC.unpack filename) ++ ".pdf"
 
 uploadFile :: FilePath -> S3Action -> File -> IO ()
-uploadFile docstore@(_:_) ctxs3action@AWS.S3Action{ AWS.s3bucket = "" } File{ fileid, filename, filestorage = FileStorageMemory content } = do
-  let filepath = docstore </> show fileid ++ '-' : BSC.unpack filename ++ ".pdf"
-  BS.writeFile filepath content
-  putStrLn $ "Document saved as " ++ filepath
-  update $ FileMovedToDisk fileid filepath
-  return ()
+uploadFile docstore@(_:_) AWS.S3Action{AWS.s3bucket = ""} File{fileid, filename, filestorage = FileStorageMemory content} = do
+    let filepath = docstore </> show fileid ++ '-' : BSC.unpack filename ++ ".pdf"
+    BS.writeFile filepath content
+    putStrLn $ "Document saved as " ++ filepath
+    _ <- update $ FileMovedToDisk fileid filepath
+    return ()
 
-uploadFile _ ctxs3action@AWS.S3Action{ AWS.s3bucket = (_:_) } file@File{fileid, filestorage = FileStorageMemory content} = do
-  let action = ctxs3action { AWS.s3object = url
-                           , AWS.s3operation = HTTP.PUT
-                           , AWS.s3body = BSL.fromChunks [content]
-                           , AWS.s3metadata = [("Content-Type","application/pdf")]
-                           }
-      url = urlFromFile file
-      bucket = AWS.s3bucket ctxs3action
-  result <- AWS.runAction action
-  case result of
-    Right _ -> do
-                putStrLn $ "AWS uploaded: " ++ bucket ++ "/" ++ url
-                update $ FileMovedToAWS fileid (BS.fromString bucket) (BS.fromString url)
-                return ()
-
-
-    -- FIXME: do much better error handling
-    Left err -> do
-                putStrLn $ "AWS failed to upload: " ++ bucket ++ "/" ++ url
-                print err 
-                return ()
+uploadFile _ ctxs3action@AWS.S3Action{AWS.s3bucket = (_:_)} file@File{fileid, filestorage = FileStorageMemory content} = do
+    let action = ctxs3action {
+          AWS.s3object = url
+        , AWS.s3operation = HTTP.PUT
+        , AWS.s3body = BSL.fromChunks [content]
+        , AWS.s3metadata = [("Content-Type","application/pdf")]
+    }
+        url = urlFromFile file
+        bucket = AWS.s3bucket ctxs3action
+    result <- AWS.runAction action
+    case result of
+         Right _ -> do
+             putStrLn $ "AWS uploaded: " ++ bucket ++ "/" ++ url
+             _ <- update $ FileMovedToAWS fileid (BS.fromString bucket) (BS.fromString url)
+             return ()
+         Left err -> do -- FIXME: do much better error handling
+             putStrLn $ "AWS failed to upload: " ++ bucket ++ "/" ++ url
+             print err 
+             return ()
 
 uploadFile _ _ _ = do
     putStrLn "No uploading/saving to disk as bucket/docstore is ''"
     return ()
 
-
 getFileContents :: S3Action -> File -> IO BS.ByteString
-getFileContents _ File{ filestorage = FileStorageDisk filepath } =
-    BS.readFile filepath `catch` (return . const BS.empty)
+getFileContents _ File{filestorage = FileStorageDisk filepath} =
+    BS.readFile filepath `catch` (\e -> do
+        putStrLn $ show e
+        return BS.empty)
 
-getFileContents s3action File{ filestorage = FileStorageMemory content, filename } = do
-    -- putStrLn $ "getFileContents local " ++ BS.toString filename
+getFileContents _ File{filestorage = FileStorageMemory content} =
     return content
-getFileContents s3action File{ filestorage = FileStorageAWS bucket url } = do
+
+getFileContents s3action File{filestorage = FileStorageAWS bucket url} = do
   -- putStrLn $ "AWS download " ++ BS.toString bucket ++ "/" ++ BS.toString url
-  result <- AWS.runAction (s3action { AWS.s3object = BS.toString url
-                                       , AWS.s3bucket = BS.toString bucket
-                                       })
+  let action = s3action {
+        AWS.s3object = BS.toString url
+      , AWS.s3bucket = BS.toString bucket
+  }
+  result <- AWS.runAction action
   case result of
-    Right rsp -> return (concatChunks (HTTP.rspBody rsp))
-    _ -> error (show result)
+       Right rsp -> return $ concatChunks $ HTTP.rspBody rsp
+       _ -> error (show result)
