bcb1122220b11ba88878009fa1fcd41c7eda5d64
Magnus Carlsson <magnus@carlssonia.org>
2011-04-07 11:47:36 +0000

    trimmed import list

--- src/Contacts/ContactsControl.hs
+++ src/Contacts/ContactsControl.hs
@@ -15,31 +15,10 @@ module Contacts.ContactsControl(
 import Control.Monad.State
 import AppView
 import Happstack.Server hiding (simpleHTTP)
-import Happstack.State (update,query)
-import Misc
 import Kontra
 import HSP (cdata)
 import Contacts.ContactsView
-import Payments.PaymentsState
-import Doc.DocState
-import Data.ByteString.UTF8 (fromString,toString)
-import Data.ByteString (ByteString,empty, hGetContents)
-import qualified Data.ByteString.Char8 as BSC
-import qualified Data.ByteString.Lazy  as L
 import KontraLink
-import Payments.PaymentsControl(readMoneyField,getPaymentChangeChange)
-import MinutesTime
-import System.Directory
-import Data.List (isPrefixOf,sort)
-import User.UserControl
-import User.UserView
-import Data.Maybe
-import Redirect
-import System.Process
-import System.IO (hClose)
-import qualified TrustWeaver as TW
-import Data.Char
-import Happstack.Util.Common
 
 
 showContacts ::Kontra Response
