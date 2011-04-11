155b9a8e3719a2b8d278b636236c74f5da8dd556
ericwnormand <ericwnormand@gmail.com>
2011-04-11 22:02:39 +0000

    first shot at using curl to sign with trustweaver (needs work)

--- scripts/snapshot.sh
+++ scripts/snapshot.sh
@@ -8,7 +8,18 @@ cp -r $repo $filename
 rm -rf $filename/_local
 tar zcf $filename.tar.gz $filename
 #sign with trustweaver
+twcert=/
+twcertpwd=abc
+twurl=http://
+curl -X POST -k --silent --show-error \
+    --cert $twcert:$twcertpwd --cacert $twcert \
+    --data-binary $filename.tar.gz \
+    -H "Content-Type: text/xml; charset=UTF-8" \
+    -H "Expect: 100-continue" \
+    -H "SOAPAction: http://www.trustweaver.com/tsswitch#Sign" \
+    $twurl
 #push to amazon
+
 #check hash from amazon
 #clean up
 rm -r $filename
