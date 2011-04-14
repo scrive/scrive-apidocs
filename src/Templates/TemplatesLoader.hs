d15ad6240f3a7d1ece8b68c810e637be0d4e0270
Gracjan Polak <gracjan@skrivapa.se>
2011-04-14 10:55:46 +0000

    Better indentation in template loader file

--- src/Templates/TemplatesLoader.hs
+++ src/Templates/TemplatesLoader.hs
@@ -11,7 +11,12 @@
 -- 'KontrakcjaTemplates' is alias for template group, but ma be changed in a future
 -----------------------------------------------------------------------------
 module Templates.TemplatesLoader
-(KontrakcjaTemplates, KontrakcjaTemplate, renderTemplateMain,templateList, readTemplates) where
+    ( KontrakcjaTemplates
+    , KontrakcjaTemplate
+    , renderTemplateMain
+    , templateList
+    , readTemplates
+    ) where
 
 import Text.StringTemplate 
 import Text.StringTemplate.Base
@@ -23,8 +28,9 @@ import Data.List
 import Data.Char
 import System.Log.Logger
 import Text.Html (stringToHtmlString)
+
 {-Names of template files -}
-templateFiles::[String]
+templateFiles :: [String]
 templateFiles = ["templates/landpages.st",
                  "templates/modals.st",
                  "templates/flash.st",
@@ -56,89 +62,90 @@ templateFiles = ["templates/landpages.st",
                  "templates/eleg.st" ]
 
 
-type KontrakcjaTemplates =  STGroup String
+type KontrakcjaTemplates = STGroup String
 type KontrakcjaTemplate = StringTemplate String
 
 --This is avaible only for special cases
-renderTemplateMain::(ToSElem a)=>KontrakcjaTemplates ->String->[(String, a)] -> (KontrakcjaTemplate-> KontrakcjaTemplate)->  IO String
+renderTemplateMain :: (ToSElem a) => KontrakcjaTemplates -> String -> [(String, a)]
+                   -> (KontrakcjaTemplate -> KontrakcjaTemplate) -> IO String
 renderTemplateMain ts name params f = do 
-                               let ts' = setEncoderGroup stringToHtmlString ts
-                               let noescape = groupStringTemplates [("noescape", newSTMP "$it$" :: StringTemplate String)]
-                               let mt =  getStringTemplate name $ mergeSTGroups noescape ts'
-                               case mt of 
-                                  Just t -> do
-                                            let t'= f (setManyAttrib params  t)   
-                                            let (e,p,st) = checkTemplateDeep t'
-                                            when (not (null e) || not (null p) || not (null st)) $
-                                                    errorM "Happstack.Server" $ "Template " ++ name ++ " problem with message " ++(show (e,p,st)) 
-                                            return $ render t'
-                                  Nothing -> do
-                                              errorM "Happstack.Server" $ "No template named " ++ name 
-                                              return ""                                   
-readTemplates::IO KontrakcjaTemplates 
-readTemplates =   do
-                   ts <- sequence (map getTemplates templateFiles)
-                   return $ groupStringTemplates (concat ts)
+    let ts' = setEncoderGroup stringToHtmlString ts
+    let noescape = groupStringTemplates [("noescape", newSTMP "$it$" :: StringTemplate String)]
+    let mt =  getStringTemplate name $ mergeSTGroups noescape ts'
+    case mt of 
+        Just t -> do
+                let t'= f (setManyAttrib params  t)   
+                let (e,p,st) = checkTemplateDeep t'
+                when (not (null e) || not (null p) || not (null st)) $
+                     errorM "Happstack.Server" $ "Template " ++ name ++ " problem with message " ++ (show (e,p,st)) 
+                return $ render t'
+        Nothing -> do
+                errorM "Happstack.Server" $ "No template named " ++ name 
+                return ""                                   
 
+readTemplates :: IO KontrakcjaTemplates 
+readTemplates = do
+    ts <- sequence (map getTemplates templateFiles)
+    return $ groupStringTemplates (concat ts)
 
-getTemplates::String -> IO [(String, StringTemplate String)]            
-getTemplates fp= do
-                 handle <- openFile fp ReadMode
-                 hSetEncoding handle utf8
-                 ts <- parseTemplates handle
-                 hClose handle
-                 return $ Prelude.map (fromJust) $ Prelude.filter isJust ts
+getTemplates :: String -> IO [(String, StringTemplate String)]            
+getTemplates fp = do
+    handle <- openFile fp ReadMode
+    hSetEncoding handle utf8
+    ts <- parseTemplates handle
+    hClose handle
+    return $ Prelude.map (fromJust) $ Prelude.filter isJust ts
 
-parseTemplates::Handle-> IO [Maybe (String,StringTemplate String) ]
-parseTemplates handle =  do
-                     e <- hIsEOF handle
-                     if (e) 
-                      then return []
-                      else liftM2 (:) (parseTemplate handle) (parseTemplates handle)
+parseTemplates :: Handle -> IO [Maybe (String,StringTemplate String)]
+parseTemplates handle = do
+    e <- hIsEOF handle
+    if (e) 
+        then return []
+        else liftM2 (:) (parseTemplate handle) (parseTemplates handle)
 
-parseTemplate::Handle->IO (Maybe (String,StringTemplate String))                              
+parseTemplate :: Handle -> IO (Maybe (String,StringTemplate String))                              
 parseTemplate handle = do
-                        ls <- parseLines handle
-                        let (name,t) = break (==  '=') $ head ls 
-                        if (null ls || null (name) || null t)
-                         then return Nothing   
-                         else do
-                              let template = intercalate "\r\n" ((tail t): (tail ls)) 
-                              return $ Just (filter isAlphaNum name,newSTMP template)
-parseLines::Handle->IO [String]                                    
+    ls <- parseLines handle
+    let (name,t) = break (==  '=') $ head ls 
+    if (null ls || null (name) || null t)
+        then return Nothing   
+        else do
+            let template = intercalate "\r\n" ((tail t): (tail ls)) 
+            return $ Just (filter isAlphaNum name,newSTMP template)
+
+parseLines :: Handle -> IO [String]                                    
 parseLines handle = do
-                     l <- hGetLine handle
-                     e <- hIsEOF handle
-                     if (e || isPrefixOf ("#") l) 
-                      then return []
-                      else fmap ((:) l) (parseLines handle)
+    l <- hGetLine handle
+    e <- hIsEOF handle
+    if (e || isPrefixOf ("#") l) 
+        then return []
+        else fmap ((:) l) (parseLines handle)
 
 {- Template checker, printing info about params-}
 templateList :: IO ()
 templateList = do 
-                ts <- fmap concat $ sequence (map getTemplates templateFiles)         
-                let tsnames = map fst ts
-                let tsgroup = groupStringTemplates ts
-                sequence_ $ map (printTemplateData tsgroup) tsnames
+    ts <- fmap concat $ sequence (map getTemplates templateFiles)         
+    let tsnames = map fst ts
+    let tsgroup = groupStringTemplates ts
+    sequence_ $ map (printTemplateData tsgroup) tsnames
                 
-printTemplateData::STGroup String -> String -> IO ()
-printTemplateData tsgroup name =  do 
-                                   let Just t = getStringTemplate name tsgroup
-                                   let (e,p,st) = checkTemplateDeep t
-                                   putStrLn $ name ++ ": " 
-                                   if (not $ null e) 
-                                      then putStrLn $ "PARSE ERROR " ++ (show e)
-                                      else if (not $ null st)  
-                                            then putStrLn $ "MISING SUBTEMPLATES " ++ (show st)
-                                            else 
-                                              do
-                                               sequence_ $ map (putStrLn . ("    " ++ )) p
-                                               putStrLn ""    
+printTemplateData :: STGroup String -> String -> IO ()
+printTemplateData tsgroup name = do 
+    let Just t = getStringTemplate name tsgroup
+    let (e,p,st) = checkTemplateDeep t
+    putStrLn $ name ++ ": " 
+    if (not $ null e) 
+        then putStrLn $ "PARSE ERROR " ++ (show e)
+        else if (not $ null st)  
+             then putStrLn $ "MISING SUBTEMPLATES " ++ (show st)
+             else do
+                 sequence_ $ map (putStrLn . ("    " ++ )) p
+                 putStrLn ""    
 
 
 
 {- For some reasons we SElem a is not of class ToSElem -}
-instance  (Stringable a) => ToSElem (SElem a) where
+instance (Stringable a) => ToSElem (SElem a) where
    toSElem (STR a) = (STR a)    
    toSElem (BS a) = (BS a)    
    toSElem (STSH a) = (STSH a)    
