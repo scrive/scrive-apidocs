3e9baa1c2b87093219e56c8922bc9901f7c42cfb
emily <emily@skrivapa.se>
2011-04-18 14:08:58 +0000

    add in basic design view for contracts (usability)

--- src/InspectXMLInstances.hs
+++ src/InspectXMLInstances.hs
@@ -71,6 +71,8 @@ instance InspectXML CSVUpload where
     inspectXML = asChild . show
 instance InspectXML DocumentType where
     inspectXML = asChild . show
+instance InspectXML DocumentFunctionality where
+    inspectXML = asChild . show
 instance InspectXML ChargeMode where
     inspectXML = asChild . show
 instance InspectXML TimeoutTime where
