a285cb4dacea34c5b51f96d0c9ba2da5b16fe86f
ericwnormand <ericwnormand@gmail.com>
2011-04-20 16:56:20 +0000

    snapshot now signs hash of zip

--- scripts/parsesignresponse.py
+++ scripts/parsesignresponse.py
@@ -10,9 +10,9 @@ def getText(nodelist):
             rc.append(node.data)
     return ''.join(rc)
 
-resp = xml.dom.minidom.parse(sys.stdin)
+resp = xml.dom.minidom.parse(open(sys.argv[1], "r"))
 out = getText(resp.getElementsByTagName("SignedDocument")[0].childNodes)
 
-outfile = open("/tmp/signed.b64", "w")
+outfile = open(sys.argv[2], "w")
 outfile.write(out)
 outfile.close()
