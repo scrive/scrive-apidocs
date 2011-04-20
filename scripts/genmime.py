f5e0a3a3e804f566105af3e3b547ebc38f8167e0
ericwnormand <ericwnormand@gmail.com>
2011-04-20 17:14:51 +0000

    slight changes

--- scripts/genmime.py
+++ scripts/genmime.py
@@ -5,7 +5,7 @@ import base64
 zipfile = open(sys.argv[1], "r").read()
 zipmsg = email.message.Message()
 zipmsg.set_type("text/plain")
-zipmsg.add_header("Content-Disposition", "inline; filename=\"" + sys.argv[1] + "\"")
+zipmsg.add_header("Content-Disposition", "attachment; filename=\"" + sys.argv[1] + "\"")
 zipmsg.set_payload(zipfile)
 
 
