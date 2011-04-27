98e7b751dadef5b0ae720e630c1a333fb9dd0c52
ericwnormand <ericwnormand@gmail.com>
2011-04-27 17:34:49 +0000

    fixed conflict (why?) in DocSeal

--- src/Doc/DocSeal.hs
+++ src/Doc/DocSeal.hs
@@ -97,15 +97,16 @@ personsFromDocument document =
 
 fieldsFromPlacement :: String -> FieldPlacement -> Seal.Field
 fieldsFromPlacement value placement  =
-    let toPtt x = (x * 72 `div` 190) - 5 -- scalling and some replacing
+    let toPttX x = (x * 72 `div` 190) + 1
+        toPttY y = (y * 72 `div` 190) - 8
         w = placementpagewidth placement
         h = placementpageheight placement 
     in    
     Seal.Field { Seal.value = value
-               , Seal.x =  toPtt $ (placementx placement * w) `div` 943
-               , Seal.y =  toPtt $ h - ((placementy placement * h) `div` 1335)
+               , Seal.x =  toPttX $ (placementx placement * w) `div` 943
+               , Seal.y =  toPttY $ h - ((placementy placement * h) `div` 1335)
                , Seal.page = placementpage placement
-               , Seal.w =  w
+               , Seal.w = w
                , Seal.h = h
                }
                
