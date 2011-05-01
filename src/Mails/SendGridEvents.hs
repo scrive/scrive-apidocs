52521561b2df123d39a70e3494f7572b87b1d63d
Gracjan Polak <gracjan@skrivapa.se>
2011-05-01 15:35:35 +0000

    Reformatted and reindented

--- src/Mails/SendGridEvents.hs
+++ src/Mails/SendGridEvents.hs
@@ -6,15 +6,15 @@
 -- Stability   :  development
 -- Portability :  portable
 --
--- Sendgrid events interface. 'handleSendgridEvent' is used when sendgrid contacts us. 
--- We do not, and probably will never use full interface, just simlpyfied version.
--- mailinfo param is set when we are sending mails. 
+-- Sendgrid events interface. 'handleSendgridEvent' is used when sendgrid contacts us.
+-- We do not, and probably will never use full interface, just simplified version.
+-- mailinfo param is set when we are sending mails.
 -----------------------------------------------------------------------------
 module Mails.SendGridEvents(handleSendgridEvent) where
 import Kontra
 import Control.Monad.Trans
 import KontraLink
-import Misc 
+import Misc
 import Data.Maybe
 import qualified Mails.MailsUtil as Mail
 import Doc.DocState
@@ -25,84 +25,78 @@ import Templates.TemplatesUtils
 import Control.Monad.State
 import qualified Data.ByteString.UTF8 as BS (fromString,toString)
 import Data.List (find)
-import System.Log.Logger
+import qualified AppLogger as Log
 
 data SendGridEventType = Delivered | Undelivered | Other String deriving Show
-data SendgridEvent = SendgridEvent {
-                            mailId :: Integer
-                          , event::SendGridEventType
-                          , info::MailInfo
-                         
-                     } deriving Show 
+data SendgridEvent = SendgridEvent
+                     { mailId :: Integer
+                     , event :: SendGridEventType
+                     , info :: MailInfo
+                     } deriving Show
 
 -- | Handler for receving delivery info from sendgrid
-handleSendgridEvent::Kontra KontraLink
+handleSendgridEvent :: Kontra KontraLink
 handleSendgridEvent = do
-                          mid <- readMailId
-                          et <- readEventType                   
-                          mi <- readMailInfo
-                          let ev = SendgridEvent {mailId = mid, event = et, info = mi}
-                          liftIO $ logM "Kontrakcja.Mail" NOTICE  $ "Sendgrid event recived " ++ (show ev)
-                          routeToHandler ev
-                          return $ LinkMain
+  mid <- readMailId
+  et <- readEventType
+  mi <- readMailInfo
+  let ev = SendgridEvent {mailId = mid, event = et, info = mi}
+  Log.mail $ "Sendgrid event recived " ++ (show ev)
+  routeToHandler ev
+  return $ LinkMain
 
-
-readMailId:: Kontra Integer
+readMailId :: Kontra Integer
 readMailId = fmap (fromMaybe 0) $ readField "id"
-                  
-             
-readEventType::Kontra SendGridEventType
+
+
+readEventType :: Kontra SendGridEventType
 readEventType = do
-                  me <- getField "event"
-                  liftIO $ putStrLn $ show me
-                  case me of
-                    Just e ->  return $ fromMaybe (Other e) (lookup e [("delivered", Delivered),("bounce", Undelivered), ("dropped", Undelivered)])
-                    Nothing -> return (Other "")
-                  
-readMailInfo:: Kontra MailInfo
+  me <- getField "event"
+  Log.debug $ show me
+  case me of
+    Just e -> return $ fromMaybe (Other e) (lookup e [("delivered", Delivered),("bounce", Undelivered), ("dropped", Undelivered)])
+    Nothing -> return (Other "")
+
+readMailInfo :: Kontra MailInfo
 readMailInfo = do
-                  mi<- readField "mailinfo"
-                  case mi of 
-                   Just i -> return i
-                   Nothing -> return None
-             
+  mi <- readField "mailinfo"
+  case mi of
+    Just i -> return i
+    Nothing -> return None
 
 
 -- | Main routing table after getting sendgrid event.
-routeToHandler::SendgridEvent -> Kontra ()
-routeToHandler (SendgridEvent {event=Other _}) = return ()                        
-routeToHandler (SendgridEvent {info= None}) = return ()                        
-routeToHandler (SendgridEvent {info=Invitation docid signlinkid,event= Delivered}) = handleDeliveredInvitation docid signlinkid
-routeToHandler (SendgridEvent {info=Invitation docid signlinkid,event= Undelivered}) = handleUndeliveredInvitation docid signlinkid
-
+routeToHandler :: SendgridEvent -> Kontra ()
+routeToHandler (SendgridEvent {event = Other _}) = return ()
+routeToHandler (SendgridEvent {info = None}) = return ()
+routeToHandler (SendgridEvent {info = Invitation docid signlinkid, event = Delivered}) = handleDeliveredInvitation docid signlinkid
+routeToHandler (SendgridEvent {info = Invitation docid signlinkid, event = Undelivered}) = handleUndeliveredInvitation docid signlinkid
 
 
 -- | Actions perform that are performed then
-
-handleDeliveredInvitation::DocumentID -> SignatoryLinkID  -> Kontra ()
+handleDeliveredInvitation :: DocumentID -> SignatoryLinkID -> Kontra ()
 handleDeliveredInvitation docid signlinkid = do
     _ <- update $ SetInvitationDeliveryStatus docid signlinkid Mail.Delivered
     return ()
-    
-handleUndeliveredInvitation::DocumentID -> SignatoryLinkID -> Kontra ()
+
+handleUndeliveredInvitation :: DocumentID -> SignatoryLinkID -> Kontra ()
 handleUndeliveredInvitation docid signlinkid = do
     mdoc <- update $ SetInvitationDeliveryStatus docid signlinkid Mail.Undelivered
     case mdoc of
-     Right doc -> do   
-                 ctx <- get
-                 title <- liftIO $ renderTemplate (ctxtemplates ctx) "invitationMailUndeliveredTitle" ()
-                 Just author <- query $ GetUserByUserID $ unAuthor $ documentauthor doc
-                 let documentauthordetails = signatoryDetailsFromUser author
-                 let fullnameemails = [(signatoryname $ documentauthordetails, signatoryemail $ documentauthordetails)]
-                 let msld = fmap signatorydetails $ find ((==) signlinkid . signatorylinkid ) $ documentsignatorylinks doc
-                 case msld of
-                  Just sld -> do    
-                    content <- liftIO $ wrapHTML (ctxtemplates ctx)=<< renderTemplate (ctxtemplates ctx) "invitationMailUndeliveredContent"
-                                                                       [("authorname",BS.toString $ signatoryname $ documentauthordetails),
-                                                                        ("email",BS.toString $ signatoryemail sld),
-                                                                        ("unsigneddoclink", show $ LinkIssueDoc $ documentid doc),
-                                                                        ("ctxhostpart",ctxhostpart ctx)  
-                                                                       ]  
-                    scheduleEmailSendout (ctxesenforcer ctx) $ emptyMail {title=BS.fromString title,content=BS.fromString content,fullnameemails = fullnameemails}
-                  Nothing -> return () 
-     _ -> return ()                                                                                  
+      Right doc -> do
+        ctx <- get
+        title <- liftIO $ renderTemplate (ctxtemplates ctx) "invitationMailUndeliveredTitle" ()
+        Just author <- query $ GetUserByUserID $ unAuthor $ documentauthor doc
+        let documentauthordetails = signatoryDetailsFromUser author
+        let fullnameemails = [(signatoryname $ documentauthordetails, signatoryemail $ documentauthordetails)]
+        let msld = fmap signatorydetails $ find ((==) signlinkid . signatorylinkid ) $ documentsignatorylinks doc
+        case msld of
+          Just sld -> do
+            content <- liftIO $ wrapHTML (ctxtemplates ctx) =<< (renderTemplate (ctxtemplates ctx) "invitationMailUndeliveredContent" $ do
+              field "authorname" $ BS.toString $ signatoryname $ documentauthordetails
+              field "email" $ BS.toString $ signatoryemail sld
+              field "unsigneddoclink" $ show $ LinkIssueDoc $ documentid doc
+              field "ctxhostpart" $ ctxhostpart ctx)
+            scheduleEmailSendout (ctxesenforcer ctx) $ emptyMail {title=BS.fromString title,content=BS.fromString content,fullnameemails = fullnameemails}
+          Nothing -> return ()
+      _ -> return ()
