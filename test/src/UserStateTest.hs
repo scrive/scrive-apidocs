b7e844e34ca534f1f739956a57252119fadfc054
emily <emily@skrivapa.se>
2011-05-11 12:00:38 +0000

    make unit tests compile

--- test/src/UserStateTest.hs
+++ test/src/UserStateTest.hs
@@ -10,7 +10,7 @@ import StateHelper
 import User.Password
 import User.UserState
 
-import Happstack.State (update, query)
+import Happstack.State
 
 import qualified Data.ByteString.UTF8 as BS
 import qualified Data.ByteString as BS
@@ -54,7 +54,7 @@ test_getUserByEmail_returnsNothing = withTestState $ do
     assert (isNothing queriedUser)
 
 test_getUserByEmail_returnsTheRightUser = withTestState $ do
-    Just user <- update $ AddUser (BS.fromString "Emily", BS.fromString "Green") (BS.fromString "emily@green.com") NoPassword Nothing
+    Just user <- addNewUser "Emily" "Green" "emily@green.com"
     queriedUser <- query $ GetUserByEmail (Email (BS.fromString "emily@green.com"))
     assert (isJust queriedUser) 
     assertEqual "For GetUserByEmail result" user (fromJust queriedUser)
@@ -64,41 +64,52 @@ test_getUserByUserID_returnsNothing = withTestState $ do
     assert (isNothing queriedUser)
 
 test_getUserByUserID_returnsTheRightUser = withTestState $ do
-    Just user <- update $ AddUser (BS.fromString "Emily", BS.fromString "Green") (BS.fromString "emily@green.com") NoPassword Nothing
+    Just user <- addNewUser "Emily" "Green" "emiy@green.com"
     queriedUser <- query $ GetUserByUserID (userid user)
     assert (isJust queriedUser)
     assertEqual "For GetUserByUserID result" user (fromJust queriedUser)
 
 test_getUserSubaccounts_returnsTheRightUsers = withTestState $ do
-    Just user0 <- update $ AddUser (BS.fromString "Emily", BS.fromString "Green") (BS.fromString "emily@green.com") NoPassword (Just (UserID 100))
-    Just user1 <- update $ AddUser (BS.fromString "Bob", BS.fromString "Blue") (BS.fromString "bob@blue.com") NoPassword (Just (UserID 100))
+    Just user0 <- addNewUserWithSupervisor 100 "Emily" "Green" "emily@green.com"
+    Just user1 <- addNewUserWithSupervisor 100 "Bob" "Blue" "bob@blue.com"
     queriedSubAccounts <- query $ GetUserSubaccounts (UserID 100)
     assertEqual "For GetUserSubaccounts result" 2 (Set.size queriedSubAccounts)
     assert $ user0 `Set.member` queriedSubAccounts
     assert $ user1 `Set.member` queriedSubAccounts
 
 test_getUserStats_returnsTheUserCount = withTestState $ do
-    Just user0 <- update $ AddUser (BS.fromString "Emily", BS.fromString "Green") (BS.fromString "emily@green.com") NoPassword Nothing
-    Just user1 <- update $ AddUser (BS.fromString "Bob", BS.fromString "Blue") (BS.fromString "bob@blue.com") NoPassword Nothing
+    Just user0 <- addNewUser "Emily" "Green" "emily@green.com"
+    Just user1 <- addNewUser "Bob" "Blue" "bob@blue.com"
     queriedStats <- query $ GetUserStats
     assertEqual "For GetUserStats" 2 (usercount queriedStats)
 
 test_getAllUsers_returnsAllUsers = withTestState $ do
-    Just user0 <- update $ AddUser (BS.fromString "Emily", BS.fromString "Green") (BS.fromString "emily@green.com") NoPassword (Just (UserID 100))
-    Just user1 <- update $ AddUser (BS.fromString "Bob", BS.fromString "Blue") (BS.fromString "bob@blue.com") NoPassword (Just (UserID 100))
+    Just user0 <- addNewUserWithSupervisor 100 "Emily" "Green" "emily@green.com"
+    Just user1 <- addNewUserWithSupervisor 100 "Bob" "Blue" "bob@blue.com"
     queriedUsers <- query $ GetAllUsers
     assertEqual "For GetAllUsers result" 2 (length queriedUsers)
     assert $ user0 `elem` queriedUsers
     assert $ user1 `elem` queriedUsers
 
 test_setUserPassword_changesPassword = withTestState $ do
-    Just user <- update $ AddUser (BS.fromString "Emily", BS.fromString "Green") (BS.fromString "emily@green.com") NoPassword Nothing
+    Just user <- addNewUser "Emily" "Green" "emily@green.com"
     passwordhash <- (createPassword (BS.fromString "Secret Password!"))
     update $ SetUserPassword user passwordhash
     queriedUser <- query $ GetUserByEmail (Email (BS.fromString "emily@green.com"))
     assert $ verifyPassword (userpassword (fromJust queriedUser)) (BS.fromString "Secret Password!")
 
 test_addUser_repeatedEmailReturnsNothing = withTestState $ do
-  user <- update $ AddUser (BS.fromString "Emily", BS.fromString "Green") (BS.fromString "emily@green.com") NoPassword Nothing
-  result <- update $ AddUser (BS.fromString "Emily", BS.fromString "Green Again") (BS.fromString "emily@green.com") NoPassword Nothing
+  user <- addNewUser "Emily" "Green" "emily@green.com"
+  result <- addNewUser "Emily" "Green Again" "emily@green.com"
   assert (isNothing result)
+
+addNewUserWithSupervisor :: Int -> String -> String -> String -> IO (Maybe User)
+addNewUserWithSupervisor superid = addNewUser' (Just superid)
+
+addNewUser :: String -> String -> String -> IO (Maybe User)
+addNewUser = addNewUser' Nothing
+
+addNewUser' :: Maybe Int -> String -> String -> String -> IO (Maybe User)
+addNewUser' msuperid firstname secondname email = do
+  muser <- update $ AddUser (BS.fromString firstname, BS.fromString secondname) (BS.fromString email) NoPassword (fmap UserID msuperid) Nothing
+  return muser
