4b257f2c402ef90fd6211276445ae6716bfbb56a
mariusz <mariusz@skrivapa.se>
2011-05-31 11:35:39 +0000

    offers not contracts in offers archive

--- src/Doc/DocControl.hs
+++ src/Doc/DocControl.hs
@@ -1381,11 +1381,8 @@ showOfferList :: Kontra (Either KontraLink String)
 showOfferList = checkUserTOSGet $ do
     -- Just user is safe here because we guard for logged in user
     Context {ctxmaybeuser = Just user, ctxtime, ctxtemplates} <- get
-    mydocuments <- query $ GetDocumentsByUser user 
-    usersICanView <- query $ GetUsersByFriendUserID $ userid user
-    friends'Documents <- mapM (query . GetDocumentsByUser) usersICanView
-    let contracts = prepareDocsForList . filter ((==) Offer . documenttype) $ 
-                      mydocuments ++ concat friends'Documents
+    rawContracts <- getDocumentsForUserByType user ((==) Contract . documenttype)
+    let contracts = prepareDocsForList rawContracts
         authornames = map getAuthorName contracts
     params <- getListParams
     liftIO $ pageOffersList ctxtemplates ctxtime user (docAndAuthorSortSearchPage params (zip contracts authornames))
