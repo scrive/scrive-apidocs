47ad6a67ad80b04adf3166f56fbb812589342f61
Gracjan Polak <gracjan@skrivapa.se>
2011-06-03 09:32:16 +0000

    Cleanup import list in Routing.hs

--- src/Routing.hs
+++ src/Routing.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE CPP #-}
 {-# OPTIONS_GHC -Wall #-}
 -----------------------------------------------------------------------------
 -- |
@@ -11,63 +10,18 @@
 -----------------------------------------------------------------------------
 module Routing where
 
---import ELegitimation.BankID as BankID
-import ActionSchedulerState
-import Control.Monad (msum, mzero)
 import Control.Monad.State
-import Control.Monad.Trans (liftIO,lift)
-import Control.Concurrent
 import Data.Functor
 import AppView as V
-import Control.Concurrent
-import Crypto
-import Data.List
 import Data.Maybe
-import Doc.DocState
 import HSP.XML (cdata)
 import Happstack.Server hiding (simpleHTTP,host,body)
-import Happstack.State (query)
-import InspectXML
-import InspectXMLInstances
 import KontraLink
-import MinutesTime
 import Misc
-import Network.Socket
-import Session
-import System.IO.Unsafe
 import Kontra
 import qualified User.UserControl as UserControl
-import User.UserView as UserView
-import qualified Data.ByteString as BS
-import qualified Data.ByteString.Lazy  as L
-import qualified Data.ByteString.UTF8 as BS
-import qualified HSP as HSP
-import qualified Data.Map as Map
-import qualified Network.AWS.Authentication as AWS
-import qualified Network.HTTP as HTTP
-import qualified Network.AWS.AWSConnection as AWS
-import Templates.Templates (readTemplates, renderTemplate, KontrakcjaTemplates, getTemplatesModTime)
-import Control.Concurrent.MVar
-import Mails.MailsConfig
-import Mails.SendGridEvents
-import Mails.SendMail
-import System.Log.Logger (Priority(..), logM)
-import qualified FlashMessage as F
 import qualified AppLogger as Log (error, security, debug)
-import qualified MemCache
-import Happstack.State (update)
 import Redirect
-import PayEx.PayExInterface -- Import so at least we check if it compiles
-import InputValidation
-import System.Directory
-import ListUtil
-import Data.Word
-import System.Time
-import Text.JSON
-import Text.JSON.String
-import Control.Monad.Reader
-import Control.Monad.Error
-import Templates.Templates
 
 type RedirectOrContent = Either KontraLink String
 
