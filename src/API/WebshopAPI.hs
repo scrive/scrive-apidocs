95e33ae018b03d82d19370a9473259b8697a8509
Gracjan Polak <gracjan@skrivapa.se>
2011-06-03 08:57:28 +0000

    Removed useless imports from WebshopAPI

--- src/API/WebshopAPI.hs
+++ src/API/WebshopAPI.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE CPP #-}
 {-# OPTIONS_GHC -Wall #-}
 -----------------------------------------------------------------------------
 -- |
@@ -18,71 +17,25 @@ module API.WebshopAPI
        webshopAPI
     ) where
 
---import ELegitimation.BankID as BankID
-import ActionSchedulerState
-import Control.Monad (msum, mzero)
 import Control.Monad.State
-import Control.Monad.Trans (liftIO,lift)
-import Control.Concurrent
 import Data.Functor
-import AppView as V
-import Control.Concurrent
-import Crypto
-import Data.List
 import Data.Maybe
 import Doc.DocState
-import HSP.XML (cdata)
 import Happstack.Server hiding (simpleHTTP,host,body)
 import Happstack.State (query)
-import InspectXML
-import InspectXMLInstances
 import KontraLink
-import MinutesTime
 import Misc
-import Network.Socket
-import Session
 import System.IO.Unsafe
 import Kontra
-import qualified User.UserControl as UserControl
-import User.UserView as UserView
 import qualified Data.ByteString as BS
-import qualified Data.ByteString.Lazy  as L
 import qualified Data.ByteString.UTF8 as BS
-import qualified Doc.DocControl as DocControl
-import qualified HSP as HSP
-import qualified Data.Map as Map
-import qualified Network.AWS.Authentication as AWS
-import qualified Network.HTTP as HTTP
-import qualified Network.AWS.AWSConnection as AWS
-import qualified TrustWeaver as TW
-import qualified Payments.PaymentsControl as Payments
-import qualified Contacts.ContactsControl as Contacts
-import qualified ELegitimation.BankID as BankID
-import Templates.Templates (readTemplates, renderTemplate, KontrakcjaTemplates, getTemplatesModTime)
-import qualified Administration.AdministrationControl as Administration
-import Control.Concurrent.MVar
-import Mails.MailsConfig
-import Mails.SendGridEvents
-import Mails.SendMail
-import System.Log.Logger (Priority(..), logM)
-import qualified FlashMessage as F
 import qualified AppLogger as Log (error, security, debug)
-import qualified MemCache
 import Happstack.State (update)
-import Redirect
 import PayEx.PayExInterface -- Import so at least we check if it compiles
 import InputValidation
-import System.Directory
-import ListUtil
-import Data.Word
-import System.Time
 import Text.JSON
-import Text.JSON.String
 import Control.Monad.Reader
-import Control.Monad.Error
 import API.API
-import Routing
-import Doc.DocView
 import API.IntegrationAPIUtils
 import Doc.DocUtils
 
