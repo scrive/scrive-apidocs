922c12c4a99a86cfe0b058b52183db9b971244c3
mariusz <mariusz@skrivapa.se>
2011-06-06 09:21:50 +0000

    fix of offer signatory name in javascript

--- public/js/global.js
+++ public/js/global.js
@@ -1294,15 +1294,9 @@ function renumberParts() {
           if (isMultiPart) {
              $(this).find(".partnumber").text("MASSUTSKICK");
           } else if( isSignatory ) {
-              if (offer) {
-		  if( idx==1 ) {
-                      $(this).find(".partnumber").text("AVSÄNDARE");
-		  }
-		  else {
-                      $(this).find(".partnumber").text("MOTTAGARE");
-		  }
-	      }
-              else    
+              if (offer) 
+		         $(this).find(".partnumber").text("MOTTAGARE");
+		      else    
                   $(this).find(".partnumber").text("PART " + idx);
               idx = idx + 1;
           }
