0b05249ca7d39f4f1b845a43380a1c02833ed509
ericwnormand <ericwnormand@gmail.com>
2011-06-10 17:16:20 +0000

    ELeg.hs compiles with -Werror

--- src/ELegitimation/ELeg.hs
+++ src/ELegitimation/ELeg.hs
@@ -1,22 +1,21 @@
-
+{-# OPTIONS_GHC -Wall -fwarn-tabs -fwarn-incomplete-record-updates -fwarn-monomorphism-restriction -fwarn-unused-do-bind -fno-warn-orphans -Werror #-}
 module ELegitimation.ELeg where
 
 import Happstack.Data
-import Happstack.State
+
 import Doc.DocState
 import MinutesTime
 import Misc
-import Data.Data
 
-data ELegTransaction = ELegTransaction { transactiontransactionid :: String
-                                             , transactionnonce :: String
-                                             , transactiontbs :: String
-                                             , transactionencodedtbs :: String
-                                             , transactionservertime :: MinutesTime
-                                             , transactionsignatorylinkid :: Maybe SignatoryLinkID
-                                             , transactiondocumentid :: DocumentID
-                                             , transactionmagichash :: Maybe MagicHash
-                                             }
+data ELegTransaction = ELegTransaction { transactiontransactionid   :: String
+                                       , transactionnonce           :: String
+                                       , transactiontbs             :: String
+                                       , transactionencodedtbs      :: String
+                                       , transactionservertime      :: MinutesTime
+                                       , transactionsignatorylinkid :: Maybe SignatoryLinkID
+                                       , transactiondocumentid      :: DocumentID
+                                       , transactionmagichash       :: Maybe MagicHash
+                                       }
                      deriving (Eq, Ord, Show, Typeable)
 
 $(deriveSerialize ''ELegTransaction)
