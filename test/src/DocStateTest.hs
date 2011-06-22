6a3cec01d39397588efe97f3b8d7ea73e8b0633c
Eric Normand <eric@skrivapa.se>
2011-06-22 14:16:28 -0300

    Tests are slightly easier to read

--- test/src/DocStateTest.hs
+++ test/src/DocStateTest.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE CPP #-}
-{-# OPTIONS_GHC -Wall -fwarn-tabs -fwarn-incomplete-record-updates -fwarn-monomorphism-restriction -fwarn-unused-do-bind -Werror -XOverloadedStrings #-}
+{-# OPTIONS_GHC -Wall -fwarn-tabs -fwarn-monomorphism-restriction -fwarn-unused-do-bind -Werror -XOverloadedStrings #-}
 
 module DocStateTest where
 
@@ -14,8 +14,11 @@ import Doc.DocState
 import Doc.DocUtils
 import MinutesTime
 import Happstack.State
+import Misc
+import Payments.PaymentsState as Payments
 
 import qualified Data.ByteString.UTF8 as BS
+import qualified Data.ByteString as BS
 import Data.Maybe
 import System.IO
 import Control.Monad.Trans
@@ -46,14 +49,11 @@ testNewDocumentDependencies :: Assertion
 testNewDocumentDependencies = do
   -- setup
   mt <- liftIO $ getMinutesTime
-  mauthor <- addNewUser "Eric" "Normand" "eric@skrivapa.se"
-  case mauthor of
-    Nothing -> assertFailure "Cannot create a new user (in setup)"
-    Just author -> do
-      
-      -- execute
-      doc <- update $ NewDocument author "Test New Document No Company" (Signable Contract) mt
-      assertInvariants doc
+  author <- assumingBasicUser
+  -- execute
+  doc <- update $ NewDocument author "Test New Document No Company" (Signable Contract) mt
+  -- assert
+  assertInvariants doc
   
 testDocumentCanBeCreatedAndFetchedByID :: Assertion
 testDocumentCanBeCreatedAndFetchedByID = do
@@ -130,3 +130,58 @@ addNewUser' msuperid firstname secondname email = do
   muser <- update $ AddUser (BS.fromString firstname, BS.fromString secondname) (BS.fromString email) NoPassword (fmap UserID msuperid) Nothing Nothing
   return muser
 
+assumingBasicUser :: IO (User)
+assumingBasicUser = do
+  muser <- addNewUser "Eric" "Normand" "eric@fds.com"
+  case muser of
+    Just user -> return user
+    Nothing -> do
+      assertFailure "Cannot create a new user (in setup)"
+      return blankUser -- should not be possible
+
+blankUser :: User
+blankUser = User {  
+                   userid                  =  UserID 0
+                 , userpassword            =  NoPassword
+                 , usersupervisor          =  Nothing 
+                 , useraccountsuspended    =  False  
+                 , userhasacceptedtermsofservice = Nothing
+                 , userfreetrialexpirationdate = Nothing
+                 , usersignupmethod = AccountRequest
+                 , userinfo = UserInfo {
+                                    userfstname = BS.empty
+                                  , usersndname = BS.empty
+                                  , userpersonalnumber = BS.empty
+                                  , usercompanyname =  BS.empty
+                                  , usercompanyposition =  BS.empty
+                                  , usercompanynumber  =  BS.empty
+                                  , useraddress =  BS.empty
+                                  , userzip = BS.empty
+                                  , usercity  = BS.empty
+                                  , usercountry = BS.empty
+                                  , userphone = BS.empty
+                                  , usermobile = BS.empty
+                                  , useremail =  Email BS.empty 
+                                   }
+                , usersettings  = UserSettings {
+                                    accounttype = PrivateAccount
+                                  , accountplan = Basic
+                                  , signeddocstorage = Nothing
+                                  , userpaymentmethod = Undefined
+                                  , preferreddesignmode = Nothing
+                                  , lang = Misc.defaultValue
+                                  }                   
+                , userpaymentpolicy = Payments.initialPaymentPolicy
+                , userpaymentaccount = Payments.emptyPaymentAccount
+              , userfriends = []
+              , userinviteinfo = Nothing
+              , userlogininfo = LoginInfo
+                                { lastsuccesstime = Nothing
+                                , lastfailtime = Nothing
+                                , consecutivefails = 0
+                                }
+              , userservice = Nothing
+              , usercompany = Nothing
+              , userapikey = Nothing
+              , userrecordstatus = LiveUser
+              }
