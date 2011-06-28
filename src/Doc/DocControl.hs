0842b7098cd9fb042b0ec6055410c3067aa44fad
Gracjan Polak <gracjanpolak@gmail.com>
2011-06-28 19:00:02 +0200

    Changed to admin only for migrateattachments

--- src/Doc/DocControl.hs
+++ src/Doc/DocControl.hs
@@ -2027,9 +2027,7 @@ migrateDocSigLinks = onlySuperUser $ do
 -}
 
 handleMigrateDocumentAuthorAttachments :: Kontra Response
-handleMigrateDocumentAuthorAttachments = do
-  Context { ctxmaybeuser = Just user } <- get
-  guard (useremail (userinfo user) == Email (BS.fromString "ericwnormand@gmail.com"))
+handleMigrateDocumentAuthorAttachments = onlySuperUser $ do
   docs <- query $ GetDocuments Nothing
   forM_ docs (\doc -> do
                          eatts <- query $ GetDocumentsByDocumentID (documentattachments doc)
