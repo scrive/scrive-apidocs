fc3daa9dd347538503c67337cd903aa66c12a436
Andrzej Rybczak <andrzej@skrivapa.se>
2011-07-13 19:37:30 +0200

    make testsuites selectable with cabal flags (using CPP)

--- test/src/TestMain.hs
+++ test/src/TestMain.hs
@@ -8,38 +8,83 @@ import System.Environment.UTF8
 import System.IO
 import Test.Framework
 
+#ifndef NO_DOCSTATE
 import DocStateTest
+#endif
+#ifndef NO_HTML
 import HtmlTest
+#endif
+#ifndef NO_INPUTVALIDATION
 import InputValidationTest
+#endif
+#ifndef NO_LOGIN
 import LoginTest
+#endif
+#ifndef NO_MAILAPI
 import MailAPITest
+#endif
+#ifndef NO_TRUSTWEAVER
 import TrustWeaverTest
+#endif
+#ifndef NO_USERSTATE
 import UserStateTest
+#endif
 
 allTests :: [Test]
-allTests = [
-    docStateTests
-  , htmlTests
-  , inputValidationTests
-  , loginTests
-  , mailApiTests
-  -- everything fails for trustweaver, so commenting out for now
-  --, trustWeaverTests
-  , userStateTests
-  ]
+allTests = tail tests
+    where
+        tests = [
+            undefined
+#ifndef NO_DOCSTATE
+          , docStateTests
+#endif
+#ifndef NO_HTML
+          , htmlTests
+#endif
+#ifndef NO_INPUTVALIDATION
+          , inputValidationTests
+#endif
+#ifndef NO_LOGIN
+          , loginTests
+#endif
+#ifndef NO_MAILAPI
+          , mailApiTests
+#endif
+#ifndef NO_TRUSTWEAVER
+          -- everything fails for trustweaver, so commenting out for now
+          --, trustWeaverTest
+#endif
+#ifndef NO_USERSTATE
+          , userStateTests
+#endif
+          ]
 
 testsToRun :: [String] -> [Either String Test]
 testsToRun [] = []
 testsToRun (t:ts) =
     case map toLower t of
          "all"             -> map Right allTests ++ rest
+#ifndef NO_DOCSTATE
          "docstate"        -> Right docStateTests : rest
+#endif
+#ifndef NO_HTML
          "html"            -> Right htmlTests : rest
+#endif
+#ifndef NO_INPUTVALIDATION
          "inputvalidation" -> Right inputValidationTests : rest
+#endif
+#ifndef NO_LOGIN
          "login"           -> Right loginTests : rest
+#endif
+#ifndef NO_MAILAPI
          "mailapi"         -> Right mailApiTests : rest
+#endif
+#ifndef NO_TRUSTWEAVER
          "trustweaver"     -> Right trustWeaverTests : rest
+#endif
+#ifndef NO_USERSTATE
          "userstate"       -> Right userStateTests : rest
+#endif
          _                 -> Left t : rest
     where
         rest = testsToRun ts
