654d19ac612222bd6ffdca2b8161e5cd19d3695e
Emily <emily@skrivapa.se>
2011-09-06 16:56:24 +0100

    fix some tests i broke with recent dev work

--- src/User/Migrations.hs
+++ src/User/Migrations.hs
@@ -14,7 +14,7 @@ addRegionToUserSettings =
     mgrTable = tableUsers
   , mgrFrom = 1
   , mgrDo = wrapDB $ \conn -> do
-      _ <- run conn "ALTER TABLE users ADD COLUMN region SMALLINT AFTER lang" []
+      _ <- run conn "ALTER TABLE users ADD COLUMN region SMALLINT" []
       _ <- run conn "UPDATE users SET region = ?" [toSql (defaultValue :: Region)]
       _ <- run conn "ALTER TABLE users ALTER COLUMN region SET NOT NULL" []
       return ()
