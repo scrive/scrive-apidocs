a224eac8b7ca9455b677f0a9058a5f153ede8d54
mariusz <mariusz@ubuntu.(none)>
2011-09-08 14:35:59 +0200

    Bugfix for not remembering fields and loosing sorting and filtering in archive Using local session storage.

---
+++ public/js/storage.js
@@ -0,0 +1,23 @@
+/* Simple local storage mechanism 
+ * Only provided methods are set and get.
+ * Always use namespace to avoid confusions
+ * May fail on some browsers so don't use it for critical data, 
+ * rather like a local cache
+ */
+
+(function(window){
+
+window.SessionStorage = {
+    set : function(namespace, field, value ){
+        if (window.sessionStorage != undefined)
+            window.sessionStorage.setItem(namespace + " " + field, value);
+        
+    },
+    get : function(namespace,field) {
+        if (window.sessionStorage != undefined)
+            return window.sessionStorage.getItem(namespace + " " + field);
+        
+    }
+}
+
+})(window); 
