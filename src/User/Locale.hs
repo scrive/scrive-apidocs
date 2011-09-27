d6e2edd18a4e93d5829376abae7be7bd4780ff7d
Emily <emily@skrivapa.se>
2011-09-27 16:32:47 +0100

    oops i forgot this file2

---
+++ src/User/Locale.hs
@@ -0,0 +1,41 @@
+module User.Locale (
+    Locale
+  , mkLocale
+  , mkLocaleFromRegion
+  , getRegion
+  , getLang
+  , HasLocale (..)
+  , targetedLocales
+  ) where
+
+import Misc
+import User.Lang
+import User.Region
+
+data Locale = Locale Region Lang
+  deriving (Bounded, Show, Read, Ord, Eq)
+
+mkLocale :: Region -> Lang -> Locale
+mkLocale region lang = Locale region lang
+
+mkLocaleFromRegion :: Region -> Locale
+mkLocaleFromRegion region = mkLocale region (defaultRegionLang region)
+
+class HasLocale a where
+  getLocale :: a -> Locale
+
+instance HasLocale Locale where
+  getLocale = id
+
+getRegion :: HasLocale a => a -> Region
+getRegion x =
+  let Locale region _lang = getLocale x in
+  region            
+
+getLang :: HasLocale a => a -> Lang
+getLang x =
+  let Locale _region lang = getLocale x in
+  lang
+
+targetedLocales :: [Locale]
+targetedLocales = map mkLocaleFromRegion allValues
