2aeb198b3162c27077ad7e129eded6062d4db6fa
Emily <emily@skrivapa.se>
2011-10-12 11:07:30 +0100

    make the scripts for running selenium tests a bit better

---
+++ selenium-test/runSeleniumTestWithServer.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "$1" == "" ]; then
+  echo "usage: ./build-scripts/runSeleniumTestWithServer.sh [all|<spec-name.rb>]"
+else
+  echo "starting selenium server, expecting you to put jar at: selenium-test/lib/selenium-server-standalone-2.7.0.jar"
+  rm -f selenium-server.out
+  java -jar selenium-test/lib/selenium-server-standalone-2.7.0.jar > selenium-server.out &
+  SSPID=$!
+  echo $SSPID > selenium-server.pid
+  sleep 10
+  
+  ./selenium-test/runSeleniumTest.sh all
+
+  echo "stopping selenium server"
+  kill `more selenium-server.pid`
+  sleep 10
+  rm selenium-server.pid
+fi
