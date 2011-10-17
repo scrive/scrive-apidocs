cc3d3e3e3f234574d4cdaf0cbf78fc08b62da075
Emily <emily@skrivapa.se>
2011-10-17 10:37:23 +0100

    tweak one of the selenium scripts so it ignores none ruby files in the spec dir

--- selenium-test/runSeleniumTest.sh
+++ selenium-test/runSeleniumTest.sh
@@ -3,7 +3,7 @@
 if [ "$1" == "" ]; then
   echo "usage: ./runSeleniumTests.sh [all|<spec-name.rb>]"
 elif [ "$1" == "all" ]; then
-  ls selenium-test/src/specs | awk '{print "spec --colour --format specdoc selenium-test/src/specs/"$1}' | sh
+  ls selenium-test/src/specs/*.rb | awk '{print "spec --colour --format specdoc "$1}' | sh
 else
   spec --colour --format specdoc selenium-test/src/specs/"$1"
 fi
