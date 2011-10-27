de4d81788407061569e7b9cc0d3fad6e57f66d18
Emily <emily@skrivapa.se>
2011-10-27 20:06:29 +0100

    add a script for deploying to dev

---
+++ build-scripts/deployToDev.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+tar -czf kontrakcja-master-dist-$1.tar.gz kontrakcja.cabal dist texts templates public files
+
+scp kontrakcja-master-dist-$1.tar.gz dev@dev.scrive.com:kontrakcja-master-dist-$1.tar.gz
+
+ssh dev@dev.scrive.com rm -r kontrakcja.cabal dist texts templates public files
+ssh dev@dev.scrive.com tar -zxf kontrakcja-master-dist-$1.tar.gz
+
+ssh dev@dev.scrive.com /home/admin/bin/cabal update
+ssh dev@dev.scrive.com /home/admin/bin/cabal install --only-dependencies
+
+ssh dev@dev.scrive.com ./restart.sh
+
+ssh dev@dev.scrive.com rm kontrakcja-master-dist-$1.tar.gz
