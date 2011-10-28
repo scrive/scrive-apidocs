0f411090554c49985f39313527cd9ea033123c05
Emily <emily@skrivapa.se>
2011-10-28 18:33:40 +0100

    try and tweak to help selenium test on build server

--- selenium-test/src/email_helper.rb
+++ selenium-test/src/email_helper.rb
@@ -5,22 +5,22 @@ require "spec/test/unit"
 require "selenium-webdriver"
 
 class EmailHelper
-    
+
   def initialize(ctx, driver, wait)
     @driver = driver
     @ctx = ctx
     @wait = wait
-    
+
     @loginhelper = LoginHelper.new(@ctx, @driver, @wait)
   end
 
   def follow_link_in_latest_mail_for email
-    
+
     link = ""
     @loginhelper.login_as(@ctx.props.tester_email, @ctx.props.tester_password)
     begin
       @driver.get(@ctx.createKontrakcjaURL("/adminonly/backdoor/" + email))
-      @wait.until { @driver.find_element :css => "a" }
+      @wait.until { @driver.find_element :css => ".mainContainer a" }
       link = (@driver.find_elements :css => ".mainContainer a").first.attribute("href")
     ensure
       @loginhelper.logout
