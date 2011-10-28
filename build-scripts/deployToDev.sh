425ea5e556e6dd0d14b2f20a8f00e29258a89b0a
Emily <emily@skrivapa.se>
2011-10-28 11:15:47 +0100

    small build script tweak

--- build-scripts/deployToDev.sh
+++ build-scripts/deployToDev.sh
@@ -4,7 +4,7 @@ tar -czf kontrakcja-master-dist-$1.tar.gz kontrakcja.cabal dist texts templates 
 
 scp kontrakcja-master-dist-$1.tar.gz dev@dev.scrive.com:kontrakcja-master-dist-$1.tar.gz
 
-ssh dev@dev.scrive.com rm -r kontrakcja.cabal dist texts templates public files
+ssh dev@dev.scrive.com rm -rf kontrakcja.cabal dist texts templates public files
 ssh dev@dev.scrive.com tar -zxf kontrakcja-master-dist-$1.tar.gz
 
 ssh dev@dev.scrive.com /home/dev/.cabal/bin/cabal update
