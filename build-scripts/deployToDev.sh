5746764346d6df74ff652c8602081f132d2b0a44
Emily <emily@skrivapa.se>
2011-10-28 11:07:53 +0100

    specify a different cabal in the dev deploy script

--- build-scripts/deployToDev.sh
+++ build-scripts/deployToDev.sh
@@ -7,8 +7,8 @@ scp kontrakcja-master-dist-$1.tar.gz dev@dev.scrive.com:kontrakcja-master-dist-$
 ssh dev@dev.scrive.com rm -r kontrakcja.cabal dist texts templates public files
 ssh dev@dev.scrive.com tar -zxf kontrakcja-master-dist-$1.tar.gz
 
-ssh dev@dev.scrive.com /home/admin/bin/cabal update
-ssh dev@dev.scrive.com /home/admin/bin/cabal install --only-dependencies
+ssh dev@dev.scrive.com /home/dev/.cabal/bin/cabal update
+ssh dev@dev.scrive.com /home/dev/.cabal/bin/cabal install --only-dependencies
 
 ssh dev@dev.scrive.com ./restart.sh
 
