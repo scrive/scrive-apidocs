d83334d46727055c595d17a081086750a8e95003
Gracjan Polak <gracjanpolak@gmail.com>
2011-11-18 19:37:01 +0100

    Improved database handling scripts

--- scripts/pg_restore_database.sh
+++ scripts/pg_restore_database.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
 echo "Executing psql restore as:"
-echo "    gunzip -c $3.gz | psql -d \"user=\'$1\' password=\'\$2\'\" -1 -c - -f $3_update.log"
+echo "    gunzip -c $3.gz | psql -d \"user=\'$1\' password=\'\$2\'\ dbname='$3'\" -1 -c - -f $3_update.log"
 echo " (take that data from kontrakcja.conf)"
 
 gunzip -c $3.gz | psql -d "user='$1' password='$2' dbname='$3'" -1 -f - -L $3_update.log
-
-# psql
-# drop database databasename;
-# \q
-
-# Now create the database again:
-
-# createdb -O databasename username
-
 
