ae29568780e8dac98064c6fe23a360f7fb874329
Gracjan Polak <gracjanpolak@gmail.com>
2011-12-15 13:58:48 +0100

    Do not append sealed file

--- pdfseal/src/SealMain.hs
+++ pdfseal/src/SealMain.hs
@@ -1,4 +1,4 @@
-module Main where
+module SealMain where
 
 import Seal
 import SealSpec
@@ -61,6 +61,48 @@ sampleSealingTexts = SealingTexts
           "Detta verifikat är utfärdat av SkrivaPå CM AB och styrker att dokument nummer $documenttitle$ har undertecknats"
         , "av parterna och är juridiskt bindande. Kursiverad information är säkert verifierad genom vår tjänst."
         , "Kontrollera dokumentet mot vår databas genom följande länk: $hostpart$/d/$documentid$."]
+    }
+
+simple_upsales_confirmation :: SealSpec
+simple_upsales_confirmation = SealSpec
+    { input = "test/upsales-confirmation.pdf"
+    , output = "test/upsales-confirmation-sealed.pdf"
+    , documentNumber = "0000001234"
+    , hostpart = "http://host.skrivapa"
+    , secretaries = [Person
+          { fullname = "Belinda Rossman (secretary)"
+          , email = "belinda@rossman.de"
+          , company = "Rossman, CO"
+          , personalnumber = "435324-222"
+          , companynumber = "435324-222"
+          , fullnameverified = False
+          , emailverified = True
+          , companyverified = False
+          , numberverified = True
+          }]
+    , persons = map (\num ->
+         Person
+          { fullname = "Lukas Duczko " ++ show num
+          , email = "lukas@duczko.se"
+          , company = "CEO, SkrivaPå"
+          , personalnumber = "123456-4567-" ++ show num
+          , companynumber = "00006-4567" ++ show num
+          , fullnameverified = False
+          , emailverified = True
+          , companyverified = False
+          , numberverified = True
+          }) [1..30::Int]
+    , initials = "LD, LD"
+      , history = map (\num -> HistEntry { histdate = "2010-09-" ++ show num ++ " 13:34"
+                                         , histcomment = "I was here and mucked around with PDFs. This is actually a very long line of text so we can really see if the line breaking works or maybe not that good."
+                                         }) [10..99::Int]
+    -- should be in 4 corners, aligned
+    , fields = [ Field {value = "Gracjan Polak", x = 7, y = 7, page = 1, w = 770, h = 1085}
+               , Field {value = "Gracjan Polak", x = 681, y = 7, page = 1, w = 770, h = 1085}
+               , Field {value = "Gracjan Polak", x = 7, y = 1058, page = 1, w = 770, h = 1085}
+               , Field {value = "Gracjan Polak", x = 681, y = 1058, page = 1, w = 770, h = 1085}
+               , Field {value = "gracjan@mail.com", x = 121, y = 347, page = 1,w = 770, h = 1085}]
+    , staticTexts = sampleSealingTexts
     }
 
 main :: IO ()
--- src/Doc/DocState.hs
+++ src/Doc/DocState.hs
@@ -339,11 +339,9 @@ attachSealedFile :: DocumentID
                  -> Update Documents (Either String Document)
 attachSealedFile documentid fid time = do
   modifySignable documentid $ \document -> 
-    Right $ document { documentsealedfiles = documentsealedfiles document ++ [fid] 
-                          , documentmtime = time }
-
-                                          
-      
+    Right $ document { documentsealedfiles = [fid] 
+                     , documentmtime = time
+                     }
 
 changeMainfile :: DocumentID -> FileID -> Update Documents (Either String Document)
 changeMainfile did fid = do
