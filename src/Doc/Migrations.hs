d76afc6fc3bd0db0b4152e23a6d97286f48cb8e1
Magnus Carlsson <magnus@carlssonia.org>
2012-01-06 15:58:22 +0100

    Type mismatch: Haskell (Word32) vs SQL (Integer) for IP addresses

--- src/Doc/Migrations.hs
+++ src/Doc/Migrations.hs
@@ -17,3 +17,27 @@ addNameColumnInSignatoryAttachments =
       _ <- run conn "ALTER TABLE signatory_attachments ADD COLUMN name TEXT NOT NULL DEFAULT ''" []
       return ()
   }
+
+changeIPAddressTypeInDocuments :: Migration
+changeIPAddressTypeInDocuments =
+  Migration
+    { mgrTable = tableDocuments
+    , mgrFrom = 1
+    , mgrDo = wrapDB $ \conn -> do
+      putStrLn "Migrating tableDocuments"
+      _ <- run conn "ALTER TABLE documents ALTER COLUMN invite_ip TYPE BIGINT" []
+      return ()
+  }
+
+changeIPAddressTypeInSignatoryLinks :: Migration
+changeIPAddressTypeInSignatoryLinks =
+  Migration
+    { mgrTable = tableSignatoryLinks
+    , mgrFrom = 1
+    , mgrDo = wrapDB $ \conn -> do
+      putStrLn "Migrating SignatoryLinks"
+      _ <- run conn "ALTER TABLE signatory_links ALTER COLUMN sign_ip TYPE BIGINT" []
+      _ <- run conn "ALTER TABLE signatory_links ALTER COLUMN seen_ip TYPE BIGINT" []
+      return ()
+  }
+
