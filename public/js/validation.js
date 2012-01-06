7a5ca6134e6404305cfd9885a0c90c821ceff594
Mariusz <mariusz@ubuntu.(none)>
2012-01-06 12:34:48 +0100

    Changes to validations

--- public/js/validation.js
+++ public/js/validation.js
@@ -1,78 +1,66 @@
-var Validation = Backbone.Model.extend({
-    addCustomValidation: function(f, c, m) { 
-        this.vlist.push({validates: f, callback: c, message: m}); 
+
+$(function(){
+window.Validation = Backbone.Model.extend({
+    defaults : {
+        validates : function() {return true;},
+        callback : function() {},
+        message : "Validation has failed",
+        next: undefined
     },
-    concat: function(validateObjList) { 
-            var newVList = [];
-            newVList = newVList.concat(this.vlist);
-
-            if (typeof validateObjList == "array")
-                for (var i in validateObjList) 
-                    newVList = newVList.concat(validateObjList[i].vlist);
-            else if (validateObjList.vlist)
-                newVList = newVList.concat(validateObjList.vlist);
-
-            //there's always only one instance so we don't need to define
-            //initialize function
-            var ValidationObject = Validation.extend({vlist: newVList}); 
-
-            return new ValidationObject;
+    concat: function(nextValidation) {
+        if (this.get("next") == undefined)
+            this.set({"next": nextValidation})
+        else
+            this.get("next").concat(nextValidation)
+        return this;
     },
-    validate: function(text, elem) {
-            for (var i in this.vlist) {
-                if (!this.vlist[i].validates(text)) {
-                    if (this.vlist[i].callback)
-                        this.vlist[i].callback(text, elem, this.vlist[i].message);
-
-                    return false;
-                }
+    validateData: function(text, elem) {
+            if (!this.get("validates")(text)) {
+               this.fail(text, elem);
+               return false;       
             }
-
-            return true;
+            else if (this.get("next") != undefined)
+               return this.get("next").validateData(text,elem); 
+            else
+               return true; 
     },
-    fail: function(f, m) {
-        for (var i in this.vlist) {
-            if (typeof f == "function") this.vlist[i].callback = f;
-            if (m) this.vlist[i].message = m;
-        }
+    fail : function(text, elem) {
+        if (this.get("callback") != undefined)
+            return this.get("callback")(text, elem, this);
+    },
+    message: function() {
+        return this.get("message");
     }
 });
 
 //Here we need to define the initialize function because in javascript objects
 //are shared by the reference. If we don't do this there will be always the
 //same callbacks at all instances.
-var NotEmptyValidation = Validation.extend({
-    initialize: function(c, m) {
-        this.vlist = [{
-                validates: function(t) {
+window.NotEmptyValidation = Validation.extend({
+    defaults: {
+           validates: function(t) {
                     if (/^\s*$/.test(t))
                         return false;
                     
                     return true;
-                }, 
-                callback: c,
-                message: m || "The value cannot be empty!"
-        }];
-    }
+            },
+           message: "The value cannot be empty!"
+        }
 });
 
-var EmailValidation = Validation.extend({
-    initialize: function(c, m) {
-        this.vlist = [{
+window.EmailValidation = Validation.extend({
+     defaults: {
             validates: function(t) {
                 if (/^[\w._%+-]+@[\w.-]+[.][a-z]{2,4}$/i.test(t))
                     return true;
                 return false;
             },
-            callback: c,
-            message: m || "Wrong email format!"
-       }];
+            message: "Wrong email format!"
     }
 });
 
-var PasswordValidation = Validation.extend({
-    initialize: function(c, m) {
-        this.vlist = [{
+window.PasswordValidation = Validation.extend({
+    defaults: {
             validates: function(t) {
                 if (t.length < 8 || t.length > 250)
                     return false;
@@ -82,15 +70,12 @@ var PasswordValidation = Validation.extend({
                 
                 return true;
             },
-            callback: c,
-            message: m || "Password must contain one digit and one letter and must be 8 characters long at least!"
-        }];
+            message: "Password must contain one digit and one letter and must be 8 characters long at least!"
     }
 });
 
-var NameValidation = Validation.extend({
-    initialize: function(c, m) {
-        this.vlist = [{
+window.NameValidation = Validation.extend({
+    defaults: {
             validates: function(t) {
                 var t = $.trim(t);
 
@@ -102,9 +87,7 @@ var NameValidation = Validation.extend({
 
                 return true;
             },
-            callback: c,
-            message: m || "Name may contain only alphabetic characters, apostrophe, hyphen or space!"
-        }];
+            message: "Name may contain only alphabetic characters, apostrophe, hyphen or space!"
     }
 });
 
@@ -113,7 +96,7 @@ jQuery.fn.validate = function(validationObject){
     var validates = true;
 
     this.each(function(){
-            if (!validationObject.validate($(this).val(), $(this))) {
+            if (!validationObject.validateData($(this).val(), $(this))) {
                 validates = false;
                 return false;
             }
@@ -124,6 +107,7 @@ jQuery.fn.validate = function(validationObject){
 
 String.prototype.validate = function(validationObject) {
     var validationObject = validationObject || (new NotEmptyValidation);
-    return validationObject.validate(this, this);
+    return validationObject.validateData(this);
 };
 
+});
