8cc0842dcd479503762442f59deb3b1dd13d0471
Gracjan Polak <gracjan@scrive.com>
2012-01-20 22:47:17 +0100

    Fixed timezone setting in script

--- scripts/pg_recreate_database.sh
+++ scripts/pg_recreate_database.sh
@@ -3,5 +3,5 @@
 
 psql -d "user='postgres' dbname='postgres'" -a -c "DROP DATABASE IF EXISTS $1;"
 psql -d "user='postgres' dbname='postgres'" -a -c "CREATE DATABASE $1 WITH OWNER $2;"
-psql -d "user='postgres' dbname='postgres'" -a -c "ALTER DATABASE kontra_kontrakcja SET TIMEZONE = 'UTC';"
+psql -d "user='postgres' dbname='postgres'" -a -c "ALTER DATABASE $1 SET TIMEZONE = 'UTC';"
 
