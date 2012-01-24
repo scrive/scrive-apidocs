eff9bb79ac589dd6c81e85f104da1519e420b184
Eric Normand <ericwnormand@gmail.com>
2012-01-24 12:29:06 -0300

    remove debugs?

--- src/SOAP/SOAP.hs
+++ src/SOAP/SOAP.hs
@@ -135,9 +135,9 @@ makeSoapCall url action cert certpwd request = tryAndJoinEither $ do
        return (Left $ "Cannot execute 'curl' for TrustWeaver: " ++ show args ++ BSL.toString stderr)
     ExitSuccess -> do
       let s = BSL.toString xml
-      Log.debug $ "length of xml string: " ++ (show $ length s)
+      --Log.debug $ "length of xml string: " ++ (show $ length s)
       let rx = readXml s
-      Log.debug $ "left or right: " ++ if isLeft rx then "Left" else "Right"
+      --Log.debug $ "left or right: " ++ if isLeft rx then "Left" else "Right"
       case rx of
         Right (SOAP result) -> return (Right result)
         Right (SOAPFault soapcode string actor) -> return (Left (soapcode ++":" ++ string ++":" ++ actor))
