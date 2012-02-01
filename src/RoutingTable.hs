bb995f2b19c678bcf625ea7d4cfd1088fb3e6288
Emily <emily@skrivapa.se>
2012-02-01 17:25:27 +0000

    bug fix #1193: stop leaking who is a user thru our forgotten password form

--- src/RoutingTable.hs
+++ src/RoutingTable.hs
@@ -411,7 +411,6 @@ forgotPasswordPagePost = do
       case muser of
         Nothing -> do
           Log.security $ "ip " ++ (show $ ctxipnumber ctx) ++ " made a failed password reset request for non-existant account " ++ (BS.toString email)
-          return LoopBack
         Just user -> do
           now <- liftIO getMinutesTime
           minv <- checkValidity now <$> (query $ GetPasswordReminder $ userid user)
@@ -429,8 +428,8 @@ forgotPasswordPagePost = do
             _ -> do -- Nothing or other ActionTypes (which should not happen)
               link <- newPasswordReminderLink user
               sendResetPasswordMail ctx link user
-          addFlashM flashMessageChangePasswordEmailSend
-          return LinkUpload
+      addFlashM flashMessageChangePasswordEmailSend
+      return LinkUpload
 
 sendResetPasswordMail :: Kontrakcja m => Context -> KontraLink -> User -> m ()
 sendResetPasswordMail ctx link user = do
