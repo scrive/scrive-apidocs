05708b476c2902d88c9b03875e631c2844b56534
Emily <emily@skrivapa.se>
2012-02-07 17:41:18 +0000

    oops i got the wrong table in my sql

--- src/Company/Migrations.hs
+++ src/Company/Migrations.hs
@@ -10,7 +10,7 @@ addEmailBrandingToCompany =
     mgrTable = tableCompanies
   , mgrFrom = 1
   , mgrDo = do
-      kRunRaw "ALTER TABLE users ADD COLUMN bars_background TEXT NULL"
-      kRunRaw "ALTER TABLE users ADD COLUMN logo BYTEA NULL"
+      kRunRaw "ALTER TABLE companies ADD COLUMN bars_background TEXT NULL"
+      kRunRaw "ALTER TABLE companies ADD COLUMN logo BYTEA NULL"
       return ()
   }
