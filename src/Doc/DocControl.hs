29a29f9e6f30fb128cdbb21f1540f1ce94e87f94
Mariusz <mariusz@.scrive.com>
2012-03-13 17:07:01 +0100

    Fix for stack overflow- temporary

--- src/Doc/DocControl.hs
+++ src/Doc/DocControl.hs
@@ -146,6 +146,7 @@ postDocumentChangeAction document@Document  { documentstatus
           case enewdoc of
              Right newdoc -> runTemplatesT (ctxlocale, ctxglobaltemplates) $ sendClosedEmails newctx newdoc
              Left errmsg -> Log.error $ "Sealing of document #" ++ show documentid ++ " failed, could not send document confirmations: " ++ errmsg
+        liftIO $ threadDelay 1000 -- TEMP. I'll remove this as fast as possible
         return ()
     -- Pending -> AwaitingAuthor
     -- main action: sendAwaitingEmail
