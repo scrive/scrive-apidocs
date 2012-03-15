5418f1819ba4121e246f9f4bbbe7d9d6c3429ffb
Mariusz <mariusz@.scrive.com>
2012-03-15 14:15:09 +0100

    Language switching fix

--- src/Doc/DocControl.hs
+++ src/Doc/DocControl.hs
@@ -1072,7 +1072,9 @@ jsonDocument did = do
     cttime <- liftIO $ getMinutesTime
     rsp <- case mdoc of
          Nothing -> return $ JSObject $ toJSObject [("error",JSString $ toJSString "No document avaible")]
-         Just doc -> JSObject <$> documentJSON msiglink cttime doc
+         Just doc -> do
+             switchLocale (getLocale doc)
+             JSObject <$> documentJSON msiglink cttime doc
     return rsp
 
 jsonDocumentGetterWithPermissionCheck ::   Kontrakcja m => DocumentID -> m (Maybe Document, Maybe SignatoryLink)
