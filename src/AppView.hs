d05c44c74d98fbb67ef768970890d8dfb1352938
Emily <emily@skrivapa.se>
2012-04-05 11:48:28 +0200

    use https resources where possible

--- src/AppView.hs
+++ src/AppView.hs
@@ -303,8 +303,7 @@ contextInfoFields :: TemplatesMonad m => Context -> Fields m
 contextInfoFields ctx@Context{ ctxlocale } = do
     field "logged" $ isJust (ctxmaybeuser ctx)
     fieldFL "flashmessages" $ map flashMessageFields $ ctxflashmessages ctx
-    field "protocol" $ if (ctxproduction ctx) then "https:" else "http:"
-    field "prefix" ""
+    field "hostpart" $ ctxhostpart ctx
     field "production" (ctxproduction ctx)
     field "ctxregion" $ codeFromRegion (getRegion ctxlocale)
     field "ctxlang" $ codeFromLang (getLang ctxlocale)
