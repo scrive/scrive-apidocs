f55e06773fca2e21af83556b2d7f7e9d0ba33cf4
Andrzej Rybczak <andrzej@skrivapa.se>
2012-04-16 11:17:47 +0200

    add forgotten file

---
+++ inc/DB.hs
@@ -0,0 +1,29 @@
+module DB (
+    module Database.HDBC.ColTypes
+  , module Database.HDBC.SqlValue
+  , module DB.Core
+  , module DB.Derive
+  , module DB.Env
+  , module DB.Exception
+  , module DB.Fetcher
+  , module DB.Functions
+  , module DB.Model
+  , module DB.Nexus
+  , module DB.SQL
+  , module DB.Types
+  , module DB.Utils
+  ) where
+
+import Database.HDBC.ColTypes
+import Database.HDBC.SqlValue
+import DB.Core
+import DB.Derive
+import DB.Env
+import DB.Exception
+import DB.Fetcher
+import DB.Functions
+import DB.Model
+import DB.Nexus
+import DB.SQL
+import DB.Types
+import DB.Utils
