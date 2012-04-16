e658c342e67795055d2744e5506a907699293c77
Andrzej Rybczak <andrzej@skrivapa.se>
2012-04-16 17:38:37 +0200

    mailer: add MonadBase{,Control} IO / remove WebMonad Response instance

--- mailer/src/Mailer.hs
+++ mailer/src/Mailer.hs
@@ -4,14 +4,26 @@ module Mailer (
   ) where
 
 import Control.Applicative
+import Control.Monad.Base
 import Control.Monad.Reader
+import Control.Monad.Trans.Control
 import Happstack.Server
 
+import Control.Monad.Trans.Control.Util
 import Crypto.RNG
 import DB.Core
 
-newtype Mailer a = Mailer { unMailer :: CryptoRNGT (DBT (ServerPartT IO)) a }
-  deriving (Applicative, CryptoRNG, FilterMonad Response, Functor, HasRqData, Monad, MonadDB, MonadIO, MonadPlus, ServerMonad, WebMonad Response)
+type InnerMailer = CryptoRNGT (DBT (ServerPartT IO))
+
+newtype Mailer a = Mailer { unMailer :: InnerMailer a }
+  deriving (Applicative, CryptoRNG, FilterMonad Response, Functor, HasRqData, Monad, MonadBase IO, MonadDB, MonadIO, MonadPlus, ServerMonad)
+
+instance MonadBaseControl IO Mailer where
+  newtype StM Mailer a = StMailer { unStMailer :: StM InnerMailer a }
+  liftBaseWith = newtypeLiftBaseWith Mailer unMailer StMailer
+  restoreM     = newtypeRestoreM Mailer unStMailer
+  {-# INLINE liftBaseWith #-}
+  {-# INLINE restoreM #-}
 
 runMailer :: CryptoRNGState -> Mailer a -> DBT (ServerPartT IO) a
 runMailer rng = runCryptoRNGT rng . unMailer
