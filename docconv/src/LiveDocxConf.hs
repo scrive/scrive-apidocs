3935ff1ba24f2826b8e22a5c24bb30e3ecb39dd5
Emily <emily@skrivapa.se>
2012-04-18 14:30:37 +0200

    well it converts the doc

---
+++ docconv/src/LiveDocxConf.hs
@@ -0,0 +1,20 @@
+module LiveDocxConf (
+    LiveDocxConf(..)
+  ) where
+
+import Configuration
+
+data LiveDocxConf = LiveDocxConf {
+    url :: String
+  , username :: String
+  , password :: String
+} deriving (Read, Show)
+
+instance Configuration LiveDocxConf where
+  confDefault = LiveDocxConf {
+      url = "https://api.livedocx.com/1.2/mailmerge.asmx"
+    , username = "emilymaygreen"
+    , password = "monkey69"
+  }
+  confOptions = []
+  confVerify _ = return $ Right ()
