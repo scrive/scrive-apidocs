477428817a7b38def6fc6d9386bb783589948ec8
Magnus Carlsson <magnus@carlssonia.org>
2012-05-04 10:50:51 +0200

    Factor out common initialization code

--- selenium-test/src/specs/sign_view_basic_reject.rb
+++ selenium-test/src/specs/sign_view_basic_reject.rb
@@ -2,52 +2,40 @@ require "rubygems"
 gem "rspec"
 require "selenium/rspec/spec_helper"
 require "spec/test/unit"
-require "selenium-webdriver"
-require "selenium-test/src/test_properties.rb"
-require "selenium-test/src/test_context.rb"
-require "selenium-test/src/email_helper.rb"
-require "selenium-test/src/login_helper.rb"
-require "selenium-test/src/doc_helper.rb"
+require "selenium-test/src/helpers.rb"
 
 describe "rejecting document" do
 
   before(:each) do
-    @wait = Selenium::WebDriver::Wait.new(:timeout => 60)
-
-    @ctx = TestContext.new
-    @driver = @ctx.createWebDriver
-
-    @emailhelper = EmailHelper.new(@ctx, @driver, @wait)
-    @loginhelper = LoginHelper.new(@ctx, @driver, @wait)
-    @dochelper = DocHelper.new(@ctx, @driver, @wait)
+    @h = Helpers.new
   end
 
   append_after(:each) do
-    @driver.quit
+    @h.quit
   end
 
   it "rejecting contract" do
 
-    @loginhelper.login_as(@ctx.props.tester_email, @ctx.props.tester_password)
+    @h.loginhelper.login_as(@h.ctx.props.tester_email, @h.ctx.props.tester_password)
     begin
-      @dochelper.uploadContract
-      @dochelper.useBasicMode
-      @dochelper.enterCounterpart(@ctx.props.first_counterpart_fstname, @ctx.props.first_counterpart_sndname, @ctx.props.first_counterpart_email)
-      @dochelper.signAndSend
+      @h.dochelper.uploadContract
+      @h.dochelper.useBasicMode
+      @h.dochelper.enterCounterpart(@h.ctx.props.first_counterpart_fstname, @h.ctx.props.first_counterpart_sndname, @h.ctx.props.first_counterpart_email)
+      @h.dochelper.signAndSend
     ensure
-      @loginhelper.logout
+      @h.loginhelper.logout
     end
 
-    @emailhelper.follow_link_in_latest_mail_for @ctx.props.first_counterpart_email
+    @h.emailhelper.follow_link_in_latest_mail_for @h.ctx.props.first_counterpart_email
 
     puts "make sure it's a signatory is in an opened state"
-    @dochelper.checkOpened
+    @h.dochelper.checkOpened
 
     puts "reject the document"
-    (@wait.until { @driver.find_element :css => ".rejectwrapper a" }).click
-    (@wait.until { @driver.find_element :css => ".modal-container a.btn-small.float-right" }).click
+    (@h.wait.until { @h.driver.find_element :css => ".rejectwrapper a" }).click
+    (@h.wait.until { @h.driver.find_element :css => ".modal-container a.btn-small.float-right" }).click
 
     puts "make sure there's a cancelled signatory"
-    @wait.until { @driver.find_elements :css => ".summary.cancelled" }
+    @h.wait.until { @h.driver.find_elements :css => ".summary.cancelled" }
   end
 end
