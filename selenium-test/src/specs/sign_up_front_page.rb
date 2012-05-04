9578df73591415a469904885a45f508eea7e7bca
Magnus Carlsson <magnus@carlssonia.org>
2012-05-04 13:21:03 +0200

    Add test for password reminder

--- selenium-test/src/specs/sign_up_front_page.rb
+++ selenium-test/src/specs/sign_up_front_page.rb
@@ -14,6 +14,20 @@ describe "sign up on front page" do
     @h.quit
   end
 
+  def reset_password(email, password)
+    puts "reset password for "+email+" to "+password
+    @h.driver.get(@h.ctx.createKontrakcjaURL "/")
+    (@h.wait.until { @h.driver.find_element :css => "a.login-button" }).click
+    (@h.wait.until { @h.driver.find_element :css => "a.s-forgot-password" }).click
+    (@h.wait.until { @h.driver.find_element :css => "input.s-forgot-password-email" }).send_keys email
+    (@h.wait.until { @h.driver.find_element :css => "a.s-forgot-password-submit" }).click
+    @h.emailhelper.follow_link_in_latest_mail_for email
+    (@h.wait.until { @h.driver.find_element :name => "password" }).send_keys password
+    (@h.wait.until { @h.driver.find_element :name => "password2" }).send_keys password
+    (@h.wait.until { @h.driver.find_element :css => "a.submit" }).click
+    @h.loginhelper.logout
+  end
+
   it "can be requested on front page and ensures users click the tos and enter their name and a password to activate" do
 
     random_email = rand(10).to_s + rand(10).to_s + rand(10).to_s + rand(10).to_s + rand(10).to_s + "-test@mailinator.com"
@@ -78,6 +92,14 @@ describe "sign up on front page" do
 
     puts "make sure we can login and out as our new user"
     @h.loginhelper.login_as(random_email, "password-12")
+    @h.loginhelper.logout
+
+    puts "reset password for new user"
+    new_password = "reset-password-123"
+    reset_password(random_email, new_password)
+
+    puts "make sure we can login with new password"
+    @h.loginhelper.login_as(random_email, new_password)
     @h.loginhelper.logout
   end
 
