4778b0408963065035d9d88c308ca592ed31b0af
Andrzej Rybczak <andrzej@skrivapa.se>
2012-05-08 19:54:10 +0200

    mail cleaner: fix typos, running frequency and get rid of unnecessary parentheses

--- mailer/src/Cleaner.hs
+++ mailer/src/Cleaner.hs
@@ -14,16 +14,15 @@ import qualified Log (mailingServer)
 cleaner :: CryptoRNGState -> String -> IO ()
 cleaner rng dbconf = do
   res <- E.try . withPostgreSQL dbconf . runCryptoRNGT rng $ do
-    Log.mailingServer $ "Removing mails that were (or should be) send " ++ show daylimit ++ " days ago."
+    Log.mailingServer $ "Removing mails that were (or should be) sent " ++ show daylimit ++ " days ago."
     removedCount <- dbUpdate $ DeleteMailsOlderThenDays daylimit
-    Log.mailingServer $ (show removedCount) ++ " mails were removed."
+    Log.mailingServer $ show removedCount ++ " mails were removed."
   case res of
-    Right () -> Log.mailingServer $ "Cleaner done. Next run in " ++ (show freq) ++ " secunds."
-    Left (e::E.SomeException) -> Log.mailingServer $ "Cleaner failed with " ++ (show $ e) ++ ". Next run in " ++ (show freq) ++ " sec."
+    Right () -> Log.mailingServer $ "Cleaner done. Next run in " ++ show freq ++ " seconds."
+    Left (e::E.SomeException) -> Log.mailingServer $ "Cleaner failed with " ++ show e ++ ". Next run in " ++ show freq ++ " seconds."
   threadDelay $ freq * second
   cleaner rng dbconf
   where
-    freq = 24 * 60
+    freq = 24 * 60 * 60 -- 24 hours
     second = 1000000
     daylimit = 14
-    
