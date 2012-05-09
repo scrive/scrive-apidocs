1d8965810a017f62ebf48f119384a8d5d632c990
Mariusz <mariusz@.scrive.com>
2012-05-09 15:04:03 +0200

    Tests for static resources

--- test/src/TestKontra.hs
+++ test/src/TestKontra.hs
@@ -53,7 +53,7 @@ import User.Locale
 import Util.FinishWith
 import qualified Data.Map as Map
 import qualified Control.Exception.Lifted as E
-
+import qualified Static.Resources as SR
 data TestEnvSt = TestEnvSt {
     teNexus           :: Nexus
   , teRNGState        :: CryptoRNGState
@@ -233,6 +233,7 @@ mkContext locale = do
         , ctxsalesaccounts = []
         , ctxmagichashes = Map.empty
         , ctxmaybepaduser = Nothing
+        , ctxstaticresources = SR.ResourceSetsForImport []
     }
 
 -- pgsql database --
