501dc213330bf8b40ffb5ed1173501acf8d18733
Gracjan Polak <gracjan@scrive.com>
2012-05-16 18:59:56 +0200

    Rewritten Company.Model using SQL2. Now I understand what is going on

--- src/Company/Model.hs
+++ src/Company/Model.hs
@@ -18,10 +18,10 @@ module Company.Model (
 import Data.Typeable
 
 import DB
+import DB.SQL2
 import API.Service.Model
 import Company.CompanyID
-import Company.Tables
-import Misc
+import Control.Monad.State
 
 newtype ExternalCompanyID = ExternalCompanyID { unExternalCompanyID :: String }
   deriving (Eq, Ord, Show)
@@ -54,63 +54,65 @@ data CompanyUI = CompanyUI {
 data GetCompanies = GetCompanies (Maybe ServiceID)
 instance MonadDB m => DBQuery m GetCompanies [Company] where
   query (GetCompanies msid) = do
-    _ <- kRun $ selectCompaniesSQL <++> SQL "WHERE service_id IS NOT DISTINCT FROM ? ORDER BY id DESC" [toSql msid]
+    kRun_ $ sqlSelect "companies" $ do
+       selectCompaniesSelectors
+       sqlWhere $ SQL "service_id IS NOT DISTINCT FROM ?" [toSql msid]
+       sqlOrderBy "id DESC"
     fetchCompanies
 
 data GetCompany = GetCompany CompanyID
 instance MonadDB m => DBQuery m GetCompany (Maybe Company) where
   query (GetCompany cid) = do
-    _ <- kRun $ selectCompaniesSQL <++> SQL "WHERE id = ?" [toSql cid]
+    kRun_ $ sqlSelect "companies" $ do
+      selectCompaniesSelectors
+      sqlWhereEq "id" cid
     fetchCompanies >>= oneObjectReturnedGuard
 
 data GetCompanyByExternalID = GetCompanyByExternalID (Maybe ServiceID) ExternalCompanyID
 instance MonadDB m => DBQuery m GetCompanyByExternalID (Maybe Company) where
   query (GetCompanyByExternalID msid ecid) = do
-    _ <- kRun $ selectCompaniesSQL <++> SQL "WHERE service_id IS NOT DISTINCT FROM ? AND external_id = ?" [toSql msid, toSql ecid]
+    kRun_ $ sqlSelect "companies" $ do
+      selectCompaniesSelectors
+      sqlWhere $ SQL "service_id IS NOT DISTINCT FROM ?" [toSql msid]
+      sqlWhereEq "external_id" ecid
     fetchCompanies >>= oneObjectReturnedGuard
 
 data CreateCompany = CreateCompany (Maybe ServiceID) (Maybe ExternalCompanyID)
 instance MonadDB m => DBUpdate m CreateCompany Company where
   update (CreateCompany msid mecid) = do
-    _ <- kRun $ mkSQL INSERT tableCompanies
-      [ sql "external_id" mecid
-      , sql "service_id" msid
-      , sql "name" ""
-      , sql "number" ""
-      , sql "address" ""
-      , sql "zip" ""
-      , sql "city" ""
-      , sql "country" ""
-      ] <++> SQL (" RETURNING " ++ selectCompaniesSelectors) []
+    kRun_ $ sqlInsert "companies" $ do
+      sqlSet "external_id" mecid
+      sqlSet "service_id" msid
+      sqlSet "name" ""
+      sqlSet "number" ""
+      sqlSet "address" ""
+      sqlSet "zip" ""
+      sqlSet "city" ""
+      sqlSet "country" ""
+      selectCompaniesSelectors
     fetchCompanies >>= exactlyOneObjectReturnedGuard
 
 data SetCompanyInfo = SetCompanyInfo CompanyID CompanyInfo
 instance MonadDB m => DBUpdate m SetCompanyInfo Bool where
   update (SetCompanyInfo cid CompanyInfo{..}) =
-    kRun01 $ mkSQL UPDATE tableCompanies [
-        sql "name" companyname
-      , sql "number" companynumber
-      , sql "address" companyaddress
-      , sql "zip" companyzip
-      , sql "city" companycity
-      , sql "country" companycountry
-      , sql "email_domain" companyemaildomain
-      ] <++> SQL "WHERE id = ?" [toSql cid]
+    kRun01 $ sqlUpdate "companies" $ do
+      sqlSet "name" companyname
+      sqlSet "number" companynumber
+      sqlSet "address" companyaddress
+      sqlSet "zip" companyzip
+      sqlSet "city" companycity
+      sqlSet "country" companycountry
+      sqlSet "email_domain" companyemaildomain
+      sqlWhereEq "id" cid
 
 data UpdateCompanyUI = UpdateCompanyUI CompanyID CompanyUI
 instance MonadDB m => DBUpdate m UpdateCompanyUI Bool where
   update (UpdateCompanyUI cid cui) = do
-    kPrepare $ "UPDATE companies SET"
-      ++ "  bars_background = ?"
-      ++ ", bars_textcolour = ?"
-      ++ ", logo = decode(?, 'base64')"
-      ++ "  WHERE id = ?"
-    kExecute01 [
-        toSql $ companybarsbackground cui
-      , toSql $ companybarstextcolour cui
-      , toSql $ companylogo cui
-      , toSql cid
-      ]
+    kRun01 $ sqlUpdate "companies" $ do
+      sqlSet "bars_background" $ companybarsbackground cui
+      sqlSet "bars_textcolour" $ companybarstextcolour cui
+      sqlSetCmd "logo" $ SQL "decode(?, 'base64')" [toSql $ companylogo cui]
+      sqlWhereEq "id" cid
 
 data GetOrCreateCompanyWithExternalID = GetOrCreateCompanyWithExternalID (Maybe ServiceID) ExternalCompanyID
 instance MonadDB m => DBUpdate m GetOrCreateCompanyWithExternalID Company where
@@ -123,34 +125,37 @@ instance MonadDB m => DBUpdate m GetOrCreateCompanyWithExternalID Company where
 data SetCompanyEmailDomain = SetCompanyEmailDomain CompanyID (Maybe String)
 instance MonadDB m => DBUpdate m SetCompanyEmailDomain Bool where
   update (SetCompanyEmailDomain cid mdomain) = do
-    kPrepare $ "UPDATE companies SET email_domain = ? WHERE id = ? AND NOT EXISTS (SELECT 1 FROM companies WHERE email_domain = ?)"
-    kExecute01 [toSql mdomain, toSql cid, toSql mdomain]
+    kRun01 $ sqlUpdate "companies" $ do
+      sqlSet "email_domain" mdomain
+      sqlWhereEq "id" cid
+      sqlWhere $ SQL "NOT EXISTS (SELECT 1 FROM companies WHERE email_domain = ?)" [toSql mdomain]
 
 data GetCompanyByEmailDomain = GetCompanyByEmailDomain String
 instance MonadDB m => DBQuery m GetCompanyByEmailDomain (Maybe Company) where
   query (GetCompanyByEmailDomain domain) = do
-    _ <- kRun $ selectCompaniesSQL <++> SQL "WHERE email_domain = ?" [toSql domain]
+    kRun_ $ sqlSelect "companies" $ do
+      selectCompaniesSelectors
+      sqlWhereEq "email_domain" domain
     fetchCompanies >>= oneObjectReturnedGuard
 
 -- helpers
 
-selectCompaniesSelectors :: String
-selectCompaniesSelectors = "id"
-  ++ ", external_id"
-  ++ ", service_id"
-  ++ ", name"
-  ++ ", number"
-  ++ ", address"
-  ++ ", zip"
-  ++ ", city"
-  ++ ", country"
-  ++ ", bars_background"
-  ++ ", bars_textcolour"
-  ++ ", encode(logo, 'base64')"
-  ++ ", email_domain"
+selectCompaniesSelectors :: (SqlResult command) => State command ()
+selectCompaniesSelectors = do
+  sqlResult "id"
+  sqlResult "external_id"
+  sqlResult "service_id"
+  sqlResult "name"
+  sqlResult "number"
+  sqlResult "address"
+  sqlResult "zip"
+  sqlResult "city"
+  sqlResult "country"
+  sqlResult "bars_background"
+  sqlResult "bars_textcolour"
+  sqlResult "encode(logo, 'base64') AS logo_base64"
+  sqlResult "email_domain"
 
-selectCompaniesSQL :: SQL
-selectCompaniesSQL = SQL ("SELECT " ++ selectCompaniesSelectors ++ " FROM companies ") []
 
 fetchCompanies :: MonadDB m => DBEnv m [Company]
 fetchCompanies = foldDB decoder []
