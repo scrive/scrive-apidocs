d9cd60897ba0717a7b6e45767787f3ef148f5e0d
Mariusz <mariusz@.scrive.com>
2012-06-12 11:38:49 +0200

    New version of GT

--- src/GuardTime.hs
+++ src/GuardTime.hs
@@ -29,6 +29,10 @@ data GuardTimeConf = GuardTimeConf
 guardTimeJars :: [String]
 guardTimeJars =
   [ "GTJavaPDF-0.3.1.jar"
+  , "PdfBundler-0.3.1.jar"
+  , "PdfExtender-0.3.1.jar"
+  , "PdfSigner-0.3.1.jar"
+  , "PdfVerifier-0.3.1.jar"
   , "bcprov-ext-jdk15on-146.jar"
   , "iText-2.1.7.jar"
   , "GTJavaSDK-0.4.4.jar"
@@ -41,6 +45,7 @@ digitallySign conf inputFileName = do
              , intercalate ":" (map ("GuardTime/" ++) guardTimeJars)
              , "com.guardtime.pdftools.PdfStamper"
              , "-i"
+             , "-n", " Scrive "
              , "-s", guardTimeURL conf
              , "-f"
              , inputFileName
