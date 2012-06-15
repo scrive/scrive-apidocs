08a67cfd66caa1ab0a1a9680e9eedd7816e0bca9
Gracjan Polak <gracjan@scrive.com>
2012-06-15 18:14:24 +0200

    Added author attachment to seal page

--- src/Doc/DocSeal.hs
+++ src/Doc/DocSeal.hs
@@ -166,10 +166,47 @@ listAttachmentsFromDocument document =
 
 findOutAttachmentDesc :: (KontraMonad m, MonadIO m, MonadDB m, TemplatesMonad m) => Document -> m [Seal.FileDesc]
 findOutAttachmentDesc document = do
-  mapM findAttachmentsForSignatoryAttachment attAndSigsNumbered
+  a <- mapM findAttachmentsForAuthorAttachment authorAttsNumbered
+  b <- mapM findAttachmentsForSignatoryAttachment attAndSigsNumbered
+  return (a ++ b)
   where
       attAndSigs = listAttachmentsFromDocument document
-      attAndSigsNumbered = zipWith (\num (at,sl) -> (num,at,sl)) [1::Int ..] attAndSigs
+      authorAtts = documentauthorattachments document
+      authorAttsNumbered = zip [1::Int ..] authorAtts
+      attAndSigsNumbered = zipWith (\num (at,sl) -> (num,at,sl)) [(length authorAtts + 1) ..] attAndSigs
+
+      authorName = fromMaybe "" $ fmap getSmartName $ getAuthorSigLink document
+
+      findAttachmentsForAuthorAttachment (num, authorattach) = do
+        let fileid' = authorattachmentfile authorattach
+        (numberOfPages,name) <- do
+          contents <- getFileIDContents fileid'
+          mfile <- dbQuery $ GetFileByFileID fileid'
+          let name' = maybe "" filename mfile
+          return (getNumberOfPDFPages contents, name')
+        numberOfPagesText <-
+          if numberOfPages==1
+           then renderLocalTemplate document "_numberOfPagesIs1" $ return ()
+           else renderLocalTemplate document "_numberOfPages" $ do
+             F.value "pages" numberOfPages
+
+        attachedByText <- renderLocalTemplate document "_documentAttachedBy" $ do
+                                     F.value "author" authorName
+
+        attachmentNumText <- renderLocalTemplate document "_attachedDocument" $ do
+                                     F.value "number" num
+
+        return $ Seal.FileDesc
+                 { fileTitle      = removeExtIfAny name
+                 , fileRole       = attachmentNumText
+                 , filePagesText  = numberOfPagesText
+                 , fileAttachedBy = attachedByText
+                 }
+
+      removeExtIfAny fname = case dropWhile (/= '.') (reverse fname) of
+                               "" -> fname
+                               x -> reverse (drop 1 x)
+
       findAttachmentsForSignatoryAttachment (num, sigattach, sl) = do
         let personName = getSmartName sl
         numberOfPages <- case signatoryattachmentfile sigattach of
