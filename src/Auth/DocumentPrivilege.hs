1f52a6f67a8d172ed08a4b3524626af8fd2d268c
Mariusz <mariusz@.scrive.com>
2012-06-22 14:47:39 +0200

    Removal of unussed stuff

--- src/Auth/DocumentPrivilege.hs
+++
@@ -1,8 +0,0 @@
--- | Privileges that have to do with a specific, existing document
-
-module Auth.DocumentPrivilege where
-
-data DocumentPrivilege = DocumentSend -- ^ Send invitations/reminders/etc
-                       | DocumentView -- ^ View a document
-                       | DocumentEdit
-               deriving (Show, Eq)
