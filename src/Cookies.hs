7e0e7da5cc851e952c4ffb9946e5ae438cf9efc1
Andrzej Rybczak <andrzej@skrivapa.se>
2012-07-03 17:14:19 +0200

    simplify Cookies module

--- src/Cookies.hs
+++ src/Cookies.hs
@@ -1,29 +1,13 @@
 module Cookies where
 
-import Control.Monad.State hiding (State)
-import Happstack.Server.Types (addHeader)
-import Happstack.Server (Response)
-import qualified Happstack.Server.Internal.Cookie as HSI
-import qualified Happstack.Server.Internal.Monads as HSI
+import Control.Monad.IO.Class
+import Happstack.Server as HS
 
 -- | Add a non-http only cookie
-addCookie :: (MonadIO m, HSI.FilterMonad Response m) => Bool -> HSI.CookieLife -> HSI.Cookie -> m ()
-addCookie issecure life cookie = do
-    l <- liftIO $ HSI.calcLife life
-    let secure = if issecure
-                    then ";Secure"
-                    else ""
-    addHeaderM "Set-Cookie" $ HSI.mkCookieHeader l cookie ++ secure
-    where
-        addHeaderM a v = HSI.composeFilter $ \res -> addHeader a v res
+addCookie :: (MonadIO m, FilterMonad Response m) => Bool -> CookieLife -> Cookie -> m ()
+addCookie issecure life cookie =
+  HS.addCookie life $ cookie { secure = issecure, httpOnly = False }
 
--- | Ripped from Happstack-Server and modified to support HttpOnly cookies
-addHttpOnlyCookie :: (MonadIO m, HSI.FilterMonad Response m) => Bool -> HSI.CookieLife -> HSI.Cookie -> m ()
+addHttpOnlyCookie :: (MonadIO m, FilterMonad Response m) => Bool -> CookieLife -> Cookie -> m ()
 addHttpOnlyCookie issecure life cookie = do
-    l <- liftIO $ HSI.calcLife life
-    let secure = if issecure
-                    then ";Secure"
-                    else ""
-    (addHeaderM "Set-Cookie") $ HSI.mkCookieHeader l cookie ++ ";HttpOnly" ++ secure
-    where
-        addHeaderM a v = HSI.composeFilter $ \res-> addHeader a v res
+  HS.addCookie life $ cookie { secure = issecure, httpOnly = True }
