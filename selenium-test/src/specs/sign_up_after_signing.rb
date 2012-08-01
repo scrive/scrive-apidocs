1ea2c86f756f5037b11734159a2921b94e4d4dcd
Mariusz <mariusz@.scrive.com>
2012-08-01 15:19:38 +0200

    Selenium checkup

--- selenium-test/src/specs/sign_up_after_signing.rb
+++ selenium-test/src/specs/sign_up_after_signing.rb
@@ -56,10 +56,10 @@ describe "sign up after signing a document" do
 
     puts "should be logged in"
     @h.wait.until { @h.driver.find_element :css => ".save.done" }
-
+    sleep 1
     puts "go to the server"
     (@h.wait.until { @h.driver.find_element :css => ".start.btn" }).click();
-
+    
     @h.loginhelper.logout
 
     puts "make sure we can login and out as our new user"
