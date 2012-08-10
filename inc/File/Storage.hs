7bc77b5ecd3109a1bee64d610067f13b32dbc0e8
Andrzej Rybczak <andrzej@skrivapa.se>
2012-08-10 16:43:50 +0200

    make fileGetContents return Nothing upon failure, not empty string

--- inc/File/Storage.hs
+++ inc/File/Storage.hs
@@ -13,8 +13,7 @@ import File.Model
 import File.File
 import qualified Data.ByteString as BS
 import qualified MemCache as MemCache
-
-
+import qualified Log
 
 {- Gets file content from somewere (Amazon for now), putting it to cache and returning as BS -}
 getFileContents :: (KontraMonad m, MonadIO m) => File -> m BS.ByteString
@@ -25,8 +24,13 @@ getFileContents file = do
       Just content -> return content
       Nothing -> do
         mcontentAWS <- liftIO $ AWS.getFileContents (ctxs3action ctx) file
-        MemCache.put (fileid file) mcontentAWS (ctxfilecache ctx)
-        return mcontentAWS
+        case mcontentAWS of
+          Nothing -> do
+            Log.debug $ "Couldn't get content for file " ++ show (fileid file) ++ ", returning empty ByteString."
+            return BS.empty
+          Just contentAWS -> do
+            MemCache.put (fileid file) contentAWS (ctxfilecache ctx)
+            return contentAWS
 
 getFileIDContents :: (KontraMonad m, MonadDB m) => FileID -> m BS.ByteString
 getFileIDContents fid = do
