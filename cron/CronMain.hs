377fc99bc75355a4c3baf8af4aed47907adf0a2f
Andrzej Rybczak <andrzej@skrivapa.se>
2012-08-16 22:58:45 +0200

    cron: use TVar instead of MVar for more sophisticated control

--- cron/CronMain.hs
+++ cron/CronMain.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE CPP #-}
 module CronMain where
 
+import Control.Applicative
 import Control.Concurrent
 import Control.Monad
 import Control.Monad.Trans
 import System.Environment
+import qualified Control.Concurrent.Thread.Group as TG
 import qualified System.Time
 
 import ActionQueue.EmailChangeRequest
@@ -44,38 +46,42 @@ main = Log.withLogger $ do
     runDBEnv $ defineMany kontraFunctions
 
   templates <- newMVar =<< liftM2 (,) getTemplatesModTime readGlobalTemplates
+  tg <- TG.new
   rng <- newCryptoRNGState
 
   let runScheduler = runQueue rng (dbConfig appConf) (SchedulerData appConf templates)
-      inDB = withPostgreSQL (dbConfig appConf) . runCryptoRNGT rng
-  t1 <- forkIO . cron (60 * 10) $ do
+      inDB = liftIO . withPostgreSQL (dbConfig appConf) . runCryptoRNGT rng
+  t1 <- forkCron tg "timeoutDocuments" (60 * 10) $ do
     Log.cron "Running timeoutDocuments..."
     runScheduler timeoutDocuments
-  t2 <- forkIO . cron (60 * 60) $ do
+  t2 <- forkCron tg "EmailChangeRequests" (60 * 60) $ do
     Log.cron "Evaluating EmailChangeRequest actions..."
     runScheduler $ actionQueue emailChangeRequest
-  t3 <- forkIO . cron (60 * 60) $ do
+  t3 <- forkCron tg "PasswordReminders" (60 * 60) $ do
     Log.cron "Evaluating PasswordReminder actions..."
     runScheduler $ actionQueue passwordReminder
-  t4 <- forkIO . cron (60 * 60) $ do
+  t4 <- forkCron tg "UserAccountRequests" (60 * 60) $ do
     Log.cron "Evaluating UserAccountRequest actions..."
     runScheduler $ actionQueue userAccountRequest
-  t5 <- forkIO . cron (60 * 60) $ do
+  t5 <-  forkCron tg "Sessions" (60 * 60) $ do
     Log.cron "Evaluating sessions..."
     runScheduler $ actionQueue session
-  t6 <- forkIO . cron 5 $ runScheduler processEvents
-  t7 <- forkIO $ cron 10 $ runScheduler $ actionQueue documentAPICallback
-  t8 <- forkIO $ if AWS.isAWSConfigOk appConf
-                    then cron 60 $ runScheduler AWS.uploadFilesToAmazon
-                    else return ()
-  t9 <- forkIO . cron (60 * 60) . inDB $ do
+  t6 <- forkCron tg "EventsProcessing" 5 $ do
+    runScheduler processEvents
+  t7 <- forkCron tg "DocumentAPICallback" 10 $ do
+    runScheduler $ actionQueue documentAPICallback
+  t8 <- forkCron tg "RecurlySync" (60 * 60) . inDB $ do
     mtime <- getMinutesTime
     ctime <- liftIO $ System.Time.toCalendarTime (toClockTime mtime)
     temps <- snd `liftM` liftIO (readMVar templates)
     when (System.Time.ctHour ctime == 0) $ -- midnight
       handleSyncWithRecurly (hostpart appConf) (mailsConfig appConf)
         temps (recurlyAPIKey $ recurlyConfig appConf) mtime
+  t9 <- if AWS.isAWSConfigOk appConf
+          then return <$> (forkCron tg "AmazonUploading" 60 $ runScheduler AWS.uploadFilesToAmazon)
+          else return []
 
   waitForTermination
-  Log.cron $ "Termination request received"
-  mapM_ killThread [t1, t2, t3, t4, t5, t6, t7, t8, t9]
+  Log.cron $ "Termination request received, waiting for jobs to finish..."
+  mapM_ stopCron (t1:t2:t3:t4:t5:t6:t7:t8:t9)
+  TG.wait tg
--- misc/Utils/Cron.hs
+++ misc/Utils/Cron.hs
@@ -1,16 +1,64 @@
-module Utils.Cron where
+module Utils.Cron (
+    CronInfo
+  , forkCron
+  , stopCron
+  ) where
 
 import Control.Arrow
+import Control.Monad
 import Control.Concurrent
-import Control.Monad.State
+import Control.Concurrent.STM
+import qualified Control.Concurrent.Thread.Group as TG
+import qualified Control.Exception as E
+
+import qualified Log
+
+newtype CronInfo = CronInfo (ThreadId, TVar (WorkerState, Command))
+
+data WorkerState = Waiting | Running
+  deriving Eq
+
+data Command = Continue | Finish
+  deriving Eq
 
 -- | Given an action f and a number of seconds t, cron will execute
 -- f every t seconds with the first execution t seconds after cron is called.
--- cron does not spawn a new thread.
-cron :: Integer -> IO () -> IO a
-cron seconds action = do
-  let (times::Int, rest::Int) = fromInteger *** fromInteger $ (seconds * 1000000) `divMod` (fromIntegral(maxBound::Int)::Integer)
-  forever $ do
-    replicateM_ times $ threadDelay maxBound
-    threadDelay rest
-    action
+forkCron :: TG.ThreadGroup -> String -> Integer -> IO () -> IO CronInfo
+forkCron tg name seconds action = do
+  ctrl <- atomically $ newTVar (Waiting, Continue)
+  tid <- forkIO $ controller ctrl
+  return $ CronInfo (tid, ctrl)
+  where
+    controller ctrl = do
+      _ <- TG.forkIO tg worker
+      let (times::Int, rest::Int) = fromInteger *** fromInteger $ (seconds * 1000000) `divMod` (fromIntegral(maxBound::Int)::Integer)
+      forever $ do
+        replicateM_ times $ threadDelay maxBound
+        threadDelay rest
+        -- start worker...
+        atomically . modifyTVar ctrl $ first (const Running)
+        -- ... and wait until it's finished
+        atomically $ do
+          (ws, _) <- readTVar ctrl
+          when (ws == Running) retry
+      where
+        worker = do
+          st <- atomically $ do
+            st@(ws, cmd) <- readTVar ctrl
+            when (ws == Waiting && cmd == Continue) retry
+            return st
+          case st of
+            (Running, Continue) -> do
+              action `E.catch` \(e::E.SomeException) ->
+                Log.error ("forkCron: exception caught in thread " ++ name ++ ": " ++ show e)
+              atomically . modifyTVar ctrl $ first (const Waiting)
+              worker
+            (_, Finish) -> Log.server $ "forkCron: finishing " ++ name ++ "..."
+            (Waiting, Continue) -> Log.error "forkCron: (Waiting, Continue) after (/= (Waiting, Continue)) condition. Something bad happened, exiting."
+
+-- | Stops given cron thread. Use that before calling wait with appropriate
+-- ThreadGroup object.
+stopCron :: CronInfo -> IO ()
+stopCron (CronInfo (tid, ctrl)) = do
+  killThread tid
+  atomically . modifyTVar ctrl $ second (const Finish)
