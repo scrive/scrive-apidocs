b77af9799c1234116b247ca6a92f88f52d689f4a
Mariusz <mariusz@.scrive.com>
2012-09-07 16:39:44 +0200

    Bugfix Not blank name for multisend when requesting attachment

--- public/js/attachmentsdesignview.js
+++ public/js/attachmentsdesignview.js
@@ -368,7 +368,10 @@ var DesignSignatoryAttachmentsView = Backbone.View.extend({
 
         _.each(attachments.document.signatories(), function(sig)  {
            if (sig.signs() && !sig.author()) {
-                var option = $("<option>").text(sig.nameOrEmail());
+                var text = sig.nameOrEmail();
+                if (sig.isCsv())
+                  text = localization.csv.title;
+                var option = $("<option>").text(text);
                 if (attachment.signatory() == sig)
                     option.attr("selected","yes");
                 option.data("signatory",sig);
