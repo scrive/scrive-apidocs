d3e2c310e43a3e83395f757b9b906360edaa3a7c
Mariusz Rak <mariusz@scrive.com>
2012-09-25 13:13:52 +0200

    Removing some rules for fast build that did not work well with ghc
    Change-Id: Ibd370077f4506ffb4f68ced870f1a8ead77367cc

--- build-scripts/runIncrementalCompile.sh
+++ build-scripts/runIncrementalCompile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rm -f kontrakcja-test.tix && cabal install --only-dependencies --reinstall --force-reinstalls && cabal configure -ftest-coverage && cabal build --ghc-options="-O0 -c" && cabal build --ghc-options="-O0 -optl -O0 -optl --reduce-memory-overheads -optl --hash-size=3"
+rm -f kontrakcja-test.tix && cabal install --only-dependencies --reinstall --force-reinstalls && cabal configure -ftest-coverage && cabal build --ghc-options="-O0 -c" && cabal build --ghc-options="-O0 -optl -O0"
 
