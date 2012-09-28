bcaa4075d0d9675f3c40e3865fc31ef351157515
Eric Normand <eric+gerritisstupid@scrive.com>
2012-09-28 14:19:18 -0500

    Add company invites to company quantity
    Change-Id: I0cc0f66bb932dd0904c236224b5183abb2c4b65a

--- src/Payments/Model.hs
+++ src/Payments/Model.hs
@@ -76,9 +76,17 @@ instance MonadDB m => DBQuery m GetCompanyQuantity Int where
       sqlWhereEq "deleted"    False
       sqlResult "count(id)"
     res <- foldDB (flip (:)) []
-    case res of
+    au <- case res of
       [x] -> return x
       _   -> return 0
+    kRun_ $ sqlSelect "companyinvites" $ do  
+      sqlWhereEq "company_id" cid
+      sqlResult "count(email)"
+    res2 <- foldDB (flip (:)) []
+    ci <- case res2 of
+      [x] -> return x
+      _   -> return 0
+    return $ au + ci
 
 data DeletePaymentPlan = DeletePaymentPlan (Either UserID CompanyID)
 instance (MonadDB m) => DBUpdate m DeletePaymentPlan () where
