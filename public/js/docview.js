e21cec5e33ebda6134b6bd38f1b3b3edc30f9120
Mariusz Rak <mariusz@scrive.com>
2012-10-04 13:55:10 +0200

    Change file name to main document for main file

--- public/js/docview.js
+++ public/js/docview.js
@@ -19,10 +19,10 @@ window.DocumentDownloadView = Backbone.View.extend({
     }
 
     var link = $("<a target='_blank' />");
-    link.attr("href", this.model.mainfile().downloadLinkForMainFile());
+    link.attr("href", this.model.mainfile().downloadLinkForMainFile(this.model.title()));
     link.append($("<div class='float-left icon' />"));
     link.append($("<div class='float-left label' />").text(localization.downloadPDF));
-    link.append($("<div class='float-left docname' />").text(this.model.mainfile().name() + ".pdf"));
+    link.append($("<div class='float-left docname' />").text(this.model.title() + ".pdf"));
     link.append($("<div class='clearfix' />"));
 
     $(this.el).append($("<div class='download' />").append(link));
