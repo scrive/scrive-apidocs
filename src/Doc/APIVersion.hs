4ac555eb188a91e0b0f10db2ec220057dc1e22af
Magnus Carlsson <magnus@scrive.com>
2012-10-18 15:11:05 +0200

    Add forgotten module
    Change-Id: Ieea3a25dcd21c697f76625a72f61974659f59dda

---
+++ src/Doc/APIVersion.hs
@@ -0,0 +1,9 @@
+module Doc.APIVersion
+ ( APIVersion(..)
+ ) where
+
+data APIVersion =
+  Frontend -- Access from our own JS frontend
+  -- Below: access to published API versions from clients
+ |  V1
+  deriving (Eq, Show)
