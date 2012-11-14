9a3f78b007e68a7fc47d64d46db2074825425c80
Anton Ekblad <anton@ekblad.cc>
2012-11-14 13:45:35 +0100

    Start cron server from devRunner.sh.

--- devRunner.sh
+++ devRunner.sh
@@ -6,14 +6,22 @@ echo ""
 echo "STARTING MAILER SERVER"
     dist/build/mailing-server/mailing-server &
     echo $! > _mailer_pid
-    echo "started mailer with pid" 
+    echo "started mailer with pid"
     sleep 1
+
+echo ""
+echo "STARTING CRON SERVER"
+    dist/build/cron/cron &
+    echo $! > _cron_pid
+    echo "started mailer with pid"
+    sleep 1
+
 
 echo ""
 echo "STARTING MAIN SERVER"
     dist/build/kontrakcja-server/kontrakcja-server "$@" &
     echo $! > _server_pid
-    echo "started server with pid" 
+    echo "started server with pid"
     more _pid
 
 wait
