d866667d78d53e4c989198538724ba21999e7add
Anton Ekblad <anton@ekblad.cc>
2012-11-14 13:47:20 +0100

    Actually display the PIDs of the started processes in devRunner.sh.
    Before, devRunner.sh would inform you that "$SERVER was started with pid"
    whereas it now actually informs you of which PID each process has instead of
    just stating the obvious.

--- devRunner.sh
+++ devRunner.sh
@@ -6,14 +6,14 @@ echo ""
 echo "STARTING MAILER SERVER"
     dist/build/mailing-server/mailing-server &
     echo $! > _mailer_pid
-    echo "started mailer with pid"
+    echo "started mailer with pid $(cat _mailer_pid)"
     sleep 1
 
 echo ""
 echo "STARTING CRON SERVER"
     dist/build/cron/cron &
     echo $! > _cron_pid
-    echo "started mailer with pid"
+    echo "started mailer with pid $(cat _cron_pid)"
     sleep 1
 
 
@@ -21,8 +21,7 @@ echo ""
 echo "STARTING MAIN SERVER"
     dist/build/kontrakcja-server/kontrakcja-server "$@" &
     echo $! > _server_pid
-    echo "started server with pid"
-    more _pid
+    echo "started server with pid $(cat _server_pid)"
 
 wait
 
