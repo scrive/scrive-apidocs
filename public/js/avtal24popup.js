ca81905ec241a7563a871ef35679ce85faa2ef36
Mariusz Rak <mariusz@scrive.com>
2012-11-15 17:20:27 +0400

    MIssing semicolons

--- public/js/avtal24popup.js
+++ public/js/avtal24popup.js
@@ -5,8 +5,8 @@
 $(function(){
 
 window.Avtal24Popup = function() {
-      var content = $("<div/>").append($("<div style='font-size: 14px; font-weight: bold; color: #000000; text-align: center; margin:10px auto'/>").text(localization.avtal24.description))
-      content.append('<iframe width="634" height="476" src="https://www.youtube-nocookie.com/embed/Hkm7xzrhYic" frameborder="0" allowfullscreen></iframe>')
+      var content = $("<div/>").append($("<div style='font-size: 14px; font-weight: bold; color: #000000; text-align: center; margin:10px auto'/>").text(localization.avtal24.description));
+      content.append('<iframe width="634" height="476" src="https://www.youtube-nocookie.com/embed/Hkm7xzrhYic" frameborder="0" allowfullscreen></iframe>');
       Confirmation.popup({
               content  : content,
               title  : localization.avtal24.title,
