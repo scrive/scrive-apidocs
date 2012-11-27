5dce617f4eb3ef809d62cabab088056aec80176f
Gracjan Polak <gracjan@scrive.com>
2012-11-27 20:41:32 +0100

    Removed useless json instances
    Change-Id: I741d25a97e7c585a9b9942133fec3e3d07f14f91

--- mailer/inc/Mails/Data.hs
+++ mailer/inc/Mails/Data.hs
@@ -12,11 +12,8 @@ module Mails.Data (
   ) where
 
 import Data.Data
-import Data.Either
 import Data.Int
 import Data.Monoid
-import Database.HDBC
-import qualified Data.ByteString.Base64 as B64
 import qualified Data.ByteString.Char8 as BS
 
 import DB.Derive
@@ -44,23 +41,6 @@ data Attachment = Attachment {
     attName    :: String
   , attContent :: BS.ByteString
   } deriving (Eq, Ord, Show, Data, Typeable)
-
-instance Convertible [Attachment] SqlValue where
-  safeConvert = jsonToSqlValue . map (\a -> a { attContent = B64.encode $ attContent a })
-
-instance Convertible SqlValue [Attachment] where
-  safeConvert v = check . partitionEithers . map f =<< jsonFromSqlValue v
-    where
-      check ([], x) = Right x
-      check ((e:_), _) = Left e
-      f a = case B64.decode $ attContent a of
-              Right ds -> Right a { attContent = ds }
-              Left e   -> Left ConvertError {
-                  convSourceValue = show $ attContent a
-                , convSourceType = "Base64 encoded ByteString"
-                , convDestType = "ByteString"
-                , convErrorMessage = "Couldn't convert from base64: " ++ e
-              }
 
 data Address = Address {
     addrName  :: String
