bf5d1d5e0e41cedc50b31cbbb0d06c3bd1898239
Gracjan Polak <gracjan@scrive.com>
2012-12-13 14:24:36 +0100

    Removed no longer used build dependency

--- build-scripts/deployToDev.sh
+++ build-scripts/deployToDev.sh
@@ -7,9 +7,6 @@ scp kontrakcja-master-dist-$1.tar.gz dev@dev.scrive.com:kontrakcja-master-dist-$
 ssh dev@dev.scrive.com rm -rf kontrakcja.cabal dist texts templates public files
 ssh dev@dev.scrive.com tar -zxf kontrakcja-master-dist-$1.tar.gz
 
-ssh dev@dev.scrive.com echo "running cabal update";cabal update
-ssh dev@dev.scrive.com echo "running cabal install --only-dependencies";cabal install --only-dependencies
-
 ssh dev@dev.scrive.com supervisorctl restart dev-mailer
 ssh dev@dev.scrive.com supervisorctl restart dev-cron
 ssh dev@dev.scrive.com supervisorctl restart dev
