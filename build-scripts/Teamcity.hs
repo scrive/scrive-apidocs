2a8483b9bb8c462db959ccefcad7751a76794606
Gracjan Polak <gracjan@scrive.com>
2012-12-17 19:44:20 +0100

    Highlight for ghc errors in Teamcity.hs

--- build-scripts/Teamcity.hs
+++ build-scripts/Teamcity.hs
@@ -3,6 +3,7 @@ module Teamcity where
 import System.Environment
 import Data.List
 import System.IO
+import Data.Char
 
 escapeTeamcityString :: String -> String
 escapeTeamcityString = concatMap escapeTeamcityChar
@@ -21,7 +22,7 @@ createTeamcityLine :: String -> [(String,String)] -> String
 createTeamcityLine messageName attributes =
   "##teamcity[" ++ messageName ++
     concatMap (\(attrname,attrvalue) ->
-           " " ++ attrname ++ "='" ++ escapeTeamcityString attrvalue ++ "'") attributes ++ "]\n"
+           " " ++ attrname ++ "='" ++ escapeTeamcityString attrvalue ++ "'") attributes ++ "]"
 
 
 
@@ -63,9 +64,23 @@ hunit line =
      then teamcityFailureMessage line
      else line
 
+ghc :: Bool -> String -> (Bool,String)
+ghc True line | "    " `isPrefixOf` line = (True, teamcityErrorMessage line)
+ghc _ line = case elemIndices ':' line of
+               [a,b,c] ->
+                 let a1 = take a line
+                     b1 = take (b-a-1) (drop (a+1) line)
+                     c1 = take (c-b-1) (drop (b+1) line)
+                 in if all isDigit b1 && all isDigit c1
+                    then (True, teamcityErrorMessage line)
+                    else (False, line)
+               x -> (False, line)
+
+
 main = do
   mode <- getArgs
   case mode of
     ["hunit"] -> interact (unlines . map hunit . lines)
+    ["ghc"]   -> interact (unlines . snd . mapAccumL ghc False . lines)
     _ -> do
       hPutStrLn stderr "Invalid argument"
