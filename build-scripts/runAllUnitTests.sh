59808da5c3c90690722cbfca309cd0b833b72c3a
Gracjan Polak <gracjan@scrive.com>
2012-12-17 18:54:06 +0100

    kontrakcja-test uses Teamcity highlighter

--- build-scripts/runAllUnitTests.sh
+++ build-scripts/runAllUnitTests.sh
@@ -1,4 +1,12 @@
 #!/bin/bash -e
 
+
+if [ x$TEAMCITY_VERSION == x ]; then
+
 ./dist/build/kontrakcja-test/kontrakcja-test --plain all
 
+else
+
+./dist/build/kontrakcja-test/kontrakcja-test --plain all | runghc build-scripts/Teamcity.hs hunit
+
+fi
