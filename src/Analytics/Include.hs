04d255beb99458beb8530fbc3e90e12f0e732903
Eric Normand <eric@scrive.com>
2012-12-27 10:42:44 -0600

    Add Analytics/Include.hs
    Change-Id: If07912403ffcae15598d3479b72179c93d3793ca

---
+++ src/Analytics/Include.hs
@@ -0,0 +1,99 @@
+module Analytics.Include 
+
+where
+
+import Templates.Templates
+import qualified Templates.Fields as F
+
+import Text.JSON
+import Text.JSON.Gen
+import qualified Text.JSON.Gen as J
+
+import Kontra
+
+import User.Model
+
+import Company.Model
+
+import Control.Monad
+import Control.Applicative
+
+import Util.HasSomeUserInfo
+import Util.HasSomeCompanyInfo
+import MinutesTime
+
+import DB
+
+import Payments.Model
+
+data AnalyticsData = AnalyticsData { aUser           :: Maybe User
+                                   , aCompany        :: Maybe Company
+                                   , aToken          :: String
+                                   , aPaymentPlan    :: Maybe PaymentPlan
+                                   , aLanguage       :: Lang
+                                   }
+                     
+getAnalyticsData :: Kontrakcja m => m AnalyticsData
+getAnalyticsData = do
+  muser <- ctxmaybeuser <$> getContext
+  mcompany <- case muser of
+    Just User{usercompany = Just cid} -> dbQuery $ GetCompany cid
+    _ -> return Nothing
+  token <- ctxmixpaneltoken <$> getContext
+  mplan <- case muser of
+    Just User{usercompany = Just cid } -> dbQuery $ GetPaymentPlan (Right cid)
+    Just User{userid} -> dbQuery $ GetPaymentPlan (Left userid)
+    Nothing -> return Nothing
+  lang <- ctxlang <$> getContext
+    
+  return $ AnalyticsData { aUser         = muser
+                         , aCompany      = mcompany
+                         , aToken        = token 
+                         , aPaymentPlan  = mplan 
+                         , aLanguage     = lang
+                         }
+
+mnop :: Monad m => (a -> m ()) -> Maybe a -> m ()
+mnop f m = maybe (return ()) f m
+
+analyticsTemplates :: Monad m => AnalyticsData -> Fields m ()
+analyticsTemplates ad = do
+  mnop (F.value "userid" . show . userid) $ aUser ad
+  F.value "token" $ aToken ad
+  F.value "properties" $ encode $ toJSValue ad
+
+companyStatus :: User -> String
+companyStatus u = case (useriscompanyadmin u, usercompany u) of
+  (True, _)        -> "admin"
+  (False, Nothing) -> "solo"
+  (False, Just _ ) -> "sub"
+  
+-- | A safe version of !!
+(??) :: [a] -> Int -> Maybe a
+(??) l i = if i < length l then Just (l !! i) else Nothing
+
+-- | Empty strings become Nothing
+maybeS :: String -> Maybe String
+maybeS "" = Nothing
+maybeS s  = Just s
+
+instance ToJSValue AnalyticsData where
+  toJSValue AnalyticsData{..} = runJSONGen $ do
+    mnop (J.value "$email") $ getEmail <$> aUser
+    mnop (J.value "userid") $ show <$> userid <$> aUser
+    
+    mnop (J.value "Signup Method") $ show <$> usersignupmethod <$> aUser
+    mnop (J.value "TOS Date" . formatMinutesTimeRealISO) $ join $ userhasacceptedtermsofservice <$> aUser
+    mnop (J.value "Full Name") $ join $ maybeS <$> getFullName <$> aUser
+    mnop (J.value "Smart Name") $ join $ maybeS <$> getSmartName <$> aUser
+    mnop (J.value "$first_name") $ join $ maybeS <$> getFirstName <$> aUser
+    mnop (J.value "$last_name") $ join $ maybeS <$> getLastName <$> aUser
+    mnop (J.value "$username") $ getEmail <$> aUser
+    mnop (J.value "Phone") $ join $ maybeS <$> (userphone . userinfo) <$> aUser    
+    mnop (J.value "Position") $ join $ maybeS <$> usercompanyposition <$> userinfo  <$> aUser
+    
+    mnop (J.value "Company Status") $ companyStatus <$> aUser
+    mnop (J.value "Company Name") $ join $ maybeS <$> (\u -> getCompanyName (u, aCompany)) <$> aUser
+
+    mnop (J.value "Payment Plan") $ show <$> ppPricePlan <$> aPaymentPlan
+    J.value "Language" $ codeFromLang aLanguage
