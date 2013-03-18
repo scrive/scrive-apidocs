4ec3f8e71718b709ee6f9636f3a6613caa27b845
Gracjan Polak <gracjan@scrive.com>
2013-03-18 18:39:44 +0100

    Removed some of forkAction madness

--- src/ForkAction.hs
+++ src/ForkAction.hs
@@ -1,67 +1,40 @@
 module ForkAction (forkAction) where
 
 import Control.Monad.IO.Class
-import Control.Concurrent.MVar
 import Control.Monad.Trans.Control
 import Database.HDBC
 import Numeric
-import System.IO.Unsafe
 import System.Time
 import qualified Control.Concurrent.Lifted as C
 import qualified Control.Exception.Lifted as E
-import qualified Data.Map as Map
 
 import DB
 import qualified Log
-
-data ForkedAction = ForkedActionStarted  {
-    title     :: String
-  , start     :: ClockTime
-  }
-  | ForkedActionDone {
-    title     :: String
-  , start     :: ClockTime
-  , finish    :: ClockTime
-  }
-  | ForkedActionError {
-    title     :: String
-  , start     :: ClockTime
-  , finish    :: ClockTime
-  , exception :: E.SomeException
-  }
-
-instance Show ForkedAction where
-  show (ForkedActionStarted {title}) = title
-  show (ForkedActionDone {title, start, finish}) = title ++ ", done (" ++ showDiffTime finish start ++ "s)"
-  show (ForkedActionError {title, start, finish, exception}) = title ++ ", error (" ++ showDiffTime finish start ++ "s): " ++ show exception
 
 showDiffTime :: ClockTime -> ClockTime -> String
 showDiffTime clock1 clock2 = showFFloat (Just 2) (fromIntegral (diffClockTimeMiliSeconds clock1 clock2) / (100 :: Double)) ""
   where
     diffClockTimeMiliSeconds (TOD sec1 pico1) (TOD sec2 pico2) = ((sec1 * 1000000000000 + pico1) - (sec2 * 1000000000000 + pico2)) `div` 10000000000
 
-allActions :: MVar (Map.Map Int ForkedAction)
-allActions = unsafePerformIO $ newMVar Map.empty
-
 forkAction :: (MonadBaseControl IO m, MonadDB m, MonadIO m) => String -> m () -> m ()
 forkAction title action = do
+  -- OMG, this logic of kClone, disconnect and friends should be put directly into
+  -- MonadbaseControl instance of DBT. getNexus and localNexus should go away, btw.
+  -- As this requires more type level hackery than available brain power allows
+  -- we will keep this hack below for now.
   nex <- kClone
   _ <- C.fork $ flip E.finally (liftIO $ disconnect nex) $ localNexus (const nex) $ do
+    Log.debug $ "forkAction: " ++ title ++ " started"
     startTime <- liftIO getClockTime
-    key <- liftIO $ modifyMVar allActions $ \themap -> do
-      let newkey = case Map.maxViewWithKey themap of
-            Nothing -> 0
-            Just ((k,_),_) -> k + 1
-      return (Map.insert newkey (ForkedActionStarted title startTime) themap, newkey)
     result <- E.try action
     endTime <- liftIO getClockTime
-    retval <- case result of
-      Left e -> do
+    case result of
+      Left (e :: E.SomeException) -> do
         kRollback
-        Log.error $ "forkAction: " ++ title ++ ": " ++ show e
-        return $ ForkedActionError title startTime endTime e
+        Log.debug $ "forkAction: " ++ title ++ " finished in " ++ showDiffTime endTime startTime ++ "s with exception " ++ show e
+        return ()
       Right _ -> do
         kCommit
-        return $ ForkedActionDone title startTime endTime
-    liftIO $ modifyMVar_ allActions $ return . Map.insert key retval
+        Log.debug $ "forkAction: " ++ title ++ " finished in " ++ showDiffTime endTime startTime ++ "s"
+        return ()
   return ()
