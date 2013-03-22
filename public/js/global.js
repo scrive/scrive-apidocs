93bbe688604d3851cc870bb5691362bf4c66d76d
Eric Normand <eric@scrive.com>
2013-03-22 14:41:33 -0500

    Add startWith to String prototype
    Change-Id: Id876a0f63c70d993781aa4e698df4a2a6e445a47

--- public/js/global.js
+++ public/js/global.js
@@ -1,3 +1,10 @@
+// add startsWith to String prototype
+if (typeof String.prototype.startsWith != 'function') {
+  String.prototype.startsWith = function (str){
+    return this.slice(0, str.length) == str;
+  };
+}
+
 // globally track errors
 window.onerror = function(msg, url, line) {
     mixpanel.track('Error', {
