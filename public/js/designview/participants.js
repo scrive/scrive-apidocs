c92fc7080c5071149294ef88b4b49b90d00dc380
Eric Normand <eric@scrive.com>
2013-05-16 16:50:20 -0500

    Two fields per line with single-line optional selector.
    Fix optional selector in type setter as well.

--- public/js/designview/participants.js
+++ public/js/designview/participants.js
@@ -1121,6 +1121,7 @@
         initialize: function(args) {
             var view = this;
             view.options = args.options;
+            view.extraClass = args.extraClass;
             var field = view.model;
             _.bindAll(view);
             view.render();
@@ -1144,18 +1145,15 @@
             }
             var values = view.options;
             var options = {
-                optional  : {name : localization.designview.optional,
-                             value : 'optional',
-                             leftMargin: 9,
-                             offset: 6},
+                optional  : {name : localization.designview.optionalField,
+                             value : 'optional'
+                            },
                 signatory : {name : localization.designview.mandatoryForRecipient,
                              value : 'signatory',
-                             leftMargin: 9,
-                             offset: 0},
+                            },
                 sender    : {name : localization.designview.mandatoryForSender,
                              value : 'sender',
-                             leftMargin: 9,
-                             offset: 0}
+                            }
             };
             var select = new Select({
                 options: _.map(_.without(values, selected), function(v) {
@@ -1179,6 +1177,8 @@
                 }
             });
             select.view().$el.addClass('design-view-action-participant-details-information-field-options');
+            if(view.extraClass)
+                select.view().$el.addClass(view.extraClass);
             view.$el.html(select.view().el);
             return view;
         }
