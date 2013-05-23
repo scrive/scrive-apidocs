1193d987041add285708341fb12bba236f2ba533
Gracjan Polak <gracjan@scrive.com>
2013-05-23 16:00:19 +0200

    Revert "Use InfoTextInput when placeholder is needed"
    This reverts commit e59382de71317392a3cfb0fb09625451a6e83625.

--- public/js/designview/participants.js
+++ public/js/designview/participants.js
@@ -754,28 +754,27 @@
 
             var value = sig.name();
             var div = $('<div />');
-            var infoinput = InfoTextInput.init( { infotext: localization.designview.fullName,
-                                                  value: value,
-                                                  cssClass: 'design-view-action-participant-details-information-field',
+            var input = $('<input />');
+            input.addClass('design-view-action-participant-details-information-field');
+            input.val(value);
+            input.attr('placeholder', localization.designview.fullName);
 
-                                                  onChange:  function() {
-                                                      setTimeout(function() {
-                                                          var str = input.val().trim();
-                                                          var i = str.indexOf(' ');
-                                                          var f, s;
-                                                          if(i >= 0) {
-                                                              f = str.slice(0,i).trim();
-                                                              s = str.slice(i+1).trim();
-                                                          } else {
-                                                              f = str.trim();
-                                                              s = '';
-                                                          }
-                                                          sig.fstnameField().setValue(f);
-                                                          sig.sndnameField().setValue(s);
-                                                      },0); // do this with the current value (not value before keypress)
-                                                  }
-                                                });
-            var input = infoinput.input();
+            input.bind('keypress keydown keyup change input', function() {
+                setTimeout(function() {
+                    var str = input.val().trim();
+                    var i = str.indexOf(' ');
+                    var f, s;
+                    if(i >= 0) {
+                        f = str.slice(0,i).trim();
+                        s = str.slice(i+1).trim();
+                    } else {
+                        f = str.trim();
+                        s = '';
+                    }
+                    sig.fstnameField().setValue(f);
+                    sig.sndnameField().setValue(s);
+                },0); // do this with the current value (not value before keypress)
+            });
 
             var optionOptions = sig.author()?['sender']:['signatory', 'sender'];
 
@@ -785,6 +784,7 @@
             });
 
             var closer = $('<div />');
+            closer.addClass('design-view-action-participant-details-information-closer');
 
             div.append(input);
             div.append(options.el);
@@ -804,20 +804,20 @@
             var value = field.value();
 
             var div = $('<div />');
-            var infoinput = InfoTextInput.init( { infotext: placeholder || name,
-                                                  value: value,
-                                                  cssClass: 'design-view-action-participant-details-information-field',
-                                                  onChange: function() {
-                                                      setTimeout(function() {
-                                                          field.setValue(input.val().trim());
-                                                          if(viewmodel.showProblems() && !field.isValid())
-                                                              input.addClass('redborder');
-                                                          else
-                                                              input.removeClass('redborder');
-                                                      },0);
-                                                  }
-                                                });
-            var input = infoinput.input();
+            var input = $('<input />');
+            input.addClass('design-view-action-participant-details-information-field');
+            input.val(value);
+            input.attr('placeholder', placeholder || name);
+
+            input.bind('keyup keydown keypress change input', function() {
+                setTimeout(function() {
+                    field.setValue(input.val().trim());
+                    if(viewmodel.showProblems() && !field.isValid())
+                        input.addClass('redborder');
+                    else
+                        input.removeClass('redborder');
+                },0);
+            });
 
             var optionOptions = ['optional', 'signatory', 'sender'];
 
