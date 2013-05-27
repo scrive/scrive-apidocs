e60d8bf31e8ef68f40afe77f5917904a50e30549
Johan Söderholm <johan@scrive.com>
2013-05-27 16:37:20 +0200

    Add create account and promotion views for all kinds of signing devices / methods, as we now have support for it.

--- public/js/signview/docsignview.js
+++ public/js/signview/docsignview.js
@@ -96,17 +96,12 @@ var DocumentSignViewModel = Backbone.Model.extend({
       return    this.document().currentSignatory() != undefined
              && this.document().currentSignatory().hasSigned()
              && this.justSaved()
-             && !this.document().currentSignatory().padDelivery()
-             && !this.document().currentSignatory().mobileDelivery()
-             && !this.document().currentSignatory().emailMobileDelivery()
              && window.PromoteScriveView != undefined;
   },
   hasCreateAccountSection : function() {
       return    this.document().currentSignatory() != undefined
              && this.document().currentSignatory().hasSigned()
              && !this.document().currentSignatory().saved()
-             && !this.document().currentSignatory().mobileDelivery()
-             && !this.document().currentSignatory().emailMobileDelivery()
              && window.CreateAccountAfterSignView != undefined;
   },
   instructionssection : function() {
