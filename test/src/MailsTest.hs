12b4b62a924314428fc807a64d40b65965f483c9
Gracjan Polak <gracjan@scrive.com>
2013-06-12 11:19:39 +0200

    Fixup after merge

--- test/src/MailsTest.hs
+++ test/src/MailsTest.hs
@@ -125,7 +125,7 @@ sendDocumentMails mailTo author = do
                                    (signatoryActor (10 `minutesAfter` now) ip (maybesignatory sl) (getEmail sl) (signatorylinkid sl))
         (Just sdoc) <- randomQuery $ GetDocumentByDocumentID docid
         -- Sending closed email
-        checkMail "Closed" $ mailDocumentClosed ctx sdoc Nothing sl
+        checkMail "Closed" $ mailDocumentClosed ctx sdoc Nothing sl False
         -- Reminder after send
         checkMail "Reminder signed" $ mailDocumentRemind Nothing ctx doc (head $ documentsignatorylinks sdoc) False
   kCommit
