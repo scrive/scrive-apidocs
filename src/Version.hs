249c57e8ebb5b372effacfab278378edb9445611
Gracjan Polak <gracjan@scrive.com>
2013-06-13 18:21:09 +0200

    Simplified Version.hs

--- src/Version.hs
+++ src/Version.hs
@@ -1,27 +1,19 @@
 {-# LANGUAGE TemplateHaskell #-}
-module Version (versionID, silenceUnusedImport) where
+module Version (versionID) where
 
 import Language.Haskell.TH.Syntax
 import Language.Haskell.TH.Lib
 import System.Environment
 import Control.Exception as E
-import Data.List
 
 {-# NOINLINE versionID #-}
 versionID :: String
-versionID = $( do
-                  ver <- fmap stringL $ runIO $ do
-                           let catchIO :: IO a -> (E.IOException -> IO a) -> IO a
-                               catchIO = E.catch
-                           buildNumber <- catchIO (getEnv "BUILD_NUMBER") (const (return "build_number"))
-                           buildVcsNumber <- catchIO (getEnv "BUILD_VCS_NUMBER") (const (return "build_vcs_number"))
-                           buildDate <- catchIO (getEnv "BUILD_DATE") (const (return "build_date"))
+versionID = $( litE =<< (fmap stringL $ runIO $ do
+                 let catchIO :: IO a -> (E.IOException -> IO a) -> IO a
+                     catchIO = E.catch
+                 buildNumber <- catchIO (getEnv "BUILD_NUMBER") (const (return "build_number"))
+                 buildVcsNumber <- catchIO (getEnv "BUILD_VCS_NUMBER") (const (return "build_vcs_number"))
+                 buildDate <- catchIO (getEnv "BUILD_DATE") (const (return "build_date"))
 
-                           return $ intercalate "." [buildDate, buildNumber, buildVcsNumber]
-                  litE ver
+                 return $ buildDate ++ "." ++ buildNumber ++ "." ++ buildVcsNumber)
    )
-
--- Due to how Template Haskell has its phases in the second phase we
--- get error about unused imports from Data.List. Lets silence that
-silenceUnusedImport :: forall a. [a] -> [[a]] -> [a]
-silenceUnusedImport = intercalate
