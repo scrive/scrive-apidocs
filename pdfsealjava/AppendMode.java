7c8d6711000fcec63721fc7f1415712b3650b6c9
Gracjan Polak <gracjan@scrive.com>
2013-06-25 14:32:19 +0200

    Initial commit of itext based functionality for pdfseal

---
+++ pdfsealjava/AppendMode.java
@@ -0,0 +1,119 @@
+/*
+ * This class is part of the book "iText in Action - 2nd Edition"
+ * written by Bruno Lowagie (ISBN: 9781935182610)
+ * For more info, go to: http://itextpdf.com/examples/
+ * This example only works with the AGPL version of iText.
+ */
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.PdfContentByte;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfStamper;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.PdfSmartCopy;
+import com.itextpdf.text.pdf.PdfCopy;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfImportedPage;
+import com.itextpdf.text.Font;
+
+public class AppendMode {
+    /** The resulting PDF. */
+    public static final String RESULT
+        = "appended.pdf";
+    public static final String INPUT
+        = "input.pdf";
+
+
+    public void prepareSealPages(OutputStream os)
+        throws IOException, DocumentException {
+
+        Document document = new Document();
+        PdfWriter writer = PdfWriter.getInstance(document, os);
+
+        document.open();
+
+        BaseFont bf;
+        Font font;
+        bf = BaseFont.createFont("STSong-Light", "UniGB-UCS2-H", BaseFont.EMBEDDED);
+
+        font = new Font(bf, 12);
+
+        // step 4
+        document.newPage();
+        document.add(new Paragraph("This page will NOT be followed by a blank page! \u5341\u950a\u57cb\u4f0f", font));
+        document.newPage();
+        // we don't add anything to this page: newPage() will be ignored
+        document.newPage();
+        document.add(new Paragraph("This page will be followed by a blank page!"));
+        document.newPage();
+        writer.setPageEmpty(false);
+        document.newPage();
+        document.add(new Paragraph("The previous page was a blank page!"));
+        // step 5
+        document.close();
+    }
+
+    /**
+     * Manipulates a PDF file src with the file dest as result
+     * @param src the original PDF
+     * @param dest the resulting PDF
+     * @throws IOException
+     * @throws DocumentException
+     */
+    public void manipulatePdf(String src, String dest) throws IOException, DocumentException {
+
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+
+        prepareSealPages(os);
+
+
+        // step 1
+        Document document = new Document();
+        // step 2
+        PdfCopy writer
+            = new PdfSmartCopy(document, new FileOutputStream(dest));
+
+        document.open();
+
+        PdfReader reader = new PdfReader(src);
+        int count = reader.getNumberOfPages();
+        for( int i=1; i<=count; i++ ) {
+            PdfImportedPage page = writer.getImportedPage(reader, i);
+            writer.addPage(page);
+        }
+        writer.freeReader(reader);
+        reader.close();
+
+        reader = new PdfReader(os.toByteArray());
+        count = reader.getNumberOfPages();
+        for( int i=1; i<=count; i++ ) {
+            PdfImportedPage page = writer.getImportedPage(reader, i);
+            writer.addPage(page);
+        }
+        writer.freeReader(reader);
+
+        reader.close();
+
+        document.close();
+    }
+
+    /**
+     * Main method.
+     *
+     * @param    args    no arguments needed
+     * @throws DocumentException
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException, DocumentException {
+        new AppendMode().manipulatePdf(INPUT, RESULT);
+    }
+}
