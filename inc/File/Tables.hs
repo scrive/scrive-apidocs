7d84cde1406fe26c233137c711fc7b60b3ebed51
Gracjan Polak <gracjan@scrive.com>
2013-09-06 12:16:06 +0300

    Added a 'purged' column to files table

--- inc/File/Tables.hs
+++ inc/File/Tables.hs
@@ -7,15 +7,16 @@ tableFiles = tblTable {
     tblName = "files"
   , tblVersion = 5
   , tblColumns = [
-      tblColumn { colName = "id",            colType = BigSerialT, colNullable = False }
-    , tblColumn { colName = "name",          colType = TextT, colNullable = False }
+      tblColumn { colName = "id",            colType = BigSerialT,  colNullable = False }
+    , tblColumn { colName = "name",          colType = TextT,       colNullable = False }
     , tblColumn { colName = "content",       colType = BinaryT }
     , tblColumn { colName = "amazon_bucket", colType = TextT }
     , tblColumn { colName = "amazon_url",    colType = TextT }
-    , tblColumn { colName = "size",          colType = IntegerT, colNullable = False }
-    , tblColumn { colName = "checksum",      colType = BinaryT, colNullable = False }
-    , tblColumn { colName = "aes_key",       colType = BinaryT, colNullable = True }
-    , tblColumn { colName = "aes_iv",        colType = BinaryT, colNullable = True }
+    , tblColumn { colName = "size",          colType = IntegerT,    colNullable = False }
+    , tblColumn { colName = "checksum",      colType = BinaryT,     colNullable = False }
+    , tblColumn { colName = "aes_key",       colType = BinaryT }
+    , tblColumn { colName = "aes_iv",        colType = BinaryT }
+    , tblColumn { colName = "purged",        colType = TimestampWithZoneT }
     ]
   , tblPrimaryKey = ["id"]
   }
