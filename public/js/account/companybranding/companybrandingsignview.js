07d896f12ec5d17ace4f258e719ea24fe50c0b95
Mariusz Rak <mariusz@scrive.com>
2013-09-27 20:16:22 +0200

    Background fix + reference screenshot

--- public/js/account/companybranding/companybrandingsignview.js
+++ public/js/account/companybranding/companybrandingsignview.js
@@ -97,7 +97,7 @@ window.CompanyBrandingSignViewSampleView = Backbone.View.extend({
                         .css("width","480px");
 
     var document = $("<div/>").css("border", "1px dashed #cccccc")
-      .css("padding", "10px").css("text-align","center");
+      .css("padding", "10px").css("text-align","center").css("background", "#e9e9e9");
     document.append(this.contentheader);
     document.append(documentpic);
     var rejectbuttoncontainer = $('<div style="float: left;padding:10px;"/>');
@@ -120,7 +120,7 @@ window.CompanyBrandingSignViewSampleView = Backbone.View.extend({
     buttonsdiv.append(rejectbuttoncontainer).append(signbuttoncontainer);
     var contentcontent = $('<div/>').css("width","500px").css("margin","auto")
       .css("padding-bottom", "10px").css("padding-top", "15px");
-    
+
     document.append(buttonsdiv);
     contentcontent.append(document);
     this.content = $('<div style="padding-bottom:20px;"/>');
