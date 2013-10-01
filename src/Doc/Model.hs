b605eb449c8f6e2b8b155b1ef2164b1d270d9bd7
Mariusz Rak <mariusz@scrive.com>
2013-10-01 12:17:28 +0200

    Fix for FbM problem - MakeDocumentSeen trhows random problems

--- src/Doc/Model.hs
+++ src/Doc/Model.hs
@@ -1590,7 +1590,8 @@ instance (MonadDB m, TemplatesMonad m) => DBUpdate m MarkDocumentSeen () where
               sqlWhereDocumentTypeIs (Signable)
               sqlWhere "signatory_links.seen_time IS NULL"
               sqlWhere "signatory_links.sign_time IS NULL"
-              sqlWhereDocumentStatusIsOneOf [Pending, Timedout, Canceled, DocumentError undefined, Rejected]
+              sqlWhereNotEq "status" Preparation
+              sqlWhereNotEq "status" Closed
 
 data AddInvitationEvidence = AddInvitationEvidence DocumentID SignatoryLinkID (Maybe String) Actor
 instance (MonadDB m, TemplatesMonad m) => DBUpdate m AddInvitationEvidence Bool where
