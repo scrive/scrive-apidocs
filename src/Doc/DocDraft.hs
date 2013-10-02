8611a7c25b04c0e06abde917466335044b80ab8d
Mariusz Rak <mariusz@scrive.com>
2013-10-02 15:06:10 +0200

    Change of structure

--- src/Doc/DocDraft.hs
+++ src/Doc/DocDraft.hs
@@ -6,10 +6,8 @@ module Doc.DocDraft (
   ) where
 
 import Control.Monad.Trans.Maybe
-import Doc.SignatoryTMP()
 import Doc.DocStateData
 import Utils.Monad
---import Utils.Prelude
 import Data.Maybe
 import Kontra
 import Util.SignatoryLinkUtils
@@ -28,6 +26,121 @@ import InputValidation
 import Data.Functor
 import Utils.Default
 import Doc.DocUtils
+import Data.String.Utils (strip)
+import Doc.SignatoryLinkID
+
+-- JSON instances
+
+
+instance MatchWithJSValue SignatoryLink where
+    matchesWithJSValue s = do
+      mid    <- fromJSValueField "id"
+      return (Just (signatorylinkid s) == (maybeRead =<< mid))
+
+instance FromJSValueWithUpdate SignatoryLink where
+    fromJSValueWithUpdate ms = do
+        author <- fromJSValueField "author"
+        signs  <- fromJSValueField "signs"
+        mfields <- fromJSValueFieldCustom "fields" (fromJSValueManyWithUpdate $ fromMaybe [] (signatoryfields <$> signatorydetails <$> ms))
+        signorder <- fromJSValueField "signorder"
+        attachments <- fromJSValueField "attachments"
+        csv <- fromJSValueField "csv"
+        sredirecturl <- fromJSValueField "signsuccessredirect"
+        rredirecturl <- fromJSValueField "rejectredirect"
+        authentication' <-  fromJSValueField "authentication"
+        delivery' <-  fromJSValueField "delivery"
+        case (mfields) of
+             (Just fields) -> return $ Just $ defaultValue {
+                    signatorylinkid            = fromMaybe (unsafeSignatoryLinkID 0) (signatorylinkid <$> ms)
+                  , signatorydetails           = defaultValue {
+                        signatorysignorder     = updateWithDefaultAndField (SignOrder 1) (signatorysignorder . signatorydetails) (SignOrder <$> signorder)
+                      , signatoryfields        = fields
+                      , signatoryisauthor      = updateWithDefaultAndField False (signatoryisauthor . signatorydetails) author
+                      , signatoryispartner     = updateWithDefaultAndField False (signatoryispartner . signatorydetails) signs
+                                                 }
+                  , signatorylinkcsvupload       = updateWithDefaultAndField Nothing signatorylinkcsvupload csv
+                  , signatoryattachments         = updateWithDefaultAndField [] signatoryattachments attachments
+                  , signatorylinksignredirecturl = updateWithDefaultAndField Nothing signatorylinksignredirecturl sredirecturl
+                  , signatorylinkrejectredirecturl = updateWithDefaultAndField Nothing signatorylinkrejectredirecturl rredirecturl
+                  , signatorylinkauthenticationmethod = updateWithDefaultAndField StandardAuthentication signatorylinkauthenticationmethod authentication'
+                  , signatorylinkdeliverymethod       = updateWithDefaultAndField EmailDelivery signatorylinkdeliverymethod delivery'
+                }
+             _ -> return Nothing
+      where
+       updateWithDefaultAndField :: a -> (SignatoryLink -> a) -> Maybe a -> a
+       updateWithDefaultAndField df uf mv = fromMaybe df (mv `mplus` (fmap uf ms))
+
+
+
+
+
+instance FromJSValue SignatoryField where
+    fromJSValue = fromJSValueWithUpdate Nothing
+
+
+instance FromJSValueWithUpdate SignatoryField where
+    fromJSValueWithUpdate msf =  do
+        ftype <- fromJSValue
+        value  <- fromJSValueField "value"
+        obligatory <- updateWithDefaultAndField True sfObligatory <$> fromJSValueField "obligatory"
+        filledbysender <- updateWithDefaultAndField False sfObligatory <$> fromJSValueField "shouldbefilledbysender"
+        placements <- updateWithDefaultAndField [] sfPlacements <$> fromJSValueField "placements"
+        case (ftype,value) of
+          (Just ft, Just v) -> do
+              let v' = case ft of
+                        EmailFT -> strip v
+                        SignatureFT _ -> ""
+                        _ -> v
+              return $ Just $ SignatoryField ft v' obligatory filledbysender placements
+          _ -> return Nothing
+      where
+       updateWithDefaultAndField :: a -> (SignatoryField -> a) -> Maybe a -> a
+       updateWithDefaultAndField df uf mv = fromMaybe df (mv `mplus` (fmap uf msf))
+
+
+instance MatchWithJSValue SignatoryField where
+    matchesWithJSValue sf = do
+      ftype <- fromJSValue
+      return (ftype ==  Just(sfType sf))
+
+instance FromJSValue SignatoryAttachment where
+    fromJSValue = do
+        name<- fromJSValueField "name"
+        description  <- fromJSValueField "description"
+        case (name,description) of
+             (Just n, Just d) -> return $ Just $ SignatoryAttachment {signatoryattachmentname  = n ,
+                                                                      signatoryattachmentdescription = d,
+                                                                      signatoryattachmentfile = Nothing}
+             _ -> return Nothing
+
+instance FromJSValue FieldType where
+   fromJSValue = do
+    s <- fromJSValueField "name"
+    t <- fromJSValueField "type"
+    filled <- (not . null) <$> fromMaybe ("" :: String) <$> fromJSValueField "value"
+    return $ case (fromMaybe "standard" t,s) of
+         ("standard",  Just "fstname")    -> Just $ FirstNameFT
+         ("standard",  Just "sndname")    -> Just $ LastNameFT
+         ("standard",  Just "email")      -> Just $ EmailFT
+         ("standard",  Just "mobile")     -> Just $ MobileFT
+         ("standard",  Just "sigpersnr")  -> Just $ PersonalNumberFT
+         ("standard",  Just "sigco")      -> Just $ CompanyFT
+         ("standard",  Just "sigcompnr")  -> Just $ CompanyNumberFT
+         ("signature", Just name       )  -> Just $ SignatureFT name
+         ("custom",    Just name       )  -> Just $ CustomFT name filled
+         ("checkbox",  Just name       )  -> Just $ CheckboxFT name
+         _ -> Nothing
+
+
+instance FromJSValue CSVUpload  where
+    fromJSValue = do
+        rows <- fromJSValue
+        case rows of
+             Just rs -> return $ Just $ CSVUpload
+                        { csvtitle = ""
+                        , csvcontents = rs
+                        }
+             _ -> return Nothing
 
 instance FromJSValue DocumentTag where
     fromJSValue = do
@@ -44,43 +157,6 @@ instance FromJSValue Lang where
       Just "en"    -> Just LANG_EN
       Just "gb"    -> Just LANG_EN
       _            -> Nothing
-
-
-{-
-instance FromJSValue DraftData where
-   fromJSValue = do
-        title' <- fromJSValueField "title"
-        invitationmessage <-  fromJSValueField "invitationmessage"
-        daystosign' <- fromJSValueField "daystosign"
-        let minDaysToSign = 1
-            maxDaysToSign = 90
-        authentication' <-  fromJSValueField "authentication"
-        delivery' <-  fromJSValueField "delivery"
-        signatories' <-  fromJSValueField "signatories"
-        lang' <- fromJSValueField "lang"
-        template' <- fromJSValueField "template"
-        tags' <- fromJSValueFieldCustom "tags" $ fromJSValueCustomMany  fromJSValueM
-        apicallbackurl' <- fromJSValueField "apicallbackurl"
-        authorattachments' <- fromJSValueFieldCustom "authorattachments" $ fromJSValueCustomMany $ fmap (join . (fmap maybeRead)) $ (fromJSValueField "id")
-        case (title', daystosign') of
-            (Just t, Just daystosign)
-             | daystosign >= minDaysToSign && daystosign <= maxDaysToSign ->
-                return $ Just DraftData {
-                                      title =  t
-                                    , invitationmessage = invitationmessage
-                                    , daystosign = daystosign
-                                    , authentication = authentication'
-                                    , delivery = delivery'
-                                    , signatories = concat $ maybeToList $ signatories'
-                                    , lang = lang'
-                                    , template = joinB template'
-                                    , tags = tags'
-                                    , apicallbackurl = apicallbackurl'
-                                    , authorattachments = authorattachments'
-                                 }
-            _ -> return Nothing
-            -}
-
 
 instance FromJSValueWithUpdate Document where
     fromJSValueWithUpdate mdoc = do
@@ -117,7 +193,6 @@ instance FromJSValueWithUpdate Document where
        mapAuth :: Maybe AuthenticationMethod -> [SignatoryLink] -> [SignatoryLink]
        mapAuth Nothing sls = sls
        mapAuth (Just au) sls = map (\sl -> sl {signatorylinkauthenticationmethod = au}) sls
-
 
 
 applyDraftDataToDocument :: Kontrakcja m =>  Document -> Document -> Actor -> m (Either String Document)
@@ -161,8 +236,8 @@ mergeAuthorDetails sigs nsigs =
             setConstantDetails a =  replaceFieldValue FirstNameFT (getFirstName a) .
                                     replaceFieldValue LastNameFT (getLastName a) .
                                     replaceFieldValue EmailFT   (getEmail a) .
-                                    (\s -> s {maybesignatory = maybesignatory a}) .
-                                    (\s -> s {signatorylinkid = signatorylinkid a})
+                                    (\s -> s {maybesignatory = maybesignatory a}) .  -- We need to be sure that we will not disconnect author
+                                    (\s -> s {signatorylinkid = signatorylinkid a})  -- And we try to keep original id of author signatory
           in case (asig, nasig') of
                ([asig'], [nasig'']) -> Just $ (setConstantDetails asig' nasig'') : nsigs'
                _ -> Nothing
