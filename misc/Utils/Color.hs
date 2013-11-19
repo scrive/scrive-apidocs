f507c774d54a2cb0e696c8d7695f4d55455d43db
Bartek Ćwikłowski <bartek@scrive.com>
2013-11-19 18:15:04 +0100

    ensureHexRGB now returns Maybe

--- misc/Utils/Color.hs
+++ misc/Utils/Color.hs
@@ -4,12 +4,12 @@ import Numeric
 import Text.Regex.TDFA
 
 -- converts strings like "rgb(1,2,3)" to "#aabbcc" notation
--- doesn't convert anything that's not of that format
-ensureHexRGB :: String -> String
+-- returns Nothing for colors in other notation (or other garbage input)
+ensureHexRGB :: String -> Maybe String
 ensureHexRGB c =
     case getAllTextSubmatches $ c =~ rgbNotationRegex of
-      [c', r, g, b] | c == c' -> "#" ++ dec2HEX r ++ dec2HEX g ++ dec2HEX b
-      _ -> c
+      [c', r, g, b] | c == c' -> Just $ "#" ++ dec2HEX r ++ dec2HEX g ++ dec2HEX b
+      _ -> Nothing
   where rgbNotationRegex = "rgb\\(([0-9]+),([0-9]+),([0-9]+)\\)" :: String
         dec2HEX s = pad0 $ showHex (read s :: Int) ""
         pad0 [x] = ['0', x]
