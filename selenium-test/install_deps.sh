a5c35b5eef03f9196ba2ac4aab6bc11c836d5da1
Bartek Ćwikłowski <bartek@scrive.com>
2013-11-29 12:11:10 +0100

    add script to install selenium tests gem deps

---
+++ selenium-test/install_deps.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+gem install --user-install --version '1.8.2' multi_json
+gem install --user-install --version '1.0.0' rubyzip
+gem install --user-install --version '1.9.3' ffi
+gem install --user-install --version '0.3.9' childprocess
+gem install --user-install --version '1.0.7' websocket
+gem install --user-install --version '2.37.0' selenium-webdriver
+gem install --user-install --version '2.14.7' rspec-core
+gem install --user-install --version '1.2.5' diff-lcs
+gem install --user-install --version '2.14.4' rspec-expectations
+gem install --user-install --version '2.14.4' rspec-mocks
+gem install --user-install --version '2.14.1' rspec
