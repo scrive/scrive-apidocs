a6d9d32343d06b69e4bc42ba0c7c0a0a5319be94
Magnus Carlsson <magnus@carlssonia.org>
2013-12-10 11:38:03 -0800

    Remove operations that now are available in package

--- inc/Control/Monad/Trans/Control/Util.hs
+++ inc/Control/Monad/Trans/Control/Util.hs
@@ -4,25 +4,6 @@ module Control.Monad.Trans.Control.Util where
 import Control.Monad
 import Control.Monad.Trans.Control
 
--- | Taken from http://www.mail-archive.com/haskell-cafe@haskell.org/msg95180.html
--- Can be there until these functions make their way to monad-control package.
-
-{-# INLINE defaultLiftWith #-}
-defaultLiftWith ∷ (Monad m, MonadTransControl tInner)
-                ⇒ (tInner m α → t m α)          -- ^ Constructor
-                → (∀ β n. t n β → tInner n β)   -- ^ Deconstructor
-                → (∀ β. StT tInner β → StT t β) -- ^ State constructor
-                → ((Run t → m α) → t m α)
-defaultLiftWith con deCon st = \f → con $ liftWith $ \run →
-                                      f $ liftM st . run . deCon
-
-{-# INLINE defaultRestoreT #-}
-defaultRestoreT ∷ (Monad m, MonadTransControl tInner)
-                ⇒ (tInner m α → t m α)     -- ^ Constructor
-                → (StT t α → StT tInner α) -- ^ State deconstructor
-                → (m (StT t α) → t m α)
-defaultRestoreT con unSt = con . restoreT . liftM unSt
-
 {-# INLINE newtypeLiftBaseWith #-}
 newtypeLiftBaseWith ∷ (Monad m, MonadBaseControl b mInner)
                     ⇒ (mInner α → m α)              -- ^ Constructor
