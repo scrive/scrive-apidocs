cf4e83cafe8945e8e8ab6d687d53535956d1b79a
Johan Söderholm <johan@scrive.com>
2013-12-17 14:15:09 +0100

    Wait 10s before signing up for a new account, to make sure that the emails are sent in the right order. Solves an issue with trying to click the wrong button (because of getting the wrong email) through the email backdoor.

--- selenium-test/src/specs/sign_up_after_signing.rb
+++ selenium-test/src/specs/sign_up_after_signing.rb
@@ -32,7 +32,7 @@ describe "sign up after signing a document" do
     @h.dochelper.partSign
 
     puts "we should be given the option to save the doc in the archive and create an account"
-    sleep 2
+    sleep 10 # wait 10s so we make sure that we know how and what emails are sent.
     @h.wait_until { @h.driver.find_element :css => "a.button.button-large" }.click
 
     puts "should be logged in and able to upload a document"
