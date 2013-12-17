64e77999a30f391d304a2ebbfc6b3b4e30bb19bd
Mariusz Rak <mariusz@scrive.com>
2013-12-17 10:08:01 +0100

    Adding missing button text

--- src/PadApplication/API.hs
+++ src/PadApplication/API.hs
@@ -44,4 +44,5 @@ apiCallCheckClient = api $ do
       value "upgrade_url" JSNull
       value "title" JSNull
       value "message" JSNull
+      value "upgrade_button" JSNull
       value "later_button" JSNull
