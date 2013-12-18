3d67b4fee3afbe7a8d0d44a6d455dcc9b17aa659
Mariusz Rak <mariusz@scrive.com>
2013-12-18 12:07:27 +0100

    Fix for Gracjans problem

--- public/js/designview/documentview.js
+++ public/js/designview/documentview.js
@@ -33,7 +33,7 @@
         },
         render: function() {
             var view = this;
-            if(view.file) {
+            if(view.file && (view.file.model != view.model.mainfile())) {
                 view.file.destroy();
                 view.file = undefined;
             }
