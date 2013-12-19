7c231b00ae1272462787178f0e90e218058ec26f
Bartek Ćwikłowski <bartek@scrive.com>
2013-12-19 13:04:52 +0100

    fix mobile signview even when not welcoming user

--- public/js/signview/docsignviewinstructions.js
+++ public/js/signview/docsignviewinstructions.js
@@ -98,7 +98,7 @@ window.DocumentSignInstructionsView = Backbone.View.extend({
     }
 
     var headline = $("<div class='headline' />");
-    if (BrowserInfo.isSmallScreen() && welcomeUser) {
+    if (BrowserInfo.isSmallScreen()) {
       headline.css('font-size', '22px');
       headline.css('margin-bottom', '0px');
     }
