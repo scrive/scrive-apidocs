13b0f83dbcfbf17f7616cbd7c03583974c95e2b5
Gracjan Polak <gracjan@scrive.com>
2013-12-31 17:54:33 +0100

    Using Log.mixlog_ in place of any other Log.xxx logging function

--- mailer/src/Cleaner.hs
+++ mailer/src/Cleaner.hs
@@ -6,12 +6,12 @@ import Crypto.RNG
 import DB
 import DB.PostgreSQL
 import Mails.Model
-import qualified Log (mailingServer)
+import qualified Log
 
 cleaner :: CryptoRNGState -> String -> IO ()
 cleaner rng dbconf = withPostgreSQL dbconf . runCryptoRNGT rng $ do
-  Log.mailingServer $ "Removing mails that were (or should be) sent " ++ show daylimit ++ " days ago."
+  Log.mixlog_ $ "Removing mails that were (or should be) sent " ++ show daylimit ++ " days ago."
   removedCount <- dbUpdate $ DeleteMailsOlderThenDays daylimit
-  Log.mailingServer $ show removedCount ++ " mails were removed."
+  Log.mixlog_ $ show removedCount ++ " mails were removed."
   where
     daylimit = 14
