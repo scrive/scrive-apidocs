a93339aa25034c800b964c3a4201bebcf8f5da4e
Gracjan Polak <gracjan@scrive.com>
2013-12-31 17:54:32 +0100

    Using Log.mixlog_ in place of any other Log.xxx logging function

--- inc/File/Migrations.hs
+++ inc/File/Migrations.hs
@@ -49,7 +49,7 @@ addFileIdSequence = Migration {
       _ <- kRunRaw $ "CREATE SEQUENCE files_id_seq"
       -- set start value to be one more than maximum already in the table or 1000 if table is empty
       Just n <- getOne $ SQL "SELECT setval('files_id_seq',(SELECT COALESCE(max(id)+1,1000) FROM files))" []
-      Log.debug $ "Table files has yet " ++ show (maxBound - n :: Int64) ++ " values to go"
+      Log.mixlog_ $ "Table files has yet " ++ show (maxBound - n :: Int64) ++ " values to go"
       -- and finally attach serial default value to files.id
       _ <- kRunRaw $ "ALTER TABLE files ALTER id SET DEFAULT nextval('files_id_seq')"
       return ()
