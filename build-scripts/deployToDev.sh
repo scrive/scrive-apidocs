698c93f0367db349537bd97407754b965ac85c2b
Gracjan Polak <gracjan@scrive.com>
2014-01-13 10:48:37 +0100

    Make deplotToDev.sh take messenger and kontrakcja-migrate into account

--- build-scripts/deployToDev.sh
+++ build-scripts/deployToDev.sh
@@ -7,8 +7,16 @@ scp kontrakcja-master-dist-$1.tar.gz dev@dev.scrive.com:kontrakcja-master-dist-$
 ssh dev@dev.scrive.com rm -rf kontrakcja.cabal dist texts templates public files
 ssh dev@dev.scrive.com tar -zxf kontrakcja-master-dist-$1.tar.gz
 
-ssh dev@dev.scrive.com supervisorctl restart dev-mailer
-ssh dev@dev.scrive.com supervisorctl restart dev-cron
-ssh dev@dev.scrive.com supervisorctl restart dev
+ssh dev@dev.scrive.com supervisorctl stop dev-messenger
+ssh dev@dev.scrive.com supervisorctl stop dev-mailer
+ssh dev@dev.scrive.com supervisorctl stop dev-cron
+ssh dev@dev.scrive.com supervisorctl stop dev
+
+ssh dev@dev.scrive.com dist/build/kontrakcja-migrate/kontrakcja-migrate
+
+ssh dev@dev.scrive.com supervisorctl start dev-messenger
+ssh dev@dev.scrive.com supervisorctl start dev-mailer
+ssh dev@dev.scrive.com supervisorctl start dev-cron
+ssh dev@dev.scrive.com supervisorctl start dev
 
 ssh dev@dev.scrive.com rm kontrakcja-master-dist-$1.tar.gz
