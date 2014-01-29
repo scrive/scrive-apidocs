156eeb566b667168e5f03c87fd672982897900b0
Jens Ohring <jens@scrive.com>
2014-01-29 11:39:49 +0100

    Remove files that shouldnt be here and fix gitignore to correctly not include them

--- frontend/app/compiled_jsx/.module-cache/c1b5f994f7b5ac519edb86aaa46fc3d53a93a95b.js
+++
@@ -1,9 +0,0 @@
-define(['jquery'], function($) {
-  var language = getCurrentLanguage();
-  var localizationFileUrl = ['/', language, '/localization/localization.js'].join('');
-
-  // TODO(jens): Either fix a global error handler or a local one
-  // When the script on the url is loaded, window.localization will be set
-  // which we use to access all translation strings
-  $.getScript(localizationFileUrl);   
-});
--- frontend/app/scripts/app.jsx
+++ frontend/app/scripts/app.jsx
@@ -1,11 +1,7 @@
 /** @jsx React.DOM */
 var App = {};
 
-require(['Underscore', 'Backbone', 'React', 'dashboard/DashboardView', 'postsignview/main_views', 'postsignview/simple_documents_service', 'common/global_events', 
-
-	 ////'text!/scripts/translations/locale-sv.json', 
-
-'legacy_code'], function(_, Backbone, React, DashboardView, MainViews, SimpleDocumentsService, GlobalEvents, trans) {
+require(['Underscore', 'Backbone', 'React', 'legacy_code'], function(_, Backbone, React) {
 
   /**
    * If we're note under Backbone router url /r/. Bail out.
@@ -15,46 +11,4 @@ require(['Underscore', 'Backbone', 'React', 'dashboard/DashboardView', 'postsign
   if(currentPath.indexOf('/r') === -1) {
     return;
   }
-
-  // TODO(jens): Move translation to real system
-  //window.localization = _.extend(JSON.parse(trans), localization);
-  //window.localization.archive = _.extend(JSON.parse(trans).archive, localization.archive);
-  //console.log(window.localization);
- 
-  App.GlobalEvents = GlobalEvents;
-
-  var Router = new Backbone.Router.extend({
-    /**
-     *  @note
-     *  Always add routes with (/), so it doesnt matter if you have trailing slash or not.
-     */
-    routes: {
-      'postsignview/:documentId/:signatoryId(/)': 'postsignview',
-      'postsignview/save-safety-copy-step2(/)': 'archivePSV',
-      '*default': 'dashboard'
-    },
-
-    dashboard: function(id) {
-      React.renderComponent(<DashboardView />,document.body);
-    },
-
-    archivePSV: function(documentId, signatoryId) {
-      var simpleDocumentList = new SimpleDocumentsService.SimpleDocumentCollection();
-      simpleDocumentList.fetch({
-        success: function(collection, response) {
-	  React.renderComponent(MainViews.ArchivePSV({document: collection.at(0)}), document.body);
-        }
-      });  
-    },
-
-    postsignview: function(documentId, signatoryId) {      
-      var LandingPage = MainViews.LandingPage;
-      React.renderComponent(<LandingPage documentId={documentId} signatoryId={signatoryId}/>, document.body);
-    }
-  });
-
-  App.router = Router;
-  
-  Backbone.history.start();
-
 });
