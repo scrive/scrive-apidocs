479a0791f2e1f2c4a6d4eade90eafb3ad3702696
Jens Ohring <jens@scrive.com>
2014-01-29 15:35:25 +0100

    Fix build scripts for dev

--- build-scripts/runDevCompile.sh
+++ build-scripts/runDevCompile.sh
@@ -6,6 +6,7 @@ if [ -z "$1" ]; then
 fi
 
 rm -f kontrakcja-test.tix
+cabal-dev --sandbox="$1" clean
 cabal-dev --sandbox="$1" install --only-dependencies
 cabal-dev --sandbox="$1" configure -ftest-coverage
 cabal-dev --sandbox="$1" build --ghc-options="-O0 -c"
