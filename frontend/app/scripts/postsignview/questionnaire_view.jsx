707408de462b6254c9db9f0a5977a3f7f86b0b71
Jens Ohring <jens@scrive.com>
2014-02-28 16:06:44 +0100

    Small bug- and stylingfixes

--- frontend/app/scripts/postsignview/questionnaire_view.jsx
+++ frontend/app/scripts/postsignview/questionnaire_view.jsx
@@ -1,8 +1,140 @@
 /** @jsx React.DOM */
 
 define(['React', 'Backbone', 'StateMachine'], function(React, Backbone, StateMachine) {
-  var Questions = {};
 
+  /**
+   *  @description
+   *  A questionnaire, asking user to answer a couple of questions and
+   *  record the answers and user informationto Mixpanel.
+   */
+  return React.createClass({
+    componentDidMount: function() {
+      this.initStateMachine();
+    },
+
+    getInitialState: function() {
+      return {question: 'LikeEsigning'};
+    },
+
+    /**
+     *  @description
+     *  StateMachine which determines which question view to display.
+     */
+    initStateMachine: function() {
+      var self = this;
+      this.questionSTM = StateMachine.create({
+        initial: self.state.question,
+        events: [
+          { from: 'LikeEsigning', name: 'yes', to: 'SendDocuments' },
+          { from: 'LikeEsigning', name: 'no', to: 'Dislike' },
+          { from: 'Dislike', name: 'no', to: 'Done' },
+          { from: 'SendDocuments', name: 'yes', to: 'DemoCta' },
+          { from: 'SendDocuments', name: 'no', to: 'OthersInYourOrg' },
+          { from: 'DelayedSignatures', name: 'yes', to: 'DemoCta' },
+          { from: 'DelayedSignatures', name: 'no', to: 'OthersInYourOrg' },
+          { from: 'OthersInYourOrg', name: 'yes', to: 'Done' },
+          { from: 'OthersInYourOrg', name: 'no', to: 'Done' },
+          { from: 'DemoCta', name: 'yes', to: 'Done' }
+        ],
+        callbacks: {
+          onchangestate: function(event, from, to) {
+	    console.log(event, from, to);
+            self.setState({question: to});
+            var extraMixpanelProperties = {};
+
+            if (to === 'Done' && event === 'yes' && from === 'OthersInYourOrg') {
+              extraMixpanelProperties['Others in your organisation information wanted'] = true;
+            }
+
+            if (to === 'Done') {
+              self.registerInMixpanel(extraMixpanelProperties);
+            }
+          }
+        }
+      });
+    },
+
+    yesanswer: function() {
+      this.questionSTM.yes();
+    },
+
+    noanswer: function() {
+      this.questionSTM.no();
+    },
+
+    /**
+     *  @description
+     *  Set phoneNumber on internal object, which is then used
+     *  when registering current user to Mixpanel
+     */
+    phoneNumber: null,
+    setPhoneNumber: function(phoneNumber) {
+      this.phoneNumber = phoneNumber;
+    },
+
+    mixpanelProperties: function() {
+      var document = this.props.document;
+      var properties = {
+        saveUrl: document.currentSignatory().saveurl(),
+        fullName: document.currentSignatory().name(),
+        firstName: document.currentSignatory().fstname(),
+        lastName: document.currentSignatory().sndname(),
+        email: document.currentSignatory().email(),
+        language: document.lang().simpleCode(),
+        companyName: document.currentSignatory().company(),
+        referringCompany: document.author().company(),
+        signupMethod: 'BySigning'
+      };
+
+      if(this.phoneNumber) {
+	properties['Phone'] = this.phoneNumber;
+      }
+
+      return properties;
+    },
+
+    registerInMixpanel: function(extraProperties) {
+      var props = _.extend(this.mixpanelProperties(), extraProperties);
+      var mail = props.email;
+      delete props.saveUrl;
+      mixpanel.alias(mail);
+      mixpanel.identify(mail);
+      mixpanel.register(props);
+      mixpanel.people.set(props);
+      mixpanel.track('Questionnaire Done');
+    },
+
+    render: function() {
+      // Which Question to display is determined by the StateMachine,
+      // by setting this.state.question
+      var Question = Questions[this.state.question];
+
+      return (
+          <div className="questionnaire-wrapper">
+            <div className="questionnaire">
+              <div className="hithere">
+                <div className="wrapper">
+                  <img className="viktor" src="/img/questionnaire/viktor.png" />
+                  <div className="heyimviktor">
+                    { localization.questionnaire.heyImViktor } <span className="normal-weight">Scrive!</span><br />
+                    { localization.questionnaire.ourEsignService }
+                  </div>
+                  <b className="notch"></b>
+                </div>
+              </div>
+
+              <div>
+                <div className="questions">
+		  <Question yesButton={this.yesanswer} noButton={this.noanswer} setPhoneNumber={this.setPhoneNumber} />
+		</div>
+	      </div>
+	    </div>
+	  </div>
+      );
+    }
+  });
+
+  var Questions = {};
   Questions.DelayedSignatures = React.createClass({
     yesButton: function() {
       mixpanel.track('Questionnaire #2 Accept');
@@ -157,7 +289,7 @@ define(['React', 'Backbone', 'StateMachine'], function(React, Backbone, StateMac
 		<div className="button button-gray" onClick={this.noButton}>
 		  <div className="label">{ localization.questionnaire.no }</div>
 		</div>
-		<div className="button button-green" onClick={this.props.yesbutton}>
+		<div className="button button-green" onClick={this.yesButton}>
 		  <div className="label">{ localization.questionnaire.yes }</div>
 		</div>
 	      </div>
@@ -212,137 +344,4 @@ define(['React', 'Backbone', 'StateMachine'], function(React, Backbone, StateMac
       );
     }
   });
-
-
-  /**
-   *  @description
-   *  A questionnaire, asking user to answer a couple of questions and
-   *  record the answers to Mixpanel.
-   */
-  return React.createClass({
-    componentDidMount: function() {
-      this.initStateMachine();
-    },
-
-    getInitialState: function() {
-      return {question: 'LikeEsigning'};
-    },
-
-    /**
-     *  @description
-     *  StateMachine which determines which question view to display.
-     */
-    initStateMachine: function() {
-      var self = this;
-      this.questionSTM = StateMachine.create({
-        initial: self.state.question,
-        events: [
-          { from: 'LikeEsigning', name: 'yes', to: 'SendDocuments' },
-          { from: 'LikeEsigning', name: 'no', to: 'Dislike' },
-          { from: 'Dislike', name: 'no', to: 'Done' },
-          { from: 'SendDocuments', name: 'yes', to: 'DemoCta' },
-          { from: 'SendDocuments', name: 'no', to: 'OthersInYourOrg' },
-          { from: 'DelayedSignatures', name: 'yes', to: 'DemoCta' },
-          { from: 'DelayedSignatures', name: 'no', to: 'OthersInYourOrg' },
-          { from: 'OthersInYourOrg', name: 'yes', to: 'Done' },
-          { from: 'OthersInYourOrg', name: 'no', to: 'Done' },
-          { from: 'DemoCta', name: 'yes', to: 'Done' }
-        ],
-        callbacks: {
-          onchangestate: function(event, from, to) {
-            self.setState({question: to});
-            var extraMixpanelProperties = {};
-
-            if (to === 'Done' && event === 'yes' && from === 'OthersInYourOrg') {
-              extraMixpanelProperties['Others in your organisation information wanted'] = true;
-            }
-
-            if (to === 'Done') {
-              self.registerInMixpanel(extraMixpanelProperties);
-            }
-          }
-        }
-      });
-    },
-
-    yesanswer: function() {
-      this.questionSTM.yes();
-    },
-
-    noanswer: function() {
-      this.questionSTM.no();
-    },
-
-    /**
-     *  @description
-     *  Set phoneNumber on internal object, which is then used
-     *  when registering current user to Mixpanel
-     */
-    phoneNumber: null,
-    setPhoneNumber: function(phoneNumber) {
-      this.phoneNumber = phoneNumber;
-    },
-
-    mixpanelProperties: function() {
-      var document = this.props.document;
-      var properties = {
-        saveUrl: document.currentSignatory().saveurl(),
-        fullName: document.currentSignatory().name(),
-        firstName: document.currentSignatory().fstname(),
-        lastName: document.currentSignatory().sndname(),
-        email: document.currentSignatory().email(),
-        language: document.lang().simpleCode(),
-        companyName: document.currentSignatory().company(),
-        referringCompany: document.author().company(),
-        signupMethod: 'BySigning'
-      };
-
-      if(this.phoneNumber) {
-	properties['Phone'] = this.phoneNumber;
-      }
-
-      return properties;
-    },
-
-    registerInMixpanel: function(extraProperties) {
-      var props = _.extend(this.mixpanelProperties(), extraProperties);
-      var mail = props.email;
-      delete props.saveUrl;
-      mixpanel.alias(mail);
-      mixpanel.identify(mail);
-      mixpanel.register(props);
-      mixpanel.people.set(props);
-      mixpanel.track('Questionnaire Done');
-    },
-
-    render: function() {
-      // Which question to display is determined by the StateMachine,
-      // by setting this.state.question
-      var Question = Questions[this.state.question];
-
-      return (
-          <div className="questionnaire-wrapper">
-            <div className="questionnaire">
-              <div className="hithere">
-                <div className="wrapper">
-                  <img className="viktor" src="/img/questionnaire/viktor.png" />
-                  <div className="heyimviktor">
-                    { localization.questionnaire.heyImViktor } <span className="normal-weight">Scrive!</span><br />
-                    { localization.questionnaire.ourEsignService }
-                  </div>
-                  <b className="notch"></b>
-                </div>
-              </div>
-
-              <div>
-                <div className="questions">
-		  <Question yesButton={this.yesanswer} noButton={this.noanswer} setPhoneNumber={this.setPhoneNumber} />
-		</div>
-	      </div>
-	    </div>
-	  </div>
-      );
-    }
-  });
-
 });
