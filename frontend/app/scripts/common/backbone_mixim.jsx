78692a9f778a3fab3536f1a5d00428c1b554530e
Mariusz Rak <mariusz@scrive.com>
2014-03-03 13:46:20 +0100

    Change backbone mixim to better one

--- frontend/app/scripts/common/backbone_mixim.jsx
+++ frontend/app/scripts/common/backbone_mixim.jsx
@@ -1,5 +1,5 @@
 /** @jsx React.DOM */
-
+/* Sample code from: http://eldar.djafarov.com/2013/11/reactjs-mixing-with-backbone */
 
 define(['Backbone', 'legacy_code'], function() {
 
@@ -8,19 +8,25 @@ var expose = {};
 var BackboneMixin = {
   componentDidMount: function() {
     // Whenever there may be a change in the Backbone data, trigger a reconcile.
-    this.getBackboneModels().forEach(function(model) {
-      model.on('add change remove', this.forceUpdate.bind(this, null), this);
-    }, this);
+    this.getBackboneModels().forEach(this.injectModel, this);
   },
-
   componentWillUnmount: function() {
     // Ensure that we clean up any dangling references when the component is
     // destroyed.
-    this.getBackboneModels().forEach(function(model) {
-      model.off(null, null, this);
+    this.__syncedModels.forEach(function(model) {
+      model.off(null, model.__updater, this);
     }, this);
+  },
+  injectModel: function(model){
+    if(!this.__syncedModels) this.__syncedModels = [];
+    if(!~this.__syncedModels.indexOf(model)){
+      var updater = this.forceUpdate.bind(this, null);
+      model.__updater = updater;
+      model.on('add change remove', updater, this);
+      this.__syncedModels.push(model);
+    }
   }
-};
+}
 
 
 
