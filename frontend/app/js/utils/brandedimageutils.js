1bd8fa8d5037101882792bee2603af0e46d9ee8e
Bartek Ćwikłowski <bartek@scrive.com>
2014-03-27 18:31:16 +0100

    workaround for mixed content warning in IE8 in branded signview

--- frontend/app/js/utils/brandedimageutils.js
+++ frontend/app/js/utils/brandedimageutils.js
@@ -9,7 +9,17 @@ window.BrandedImageUtil = {
     $(el).css('background', 'url(' + BrandedImageUtil.getBrandedImageURL(image, colour) + ')');
   },
   getBrandedImageURL: function(image, colour) {
-    return '/branded_signview_image?file=' + image + '&colour=' + encodeURIComponent(colour);
+    var baseUrl;
+    if (BrowserInfo.isIE8orLower()) {
+      // workaround for IE8 bug, that displays mixed content warning
+      // for background urls that don't have explicit https prefix
+      // http://stackoverflow.com/questions/6961760/what-exactly-are-the-rules-for-avoiding-the-mixed-content-warning-in-ie-due-to
+      var loc = window.location;
+      baseUrl = loc.protocol + '//' + loc.host;
+    } else {
+      baseUrl = '';
+    }
+    return baseUrl + '/branded_signview_image?file=' + image + '&colour=' + encodeURIComponent(colour);
   }
 };
 
