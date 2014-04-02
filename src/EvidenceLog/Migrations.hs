340e64a2f02fd724c92489f12c4d5969c271f0d5
Gracjan Polak <gracjan@scrive.com>
2014-04-02 08:53:33 +0200

    Faster migration for evidence_log

--- src/EvidenceLog/Migrations.hs
+++ src/EvidenceLog/Migrations.hs
@@ -2,6 +2,7 @@ module EvidenceLog.Migrations where
 
 import DB
 import EvidenceLog.Tables
+import Data.Monoid
 
 evidenceLogFixColumns :: MonadDB m => Migration m
 evidenceLogFixColumns =
@@ -10,16 +11,16 @@ evidenceLogFixColumns =
   , mgrFrom = 3
   , mgrDo = do
     -- ip addresses are 32bits long
-    runSQL_ "ALTER TABLE evidence_log ALTER COLUMN request_ip_v4 TYPE INTEGER"
-    -- this is completely useless
-    runSQL_ "ALTER TABLE evidence_log DROP COLUMN request_ip_v6"
+    runSQL_ $ "ALTER TABLE evidence_log ALTER COLUMN request_ip_v4 TYPE INTEGER," <>
+                                        -- this is completely useless
+                                        "DROP COLUMN request_ip_v6," <>
 
-    runSQL_ "ALTER TABLE evidence_log ALTER COLUMN email TYPE TEXT"
-    runSQL_ "ALTER TABLE evidence_log ALTER COLUMN text TYPE TEXT"
-    runSQL_ "ALTER TABLE evidence_log ALTER COLUMN version_id TYPE TEXT"
-    runSQL_ "ALTER TABLE evidence_log ALTER COLUMN api_user TYPE TEXT"
-    runSQL_ "ALTER TABLE evidence_log ALTER COLUMN message_text TYPE TEXT"
-    runSQL_ "ALTER TABLE evidence_log ALTER COLUMN client_name TYPE TEXT"
+                                        "ALTER COLUMN email TYPE TEXT," <>
+                                        "ALTER COLUMN text TYPE TEXT," <>
+                                        "ALTER COLUMN version_id TYPE TEXT," <>
+                                        "ALTER COLUMN api_user TYPE TEXT," <>
+                                        "ALTER COLUMN message_text TYPE TEXT," <>
+                                        "ALTER COLUMN client_name TYPE TEXT"
   }
 
 addClientTimeNameToEvidenceLog :: MonadDB m => Migration m
