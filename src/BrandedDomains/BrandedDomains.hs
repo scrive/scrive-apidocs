fe8ccfe28eec993563f2d9e36505d9865ff323ae
Gracjan Polak <gracjan@scrive.com>
2014-04-04 07:27:08 +0200

    Add BrandedDomainID and GetBrandedDomains database call

--- src/BrandedDomains/BrandedDomains.hs
+++ src/BrandedDomains/BrandedDomains.hs
@@ -5,6 +5,10 @@ module BrandedDomains.BrandedDomains
   ) where
 
 import Data.List
+import BrandedDomains.BrandedDomainID
+import BrandedDomains.Tables
+import DB
+import qualified Log
 
 type BrandedDomains = [BrandedDomain]
 
@@ -36,3 +40,39 @@ data BrandedDomain = BrandedDomain {
 
 findBrandedDomain :: String -> BrandedDomains -> Maybe BrandedDomain
 findBrandedDomain s = find (\d -> bdurl d `isPrefixOf` s)
+
+
+fetchBrandedDomain :: (BrandedDomainID, String, String, String, String, String, String, String, String, String, String, String, String, String, String, String, String, String, String, String, String, String, String) -> BrandedDomain
+fetchBrandedDomain (_id, url, logolink, barscolour, barstextcolour, barssecondarycolour, backgroundcolour, backgroundcolorexternal, mailsbackgroundcolor, mailsbuttoncolor, mailstextcolor, signviewprimarycolour, signviewprimarytextcolour, signviewsecondarycolour, signviewsecondarytextcolour, buttonclass, servicelinkcolour, headercolour, textcolour, pricecolour, smsoriginator, emailoriginator, contactemail)
+       = BrandedDomain
+         { bdurl                         = url
+         , bdlogolink                    = logolink
+         , bdbarscolour                  = barscolour
+         , bdbarstextcolour              = barstextcolour
+         , bdbarssecondarycolour         = barssecondarycolour
+         , bdbackgroundcolour            = backgroundcolour
+         , bdbackgroundcolorexternal     = backgroundcolorexternal
+         , bdmailsbackgroundcolor        = mailsbackgroundcolor
+         , bdmailsbuttoncolor            = mailsbuttoncolor
+         , bdmailstextcolor              = mailstextcolor
+         , bdsignviewprimarycolour       = signviewprimarycolour
+         , bdsignviewprimarytextcolour   = signviewprimarytextcolour
+         , bdsignviewsecondarycolour     = signviewsecondarycolour
+         , bdsignviewsecondarytextcolour = signviewsecondarytextcolour
+         , bdbuttonclass                 = buttonclass
+         , bdservicelinkcolour           = servicelinkcolour
+         , bdheadercolour                = headercolour
+         , bdtextcolour                  = textcolour
+         , bdpricecolour                 = pricecolour
+         , bdsmsoriginator               = smsoriginator
+         , bdemailoriginator             = emailoriginator
+         , bdcontactemail                = contactemail
+         }
+
+
+data GetBrandedDomains = GetBrandedDomains
+instance (MonadDB m, Log.MonadLog m) => DBQuery m GetBrandedDomains [BrandedDomain] where
+  query (GetBrandedDomains) = do
+    runQuery_ . sqlSelect "branded_domains" $ do
+      mapM_ (sqlResult . raw . colName) (tblColumns tableBrandedDomains)
+    fetchMany fetchBrandedDomain
