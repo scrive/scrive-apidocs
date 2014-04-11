9c84459ca21343dff668463031ee7d4d8bae412c
Gracjan Polak <gracjan@scrive.com>
2014-04-11 06:55:53 +0200

    List basic information about branded domain in a table

--- frontend/app/js/admin/brandeddomains.js
+++ frontend/app/js/admin/brandeddomains.js
@@ -4,9 +4,29 @@ define(['Backbone', 'legacy_code'], function() {
 
 window.BrandedDomainAdminListDefinition = function() {
     var cells = [
-        new Cell({name: "Prefix", width:"80px", field: "url", special: "rendered",
+        new Cell({name: "URL", width:"80px", field: "url", special: "rendered",
                     rendering: function(_, _, doc) {
                         return jQuery("<span/>").text(doc.field("url"));
+                    }
+        }),
+        new Cell({name: "Contact Email", width:"80px", field: "contact_email", special: "rendered",
+                    rendering: function(_, _, doc) {
+                        return jQuery("<span/>").text(doc.field("contact_email"));
+                    }
+        }),
+        new Cell({name: "Email Originator", width:"80px", field: "email_originator", special: "rendered",
+                    rendering: function(_, _, doc) {
+                        return jQuery("<span/>").text(doc.field("email_originator"));
+                    }
+        }),
+        new Cell({name: "SMS Originator", width:"80px", field: "sms_originator", special: "rendered",
+                    rendering: function(_, _, doc) {
+                        return jQuery("<span/>").text(doc.field("sms_originator"));
+                    }
+        }),
+        new Cell({name: "Logo", width:"80px", field: "url", special: "rendered",
+                    rendering: function(_, _, doc) {
+                        return jQuery("<img/>").attr("src",doc.field("logolink"));
                     }
         })
     ];
