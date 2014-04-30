3b566fc477cd7930298c1fddf170888e97dd640d
Bartek Ćwikłowski <bartek@scrive.com>
2014-04-30 19:41:45 +0200

    use png32 for transparent images and png8 for grayscale ones in text_to_image

--- src/ServerUtils/ServerUtils.hs
+++ src/ServerUtils/ServerUtils.hs
@@ -114,7 +114,7 @@ handleTextToImage = do
             , "-gravity", if (left) then "West" else "Center"
             , "-font", font
             , "label:" ++ (if null text then " " else text)
-            , "PNG8:" ++ fpath ]
+            , (if transparent then "PNG32:" else "PNG8:") ++ fpath ]
       drawerexitcode <- waitForProcess drawer
       case drawerexitcode of
           ExitFailure msg -> do
