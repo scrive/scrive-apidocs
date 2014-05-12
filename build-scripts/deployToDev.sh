5d347207675f8d594cb48469f8c58ff59f0cd44f
Gracjan Polak <gracjan@scrive.com>
2014-05-12 09:56:40 +0200

    Improve deployToDev.sh script

--- build-scripts/deployToDev.sh
+++ build-scripts/deployToDev.sh
@@ -1,6 +1,14 @@
 #!/bin/bash -e
 
-tar -cz dist texts templates frontend/dist frontend/app files GuardTime scrivepdftools | ssh dev@dev.scrive.com tar -C kontrakcja -xz
+ssh dev@dev.scrive.com bash -s <<EOF
+
+cd kontrakcja
+
+rm -rf dist texts templates frontend/dist frontend/app files
+
+EOF
+
+tar -cz --exclude '*-tmp' dist texts templates frontend/dist frontend/app files GuardTime scrivepdftools | ssh dev@dev.scrive.com tar -C kontrakcja -xvz
 
 ssh dev@dev.scrive.com bash -s <<EOF
 
