c2a3952f5ff4c4c7ed24a33e2393a86299ab8965
Gracjan Polak <gracjan@scrive.com>
2014-05-14 21:28:46 +0200

    Add separate instances for strict and lazy state monads

--- inc/Happstack/Server/Instances.hs
+++ inc/Happstack/Server/Instances.hs
@@ -4,6 +4,7 @@ module Happstack.Server.Instances where
 import Control.Monad.Trans
 import Database.PostgreSQL.PQTypes
 import Happstack.Server
+import Control.Monad.State.Strict
 
 instance FilterMonad r m => FilterMonad r (DBT m) where
   setFilter f   = lift $ setFilter f
@@ -23,3 +24,22 @@ instance ServerMonad m => ServerMonad (DBT m) where
 
 instance WebMonad r m => WebMonad r (DBT m) where
   finishWith = lift . finishWith
+
+instance (FilterMonad res m) => FilterMonad res (StateT s m) where
+    setFilter f   = lift $ setFilter f
+    composeFilter = lift . composeFilter
+    getFilter   m = mapStateT (\m' ->
+                                   do ((b,s), f) <- getFilter m'
+                                      return ((b, f), s)) m
+
+instance (WebMonad a m) => WebMonad a (StateT s m) where
+    finishWith    = lift . finishWith
+
+instance (ServerMonad m) => ServerMonad (StateT s m) where
+    askRq         = lift askRq
+    localRq f     = mapStateT (localRq f)
+
+instance (Monad m, HasRqData m) => HasRqData (StateT s m) where
+    askRqEnv      = lift askRqEnv
+    localRqEnv f  = mapStateT (localRqEnv f)
+    rqDataError e = lift (rqDataError e)
