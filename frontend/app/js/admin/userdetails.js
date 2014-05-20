7317e1e00a249063e7f9d63354d468263548caff
Mariusz Rak <mariusz@scrive.com>
2014-05-20 16:08:05 +0200

    Danish synch + enable danish

--- frontend/app/js/admin/userdetails.js
+++ frontend/app/js/admin/userdetails.js
@@ -203,7 +203,7 @@ var AdminUserDetailsView = Backbone.View.extend({
         , {name: localization.account.accountSecurity.langES, value: "es", hidden: true}
         , {name: localization.account.accountSecurity.langPT, value: "pt", hidden: true}
         , {name: localization.account.accountSecurity.langNL, value: "nl", hidden: true}
-        , {name: localization.account.accountSecurity.langDA, value: "da", hidden: true}
+        , {name: localization.account.accountSecurity.langDA, value: "da"}
         , {name: localization.account.accountSecurity.langNO, value: "no", hidden: true}
       ];
       var lname = _.findWhere(languages, {value :model.lang()}).name;
