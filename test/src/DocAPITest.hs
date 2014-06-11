96908b68365a39d4e37f52c6ba3dd4479a31f667
Gracjan Polak <gracjan@scrive.com>
2014-06-11 21:31:52 +0200

    Add tests for change main file (with and without placement adjustment)

--- test/src/DocAPITest.hs
+++ test/src/DocAPITest.hs
@@ -5,9 +5,11 @@ import Control.Monad.Trans
 import Control.Applicative
 import Happstack.Server
 import Test.Framework
-import qualified Data.ByteString.Lazy.Char8 as BS
+import qualified Data.ByteString.Lazy.UTF8 as BS
 import Doc.DocStateData
 import Text.JSON
+import Text.JSON.FromJSValue
+import Text.JSON.Gen
 
 import Context
 import Doc.Model
@@ -22,6 +24,8 @@ docAPITests env = testGroup "DocAPI" $
   map (\d -> testThat (d ++ " updates correctly") env (void $ testUpdateDoc d)) jsonDocs
   ++ [
     testThat "settings auto reminder works" env testSetAutoReminder
+  , testThat "change main file works" env testChangeMainFile
+  , testThat "change main file moves placements" env testChangeMainFileMovePlacements
   ]
 
 jsonDocs :: [String]
@@ -42,7 +46,7 @@ testUpdateDoc updateJsonPath = do
   (_, _ctx') <- runTestKontra req ctx $ apiCallCreateFromFile
 
   docs <- randomQuery $ GetDocumentsByAuthor (userid user)
-  assertBool "just one doc" (length docs == 1)
+  assertEqual "just one doc" 1 (length docs)
   let doc = head docs
 
   req' <- mkRequest POST [("json", inText cont)]
@@ -50,11 +54,11 @@ testUpdateDoc updateJsonPath = do
 
   req'' <- mkRequest POST []
   (rsp, _) <- runTestKontra req'' ctx $ apiCallReady $ documentid doc
-  let rspString = BS.unpack $ rsBody rsp
+  let rspString = BS.toString $ rsBody rsp
       Ok (JSObject response) = decode rspString
       Just (JSString sts) = lookup "status" $ fromJSObject response
 
-  assertBool "status is pending" (fromJSString sts == "Pending")
+  assertEqual "status is pending" "Pending" (fromJSString sts)
 
   return ctx
 
@@ -66,4 +70,163 @@ testSetAutoReminder = do
   req <- mkRequest POST [("days", inText "3")]
   (res, _) <- runTestKontra req ctx $ apiCallSetAutoReminder (documentid doc)
 
-  assertEqual ("response code is 202 (response is " ++ show res ++ ")") (rsCode res) 202
+  assertEqual "response code is 202" 202 (rsCode res)
+
+testChangeMainFile :: TestEnv ()
+testChangeMainFile = do
+  (Just user) <- addNewUser "Bob" "Blue" "bob@blue.com"
+  ctx <- (\c -> c { ctxmaybeuser = Just user }) <$> mkContext defaultValue
+
+  req <- mkRequest POST [ ("expectedType", inText "text")
+                        , ("file", inFile "test/pdfs/simple.pdf")]
+  (rsp1, _ctx') <- runTestKontra req ctx $ apiCallCreateFromFile
+
+  let rspString = BS.toString $ rsBody rsp1
+      Ok (JSObject response) = decode rspString
+      Just (JSString sts) = lookup "id" $ fromJSObject response
+
+  docid <- liftIO $ readIO (fromJSString sts)
+  req' <- mkRequest POST [ ("file", inFile "test/pdfs/simple.pdf")]
+
+  (rsp,_) <- runTestKontra req' ctx $ apiCallChangeMainFile $ docid
+
+  assertEqual "suceeded" 202 (rsCode rsp)
+
+  return ()
+
+
+mapObjectEntry :: String -> (Maybe JSValue -> Maybe JSValue) -> JSValue -> JSValue
+mapObjectEntry key func old@(JSObject obj) =
+  case lookup (toJSKey key) (fromJSObject obj) of
+    Nothing -> case func Nothing of
+      Nothing -> old
+      Just newValue -> JSObject (toJSObject ((toJSKey key,newValue) : (fromJSObject obj)))
+    Just oldValue -> case func (Just oldValue) of
+      Nothing -> JSObject (toJSObject (filter ((/= toJSKey key) . fst) (fromJSObject obj)))
+      Just newValue -> JSObject (toJSObject (map replaceValue (fromJSObject obj)))
+        where
+          replaceValue (k,_) | k == toJSKey key = (k,newValue)
+          replaceValue x = x
+
+mapObjectEntry _key _func somethingElse = somethingElse
+
+
+testChangeMainFileMovePlacements :: TestEnv ()
+testChangeMainFileMovePlacements = do
+  let anchorpdf1 = "test/pdfs/anchor-Signature.pdf"
+  let anchorpdf2 = "test/pdfs/anchor-Namnteckning.pdf"
+  let anchorpdf3 = "test/pdfs/anchor-Unterschrift-2.pdf"
+  let noanchorpdf = "test/pdfs/simple.pdf"
+
+  (Just user) <- addNewUser "Bob" "Blue" "bob@blue.com"
+  ctx <- (\c -> c { ctxmaybeuser = Just user }) <$> mkContext defaultValue
+
+  req <- mkRequest POST [ ("expectedType", inText "text")
+                        , ("file", inFile anchorpdf1)]
+  (rsp1, _ctx') <- runTestKontra req ctx $ apiCallCreateFromFile
+
+  let rspString = BS.toString $ rsBody rsp1
+      Ok jsvalue@(JSObject response) = decode rspString
+      Just (JSString sts) = lookup "id" $ fromJSObject response
+
+  docid <- liftIO $ readIO (fromJSString sts)
+
+
+  -- update field placement:
+  {-
+     "anchors":[{"text":"Signature","pages":[1,2]},
+                {"text":"Namnteckning","pages":[1,2]},
+                {"text":"Unterschrift","index":2,"pages":[1,2,3]}]
+  -}
+  let updatejs = mapObjectEntry "signatories"
+                 (fmap (mapFirstInArray (mapObjectEntry "fields" (fmap (addAnchoredField)))))
+                 jsvalue
+      mapFirstInArray :: (JSValue -> JSValue) -> JSValue -> JSValue
+      mapFirstInArray func (JSArray (x:xs)) = JSArray (func x : xs)
+      mapFirstInArray _func x = x
+      addAnchoredField :: JSValue -> JSValue
+      addAnchoredField (JSArray arr) =
+        JSArray (arr ++ [runJSONGen $ do
+                            value "name" ("anchored-field" :: String)
+                            value "type" ("custom" :: String)
+                            value "value" ("value!!" :: String)
+                            objects "placements" [do
+                              value "xrel" (0.5 :: Double)
+                              value "yrel" (0.5 :: Double)
+                              value "wrel" (0.2 :: Double)
+                              value "hrel" (0.2 :: Double)
+                              value "fsrel" (0.02 :: Double)
+                              value "page" (1 :: Int)
+                              objects "anchors" [do
+                                                    value "text" ("Signature" :: String)
+                                                    value "index" (1::Int)
+                                                    value "pages" [1,2::Int],
+                                                 do
+                                                    value "text" ("Namnteckning" :: String)
+                                                    value "index" (1::Int)
+                                                    value "pages" [1,2::Int],
+                                                 do
+                                                    value "text" ("Unterschrift" :: String)
+                                                    value "index" (2::Int)
+                                                    value "pages" [1,2,3::Int]]]])
+      addAnchoredField x = x
+
+  let getPositionsFromResponse :: Response -> TestEnv [(Int,Double,Double)]
+      getPositionsFromResponse rsp = do
+        let rspString1 = BS.toString $ rsBody rsp
+            Ok js = decode rspString1
+        Just (positions :: [[[(Int,Double,Double)]]]) <- withJSValue js $ do
+              fromJSValueFieldCustom "signatories" . fromJSValueCustomMany $ do
+                fromJSValueFieldCustom "fields" . fromJSValueCustomMany $ do
+                  fromJSValueFieldCustom "placements" . fromJSValueCustomMany $ do
+                    xrel <- fromJSValueField "xrel"
+                    yrel <- fromJSValueField "yrel"
+                    page <- fromJSValueField "page"
+                    return ((,,) <$> page <*> xrel <*> yrel)
+        return (concat (concat positions))
+  do
+    liftIO $ putStrLn "POST update"
+    req' <- mkRequest POST [("json", inText (encode updatejs))]
+    (rsp,_) <- runTestKontra req' ctx $ apiCallUpdate $ docid
+    assertEqual "update call suceeded" 200 (rsCode rsp)
+    poss <- getPositionsFromResponse rsp
+    assertEqual "positions in initial anchors-Signature" [(1,0.5,0.5)] poss
+
+  do
+    req' <- mkRequest POST [ ("file", inFile anchorpdf2)]
+    (rsp,_) <- runTestKontra req' ctx $ apiCallChangeMainFile $ docid
+    assertEqual "suceeded" 202 (rsCode rsp)
+    poss <- getPositionsFromResponse rsp
+    assertEqual "positions after change to anchors-Namnteckning" [(2,0.5,0.800784)] poss
+
+  do
+    req' <- mkRequest POST [ ("file", inFile noanchorpdf)]
+    (rsp,_) <- runTestKontra req' ctx $ apiCallChangeMainFile $ docid
+    assertEqual "suceeded" 202 (rsCode rsp)
+    poss <- getPositionsFromResponse rsp
+    assertEqual "positions after change to no anchors document" [(2,0.5,0.800784)] poss
+
+  do
+    req' <- mkRequest POST [ ("file", inFile anchorpdf2)]
+    (rsp,_) <- runTestKontra req' ctx $ apiCallChangeMainFile $ docid
+    assertEqual "suceeded" 202 (rsCode rsp)
+    poss <- getPositionsFromResponse rsp
+    assertEqual "positions after change back to anchors-Namnteckning" [(2,0.5,0.800784)] poss
+
+  do
+    req' <- mkRequest POST [ ("file", inFile anchorpdf3)]
+    (rsp,_) <- runTestKontra req' ctx $ apiCallChangeMainFile $ docid
+    assertEqual "suceeded" 202 (rsCode rsp)
+    poss <- getPositionsFromResponse rsp
+    assertEqual "positions after change to anchors-Unterschrift-2" [(3,0.5,1.1015681)] poss
+
+  do
+    -- it should return to the original position at this point
+    req' <- mkRequest POST [ ("file", inFile anchorpdf1)]
+    (rsp,_) <- runTestKontra req' ctx $ apiCallChangeMainFile $ docid
+    assertEqual "suceeded" 202 (rsCode rsp)
+    poss <- getPositionsFromResponse rsp
+    -- we are almost exactly in the place we started
+    assertEqual "positions after change to anchors-Signature" [(1,0.5,0.50000006)] poss
+
+  return ()
