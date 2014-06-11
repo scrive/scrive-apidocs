8d53bd12575f557710a16e6f5ea242033f637255
Bartek Ćwikłowski <bartek@scrive.com>
2014-06-11 13:43:18 +0200

    update JMeter benchmark for AVIS scenario (now number of users/loops is configurable from run.sh)

--- benchmark/avis/run.sh
+++ benchmark/avis/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if [ "$#" -lt 11 ]; then
-    echo "usage: $0 AUTHORCOMPANYID TEMPLATEID NUMBEROFPAGES FILEPATH SERVER PORT PROTOCOL TOKEN_CREDENTIALS_IDENTIFIER CLIENT_CREDENTIALS_IDENTIFIER TOKEN_CREDENTIALS_SECRET CLIENT_CREDENTIALS_SECRET"
+if [ "$#" -lt 13 ]; then
+    echo "usage: $0 AUTHORCOMPANYID TEMPLATEID NUMBEROFPAGES FILEPATH SERVER PORT PROTOCOL TOKEN_CREDENTIALS_IDENTIFIER CLIENT_CREDENTIALS_IDENTIFIER TOKEN_CREDENTIALS_SECRET CLIENT_CREDENTIALS_SECRET NUMBER_OF_CONCURRENT_USERS NUMBER_OF_LOOPS"
     echo ""
     echo "AUTHORCOMPANYID - id of template's author's company - e.g. 1"
     echo "TEMPLATEID - id of template - e.g. 123"
@@ -10,7 +10,9 @@ if [ "$#" -lt 11 ]; then
     echo "SERVER - hostname of the server to run the tests against - e.g. 127.0.0.1"
     echo "PROTOCOL - protocol - e.g. https"
     echo "[TOKEN|CLIENT]_CREDENTIALS_[IDENTIFIER/SECRET] - credentials from account page of template author"
+    echo "NUMBER_OF_CONCURRENT_USERS - number of concurrent users replaying test scenario"
+    echo "NUMBER_OF_LOOPS - number of scenario loops for every concurrent user"
     exit 1
 fi
 
-JVM_ARGS="-DCookieManager.save.cookies=true" jmeter -n -t avis.jmx "-Jauthorcompanyid=$1" "-Jtemplateid=$2" "-Jnumberofpages=$3" "-Jfilepath=$4" "-Jserver=$5" "-Jport=$6" "-Jprotocol=$7" "-Jtoken_credentials_identifier=$8" "-Jclient_credentials_identifier=$9" "-Jtoken_credentials_secret=${10}" "-Jclient_credentials_secret=${11}"
+JVM_ARGS="-DCookieManager.save.cookies=true" jmeter -n -t avis.jmx "-Jauthorcompanyid=$1" "-Jtemplateid=$2" "-Jnumberofpages=$3" "-Jfilepath=$4" "-Jserver=$5" "-Jport=$6" "-Jprotocol=$7" "-Jtoken_credentials_identifier=$8" "-Jclient_credentials_identifier=$9" "-Jtoken_credentials_secret=${10}" "-Jclient_credentials_secret=${11}" "-Jnumber_of_concurrent_users=${12}" "-Jnumber_of_loops=${13}"
