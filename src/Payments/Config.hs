7a0f65488c090fb19afb1ebacc87a15d94686c27
Gracjan Polak <gracjan@scrive.com>
2014-06-12 15:02:08 +0200

    Unjson instance for LogicaConf

--- src/Payments/Config.hs
+++ src/Payments/Config.hs
@@ -1,7 +1,25 @@
 module Payments.Config (RecurlyConfig(..)) where
 
+import Control.Applicative
+import Data.Unjson
+
 data RecurlyConfig = RecurlyConfig { recurlySubdomain  :: String
                                    , recurlyAPIKey     :: String
                                    , recurlyPrivateKey :: String
                                    }
                    deriving (Show, Read, Eq, Ord)
+
+unjsonRecurlyConfig :: UnjsonDef RecurlyConfig
+unjsonRecurlyConfig = objectOf $ pure RecurlyConfig
+  <*> field' "subdomain"
+      recurlySubdomain
+      "Recurly subdomain"
+  <*> field' "api_key"
+      recurlyAPIKey
+      "Recurly API key"
+  <*> field' "private_key"
+      recurlyPrivateKey
+      "Recurly private key"
+
+instance Unjson RecurlyConfig where
+  unjsonDef = unjsonRecurlyConfig
