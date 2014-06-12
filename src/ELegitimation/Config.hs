ae79a23e91319379fe23c36e393404ab437b36b1
Gracjan Polak <gracjan@scrive.com>
2014-06-12 15:03:07 +0200

    Unjson instance for LogicaConf

--- src/ELegitimation/Config.hs
+++ src/ELegitimation/Config.hs
@@ -1,5 +1,8 @@
 module ELegitimation.Config(LogicaConfig(..)) where
 
+import Data.Unjson
+import Control.Applicative
+
 data LogicaConfig = LogicaConfig { logicaEndpoint      :: String,  -- ^ URL to Logica
                                    logicaServiceID     :: String,  -- ^ ServiceID from Logica
                                    logicaCertFile      :: String,  -- ^ Path to certificate file
@@ -7,3 +10,24 @@ data LogicaConfig = LogicaConfig { logicaEndpoint      :: String,  -- ^ URL to L
                                    logicaMBIEndpoint   :: String   -- ^ URL for MobileBankID at Logica
                                  }
                   deriving (Show, Read, Ord, Eq)
+
+unjsonLogicaConfig :: UnjsonDef LogicaConfig
+unjsonLogicaConfig = objectOf $ pure LogicaConfig
+  <*> field' "endpoint"
+      logicaEndpoint
+      "URL"
+  <*> field' "service_id"
+      logicaServiceID
+      "Service ID"
+  <*> field' "cert_file"
+      logicaCertFile
+      "Path to certification file"
+  <*> field' "mbi_display_name"
+      logicaMBIDisplayName
+      "Mobile BankiD display name"
+  <*> field' "mbi_endpoint"
+      logicaMBIEndpoint
+      "Mobile BankiD endpoint"
+
+instance Unjson LogicaConfig where
+  unjsonDef = unjsonLogicaConfig
