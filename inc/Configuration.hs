42f17b904696a2c460ccab0cfa4d5c7db4dc1523
Gracjan Polak <gracjan@scrive.com>
2014-06-13 10:26:17 +0200

    Implement readConfig2

--- inc/Configuration.hs
+++ inc/Configuration.hs
@@ -1,5 +1,6 @@
 module Configuration (
     readConfig
+    , readConfig2
   ) where
 
 import System.Exit
@@ -8,6 +9,10 @@ import qualified Control.Exception.Lifted as E
 import Control.Monad.Base
 import Control.Monad.Trans.Control
 import Utils.Default
+import qualified Data.ByteString.Lazy as BSL
+import qualified Data.ByteString.Lazy.UTF8 as BSL (toString)
+import Data.Unjson
+import Data.Aeson.Encode.Pretty
 
 readConfig :: forall a m . (Read a, Show a, HasDefaultValue a, Monad m, MonadBaseControl IO m) => (String -> m ()) -> String -> [String] -> FilePath -> m a
 readConfig logger _appname _args path = do
@@ -22,4 +27,26 @@ readConfig logger _appname _args path = do
       logger $ "Please provide proper config file: " ++ path
       logger $ "Example configuration:"
       logger $ ppShow (defaultValue :: a)
+      liftBase exitFailure
+
+readConfig2 :: forall a m . (Unjson a, Read a, HasDefaultValue a, Monad m, MonadBaseControl IO m) => (String -> m ()) -> FilePath -> m a
+readConfig2 logger path = do
+  logger "Reading configuration file..."
+  bsl' <- liftBase (BSL.readFile path)
+  let bsl = BSL.dropWhile (`elem` [10,13,32]) bsl'
+  res <- E.try $ if "{" `BSL.isPrefixOf` bsl
+    then do -- json mode!
+      parseEither bsl
+    else do
+      liftBase (readIO (BSL.toString bsl))
+  case res of
+    Right app_conf -> do
+      logger "Configuration file read and parsed."
+      return app_conf
+    Left (e::E.SomeException) -> do
+      let ud :: UnjsonDef a = unjsonDef
+      logger $ "Error while reading '" ++ path ++ "' config file: " ++ show e
+      logger $ "Configuration documentation:\n" ++ render ud
+      logger $ "Example configuration:\n" ++ BSL.toString (encodePretty' (defConfig { confSort = compare }) (serialize ud (defaultValue :: a)))
+      logger ""
       liftBase exitFailure
