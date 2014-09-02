4876b8483a9e484362023a0e226578b61e83a9a1
Mariusz Rak <mariusz@scrive.com>
2014-09-02 14:30:46 +0200

    Remove Utils.FilePath. Implementation used there was removing extention twice, causing problems

--- misc/Utils/FilePath.hs
+++
@@ -1,8 +0,0 @@
-module Utils.FilePath ( takeBaseName
-                      ) where
-
-import qualified System.FilePath.Posix as PathPosix
-import qualified System.FilePath.Windows as PathWindows
-
-takeBaseName :: FilePath -> String
-takeBaseName = PathWindows.takeBaseName . PathPosix.takeBaseName
