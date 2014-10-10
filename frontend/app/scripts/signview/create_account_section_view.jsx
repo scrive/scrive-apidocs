60561d4c7361efde86d88b1596c5658b012c1ace
Johan Söderholm <johan@scrive.com>
2014-10-10 09:56:16 +0200

    Add some workaround for LS seeing Viktor (through the questionnaire).

--- frontend/app/scripts/signview/create_account_section_view.jsx
+++ frontend/app/scripts/signview/create_account_section_view.jsx
@@ -21,15 +21,19 @@ define(['React', 'common/language_service', 'postsignview/questionnaire_view', '
         return true;
       if (null !== /^budget.*scrive.com/.exec(location.host))
         return true;
-      //HACK. We should have such option per account.
+      //HACK. We should have such option per account. Maybe like this: https://trello.com/c/yKh4n1sG/115-purge-documents-linger-time-should-be-configurable-unionen-v3-t3
       if (this.props.document.author().company() == 'Stanley Security Solutions')
         return true;
     },
     isNJ : function() {
       return null !== /^nj.*scrive.com/.exec(location.host);
     },
+    isNotInterestedInQuestionnaire: function() {
+      // We need to tell this environment somehow that this is a branded domain. This was a quicky fix for LoxySoft and has to be reworked.
+      return null !== /^(backbone|loxysoft|caspeco).scrive.com/.exec(location.host);
+    },
     isQuestionaire : function() {
-      return (this.props.document.currentSignatory().company() !== '');
+      return (!this.isNotInterestedInQuestionnaire() && this.props.document.currentSignatory().company() !== '');
     },
     isSaveCopy : function() {
       return !this.isHidden() && !this.isNJ() && !this.isQuestionaire();
@@ -39,7 +43,7 @@ define(['React', 'common/language_service', 'postsignview/questionnaire_view', '
       if (this.isNJ()) {
         promotionName = "NJ";
       }
-      else if (this.isQuestionaire) {
+      else if (this.isQuestionaire()) {
         promotionName = "Questionnaire";
       }
       mixpanel.track("Store copy button shown", promotionName ? { promo: promotionName } : {});
