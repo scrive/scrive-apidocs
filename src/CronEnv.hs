1ba4654389259a3bfa6f1bda2cc3015b4972e37d
Andrzej Rybczak <andrzej@scrive.com>
2014-10-16 19:46:43 +0200

    sort import list in src/CronEnv.hs

--- src/CronEnv.hs
+++ src/CronEnv.hs
@@ -1,16 +1,17 @@
 module CronEnv (runScheduler) where
 
-import ActionQueue.Monad (ActionQueueT, runQueue)
-import ActionQueue.Scheduler (SchedulerData(..))
-import qualified Amazon as AWS
-import AppConf (AppConf, amazonConfig)
-import qualified Data.ByteString as BS
 import Control.Concurrent (MVar)
 import Control.Monad.Trans (MonadIO)
-import File.FileID (FileID)
-import qualified MemCache
 import Data.Time (UTCTime)
+import qualified Data.ByteString as BS
+
+import ActionQueue.Monad (ActionQueueT, runQueue)
+import ActionQueue.Scheduler (SchedulerData(..))
+import AppConf (AppConf, amazonConfig)
+import File.FileID (FileID)
 import Templates (KontrakcjaGlobalTemplates)
+import qualified Amazon as AWS
+import qualified MemCache
 
 runScheduler :: MonadIO m => AppConf -> MemCache.MemCache FileID BS.ByteString
                           -> MVar (UTCTime, KontrakcjaGlobalTemplates) -> ActionQueueT (AWS.AmazonMonadT m) SchedulerData a -> m a
