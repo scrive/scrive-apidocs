32caed4c8e8921ccc23705e00e822d45565ed9a3
Andrzej Rybczak <andrzej@scrive.com>
2014-10-16 21:49:29 +0200

    runDevCompile: force reinstalls during cabal install phase

--- build-scripts/runDevCompile.sh
+++ build-scripts/runDevCompile.sh
@@ -7,7 +7,7 @@ fi
 
 rm -f kontrakcja-test.tix
 cabal-dev --sandbox="$1" clean
-cabal-dev --sandbox="$1" install --only-dependencies
+cabal-dev --sandbox="$1" install --only-dependencies --force-reinstalls
 cabal-dev --sandbox="$1" configure -ftest-coverage
 cabal-dev --sandbox="$1" build --ghc-options="-O0 -c"
 
