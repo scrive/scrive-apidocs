d5481b37ecb7a57e9b235e1f7cba5b3709504de1
Andrzej Rybczak <andrzej@scrive.com>
2014-10-16 19:46:41 +0200

    sort import list in inc/DB/ExplainAnalyze.hs

--- inc/DB/ExplainAnalyze.hs
+++ inc/DB/ExplainAnalyze.hs
@@ -2,12 +2,12 @@
 
 module DB.ExplainAnalyze where
 
-import DB.Fetcher
 import DB.Core
-
-import DB.SQL2
-import DB.SQL
+import DB.Fetcher
 import DB.Functions
+import DB.SQL2
+
+import DB.SQL
 
 kExplainAnalyze :: (SqlTurnIntoSelect s, MonadDB m) => s -> m String
 kExplainAnalyze cmd = do
