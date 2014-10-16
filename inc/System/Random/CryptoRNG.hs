6ebd01e2b0562130fc5e63342e7ee15095e25782
Andrzej Rybczak <andrzej@scrive.com>
2014-10-16 19:46:41 +0200

    sort import list in inc/System/Random/CryptoRNG.hs

--- inc/System/Random/CryptoRNG.hs
+++ inc/System/Random/CryptoRNG.hs
@@ -2,10 +2,10 @@
 
 module System.Random.CryptoRNG where
 
-import Crypto.RNG (Random, random)
-
 import Control.Monad(liftM)
 import System.Random (StdGen, mkStdGen)
+
+import Crypto.RNG (Random, random)
 
 instance Random StdGen where
   random = mkStdGen `liftM` random
