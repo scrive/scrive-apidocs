a1949258b36479c841ba552a0d633b07de64fc2c
Andrzej Rybczak <andrzej@scrive.com>
2014-10-16 19:46:46 +0200

    sort import list in test/src/BrandedDomainTest.hs

--- test/src/BrandedDomainTest.hs
+++ test/src/BrandedDomainTest.hs
@@ -1,16 +1,14 @@
 module BrandedDomainTest (brandedDomainTests) where
 
---import Control.Monad
 import Test.Framework
 
 import BrandedDomain.BrandedDomain
 import BrandedDomain.Model
 import Company.Model
-import User.Model
---import BrandedDomain.BrandedDomainID
 import DB
 import TestingUtil
 import TestKontra
+import User.Model
 import Utils.Default
 
 brandedDomainTests :: TestEnvSt -> Test
