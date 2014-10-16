8ba15f53a40428c14cff14d7a7178b5b014442f2
Andrzej Rybczak <andrzej@scrive.com>
2014-10-16 19:46:47 +0200

    sort import list in test/src/EvidenceAttachmentsTest.hs

--- test/src/EvidenceAttachmentsTest.hs
+++ test/src/EvidenceAttachmentsTest.hs
@@ -1,13 +1,14 @@
 module EvidenceAttachmentsTest (evidenceAttachmentsTest) where
 
 import Control.Applicative((<$>))
-import qualified Data.ByteString.Char8 as BS
 import Data.List (sort)
 import Test.Framework (Test, testGroup)
 import Test.Framework.Providers.HUnit (testCase)
 import Test.HUnit (assertEqual, Assertion)
-import qualified Doc.EvidenceAttachments as E
+import qualified Data.ByteString.Char8 as BS
+
 import TestKontra
+import qualified Doc.EvidenceAttachments as E
 
 evidenceAttachmentsTest :: TestEnvSt -> Test
 evidenceAttachmentsTest _ = testGroup "EvidenceAttachments"
