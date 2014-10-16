79d15088e21d5ea9a6c0806f2dcbee737c54f4c2
Andrzej Rybczak <andrzej@scrive.com>
2014-10-16 19:46:43 +0200

    sort import list in src/AppDBTables.hs

--- src/AppDBTables.hs
+++ src/AppDBTables.hs
@@ -3,33 +3,32 @@ module AppDBTables (
   , kontraTables
   ) where
 
-import DB.Model
-import DB.SQLFunction
-
 import ActionQueue.Tables
+import Attachment.Tables
+import BrandedDomain.Tables
 import Company.Tables
 import CompanyAccounts.Tables
 import Cron.Tables
-import Doc.Tables
+import DB.Model
+import DB.SQLFunction
 import Doc.API.Callback.Tables
-import Doc.Tokens.Tables
-import HostClock.Tables
-import User.Tables
-import User.History.Tables
-import File.Tables
-import Mails.Tables
-import Session.Tables
-import OAuth.Tables
-import SMS.Tables
-import ELegitimation.ELegTransaction.Tables
-import EvidenceLog.Tables
-import Payments.Tables
-import Attachment.Tables
-import ThirdPartyStats.Tables
-import User.CallbackScheme.Tables
 import Doc.AutomaticReminder.Tables
 import Doc.SMSPin.Tables
-import BrandedDomain.Tables
+import Doc.Tables
+import Doc.Tokens.Tables
+import ELegitimation.ELegTransaction.Tables
+import EvidenceLog.Tables
+import File.Tables
+import HostClock.Tables
+import Mails.Tables
+import OAuth.Tables
+import Payments.Tables
+import Session.Tables
+import SMS.Tables
+import ThirdPartyStats.Tables
+import User.CallbackScheme.Tables
+import User.History.Tables
+import User.Tables
 
 kontraFunctions :: [SQLFunction]
 kontraFunctions = [
