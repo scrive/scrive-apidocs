93a4882ba2f8825f9c357480f5ee1ae4fa8c38df
Bartek Ćwikłowski <bartek@scrive.com>
2014-11-28 15:19:56 +0100

    dragging fields in designview now cannot enlarge any parent nodes v2 (can enlarge but not scroll)

--- frontend/app/js/fieldplacementview.js
+++ frontend/app/js/fieldplacementview.js
@@ -95,7 +95,7 @@ window.draggebleField = function(dragHandler, fieldOrPlacementFN, widthFunction,
 
     dragHandler.draggable({
         appendTo : ".design-view-frame",
-        containment: ".design-view-document-pages",
+        scroll: false,
         cursorAt : cursorNormalize ? { top :7 , left :7} : undefined,
         helper: function(event) {
             initFP();
