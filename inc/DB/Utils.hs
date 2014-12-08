118139b6cdea03f0d14080608fc8d5ad64faf04c
Andrzej Rybczak <andrzej@scrive.com>
2014-12-08 13:34:57 +0100

    db.utils: restrict loopOnUniqueViolation to at most 10 iterations

--- inc/DB/Utils.hs
+++ inc/DB/Utils.hs
@@ -10,9 +10,14 @@ import Database.PostgreSQL.PQTypes
 -- Needed for clean execution of cases "try to update a row
 -- and insert a new one if it doesn't exist", as they're prone
 -- to race condition and may throw UniqueViolation on the insert.
-loopOnUniqueViolation :: MonadCatch m => m a -> m a
-loopOnUniqueViolation action = catch action $ \dbe@DBException{..} -> do
-  case cast dbeError of
-    Just DetailedQueryError{..}
-      | qeErrorCode == UniqueViolation -> loopOnUniqueViolation action
-    _ -> throwM dbe
+-- Prevent it from looping forever by bailing out after 10 tries.
+loopOnUniqueViolation :: forall m a. MonadCatch m => m a -> m a
+loopOnUniqueViolation action = loop 1
+  where
+    loop :: Int -> m a
+    loop 10 = action
+    loop !n = catch action $ \dbe@DBException{..} -> do
+      case cast dbeError of
+        Just DetailedQueryError{..}
+          | qeErrorCode == UniqueViolation -> loop $ n+1
+        _ -> throwM dbe
