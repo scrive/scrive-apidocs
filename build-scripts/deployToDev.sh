e01e4bb148bc30c732148aa97ff2864fb4b1784b
Mariusz Rak <mariusz@scrive.com>
2014-12-09 13:39:25 +0100

    Change deployment procedure to deploy to staging in same way we deploy to staging and dev

--- build-scripts/deployToDev.sh
+++ build-scripts/deployToDev.sh
@@ -8,7 +8,7 @@ rm -rf dist texts templates frontend/dist frontend/app files
 
 EOF
 
-tar -cz --exclude '*-tmp' dist texts templates frontend/dist frontend/app files GuardTime scrivepdftools | ssh dev@dev.scrive.com tar -C kontrakcja -xvz
+tar -cz --exclude=.git* --exclude=_local* --exclude=_darcs* --exclude=log --exclude=dist/build/*/*-tmp * | ssh dev@dev.scrive.com tar -C kontrakcja -xvz
 
 ssh dev@dev.scrive.com bash -xe -s <<EOF
 
