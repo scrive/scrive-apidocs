fb07a236d5bbec37611258bf29d3e73f882ccea4
Gracjan Polak <gracjan@scrive.com>
2014-12-19 09:25:55 +0100

    Use whole calendar months

--- pure_sql/invoice_stats.sql
+++ pure_sql/invoice_stats.sql
@@ -76,14 +76,12 @@ SELECT escape_for_csv(companies.name) AS "Company name"
           FROM chargeable_items chi
          WHERE chi.company_id = companies.id
            AND chi.type = 1 -- sms
-           AND chi.time >= thetime.time
-           AND chi.time < thetime.time + interval '1 month') as "SMSes sent"
+           AND date_trunc('month', chi.time) = thetime.time) as "SMSes sent"
      , (SELECT sum(chi.quantity)
           FROM chargeable_items chi
          WHERE chi.company_id = companies.id
            AND chi.type = 2 -- eleg signatures
-           AND chi.time >= thetime.time
-           AND chi.time < thetime.time + interval '1 month') as "E-leg signatures"
+           AND date_trunc('month', chi.time) = thetime.time) as "E-leg signatures"
      , (SELECT count(*)
           FROM users
          WHERE (users.deleted IS NULL OR users.deleted > thetime.time)
