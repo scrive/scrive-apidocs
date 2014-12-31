6dc57ef67abe5efe5f0b1fb857e514fe26f80576
Gracjan Polak <gracjan@scrive.com>
2014-12-31 17:05:35 +0100

    Generalize mailer/src/ServiceChecker.hs

--- mailer/src/ServiceChecker.hs
+++ mailer/src/ServiceChecker.hs
@@ -2,9 +2,12 @@ module ServiceChecker (
     serviceAvailabilityChecker
   ) where
 
-import Control.Concurrent
+import Control.Concurrent.Lifted
 import Control.Monad
+import Control.Monad.Base
+import Control.Monad.Catch
 import Control.Monad.IO.Class
+import Control.Monad.Trans.Control
 import Data.Monoid
 
 import Crypto.RNG
@@ -16,7 +19,7 @@ import MinutesTime
 import Sender
 import qualified Log
 
-serviceAvailabilityChecker :: MailingServerConf -> CryptoRNGState -> ConnectionSource -> (Sender, Sender) -> MVar Sender -> (forall a. IO a -> IO a)  -> IO ()
+serviceAvailabilityChecker :: forall m . (MonadBaseControl IO m, Log.MonadLog m, MonadMask m, MonadIO m, Monad m, Functor m, MonadBase IO m) => MailingServerConf -> CryptoRNGState -> ConnectionSource -> (Sender, Sender) -> MVar Sender -> (forall a. m a -> m a)  -> m ()
 serviceAvailabilityChecker conf rng cs (master, slave) msender interruptible = do
     Log.mixlog_ $ "Running service checker"
     mid <- inDB $ do
@@ -34,24 +37,24 @@ serviceAvailabilityChecker conf rng cs (master, slave) msender interruptible = d
       if any (isDelivered mid) events
         then do
           Log.mixlog_ $ "Service testing emails were delivered successfully."
-          liftIO $ modifyMVar_ msender $ \sender -> do
+          modifyMVar_ msender $ \sender -> do
             when (sender /= master) $
               Log.mixlog_ $ "Restoring service " ++ show master ++ "."
             return master
         else do
           Log.mixlog_ $ "Service testing emails failed to be delivered within 11 minutes."
-          oldsender <- liftIO $ takeMVar msender
+          oldsender <- takeMVar msender
           when (oldsender == master) $ do
             Log.mixlog_ $ "Switching to " ++ show slave ++ " and resending all emails that were sent within this time."
             time <- minutesBefore 10 `fmap` currentTime
             n <- dbUpdate $ ResendEmailsSentSince time
             Log.mixlog_ $ show n ++ " emails set to be resent."
-          liftIO $ putMVar msender slave
+          putMVar msender slave
       success <- dbUpdate $ DeleteEmail mid
       when (not success) $
         Log.mixlog_ $ "Couldn't delete service testing email #" ++ show mid
   where
-    inDB :: CryptoRNGT (DBT IO) a -> IO a
+    inDB :: CryptoRNGT (DBT m) a -> m a
     inDB = withPostgreSQL cs . runCryptoRNGT rng
 
     isDelivered mid (_, emid, _, SendGridEvent _ SG_Delivered{} _) = mid == emid
