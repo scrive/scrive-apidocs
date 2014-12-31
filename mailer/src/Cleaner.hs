4da3423450d1c95cb3746f7a6454c844a4ff185d
Gracjan Polak <gracjan@scrive.com>
2014-12-31 17:03:56 +0100

    Generalize mailer/src/Cleaner.hs

--- mailer/src/Cleaner.hs
+++ mailer/src/Cleaner.hs
@@ -2,13 +2,16 @@ module Cleaner (
     cleaner
   ) where
 
+import Control.Monad.Base
+import Control.Monad.Catch
+
 import Crypto.RNG
 import DB
 import DB.PostgreSQL
 import Mails.Model
 import qualified Log
 
-cleaner :: CryptoRNGState -> ConnectionSource -> IO ()
+cleaner :: (Log.MonadLog m, MonadMask m, MonadBase IO m) => CryptoRNGState -> ConnectionSource -> m ()
 cleaner rng cs = withPostgreSQL cs . runCryptoRNGT rng $ do
   Log.mixlog_ $ "Removing mails that were (or should be) sent " ++ show daylimit ++ " days ago."
   removedCount <- dbUpdate $ DeleteMailsOlderThenDays daylimit
