5cb5992938a726945d2b957fdaccd6d6e7866128
Gracjan Polak <gracjan@scrive.com>
2014-12-31 17:55:27 +0100

    Generalize messeger/src/Cleaner.hs

--- messenger/src/Cleaner.hs
+++ messenger/src/Cleaner.hs
@@ -2,13 +2,17 @@ module Cleaner (
     cleaner
   ) where
 
+import Control.Monad.Base
+import Control.Monad.Catch
+import Control.Monad.IO.Class
+
 import Crypto.RNG
 import DB
 import DB.PostgreSQL
 import SMS.Model
 import qualified Log
 
-cleaner :: CryptoRNGState -> ConnectionSource -> IO ()
+cleaner :: (Log.MonadLog m, MonadIO m, MonadMask m, MonadBase IO m) => CryptoRNGState -> ConnectionSource -> m ()
 cleaner rng cs = withPostgreSQL cs . runCryptoRNGT rng $ do
   Log.mixlog_ $ "Removing mails that were (or should be) sent " ++ show daylimit ++ " days ago."
   removedCount <- dbUpdate $ DeleteSMSesOlderThenDays daylimit
