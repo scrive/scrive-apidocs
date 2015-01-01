7f8d68d303b6091780104320d844557d2ef9a77f
Gracjan Polak <gracjan@scrive.com>
2015-01-01 17:32:57 +0100

    Sort imports

--- inc/DB/ExplainAnalyze.hs
+++ inc/DB/ExplainAnalyze.hs
@@ -5,8 +5,9 @@ module DB.ExplainAnalyze where
 import DB.Core
 import DB.Fetcher
 import DB.Functions
-import DB.SQL
 import DB.SQL2
+
+import DB.SQL
 
 kExplainAnalyze :: (SqlTurnIntoSelect s, MonadDB m) => s -> m String
 kExplainAnalyze cmd = do
