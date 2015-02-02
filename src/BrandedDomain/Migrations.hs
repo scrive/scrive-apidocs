b421d7148b9ef97b6afed6b5d4dd3e60ee3b67db
Mariusz Rak <mariusz@scrive.com>
2015-02-02 18:30:16 +0100

    Handle BrandedDomains with no logo set in themes branding migrations

--- src/BrandedDomain/Migrations.hs
+++ src/BrandedDomain/Migrations.hs
@@ -3,6 +3,7 @@ module BrandedDomain.Migrations where
 import Control.Monad
 import Control.Monad.Catch
 import Data.Int
+import Data.Maybe
 import qualified Data.ByteString.Base64 as B64
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.ByteString.UTF8 as BS
@@ -134,11 +135,12 @@ addThemesToBrandedDomainAndMainDomain =
                  sqlResult "id"
                  sqlResult "url"
                  sqlResult "logo"
-        domains :: [(Int64, String, Binary BS.ByteString)] <- fetchMany id
+        let defaultLogo = Binary $ B64.decodeLenient $ BS.fromString $ "iVBORw0KGgoAAAANSUhEUgAAAMgAAAAwCAYAAABUmTXqAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAACaVJREFUeNrsXd1x4zYQhj33HnYQuQJTM3k/6ekeLVVgc1KArQokVSCpgIzkCiQ/3pN575kxXYGZCsJUkGCdZYxjiMUCBPR32BnMnS0KxGLx7R+AtRCRIkWKFClSpEiRIkWKFClSpKOgC5uHf/ny60D+A+1atgT/r6Nctkq2V9mK37/+tgvNjBxfKv8ZyfZZthTHqBKMp5Dtm2w7OaYiLoHjIylHVYYDYn29yzLk2rpgLrp7XHhJx/cBI48+GZLjgzE9yHYrW8/y6yWMR7alHFO1J+HXSuZnZbwwJ5vGnC/avi+fGzb6m6F8arAXqJR2++IJx7HWzH8px5Exvt9DPu4c1hnwuQohxwvDgBcIDN8ECzOTzOQdhQLAmHoALkzqXI5nGRAUt4SSgXfPGs8/awBy0QKQqebVAJaVCr5A/MFaedN8DIt2wlBwUw9DqZDfmS/eLgkT9xIIHAI1zbN8z8JVILI9I4ATD+OBPhbQJwrMGzBwnM+OmrErgSVayzH8KdtdwPdQfa8M3smzJ3DUcpzKfl8QtP4BghO53ZMwH9A028YZL4b4x5Wgzzd8R1dwLFD4A3F4ShAoLz54a6FbnQWT2rwklPAzgjiEYvDC66XGreK6SeAizRttqQToLO2DbgIXHM+BwZugdUsdgZHAQkS34dgoRd68WRN0B3s21mNPSriTHGv61Ph5bRh0HQxtdJpBE+CbBAJmMadiEgtwVJgM+IYgLvF3KQryMyPhUE/u0CbTtScA+7ImwlNscmtIyrTNEddryFGOeWP80McNw/o4ybE1SFdcF4rZzCVLgJZpbXA38maGphHImcwxO5OhBIb3hsUMkzrk8Ix9vlmCo8BxN7NYNkF6T9HgA1QAXLcu6wISA8+bZvbKYo7mHCWMvE8ZCpgtR8qCUJoAfMmx60Qio0PJ0JYI/CGg7WkmZWoAhxV48bmZfN/GANwU3z1hdGtj3R67ZvAac1sqGrdeOHcMBQCWpOqQdqcs8VPb+xgKaczxThTeM8nDCvtOPchRG4OkBkT7oMwQm4w0WoLy50HTjF20A0wwWq2NIZGQMgJyk7mH2OwKtKovcBj4grjuqs3NaQGJa8ZHp1TLJujQKo4McuxzwdHg991CMOTY6wKQnkHz+RBcJYi0H7oHTVoYJjXzMK7MMLkLQ5D6YJg7MO+TfW7c1fONlj8zxSSObvOAG3sIOpXbWY7Ia2ZQCNNQAPFJuUFYTSGMCLcv87iYMvGxG93m/qUOk16DIxcHJIwzqLkaINBtiPL7Vy2B+YBwqyYe2c0Ul/N/Y7a1IipAKNM28MhAbQ7bWnOi7g0T4ZsyG3dCOTZCgeMoznshSCYetavN3gclR6+WFfui5HjvGqSXhBW5l4vBy4FDZICrUQeESS4CLKJC8plp5qG0nOz5sR2GhKM0kr8bzby+W0nOmB32PkaEHPMAfOawbaDj0xUgT8SXwfXZAkjEv4f78tDCx5Sgzq15DKxpuT64TvB5qHNdnqzkG6FdOZaZvfeB7lWybzkiUNvWcwpri2u1VIBshPngX1ovWthoQncJNOur+NiUK10yEZp3tbouh/bpGZpofqTgeM9wYXq7LYYYyc9IlwcVF2URKqYcXWMf4Wlt5VYAAcbkYOeCf9REBcyoMYlCBYxsfyjg6epe5Uey1m40vy+PBMAUPWkAUi9+yora7n30fGaVPCo3lowumz6qYXJsqIcDucOJgFQibPv/DRuGHc4DvR7JItu7++fRiuyEPnVvCmLZex9IP4kTpsuWyQMfNLT/DFoINqje8FTnKVLvyC2ciXSKMNWlQh32Pkwu1ukBBEEC6cChoFO/vhbZ1vVeyKHIsLN+Ktd4qQ1bnRW5c+zvvACCIIFMzJX42MIPCZaHEwNJQsxbdQoMYCKlsASC9b2PE1IYdJBOAUV1G5SswwD9yzqNl3oAyRMjwL0WkXxakbZjJnCnZaTGFC73PpD+IoDzdCC+c28A0QCm9SXK0esaNNfKzxwCK9JX+p9qMhCR/NBO6K8t3zbiCqt7H2rwTqyl2cm6WK5mG10zuJMxw1O2fbzDMGYgN1X8e51pTg6UOz87QndQt7hHdbDusPfB0dapr3vjB3WxPAoDBLHD9C51enSAPm2FO/epRrvlIcZJ7PxWZ1pHa0XEHACKpbDf+/hOaco5LTXuGSQDJscsx0/K5ZpW8+i7ZAz0J995K+hjLar2aQMInMpc+V6wys3F1htyIswByUNbkcKwgJeEe8W9bAXPPBByLD3LEXjR3Y5d2iQOagui29GshL+NQ5W+MWOJldDftVgr8YovosoIPYrzpZVoP0HRQ4uvkxV3bTxq5Jjge8ee+VkbxsKPQQzoTQKViWHHNISPnNqWDDJonTvCkuYncHykC20MSqPTYkNLnxOxzoNHOc4IQFsfsr00BFKUdelCN8RnTQaog393PkDCiIvmZwwOU7Cus6iF5WKj5nDhoxQR9jH1KcdLRqA1ClBHib0TjUJYGkCyda2IiNqLAsfuzK2Hqwv5aAnC3GCp1h0qbSaoKJ3k+PX6eiTbDNuoDSAbQd87X3OLuxkYgZdviUdyjcs3NwRW0O+bDZCVsqCUUEy3087JioAFsQmWXWLTieEdD1j9cWAjRwzIKdmXbXKUYEhke8E1OcW2hd/BZ/8F6ZhSXRnM0xSzTytEY8lFN/qE94zAfK5zAfCmH1VaJ1G00AYTAWXtBiibmAPBrwQ/PJWjI55oJ3gVIXeOVWRAjmODHOvqj9rLeYoXcs+QI4xTV/Vmq/FmUvxs2CxC9iL4u97qZak2st1F35lqb+25cqG2qJpNYbcObmiw/on3grzeGI+Ou1y/ZngSweUoLQSH135zJ30s+CV+6otSU00bWYItY2iguuBDSK1ea5yN+MEIvQJTvFV1rU2A39+HHIeEHDkeRHLZMkFXYr8nMHc2rgyCpB9ojHWJyp34cckUfG88gTFHkISUY+fkStuFKdAQfQyoQiK8RPNnXRURz3z1MWbxNca5OKIyPQeOQypf2SuTsvMsx/oPIfUZciwY7yyo+yBLtCYTzygvEBhXXd0Ypbym6wTXJwWu8HBl9YODw7QnUgQqtzRDr2DZQY7w3T73hPCX19dK0OfA5vAMO+BTrltei49iDaZgucS2l7+bp/zxxwER/xTYnlxdqXMN0hvJkLazTJN9lDNCOd4o60wnR3ChOv0RT9z3WCgxCazXlQTHO58XnhiqL0wd3YlXBHbic1xUzS4ffm/o/i1A2mZBqgPJMVViUO+E+x6JBEYpIkWKFClSpEiRIkWKFOlA9I8AAwCUX9HYqlxePgAAAABJRU5ErkJggg=="
+        domains :: [(Int64, String, Maybe (Binary BS.ByteString))] <- fetchMany id
         forM_ domains $ \(did, durl, dlogo) -> do
             runQuery_ . sqlInsert "themes" $  do
               sqlSet "name" (durl ++ "_theme")
-              sqlSet "logo" (dlogo)
+              sqlSet "logo" (fromMaybe defaultLogo dlogo)
               sqlSet "brand_color" $ ("#000000":: String)
               sqlSet "brand_text_color" $ ("#ffffff":: String)
               sqlSet "action_color" $ ("#000000":: String)
@@ -161,7 +163,7 @@ addThemesToBrandedDomainAndMainDomain =
               sqlSet "service_theme" themeId
               sqlSet "login_theme" themeId
               sqlSet "browser_title" ("Scrive":: String)
-              sqlSet "favicon" $ dlogo
+              sqlSet "favicon" $ (fromMaybe defaultLogo dlogo)
               sqlSet "participant_color_1" ("#ff3377":: String)
               sqlSet "participant_color_2" ("#009999":: String)
               sqlSet "participant_color_3" ("#ffd700":: String)
