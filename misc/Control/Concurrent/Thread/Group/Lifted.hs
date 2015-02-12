c146a25a358fdd32719ed86ce420215dbcc6ff71
Andrzej Rybczak <andrzej@scrive.com>
2015-02-12 10:52:13 +0100

    misc: complete lifted version of threads (to be released as lifted-threads)

--- misc/Control/Concurrent/Thread/Group/Lifted.hs
+++ misc/Control/Concurrent/Thread/Group/Lifted.hs
@@ -1,73 +1,89 @@
-module Control.Concurrent.Thread.Group.Lifted
-  ( new, nrOfRunning, wait, fork, forkOS, forkOn,
-    forkIOWithUnmask, forkOnWithUnmask,
+-- | This is a wrapped version of "Control.Concurrent.Thread.Group"
+-- with types generalised from 'IO' to all monads in either 'MonadBase'
+-- or 'MonadBaseControl'.
+module Control.Concurrent.Thread.Group.Lifted (
     TG.ThreadGroup
-  )
-where
+  , new
+  , TG.nrOfRunning
+  , wait
+  , waitN
+
+  -- * Forking threads
+  , fork
+  , forkOS
+  , forkOn
+  , forkWithUnmask
+  , forkOnWithUnmask
+  ) where
 
 import Control.Concurrent (ThreadId)
-import Control.Concurrent.STM
 import Control.Concurrent.Thread (Result)
 import Control.Monad.Base
 import Control.Monad.Trans.Control
 import qualified Control.Concurrent.Thread.Group as TG
 
-new :: (MonadBase IO m) => m TG.ThreadGroup
+-- | Generalized version of 'TG.new'.
+new :: MonadBase IO m => m TG.ThreadGroup
 new = liftBase TG.new
 
-nrOfRunning :: TG.ThreadGroup -> STM Int
-nrOfRunning = TG.nrOfRunning
+-- | Generalized version of 'TG.wait'.
+wait :: MonadBase IO m => TG.ThreadGroup -> m ()
+wait = liftBase . TG.wait
 
-wait :: (MonadBase IO m) => TG.ThreadGroup -> m ()
-wait tg = liftBase (TG.wait tg)
+-- | Generalized version of 'TG.waitN'.
+waitN :: MonadBase IO m => Int -> TG.ThreadGroup -> m ()
+waitN i = liftBase . TG.waitN i
 
+-- | Generalized version of 'TG.forkIO'.
+fork :: MonadBaseControl IO m
+     => TG.ThreadGroup
+     -> m a
+     -> m (ThreadId, m (Result a))
+fork tg action = liftBaseWith $ \runInBase -> fixTypes
+  =<< TG.forkIO tg (runInBase action)
 
-fork :: forall m α . (MonadBaseControl IO m)
-     => TG.ThreadGroup -> m α
-     -> m (ThreadId, m (Result α))
-fork tg action = liftBaseWith (\runInBase -> TG.forkIO tg (runInBase action) >>= fixTypes)
+-- | Generalized version of 'TG.forkOS'.
+forkOS :: MonadBaseControl IO m
+       => TG.ThreadGroup
+       -> m a
+       -> m (ThreadId, m (Result a))
+forkOS tg action = liftBaseWith $ \runInBase -> fixTypes
+  =<< TG.forkOS tg (runInBase action)
+
+-- | Generalized version of 'TG.forkOn'.
+forkOn :: MonadBaseControl IO m
+       => Int
+       -> TG.ThreadGroup
+       -> m a
+       -> m (ThreadId, m (Result a))
+forkOn i tg action = liftBaseWith $ \runInBase -> fixTypes
+  =<< TG.forkOn i tg (runInBase action)
+
+-- | Generalized version of 'TG.forkIOWithUnmask'.
+forkWithUnmask :: MonadBaseControl IO m
+               => TG.ThreadGroup
+               -> ((forall b. m b -> m b) -> m a)
+               -> m (ThreadId, m (Result a))
+forkWithUnmask tg action = liftBaseWith $ \runInBase -> fixTypes
+  =<< TG.forkIOWithUnmask tg (\unmask -> runInBase (action (liftBaseOp_ unmask)))
+
+-- | Generalized version of 'TG.forkOnWithUnmask'.
+forkOnWithUnmask :: MonadBaseControl IO m
+                 => Int
+                 -> TG.ThreadGroup
+                 -> ((forall b. m b -> m b) -> m a)
+                 -> m (ThreadId, m (Result a))
+forkOnWithUnmask i tg action = liftBaseWith $ \runInBase -> fixTypes
+  =<< TG.forkOnWithUnmask i tg (\unmask -> runInBase (action (liftBaseOp_ unmask)))
+
+----------------------------------------
+
+fixTypes :: MonadBaseControl IO m
+         => (ThreadId, IO (Result (StM m a)))
+         -> IO (ThreadId, m (Result a))
+fixTypes = return . fmap (\c -> liftBase c >>= mapMEither restoreM)
   where
-    fixTypes :: (ThreadId, IO (Result (StM m α))) -> IO (ThreadId, m (Result α))
-    fixTypes = return . fmap f
-    f :: IO (Result (StM m α)) -> m (Result α)
-    f c = liftBase c >>= mapMEither restoreM
-
-forkOS :: forall m α . (MonadBaseControl IO m) => TG.ThreadGroup -> m α -> m (ThreadId, m (Result α))
-forkOS tg action = liftBaseWith (\runInBase -> TG.forkOS tg (runInBase action) >>= fixTypes)
-  where
-    fixTypes :: (ThreadId, IO (Result (StM m α))) -> IO (ThreadId, m (Result α))
-    fixTypes = return . fmap f
-    f :: IO (Result (StM m α)) -> m (Result α)
-    f c = liftBase c >>= mapMEither restoreM
-
-forkOn :: forall m α . (MonadBaseControl IO m) => Int -> TG.ThreadGroup -> m α -> m (ThreadId, m (Result α))
-forkOn i tg action = liftBaseWith (\runInBase -> TG.forkOn i tg (runInBase action) >>= fixTypes)
-  where
-    fixTypes :: (ThreadId, IO (Result (StM m α))) -> IO (ThreadId, m (Result α))
-    fixTypes = return . fmap f
-    f :: IO (Result (StM m α)) -> m (Result α)
-    f c = liftBase c >>= mapMEither restoreM
-
-forkOnWithUnmask :: forall m α . (MonadBaseControl IO m)
-                 => Int -> TG.ThreadGroup -> ((forall β. m β -> m β) -> m α) -> m (ThreadId, m (Result α))
-forkOnWithUnmask i tg action = liftBaseWith (\runInBase -> TG.forkOnWithUnmask i tg (\unmask -> runInBase (action (liftBaseOp_ unmask))) >>= fixTypes)
-  where
-    fixTypes :: (ThreadId, IO (Result (StM m α))) -> IO (ThreadId, m (Result α))
-    fixTypes = return . fmap f
-    f :: IO (Result (StM m α)) -> m (Result α)
-    f c = liftBase c >>= mapMEither restoreM
-
-forkIOWithUnmask :: forall m α . (MonadBaseControl IO m)
-                 => TG.ThreadGroup -> ((forall β. m β -> m β) -> m α) -> m (ThreadId, m (Result α))
-forkIOWithUnmask tg action = liftBaseWith (\runInBase -> TG.forkIOWithUnmask tg (\unmask -> runInBase (action (liftBaseOp_ unmask))) >>= fixTypes)
-  where
-    fixTypes :: (ThreadId, IO (Result (StM m α))) -> IO (ThreadId, m (Result α))
-    fixTypes = return . fmap f
-    f :: IO (Result (StM m α)) -> m (Result α)
-    f c = liftBase c >>= mapMEither restoreM
-
-
--- mapMEither is the missing instance of Traversable for Either
-mapMEither :: (Monad m) => (a -> m b) -> Either c a -> m (Either c b)
-mapMEither f (Right r) = f r >>= \v -> return (Right v)
-mapMEither _ (Left v) = return (Left v)
+    -- missing instance of Traversable for Either
+    mapMEither :: Monad m => (a -> m b) -> Either c a -> m (Either c b)
+    mapMEither f (Right r) = f r >>= \v -> return (Right v)
+    mapMEither _ (Left v) = return (Left v)
