6811bf512931e863b2af799a5330b43f42726bf6
Jonathan Jouty <jonathan@scrive.com>
2015-03-06 13:19:18 +0100

    Work in Progress: API Unit tests

--- test/src/DocJSONTest.hs
+++ test/src/DocJSONTest.hs
@@ -1,25 +1,27 @@
 {-# LANGUAGE OverloadedStrings #-}
 module DocJSONTest (docJSONTests) where
 
+import DocJSONTypes
+
 import Control.Applicative
 import Control.Monad.IO.Class
-import Control.Monad
+--import Control.Monad
 --import Control.Monad.Trans
 --import Data.Maybe
 import Happstack.Server
 --import Network.URI
-import Data.Typeable
+--import Data.Typeable
 import Test.Framework
-import Data.Int
+--import Data.Int
 --import Test.QuickCheck
-import Data.Text
+--import Data.Text
 import qualified Data.HashMap.Strict as H
 import qualified Data.Vector as V
 --import Text.JSON
 import qualified Data.Unjson as U
 import Data.Unjson
 import Data.Aeson
-import Data.Typeable
+--import Data.Typeable
 --import Text.JSON.FromJSValue
 --import Text.JSON.Gen
 import qualified Data.ByteString.Lazy as B
@@ -29,7 +31,7 @@ import Context
 --import DB
 import Doc.API.V1.Calls
 --import Doc.API.V1.DocumentToJSON
-import Doc.DocStateData
+--import Doc.DocStateData
 --import Doc.Model
 --import OAuth.Model
 import TestingUtil
@@ -37,25 +39,9 @@ import TestKontra as T
 --import User.Model
 --import Util.HasSomeUserInfo
 import Utils.Default
-import qualified Log
-import Utils.Read
-import DB.Derive
-
-newtype Int64AsString = Int64AsString Int64
-  deriving (Eq, Ord, Typeable)
-$(newtypeDeriveUnderlyingReadShow ''Int64AsString)
-
-fromInt64AsString :: Int64AsString -> Int64
-fromInt64AsString (Int64AsString i) = i
-
-unjsonInt64AsString :: UnjsonDef Int64AsString
-unjsonInt64AsString = unjsonInvmapR
-                        ((maybe (fail "Can't parse Int64AsString") return) . maybeRead)
-                        --(show . fromInt64AsString :: Int64AsString -> String)
-                        (show . fromInt64AsString)
-                        unjsonDef
-instance Unjson Int64AsString where
-  unjsonDef = unjsonInt64AsString
+--import qualified Log
+--import Utils.Read
+--import DB.Derive
 
 docJSONTests :: TestEnvSt -> Test
 docJSONTests env = testGroup "DocJSON"
@@ -72,31 +58,33 @@ firstTest = do
                                       ] []
   (resDoc, _) <- runTestKontra reqDoc ctx $ apiCallV1CreateFromFile
   assertEqual "We should get a 201 response" 201 (rsCode resDoc)
-  testJSONWith "test/json/test1.json" docUnjsonDef (rsBody resDoc)
+  testJSONWith "test/json/test1.json" (rsBody resDoc) docUnjsonDef
 
 
-data Doc = Doc
-  { docid :: Int64AsString
-  , title :: String
-  }
-  deriving (Show)
-
-docUnjsonDef :: UnjsonDef Doc
-docUnjsonDef = objectOf $ pure Doc
-  <*> field "id"
-            docid
-            "Doc id"
-  <*> field "title"
-            title
-            "Doc title"
-
-testJSONWith :: FilePath -> UnjsonDef a -> BS.ByteString -> TestEnv ()
-testJSONWith fp useUnjsonDef jsonBS = do
+-- | Takes a 'FilePath' to a JSON file with the desired structure (with 'null'
+-- considered as a type), a 'ByteString' with the JSON to test, and a
+-- 'UnjsonDef' which the ByteString should parse with.
+--
+-- Checks that the ByteString parses, and also that it matches the JSON given
+-- in 'FilePath' (using (==) and 'removeValues').
+testJSONWith :: FilePath -> BS.ByteString -> UnjsonDef a -> TestEnv ()
+testJSONWith fp jsonBS useUnjsonDef = do
   jsonFileBS <- liftIO $ B.readFile fp
   let Just value    = decode jsonBS
       Just jsonFile = decode jsonFileBS
-      U.Result unjsoned problems = parse useUnjsonDef value
+      -- FIXME Can we use the 'a' in U.Result to test stuff?
+      U.Result _ problems = parse useUnjsonDef value
+  -- Using Unjson we check several things about the JSON in 'jsonBS':
+  -- 1) That the correct fields exist
+  -- 2) That these are of the correct type
+  -- 3) That some JSON String types have the correct "underlying" type or set
+  --    of values (e.g. An Int64 inside JSON String, or a datatype inside a
+  --    JSON String)
   assertEqual "There should be no problems in Result" [] problems
+  -- The second check is similar, but not as powerful or helpful as Unjson, but
+  -- what it does that Unjson cannot do is:
+  -- 1) Check that no extra fields exist
+  -- 2) Check that Null fields match, regardless of what their type "should" be
   assertEqual ("JSON structure and types (including 'null') should match that in " ++ fp)
               (removeValues jsonFile) (removeValues value)
 
---
+++ test/src/DocJSONTypes.hs
@@ -0,0 +1,239 @@
+module DocJSONTypes where
+
+import Data.Int
+import Control.Applicative
+import Data.Unjson
+import Data.Time
+import System.Locale
+
+import DB.Derive
+import Utils.Read
+
+newtype Int64AsString = Int64AsString Int64
+  deriving (Eq, Ord)
+$(newtypeDeriveUnderlyingReadShow ''Int64AsString)
+
+fromInt64AsString :: Int64AsString -> Int64
+fromInt64AsString (Int64AsString i) = i
+
+unjsonInt64AsString :: UnjsonDef Int64AsString
+unjsonInt64AsString = unjsonInvmapR
+                        ((maybe (fail "Can't parse Int64AsString") return) . maybeRead)
+                        (show . fromInt64AsString)
+                        unjsonDef
+instance Unjson Int64AsString where
+  unjsonDef = unjsonInt64AsString
+
+data Doc = Doc
+  { docid                :: Int64AsString
+  , doctitle             :: String
+  -- TODO , docmainfiles         :: ![MainFile]
+  -- TODO sealed file?
+  -- TODO , docauthorattachments :: ![AuthorAttachment]
+  -- TODO evidence attachments?
+  , docmtime             :: UTCTime
+  , docctime             :: UTCTime
+  , doctimeouttime       :: Maybe UTCTime
+  , docautoremindtime    :: Maybe UTCTime
+  -- TODO , docstatus            :: !DocumentStatus
+  -- TODO state = docstatus
+  , docsignatorylinks    :: [SigLink]
+  -- TODO signorder
+  -- TODO authentication
+  -- TODO delivery
+  -- TODO , doctype              :: !DocumentType
+  , docdaystosign        :: Int32
+  , docdaystoremind      :: Maybe Int32
+  , docshowheader        :: Bool
+  , docshowpdfdownload   :: Bool
+  , docshowrejectoption  :: Bool
+  , docshowfooter        :: Bool
+  , docinvitetext        :: String
+  , docconfirmtext       :: String
+  -- TODO , doclang              :: !Lang
+  -- TODO , doctags              :: !(S.Set DocumentTag)
+  , docapicallbackurl    :: (Maybe String)
+  , docunsaveddraft      :: Bool
+  -- TODO deleted
+  -- TODO reallydeleted
+  -- TODO canperformsigning
+  , docobjectversion     :: Int64
+  -- TODO process
+  -- TODO isviewedbyauthor
+  -- TODO canberestarted
+  -- TODO canbeprolonged
+  -- TODO canbecanceled
+  -- TODO canseeallattachments
+  -- TODO accesstoken (AKA MagicHash)
+  -- TODO , doctimezonename      :: !TimeZoneName
+  }
+
+docUnjsonDef :: UnjsonDef Doc
+docUnjsonDef = objectOf $ pure Doc
+  <*> field "id"         docid         "Doc id"
+  <*> field "title"      doctitle      "Doc title"
+  -- TODO , docmainfiles         :: ![MainFile]
+  -- TODO sealed file?
+  -- TODO , docauthorattachments :: ![AuthorAttachment]
+  -- TODO evidence attachments?
+  <*> fieldBy "time"              docmtime             "" unjsonISOTime
+  <*> fieldBy "ctime"             docctime             "" unjsonISOTime
+  <*> fieldOptBy "timeouttime"    doctimeouttime       "" unjsonISOTime
+  <*> fieldOptBy "autoremindtime" docautoremindtime    "" unjsonISOTime
+  -- TODO , docstatus            :: !DocumentStatus
+  -- TODO state = docstatus
+  <*> fieldBy "signatories" docsignatorylinks "signatories" (arrayOf siglinkUnjsonDef)
+  -- TODO signorder
+  -- TODO authentication
+  -- TODO delivery
+  -- TODO , doctype              :: !DocumentType
+  <*> field "daystosign"   docdaystosign   "daystosign"
+  <*> fieldOpt "daystoremind" docdaystoremind "daystoremind"
+  <*> field "showheader"       docshowheader       "showheader"
+  <*> field "showpdfdownload"  docshowpdfdownload  "showpdfdownload"
+  <*> field "showrejectoption" docshowrejectoption "showrejectoption"
+  <*> field "showfooter"       docshowfooter       "showfooter"
+  <*> field "invitationmessage"   docinvitetext  "docinvitetext"    -- FIXME check for <p></p>?
+  <*> field "confirmationmessage" docconfirmtext "docconfirmtext"   -- FIXMe check for <p></p>?
+  -- TODO , doclang              :: !Lang
+  -- TODO , doctags              :: !(S.Set DocumentTag)
+  <*> fieldOpt "apicallbackurl" docapicallbackurl "apicallbackurl"
+  <*> field "saved" docunsaveddraft "saved"
+  -- TODO deleted
+  -- TODO reallydeleted
+  -- TODO canperformsigning
+  <*> field "objectversion" docobjectversion "objectversion"
+  -- TODO process
+  -- TODO isviewedbyauthor
+  -- TODO canberestarted
+  -- TODO canbeprolonged
+  -- TODO canbecanceled
+  -- TODO canseeallattachments
+  -- TODO accesstoken (AKA MagicHash)
+  -- TODO , doctimezonename      :: !TimeZoneName
+
+data SigLink = SigLink
+  { siglinkId :: Int64AsString
+  , siglinkCurrent :: Bool
+  , siglinkSignorder :: Int
+  , siglinkUndeliveredInvitation :: Bool
+  , siglinkUndeliveredMailInvitation :: Bool
+  , siglinkUndeliveredSMSInvitation :: Bool
+  , siglinkDeliveredInvitation :: Bool
+  , siglinkDelivery :: Delivery
+  , siglinkConfirmationdelivery :: Confirmation
+  , siglinkSigns :: Bool
+  , siglinkAuthor :: Bool
+  , siglinkSaved :: Bool
+  , siglinkDatamismatch ::     Maybe String
+  , siglinkSigndate ::         Maybe UTCTime
+  , siglinkSeendate ::         Maybe UTCTime
+  , siglinkReaddate ::         Maybe UTCTime
+  , siglinkRejecteddate ::     Maybe UTCTime
+  , siglinkRejectionreason ::  Maybe String
+  -- TODO , siglinkFields :: TODO
+  -- TODO , siglinkStatus ::  "draft"
+  -- TODO , siglinkAttachments ::  []
+  -- TODO , siglinkCsv ::  null
+  , siglinkInpadqueue :: Bool
+  , siglinkUserid ::  Maybe Int64AsString
+  , siglinkSignsuccessredirect :: Maybe String
+  , siglinkRejectredirect ::  Maybe String
+  , siglinkAuthentication :: Authentication
+  -- TODO signlink
+  }
+
+siglinkUnjsonDef :: UnjsonDef SigLink
+siglinkUnjsonDef = objectOf $ pure SigLink
+  <*> field "id"                        siglinkId                             "signatories id"
+  <*> field "current"                   siglinkCurrent                        "signatories current"
+  <*> field "signorder"                 siglinkSignorder                      "signatories signorder"
+  <*> field "undeliveredInvitation"     siglinkUndeliveredInvitation          "signatories undeliveredInvitation"
+  <*> field "undeliveredMailInvitation" siglinkUndeliveredMailInvitation      ""
+  <*> field "undeliveredSMSInvitation"  siglinkUndeliveredSMSInvitation       ""
+  <*> field "deliveredInvitation"       siglinkDeliveredInvitation            ""
+  <*> field "delivery"                  siglinkDelivery                       ""
+  <*> field "confirmationdelivery"      siglinkConfirmationdelivery           ""
+  <*> field "signs"                     siglinkSigns                          ""
+  <*> field "author"                    siglinkAuthor                         ""
+  <*> field "saved"                     siglinkSaved                          ""
+  <*> fieldOpt "datamismatch"           siglinkDatamismatch                   ""
+  <*> fieldOptBy "signdate"             siglinkSigndate                       "" unjsonISOTime
+  <*> fieldOptBy "seendate"             siglinkSeendate                       "" unjsonISOTime
+  <*> fieldOptBy "readdate"             siglinkReaddate                       "" unjsonISOTime
+  <*> fieldOptBy "rejecteddate"         siglinkRejecteddate                   "" unjsonISOTime
+  <*> fieldOpt "rejectionreason"        siglinkRejectionreason                ""
+  -- TODO , siglinkFields :: TODO
+  -- TODO , siglinkStatus ::  "draft"
+  -- TODO , siglinkAttachments ::  []
+  -- TODO , siglinkCsv ::  null
+  <*> field "inpadqueue"                siglinkInpadqueue                     ""
+  <*> fieldOpt "userid"                 siglinkUserid                         ""
+  <*> fieldOpt "signsuccessredirect"    siglinkSignsuccessredirect            ""
+  <*> fieldOpt "rejectredirect"         siglinkRejectredirect                 ""
+  <*> field "authentication"            siglinkAuthentication                 ""
+  -- TODO signlink
+
+readISOTime :: String -> Maybe UTCTime
+readISOTime = parseTime defaultTimeLocale "%0Y-%0m-%0dT%0H:%0M:%0SZ"
+unjsonISOTime :: UnjsonDef UTCTime
+unjsonISOTime = unjsonInvmapR
+                  ((maybe (fail "Can't parse ISO time format") return) . readISOTime)
+                  show
+                  unjsonDef
+
+data Delivery = DeliveryEmail | DeliveryPad | DeliveryAPI | DeliveryMobile
+              | DeliveryEmailMobile
+  deriving (Show)
+
+unjsonDelivery :: UnjsonDef Delivery
+unjsonDelivery = unjsonInvmapR
+                   ((maybe (fail "Can't parse Delivery") return) . readDelivery)
+                   show
+                   unjsonDef
+    where readDelivery :: String -> Maybe Delivery
+          readDelivery "email"        = Just DeliveryEmail
+          readDelivery "pad"          = Just DeliveryPad
+          readDelivery "api"          = Just DeliveryAPI
+          readDelivery "mobile"       = Just DeliveryMobile
+          readDelivery "email_mobile" = Just DeliveryEmailMobile
+          readDelivery _ = Nothing
+
+instance Unjson Delivery where
+  unjsonDef = unjsonDelivery
+
+data Confirmation = ConfirmationEmail | ConfirmationMobile
+                  | ConfirmationEmailMobile | ConfirmationNone
+  deriving (Show)
+
+unjsonConfirmation :: UnjsonDef Confirmation
+unjsonConfirmation = unjsonInvmapR
+                     ((maybe (fail "Can't parse Confirmation") return) . readConfirmation)
+                     show
+                     unjsonDef
+    where readConfirmation :: String -> Maybe Confirmation
+          readConfirmation "email"        = Just ConfirmationEmail
+          readConfirmation "mobile"       = Just ConfirmationMobile
+          readConfirmation "email_mobile" = Just ConfirmationEmailMobile
+          readConfirmation "none"         = Just ConfirmationNone
+          readConfirmation _ = Nothing
+
+instance Unjson Confirmation where
+  unjsonDef = unjsonConfirmation
+
+data Authentication = AuthenticationStandard | AuthenticationELeg | AuthenticationSMS
+  deriving (Show)
+
+unjsonAuthentication :: UnjsonDef Authentication
+unjsonAuthentication = unjsonInvmapR
+                       ((maybe (fail "Can't parse Authentication") return) . readAuthentication)
+                       show
+                       unjsonDef
+    where readAuthentication :: String -> Maybe Authentication
+          readAuthentication "standard" = Just AuthenticationStandard
+          readAuthentication "eleg"     = Just AuthenticationELeg
+          readAuthentication "sms_pin"  = Just AuthenticationSMS
+          readAuthentication _ = Nothing
+
+instance Unjson Authentication where
+  unjsonDef = unjsonAuthentication
