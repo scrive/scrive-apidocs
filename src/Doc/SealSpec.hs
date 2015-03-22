0b487b4701be8534a45a27a09a937416f93fe1e9
Gracjan Polak <gracjan@scrive.com>
2015-03-22 13:18:38 +0100

    Remove Text.JSON.Gen from SealSpec.

--- src/Doc/SealSpec.hs
+++ src/Doc/SealSpec.hs
@@ -2,7 +2,6 @@ module Doc.SealSpec where
 
 import qualified Data.ByteString.Base64 as B64
 import qualified Data.ByteString.Char8 as BS
-import qualified Text.JSON.Gen as J
 import qualified Data.Unjson as Unjson
 import Control.Applicative
 import Data.Functor.Invariant
@@ -80,25 +79,6 @@ unjsonPerson = Unjson.objectOf $ pure Person
         companyNumberText
         ""
 
-instance J.ToJSValue Person where
-  toJSValue person = J.runJSONGen $ do
-    J.value "fullname" $ fullname person
-    J.value "company" $ company person
-    J.value "personalnumber" $ personalnumber person
-    J.value "companynumber" $ companynumber person
-    J.value "email" $ email person
-    J.value "phone" $ phone person
-    J.value "fullnameverified" $ fullnameverified person
-    J.value "companyverified" $ companyverified person
-    J.value "numberverified" $ numberverified person
-    J.value "emailverified" $ emailverified person
-    J.value "phoneverified" $ phoneverified person
-    J.value "fields" $ fields person
-    J.value "signtime" $ signtime person
-    J.value "signedAtText" $ signedAtText person
-    J.value "personalNumberText" $ personalNumberText person
-    J.value "companyNumberText" $ companyNumberText person
-
 -- | Field coordinates are in screen coordinate space. That means:
 --
 -- * upper left corner is (0,0)
@@ -166,31 +146,6 @@ unjsonField = Unjson.unionOf
                  <*> Unjson.fieldOpt "keyColor" keyColor ""
               )]
 
-instance J.ToJSValue Field where
-  toJSValue Field{..} = J.runJSONGen $ do
-    J.value "value" value
-    J.value "x" x
-    J.value "y" y
-    J.value "page" page
-    J.value "fontSize" fontSize
-    J.value "greyed" greyed
-    J.value "includeInSummary" includeInSummary
-  toJSValue FieldJPG{..} = J.runJSONGen $ do
-    -- Beware of changing the imports; if BS refers to
-    -- Data.ByteString, you will get [Word8] instead of
-    -- a String and the whole JSON will be badly rendered.
-    -- Yet another reason to ditch StringS.
-    J.value "valueBase64" (BS.unpack $ B64.encode valueBinary :: String)
-    J.value "x" x
-    J.value "y" y
-    J.value "page" page
-    J.value "image_w" image_w
-    J.value "image_h" image_h
-    J.value "includeInSummary" includeInSummary
-    J.value "onlyForSummary" onlyForSummary
-    J.value "keyColor" $ case keyColor of
-                           Just (r,g,b) -> Just [r,g,b]
-                           Nothing -> Nothing
 
 -- | An attachment that will be put into a PDF. Attachments are put in
 -- order.  File name should be without any directory parts. File
@@ -209,11 +164,6 @@ unjsonSealAttachment = Unjson.objectOf $ pure SealAttachment
    <*> Unjson.fieldBy "fileBase64Content" fileContent ""
          (invmap (B64.decodeLenient . BS.pack . Text.unpack) (Text.pack . BS.unpack . B64.encode) Unjson.unjsonDef)
 
-instance J.ToJSValue SealAttachment where
-  toJSValue SealAttachment{..} = J.runJSONGen $ do
-   J.value "fileName" fileName
-   J.value "mimeType" mimeType
-   J.value "fileBase64Content" (BS.unpack $ B64.encode fileContent :: String)
 
 data SealSpec = SealSpec
     { input          :: String
@@ -246,21 +196,6 @@ unjsonSealSpec = Unjson.objectOf $ pure SealSpec
     <*> Unjson.fieldBy "attachments" attachments "" (Unjson.arrayOf unjsonSealAttachment)
     <*> Unjson.fieldBy "filesList" filesList "" (Unjson.arrayOf unjsonFileDesc)
 
-instance J.ToJSValue SealSpec where
-  toJSValue SealSpec{..} = J.runJSONGen $ do
-    J.value "input" input
-    J.value "output" output
-    J.value "documentNumberText" documentNumberText
-    J.value "persons" persons
-    J.value "secretaries" secretaries
-    J.value "initiator" initiator
-    J.value "history" history
-    J.value "initialsText" initialsText
-    J.value "hostpart" hostpart
-    J.value "staticTexts" staticTexts
-    J.value "attachments" attachments
-    J.value "filesList" filesList
-
 
 data FileDesc = FileDesc
     { fileTitle      :: String
@@ -279,27 +214,12 @@ unjsonFileDesc = Unjson.objectOf $ pure FileDesc
     <*> Unjson.field "attachedBy" fileAttachedBy ""
     <*> Unjson.fieldOpt "input" fileInput ""
 
-instance J.ToJSValue FileDesc where
-  toJSValue FileDesc{..} = J.runJSONGen $ do
-    J.value "title" fileTitle
-    J.value "role" fileRole
-    J.value "pagesText" filePagesText
-    J.value "attachedBy" fileAttachedBy
-    J.value "input" fileInput
-
 data PreSealSpec = PreSealSpec
     { pssInput  :: String
     , pssOutput :: String
     , pssFields :: [Field]
     }
     deriving (Eq,Ord,Show,Read)
-
-instance J.ToJSValue PreSealSpec where
-  toJSValue PreSealSpec{..} = J.runJSONGen $ do
-    J.value "input" pssInput
-    J.value "output" pssOutput
-    J.value "fields" $ map J.toJSValue $ pssFields
-    J.value "preseal" True
 
 unjsonPreSealSpec :: Unjson.UnjsonDef PreSealSpec
 unjsonPreSealSpec = Unjson.objectOf $ pure PreSealSpec
@@ -320,12 +240,6 @@ unjsonHistEntry = Unjson.objectOf $ pure HistEntry
     <*> Unjson.field "date" histdate ""
     <*> Unjson.field "comment" histcomment ""
     <*> Unjson.field "address" histaddress ""
-
-instance J.ToJSValue HistEntry where
-  toJSValue HistEntry{..} = J.runJSONGen $ do
-    J.value "date" histdate
-    J.value "comment" histcomment
-    J.value "address" histaddress
 
 
 {- |  Static (almost) text for sealing document.
@@ -358,16 +272,3 @@ unjsonSealingTexts = Unjson.objectOf $ pure SealingTexts
    <*> Unjson.field "verificationFooter" verificationFooter ""
    <*> Unjson.field "hiddenAttachmentText" hiddenAttachmentText ""
    <*> Unjson.field "onePageText" onePageText ""
-
-instance J.ToJSValue SealingTexts where
-  toJSValue SealingTexts{..} = J.runJSONGen $ do
-   J.value "verificationTitle" verificationTitle
-   J.value "partnerText" partnerText
-   J.value "initiatorText" initiatorText
-   J.value "documentText" documentText
-   J.value "eventsText" eventsText
-   J.value "dateText" dateText
-   J.value "historyText" historyText
-   J.value "verificationFooter" verificationFooter
-   J.value "hiddenAttachmentText" hiddenAttachmentText
-   J.value "onePageText" onePageText
