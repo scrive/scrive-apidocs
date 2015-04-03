9418dbe4cff308e1353556cdb2b47c8b32a146c7
Bartek Ćwikłowski <bartek@scrive.com>
2015-04-03 15:12:47 +0200

    fix style of takeScreenshot.js

--- frontend/app/js/takeScreenshot.js
+++ frontend/app/js/takeScreenshot.js
@@ -1,4 +1,4 @@
-define(['Backbone', 'html2canvas', 'legacy_code'], function(Backbone, html2canvas) {
+define(["Backbone", "html2canvas", "legacy_code"], function (Backbone, html2canvas) {
 
 /* takeScreenshot(success, error, timeout, timeoutval):
     Render a canvas with current viewport and calls exactly one of three callbacks:
@@ -9,57 +9,58 @@ define(['Backbone', 'html2canvas', 'legacy_code'], function(Backbone, html2canva
 */
 
     var scaleCanvas = function (canvas, scale) {
-      var newCanvas = document.createElement('canvas');
+      var newCanvas = document.createElement("canvas");
       newCanvas.width = Math.ceil(canvas.width * scale);
       newCanvas.height = Math.ceil(canvas.height * scale);
 
-      var ctx = newCanvas.getContext('2d');
+      var ctx = newCanvas.getContext("2d");
       ctx.drawImage(canvas, 0, 0, canvas.width, canvas.height, 0, 0, canvas.width * scale, canvas.height * scale);
       return newCanvas;
     };
 
-    window.takeScreenshot = function(success, error, timeout, timeoutval, extraErrorLogParams) {
-        var mixpanelScreenshotError = function(reason, extraParams) {
-          var errorInfo = {'Reason': reason,
-                           'Browser': $.browser.name,
-                           'Browser version': $.browser.version,
-                           'Platform': $.browser.platform};
-          mixpanel.track('Take screenshot failed', _.extend(errorInfo, extraParams, extraErrorLogParams));
+    window.takeScreenshot = function (success, error, timeout, timeoutval, extraErrorLogParams) {
+        var mixpanelScreenshotError = function (reason, extraParams) {
+          var errorInfo = {"Reason": reason,
+                           "Browser": $.browser.name,
+                           "Browser version": $.browser.version,
+                           "Platform": $.browser.platform};
+          mixpanel.track("Take screenshot failed", _.extend(errorInfo, extraParams, extraErrorLogParams));
         };
 
         var callbackCalled = false;
         function timedout() {
             if (!callbackCalled) {
                 callbackCalled = true;
-                mixpanelScreenshotError('Timeout');
+                mixpanelScreenshotError("Timeout");
                 timeout();
             }
         };
 
-        if(timeout)
+        if (timeout) {
             window.setTimeout(timedout, timeoutval);
+        }
 
         if (window.html2canvas === undefined) {
           window.html2canvas = html2canvas;
         }
-        html2canvas($('body'), {logging: true, type: 'view'}).then(function(canvas) {
+        html2canvas($("body"), {logging: true, type: "view"}).then(function (canvas) {
           var newCanvas = scaleCanvas(canvas, 0.6); // smallest scale that still makes text readable
           canvas = null;
           if (!callbackCalled) {
             callbackCalled = true;
-            var errorInfo = {'Browser': $.browser.name,
-                             'Browser version': $.browser.version,
-                             'Platform': $.browser.platform};
-            mixpanel.track('Take screenshot success', _.extend(errorInfo, extraErrorLogParams));
+            var errorInfo = {"Browser": $.browser.name,
+                             "Browser version": $.browser.version,
+                             "Platform": $.browser.platform};
+            mixpanel.track("Take screenshot success", _.extend(errorInfo, extraErrorLogParams));
             success(newCanvas);
           }
-        })['catch'](function(err) { // use catch as key, because IE8 cannot handle catch attribute
+        })["catch"](function (err) { // use catch as key, because IE8 cannot handle catch attribute
           if (!callbackCalled) {
             callbackCalled = true;
             if (err === "No canvas support") {
-              mixpanelScreenshotError('Not supported browser');
+              mixpanelScreenshotError("Not supported browser");
             } else {
-              mixpanelScreenshotError('Error', {'Error message': err});
+              mixpanelScreenshotError("Error", {"Error message": err});
             }
             error(err);
           }
