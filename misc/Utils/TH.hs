171c37ff83b11bc97e8e2eb024f67707f8805f6e
Andrzej Rybczak <andrzej@scrive.com>
2015-05-12 15:02:40 +0200

    make Utils.TH work with ghc 7.6.*

--- misc/Utils/TH.hs
+++ misc/Utils/TH.hs
@@ -3,9 +3,16 @@ module Utils.TH where
 import Language.Haskell.TH
 import Prelude
 
+-- | Evaluates to a function
+-- @
+--   \t -> case t of
+--     Con{} -> True
+--     _     -> False
+-- @
 isConstr :: Name -> Q Exp
-isConstr _con = [|
-    \t -> case t of
-      $(recP _con []) -> True
-      _               -> False
-  |]
+isConstr con = do
+  t <- newName "t"
+  return $ LamE [VarP t] $ CaseE (VarE t) [
+      Match (RecP con []) (NormalB $ ConE 'True) []
+    , Match WildP (NormalB $ ConE 'False) []
+    ]
