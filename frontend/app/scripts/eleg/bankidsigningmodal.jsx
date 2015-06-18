254625f34018cebd8061ace30a190f68452ebadb
Bartek Ćwikłowski <bartek@scrive.com>
2015-06-18 10:49:44 +0200

    fix bankid problems on android devices by always using non-iframe bankid invocation method

--- frontend/app/scripts/eleg/bankidsigningmodal.jsx
+++ frontend/app/scripts/eleg/bankidsigningmodal.jsx
@@ -6,17 +6,33 @@ var updateLoadingDialogWithBankIDStatus = function(bankID) {
   var div = $("<div/>");
   if (!bankID.isFaultStatus() && !bankID.isWaitingForToken()) {
     div.append($("<p/>").html(bankID.statusMessage()));
-    if (bankID.thisDevice() && (bankID.isStatusOutstanding() || bankID.isStatusNoClient()) && bankID.activeForAtLeast5Sec()) {
-      div.append($("<a class='button button-green button-tiny'/>").text(localization.docsignview.eleg.bankid.rfa18).click(function() {
+    if (bankID.thisDevice() && (bankID.isStatusOutstanding() || bankID.isStatusNoClient())) {
+      if (BrowserInfo.isAndroid()) {
+        // on android devices, dont wait 5s, fall back to the other method immediately
+        // since iframes dont work at all, as a bonus, there's no need to click
+        // another button
+
+        // but first changing window.location from https:// to bankid:// scheme
+        // when window.onbeforeunload is set causes alert on android devices
+        // so disable onbeforeunload for some time
+        ReloadManager.stopBlocking();
+        setTimeout(function() {
+          ReloadManager.startBlocking();
+        }, 5000);
+
         window.location = bankID.bankIdUrl();
-      }));
+      } else if (bankID.activeForAtLeast5Sec()) {
+        div.append($("<a class='button button-green button-tiny'/>").text(localization.docsignview.eleg.bankid.rfa18).click(function() {
+          window.location = bankID.bankIdUrl();
+        }));
+      }
     }
   }
   LoadingDialog.open({header: div});
 };
 
 var addBankIDIframeIfItsNeeded = function(bankID) {
-  if (!bankID.isFaultStatus() && !bankID.isWaitingForToken() && bankID.thisDevice() && ($("#bankid-"+ bankID.autoStartToken()).size() == 0)) {
+  if (!bankID.isFaultStatus() && !bankID.isWaitingForToken() && bankID.thisDevice() && ($("#bankid-"+ bankID.autoStartToken()).size() == 0) && !BrowserInfo.isAndroid()) {
     $("body").append($("<iframe width='0' height='0' class='bankid-iframe'/>").attr('id','bankid-'+ bankID.autoStartToken()).attr('src',bankID.bankIdUrl()));
   }
 };
