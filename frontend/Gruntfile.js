02e70cff0be3fcba9c38c1a7f7c9d6ae211892a0
Ola Holmström <ola@scrive.com>
2015-11-17 17:03:27 +0100

    Clean grunt conf.
    https://trello.com/c/5Zmueenu/392-clean-grunt-conf

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -1,164 +1,57 @@
-'use strict';
+var JSX_BIN = __dirname + "/node_modules/react-tools/bin/jsx ";
 
-var LIVERELOAD_PORT = 35729;
-var lrSnippet = require('connect-livereload')({ port: LIVERELOAD_PORT });
-var mountFolder = function(connect, dir) {
-  return connect.static(require('path').resolve(dir));
-};
+module.exports = function (grunt) {
+  require("load-grunt-tasks")(grunt);
+  require("time-grunt")(grunt);
+  require("./custom_grunt_tasks/deploybuild")(grunt);
 
-module.exports = function(grunt) {
-  require('load-grunt-tasks')(grunt);
-  require('time-grunt')(grunt);
-  grunt.loadNpmTasks('grunt-shell-spawn');
-  grunt.loadNpmTasks('grunt-karma');
-  grunt.loadNpmTasks('grunt-jscs');
-  require('./custom_grunt_tasks/deploybuild')(grunt);
-
-  // configurable paths
   var yeomanConfig = {
-    app: 'app',
-    dist: 'dist',
-    kontrakcja: '../'
+    app: require("./bower.json").appPath || "app",
+    dist: "dist",
+    kontrakcja: "../"
   };
-
-  try {
-    yeomanConfig.app = require('./bower.json').appPath || yeomanConfig.app;
-  } catch (e) {
-  }
 
   grunt.initConfig({
     yeoman: yeomanConfig,
+
     watch: {
       less: {
         files: ["<%= yeoman.app %>/less/**/*.less", "<%= yeoman.app %>/scripts/temporary_less_directory/**/*.less"],
-        tasks: ["less:compile", "autoprefixer:compile"]
+        tasks: ["compileStyles"]
       },
       localization: {
         files: [
-          '<%= yeoman.kontrakcja %>/texts/**/*.json',
-          '<%= yeoman.kontrakcja %>/templates/javascript-langs.st'
+          "<%= yeoman.kontrakcja %>/texts/**/*.json",
+          "<%= yeoman.kontrakcja %>/templates/javascript-langs.st"
         ],
         tasks: ["updateLocalization"]
-      },
-      livereload: {
-        options: {
-          livereload: LIVERELOAD_PORT
-        },
-        files: [
-          '<%= yeoman.app %>/less/*.css',
-          '.tmp/styles/{,*/}*.css',
-          '{.tmp,<%= yeoman.app %>}/compiled_jsx/**/*.js',
-          '<%= yeoman.app %>/js/**/*.js',
-          '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
-        ]
       }
     },
+
     jscs: {
       src: require("./jscs_manifest.json"),
       options: {
         preset: "yandex",
-        validateQuoteMarks: "\"", // scrive uses double quotes
-        disallowQuotedKeysInObjects: null, // allow quoting in object keys
+        validateQuoteMarks: "\"",
+        disallowQuotedKeysInObjects: null,
         esprima: "esprima-fb"
       }
     },
-    connect: {
-      proxyserver: {
-        // Proxy connections to either kontratcja or angularjs
-        options: {
-          debug: true,
-          hostname: '*',
-          port: 9000,
-          middleware: function(connect, options) {
-            // Configure grunt-connect-proxy
 
-            var config = [ // Serve static files.
-              connect.static(options.base),
-              // Make empty directories browsable.
-              connect.directory(options.base)
-            ];
-            var proxy = require('grunt-connect-proxy/lib/utils').proxyRequest;
-            config.unshift(proxy);
-            return config;
-          }
-        },
-        proxies: [
-          // Serve most of the static files with grunt (since it's faster atleast on mac)
-          {
-            context: ['/js', '/libs', '/compiled_jsx', '/img', '/css', '/bower_components'],
-            host: '127.0.0.1',
-            port: 9001,
-            https: false
-          },
-          // this gives access to kontrakcja under root
-          {
-            context: ['/'],
-            host: '127.0.0.1',
-            port: 8000,
-            https: false
-          }
-        ]
-      },
-      livereload: {
-        // this is the main server for serving static files
-        options: {
-          // Change this to '0.0.0.0' to access the server from outside.
-          hostname: '*',
-          port: 9001,
-          middleware: function(connect) {
-            return [
-              lrSnippet,
-              mountFolder(connect, '.tmp'),
-              mountFolder(connect, yeomanConfig.app)
-            ];
-          }
-        }
-      },
-      test: {
-        options: {
-          port: 9005,
-          middleware: function(connect) {
-            return [
-              mountFolder(connect, '.tmp'),
-              mountFolder(connect, 'test')
-            ];
-          }
-        }
-      },
-      dist: {
-        options: {
-          port: 9001,
-          middleware: function(connect) {
-            return [
-              mountFolder(connect, yeomanConfig.dist)
-            ];
-          }
-        }
-      }
-    },
     karma: {
       unitSingleRun: {
         singleRun: true,
-        configFile: 'karma.conf.js',
-        autoWatch: false // If true: run tests automatically when a file changes. Should be false on CI server.
-      },
-      unitWatch: {
-        singleRun: false,
-        configFile: 'karma.conf.js',
-        autoWatch: true
+        configFile: "karma.conf.js",
+        autoWatch: false
       }
     },
-    open: {
-      server: {
-        url: 'http://localhost:<%= connect.proxyserver.options.port %>/r/'
-      }
-    },
+
     requirejs: {
       options: {
-        baseUrl: '<%= yeoman.app %>/compiled_jsx/',
-        mainConfigFile: '<%= yeoman.app %>/compiled_jsx/requirejs_config.js',
-        out: '<%= yeoman.dist %>/optimized-system.js',
-        include: ['../bower_components/requirejs/require.js'],
+        baseUrl: "<%= yeoman.app %>/compiled_jsx/",
+        mainConfigFile: "<%= yeoman.app %>/compiled_jsx/requirejs_config.js",
+        out: "<%= yeoman.dist %>/optimized-system.js",
+        include: ["../bower_components/requirejs/require.js"],
         preserveLicenseComments: false
       },
       withSourceMap: {
@@ -174,17 +67,18 @@ module.exports = function(grunt) {
         }
       }
     },
+
     clean: {
       dist: {
         files: [
           {
             dot: true,
             src: [
-              '.tmp',
-              '<%= yeoman.app %>/compiled_jsx/',
-              '<%= yeoman.app %>/localization/',
-              '<%= yeoman.dist %>/*',
-              '!<%= yeoman.dist %>/.git*'
+              ".tmp",
+              "<%= yeoman.app %>/compiled_jsx/",
+              "<%= yeoman.app %>/localization/",
+              "<%= yeoman.dist %>/*",
+              "!<%= yeoman.dist %>/.git*"
             ]
           }
         ]
@@ -194,160 +88,142 @@ module.exports = function(grunt) {
           {
             dot: true,
             src: [
-              '.tmp',
-              '<%= yeoman.app %>/compiled_jsx/'
+              ".tmp",
+              "<%= yeoman.app %>/compiled_jsx/"
             ]
           }
         ]
       }
     },
-    jshint: {
-      options: {
-        // this .jshintrc contains jshint options, which may overwrite options we set here
-        jshintrc: '.jshintrc',
-        ignores: [
-        ]
-      },
-      all: [
-        '<%= yeoman.app %>/compiled_jsx/**/*.js'
-      ]
-    },
+
     deploybuild: {
       localization: {
         files: {
           src: [
-            '<%= yeoman.app %>/localization/*.js'
+            "<%= yeoman.app %>/localization/*.js"
           ]
         }
       },
       dist: {
         files: {
           src: [
-            '<%= yeoman.dist %>/*.js',
-            '<%= yeoman.dist %>/*.css'
+            "<%= yeoman.dist %>/*.js",
+            "<%= yeoman.dist %>/*.css"
           ]
         }
       }
     },
+
     shell: {
       options: {
         stdout: true,
         stderr: true,
         failOnError: true
       },
-      tar: {
-        command: [
-          'echo "Version $VER" > <%= yeoman.dist %>/VERSION',
-          'tar -czf growth-$VER.tar.gz ./dist/'
-        ].join(';')
-      },
       updateLastBuilt: {
-        command: 'echo "$(date +%s)" > <%= yeoman.dist %>/LAST_BUILT'
-      },
-      // TODO(jens): Move this compilation to a grunt plugin
-      // The existing grunt-react plugin doesnt support -w parameter as far as I can see
-      compileJsxWatch: {
-        command: './node_modules/react-tools/bin/jsx -x jsx -w <%= yeoman.app %>/scripts/ <%= yeoman.app %>/compiled_jsx/',
-        options: {
-          async: true
-        }
+        command: "echo '$(date +%s)' > <%= yeoman.dist %>/LAST_BUILT"
       },
       compileJsx: {
-        command: './node_modules/react-tools/bin/jsx -x jsx <%= yeoman.app %>/scripts/ <%= yeoman.app %>/compiled_jsx/'
+        command: JSX_BIN + " -x jsx <%= yeoman.app %>/scripts/ <%= yeoman.app %>/compiled_jsx/"
+      },
+      watchJsx: {
+        command: JSX_BIN + " -x jsx -w <%= yeoman.app %>/scripts/ <%= yeoman.app %>/compiled_jsx/"
       },
       compileLocalization: {
-        command: 'cabal build localization',
+        command: "cabal build localization",
         options: {
           execOptions: {
-            cwd: '<%= yeoman.kontrakcja %>'
+            cwd: "<%= yeoman.kontrakcja %>"
           }
         }
       },
       generateLocalization: {
-        command: './dist/build/localization/localization',
+        command: "./dist/build/localization/localization",
         options: {
           execOptions: {
-            cwd: '<%= yeoman.kontrakcja %>'
+            cwd: "<%= yeoman.kontrakcja %>"
           }
         }
       }
     },
+
     copy: {
       dist: {
         files: [
           {
             expand: true,
             dot: true,
-            cwd: '<%= yeoman.app %>',
-            dest: '<%= yeoman.dist %>',
+            cwd: "<%= yeoman.app %>",
+            dest: "<%= yeoman.dist %>",
             src: [
-              './*.{html,xml,jpg,png,ico,txt}',
-              'api-explorer/**/*',
-              'enable-cookies/**/*',
-              'css/fonts/*',
-              'css/assets/*',
-              'fonts/**/*',
-              'img/**/*',
-              'newsletter/**/*',
-              'pdf/**/*',
-              'libs/tiny_mce/**/*',
-              // Static localization files - already versioned
-              'localization/*.*.js',
-
-              // Shims that need to be loaded separately
-              'libs/html5shiv.js',
-
-              'bower_components/json3/**',
-              'bower_components/es5-shim/**'
+              "./*.{html,xml,jpg,png,ico,txt}",
+              "api-explorer/**/*",
+              "enable-cookies/**/*",
+              "css/fonts/*",
+              "css/assets/*",
+              "fonts/**/*",
+              "img/**/*",
+              "newsletter/**/*",
+              "pdf/**/*",
+              "libs/tiny_mce/**/*",
+              "localization/*.*.js",
+              "libs/html5shiv.js",
+              "bower_components/json3/**",
+              "bower_components/es5-shim/**"
             ]
-          },
+          }
         ]
       }
     },
+
     gjslint: {
       options: {
         flags: [
-          '--disable 1,2,5,110,120,131,220', // TODO(jens): Fix so these pass
-          '--custom_jsdoc_tags jsx,note,description'
+          "--disable 1,2,5,110,120,131,220",
+          "--custom_jsdoc_tags jsx,note,description"
         ],
         reporter: {
-          name: 'console'
+          name: "console"
         }
       },
       all: {
         src: [
-          '<%= yeoman.app %>/js/**/*.js',
-          '<%= yeoman.app %>/compiled_jsx/**/*.js'
+          "Gruntfile.js",
+          "<%= yeoman.app %>/js/**/*.js",
+          "<%= yeoman.app %>/compiled_jsx/**/*.js"
         ]
       }
     },
+
     cssmin: {
       options: {
         report: true
       },
       dist: {
         files: {
-          '<%= yeoman.dist %>/all-styling-minified.css': [
-            '<%= yeoman.app %>/css/*.css',
-            '<%= yeoman.app %>/less/less-compiled.css',
-            '!<%= yeoman.app %>/css/tinymce.css'
+          "<%= yeoman.dist %>/all-styling-minified.css": [
+            "<%= yeoman.app %>/css/*.css",
+            "<%= yeoman.app %>/less/less-compiled.css"
           ]
         }
       },
       dev: {
         files: {
-          '<%= yeoman.app %>/less/regular-css-compiled.css': [
-            '<%= yeoman.app %>/css/*.css',
-            '!<%= yeoman.app %>/css/tinymce.css'
+          "<%= yeoman.app %>/less/regular-css-compiled.css": [
+            "<%= yeoman.app %>/css/*.css"
           ]
         }
       }
     },
+
     concurrent: {
-      dist: [
-        'copy:dist',
-      ]
+      dist: ["copy:dist"],
+      watch: {
+        tasks: ["watch", "shell:watchJsx"],
+        options: {logConcurrentOutput: true}
+      }
     },
+
     autoprefixer: {
       options: {
         browsers: ["ie >= 8", "chrome >= 31", "ff >= 38"]
@@ -356,13 +232,14 @@ module.exports = function(grunt) {
         src: "<%= yeoman.app %>/less/less-compiled.css"
       }
     },
+
     less: {
       compile: {
         options: {
-          paths: ['<%= yeoman.app %>/less'],
+          paths: ["<%= yeoman.app %>/less"],
           sourceMap: true,
-          sourceMapFilename: '<%= yeoman.app %>/less/less-compiled.css.map',
-          sourceMapURL: '/less/less-compiled.css.map'
+          sourceMapFilename: "<%= yeoman.app %>/less/less-compiled.css.map",
+          sourceMapURL: "/less/less-compiled.css.map"
         },
         files: {
           "<%= yeoman.app %>/less/less-compiled.css": "<%= yeoman.app %>/less/index.less"
@@ -371,140 +248,59 @@ module.exports = function(grunt) {
     }
   });
 
-
-  /**
-   *  User facing Grunt Tasks
-   */
-
-  grunt.registerTask('compileJsxWatch', function(target) {
+  grunt.registerTask("compileGenerateLocalization", function (target) {
     return grunt.task.run([
-      // compiled_jsx directory need to exist for it to start watch, i.e. run compileJsx first
-      'shell:compileJsx',
-      'shell:compileJsxWatch'
+      "shell:compileLocalization",
+      "shell:generateLocalization",
+      "deploybuild:localization"
     ]);
   });
 
-  grunt.registerTask('compileGenerateLocalization', function(target) {
+  grunt.registerTask("updateLocalization", function (target) {
     return grunt.task.run([
-      'shell:compileLocalization',
-      'shell:generateLocalization',
-      'deploybuild:localization'
+      "shell:generateLocalization",
+      "deploybuild:localization"
     ]);
   });
 
-  grunt.registerTask('updateLocalization', function(target) {
-    return grunt.task.run([
-      'shell:generateLocalization',
-      'deploybuild:localization'
-    ]);
-  });
-
-  /**
-   *  Get a production looking enviroment, i.e.
-   *  serve files compiled files from dist/
-   *
-   *  @note
-   *  Expects 'grunt build' to have been run.
-   *
-   */
-  grunt.registerTask('server:dist_run', function(target) {
-    grunt.task.run([
-      'configureProxies:proxyserver',
-      'connect:proxyserver',
-      'connect:dist:keepalive'
-    ]);
-  });
-
-  /**
-   *  Get a production looking enviroment, i.e.
-   *  serve files compiled files from dist/
-   *  build task is run as a dependency of this task
-   */
-  grunt.registerTask('server:dist', [
-    'build',
-    'server:dist_run'
-  ]);
-
-
-  /**
-   *  Serve and compile static files, while developing.
-   */
-  grunt.registerTask('server', function(target) {
-    grunt.task.run([
-      'clean:server',
-      'less:compile',
-      'autoprefixer:compile',
-      'cssmin:dev',
-      'compileJsxWatch',
-      'compileGenerateLocalization',
-      'configureProxies:proxyserver',
-      'connect:proxyserver',
-      'connect:livereload',
-      'watch'
-    ]);
-  });
-
-
-  /**
-   *  Commands to run tests, mainly for CI-server usage
-   *
-   *  All tests expects 'grunt build' to have been run
-   */
-  grunt.registerTask('test', ['karma:unitSingleRun']);
-
-  /**
-   *  Unit tests (with karma + jasmine)
-   */
-  grunt.registerTask('unittests', [
-    'connect:test',
-    'karma:unitWatch'
-  ]);
-
-  grunt.registerTask('unittests:singleRun', [
-    'connect:test',
-    'karma:unitSingleRun'
-  ]);
-
-  /**
-   *  Build files for production
-   */
-  grunt.registerTask('build', function(target) {
+  grunt.registerTask("build", function (target) {
     var tasks = [
-      'clean:dist',
-      'compileGenerateLocalization',
-      'concurrent:dist',
-      'shell:compileJsx'
+      "clean:dist",
+      "compileGenerateLocalization",
+      "concurrent:dist",
+      "shell:compileJsx"
     ];
 
-    if (target === 'sourcemap') {
-      tasks.push('requirejs:withSourceMap');
+    if (target === "sourcemap") {
+      tasks.push("requirejs:withSourceMap");
     } else {
-      tasks.push('requirejs:noSourceMap');
+      tasks.push("requirejs:noSourceMap");
     }
 
     tasks = tasks.concat([
-      'less:compile',
-      'autoprefixer:compile',
-      'cssmin:dist',
-      'deploybuild:dist',
-      'shell:updateLastBuilt'
+      "compileStyles",
+      "cssmin:dist",
+      "deploybuild:dist",
+      "shell:updateLastBuilt"
     ]);
 
     return grunt.task.run(tasks);
   });
 
-  grunt.registerTask('package', [
-    'build',
-    'shell:tar'
-  ]);
+  grunt.registerTask("server", function (target) {
+    grunt.task.run([
+      "clean:server",
+      "compileStyles",
+      "cssmin:dev",
+      "compileGenerateLocalization",
+      "shell:compileJsx",
+      "concurrent:watch"
+    ]);
+  });
 
-  grunt.registerTask('validateJs', [
-    'gjslint'
-  ]);
-
-  grunt.registerTask('default', [
-    'gjslint',
-    'build',
-    'test'
-  ]);
+  grunt.registerTask("compileStyles", ["less:compile", "autoprefixer:compile"]);
+  grunt.registerTask("server:dist", ["build"]);
+  grunt.registerTask("test", ["karma:unitSingleRun"]);
+  grunt.registerTask("validateJs", ["gjslint"]);
+  grunt.registerTask("default", ["gjslint", "build", "test"]);
 };
