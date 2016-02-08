9f17d4dc406a40088a2fac434610ca2dd00d734e
Jonathan Jouty <jonathan@scrive.com>
2016-02-08 11:34:49 +0000

    Remove 'text_to_image' from backend and related templates, texts, font files
    Was used by Signature Typer for IE 8
    https://trello.com/c/SC9WbG1w/415-drop-support-for-ie8

--- src/ServerUtils/ServerUtils.hs
+++ src/ServerUtils/ServerUtils.hs
@@ -1,22 +1,18 @@
 module ServerUtils.ServerUtils (
      handleParseCSV
    , handleSerializeImage
-   , handleTextToImage
    , brandedImage
   ) where
 
---import Happstack.Server hiding (dir, simpleHTTP)
 import Control.Monad.Trans
 import Data.Char (toLower)
 import Data.Functor
 import Happstack.Server hiding (dir, simpleHTTP)
 import Log as Log
-import Numeric
 import System.Directory (getCurrentDirectory)
 import System.Exit
 import System.FilePath ((</>), takeBaseName)
 import System.Path (secureAbsNormPath)
-import System.Process hiding (readProcessWithExitCode)
 import System.Process.ByteString.Lazy (readProcessWithExitCode)
 import Text.JSON
 import Text.JSON.Gen hiding (object)
@@ -34,7 +30,6 @@ import KontraPrelude
 import ServerUtils.BrandedImagesCache
 import Util.CSVUtil
 import Util.MonadUtils
-import Utils.Directory
 import qualified MemCache as MemCache
 
 -- Read a csv file from POST, and returns a JSON with content
@@ -72,47 +67,6 @@ handleSerializeImage = do
       else badRequest' "Not image"
  where badRequest' s = return $ (setHeader "Content-Type" "text/plain; charset=UTF-8" $ Web.toResponse $ (s :: String)) {rsCode = 400}
        goodRequest js = return $ (setHeader "Content-Type" "text/plain; charset=UTF-8" $ Web.toResponse $ encode js) {rsCode = 200}
-
--- Based on text, returns an image of this text, drawn using `handwriting` font.
--- Expected text, dimentions, font and format (base64 or plain) are passed as parameters.
-handleTextToImage :: Kontrakcja m =>  m Response
-handleTextToImage = do
-    text <- fmap (take 50) $ guardJustM $ getField "text"
-    (width::Int)  <- fst <$> (guardJustM $ join <$> fmap (listToMaybe . readDec) <$> getField "width")
-    (height::Int) <- fst <$> (guardJustM $ join <$> fmap (listToMaybe . readDec) <$> getField "height")
-    (font, fontSize) <- do
-              mfont <- getField "font"
-              return $ case mfont of
-                        Just "JenniferLynne"    -> ("frontend/app/fonts/JenniferLynne.ttf",22)
-                        Just "TalkingToTheMoon" -> ("frontend/app/fonts/TalkingToTheMoon.ttf",20)
-                        _                       -> ("frontend/app/fonts/TheOnlyException.ttf",16)
-    base64 <- isFieldSet "base64"
-    transparent <- isFieldSet "transparent"
-    left <- isFieldSet "left"
-
-    mfcontent <- withSystemTempDirectory' "text_to_image" $ \tmppath -> do
-      let fpath = tmppath ++ "/text_to_image.png"
-      (_,_,_, drawer) <- liftIO $ createProcess $ proc "convert"
-            [ "-size", (show width ++ "x" ++ show height)
-            , "-background", if (transparent) then "transparent" else "white"
-            , "-pointsize", show (pointSize width height (length text) fontSize)
-            , "-gravity", if (left) then "West" else "Center"
-            , "-font", font
-            , "label:" ++ (if null text then " " else text)
-            , "PNG32:" ++ fpath ]
-      drawerexitcode <- liftIO $ waitForProcess drawer
-      case drawerexitcode of
-          ExitFailure msg -> do
-            logInfo "text_to_image failed" $ object [
-                "error" .= msg
-              ]
-            return Nothing
-          ExitSuccess -> (liftIO $ BSL.readFile fpath) >>= (return . Just)
-    case mfcontent of
-         Just fcontent -> if base64
-                             then ok $ toResponseBS (BSUTF8.fromString "text/plain") $ BSL.fromChunks [BSUTF8.fromString "data:image/png;base64,", B64.encode $ BSL.toStrict fcontent]
-                             else ok $ setHeaderBS "Cache-Control" "max-age=600" $ toResponseBS (BSUTF8.fromString "image/png") $ fcontent
-         Nothing -> internalError
 
 -- Take any image and brand it by replacing black, non-transparent colors with the specified colour.
 -- Expecting color and filename to be passed as parameters.
@@ -153,14 +107,3 @@ brandImage file color = do
           ]
         internalError
       ExitSuccess -> return out
-
-
-
-
--- Point scale - some heuristic for pointsize, based on size of image, type of font and lenght of text.
--- Result should be point size that will result in best fit. It is also expected that this value does not change much on text lenght change.
--- Magic font dependend value sugests what point size will fill in 30x30 squere.
-
-pointSize :: Int -> Int  -> Int -> Int -> Int
-pointSize width height textl fontSize =
-    min ((height - 20)  * fontSize `div` 30) ((width `div` (textl + 1)) * fontSize `div` 11)
