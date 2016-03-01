a0a47680a29e1ccbf36330bbc31f734770fbf9b3
Ola Holmström <ola@scrive.com>
2016-03-01 17:30:32 +0100

    Header person name moved little to the left in signview
    https://trello.com/c/cy0Y11Pd/1519-header-person-name-moved-little-to-the-left-in-signview

--- frontend/app/scripts/signview/header.jsx
+++ frontend/app/scripts/signview/header.jsx
@@ -33,7 +33,7 @@ define(['React', 'Backbone', 'common/backbone_mixin'], function(React, Backbone,
                   </div>
                 </div>
               </div>
-              <div className="col-xs-6 right content">
+              <div className="col-xs-6 content">
                 <div className="vertical">
                   <div className="middle">
                     {/* if */ hasLink &&
