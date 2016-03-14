66956a0d95f70d147c51ec0e1956b4356bb40e45
Ola Holmström <ola@scrive.com>
2016-03-14 17:14:47 +0100

    update jquery

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -209,7 +209,8 @@ module.exports = function (grunt) {
           beautify: true
         },
         src: [
-          "<%= yeoman.app %>/bower_components/jquery/jquery.js",
+          "<%= yeoman.app %>/bower_components/jquery/dist/jquery.js",
+          "<%= yeoman.app %>/bower_components/jquery-migrate/jquery-migrate.js",
           "<%= yeoman.app %>/bower_components/underscore/underscore.js",
           "<%= yeoman.app %>/bower_components/backbone/backbone.js",
           "<%= yeoman.app %>/bower_components/react/react-with-addons.js",
