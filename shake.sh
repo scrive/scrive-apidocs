ac642d3ca7f46294ae2d6d0883a5bb2443230839
Jonathan Jouty <jonathan@scrive.com>
2016-05-26 14:27:08 +0100

    Add locale settings to shake.sh to avoid TeamCity troubles

--- shake.sh
+++ shake.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
 mkdir -p _shake
 ghc --make Shake/Shake.hs -Werror -Wall -rtsopts -with-rtsopts=-I0 -outputdir=_shake -o _shake/build && _shake/build --color "$@"
