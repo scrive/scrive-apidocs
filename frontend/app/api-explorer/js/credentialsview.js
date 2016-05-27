919574f9eca693070647f4f7e568621c2d1887f9
Kent Karlsson <kent@scrive.com>
2016-05-27 16:25:09 +0200

    Changes to fetch email, and some error handlig
    API-explorer now tries to fetch profile data for the authoriosation,
    and pick out the email in order to be able to show that. Also shows the
    access level, and there is also some error handling (showing an alert
    on HTTP error).

--- frontend/app/api-explorer/js/credentialsview.js
+++ frontend/app/api-explorer/js/credentialsview.js
@@ -12,26 +12,30 @@ window.CredentialsView = Backbone.View.extend({
     var credentiasBox = $(this.el).empty();
     var credentiasRow = $("<div class='row'/>");
     var credentiasCol0 = $("<div class='col-xs-2'/>");
-    var credentiasCol1 = $("<div class='col-xs-3'/>");
+    var credentiasCol1 = $("<div class='col-xs-2'/>");
     var credentiasCol2 = $("<div class='col-xs-2 '/>");
-    var credentiasCol3 = $("<div class='col-xs-3 '/>");
+    var credentiasCol3 = $("<div class='col-xs-2 '/>");
     var credentiasCol4 = $("<div class='col-xs-2 '/>");
+    var credentiasCol5 = $("<div class='col-xs-2 '/>");
     credentiasCol0
-      .append("<p><b>Credentials obtained for</b><br/><span>" + oauth.email() + "</span></p>");
+      .append("<p><b>Credentials obtained for</b><br/><span>"  + oauth.email()                + "</span></p>");
     credentiasCol1
-      .append("<p><b>Client credentials identifier</b><br/><span>" + oauth.consumer_key() + "</span></p>");
+      .append("<p><b>Client credentials id</b><br/><span>"     + oauth.consumer_key()         + "</span></p>");
     credentiasCol2
       .append("<p><b>Client credentials secret</b><br/><span>" + oauth.client_shared_secret() + "</span></p>");
     credentiasCol3
-      .append("<p><b>Token credentials identifier</b><br/><span>" + oauth.final_token() + "</span></p>");
+      .append("<p><b>Token credentials id</b><br/><span>"      + oauth.final_token()          + "</span></p>");
     credentiasCol4
-      .append("<p><b>Token credentials secret</b><br/><span>" + oauth.final_token_secret() + "</span></p>");
+      .append("<p><b>Token credentials secret</b><br/><span>"  + oauth.final_token_secret()   + "</span></p>");
+    credentiasCol5
+      .append("<p><b>Access level</b><br/><span>"              + oauth.priviliges()           + "</span></p>");
     credentiasBox.append(credentiasRow
       .append(credentiasCol0)
       .append(credentiasCol1)
       .append(credentiasCol2)
       .append(credentiasCol3)
       .append(credentiasCol4)
+      .append(credentiasCol5)
     );
     return this;
   }
