67fca9fde5167c73c3868fe410eee88bf6d22ac6
Ola Holmström <ola.holmstrom@gmail.com>
2016-06-09 10:05:27 +0200

    Small fixes to make kontrakcja build.
    Updating `entropy` library that includes bugfix for
    https://github.com/TomMD/entropy/issues/27
    
    Also make Shake not abort on warnings.

--- shake.sh
+++ shake.sh
@@ -2,4 +2,5 @@
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 mkdir -p _shake
-ghc --make Shake/Shake.hs -Werror -Wall -rtsopts -with-rtsopts=-I0 -outputdir=_shake -o _shake/build && _shake/build --color "$@"
+export SHAKE_BUILD_CABAL_CONFIGURE_OPTS="--ghc-options=\"-fno-warn-deprecated-flags\""
+ghc --make Shake/Shake.hs -Wall -rtsopts -with-rtsopts=-I0 -outputdir=_shake -o _shake/build && _shake/build --color "$@"
