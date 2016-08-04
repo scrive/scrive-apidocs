3b302158fba39205bff461c64113f42221a473ea
Bartek Ćwikłowski <bartek@scrive.com>
2016-08-04 16:48:51 +0200

    un-rot Interactive module

--- backend/lib/Interactive.hs
+++ backend/lib/Interactive.hs
@@ -4,46 +4,58 @@ module Interactive where
 
 import Control.Concurrent
 import Log
-import System.Environment
+import Log.Configuration
 import System.IO
 import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy.UTF8 as BSL8
 
+import AppDBTables
 import AppConf
 import AppControl
 import Configuration
 import Crypto.RNG
 import DB
+import User.Lang
 import DB.PostgreSQL
 import KontraPrelude
 import Templates
 import qualified Amazon as AWS
 import qualified Doc.RenderedPages as RenderedPages
+import Text.StringTemplates.Templates (TemplatesT)
 import qualified MemCache
 
-run :: AWS.AmazonMonadT (CryptoRNGT (DBT IO)) a -> IO a
-run m = Log.withLogger $ do
+run :: TemplatesT (AWS.AmazonMonadT (CryptoRNGT (DBT (LogT IO)))) a -> IO a
+run m = do
   hSetEncoding stdout utf8
   hSetEncoding stderr utf8
 
-  appConf <- do
-    appname <- getProgName
-    args <- getArgs
-    readConfig logInfo_ appname args "kontrakcja.conf"
+  appConf <- readConfig putStrLn "kontrakcja.conf"
+
+  rng <- newCryptoRNGState
+  lr@LogRunner{..} <- mkLogRunner "kontrakcja" (logConfig appConf) rng
 
   let connSettings = pgConnSettings $ dbConfig appConf
+  globalTemplates <- readGlobalTemplates
   appGlobals <- do
     templates <- newMVar =<< liftM2 (,) getTemplatesModTime readGlobalTemplates
     filecache <- MemCache.new BS.length 50000000
+    lesscache <- MemCache.new BSL8.length 50000000
+    brandedimagescache <- MemCache.new BSL8.length 50000000
     docs <- MemCache.new RenderedPages.pagesCount 1000
-    rng <- newCryptoRNGState
-    connpool <- createPoolSource connSettings
+    connpool <- createPoolSource $ connSettings kontraComposites
     return AppGlobals {
         templates = templates
       , filecache = filecache
       , docscache = docs
       , cryptorng = rng
       , connsource = connpool
+      , mrediscache = Nothing
+      , lesscache = lesscache
+      , brandedimagescache = brandedimagescache
+      , logrunner = lr
       }
 
-  withPostgreSQL (defaultSource connSettings) . runCryptoRNGT (cryptorng appGlobals) .
-   AWS.runAmazonMonadT (AWS.AmazonConfig (amazonConfig appConf) (filecache appGlobals)) $ m
+  ConnectionSource pool <- ($ maxConnectionTracker) `liftM` (createPoolSource $ connSettings kontraComposites)
+
+  withLoggerWait $ withPostgreSQL pool . runCryptoRNGT (cryptorng appGlobals) .
+   AWS.runAmazonMonadT (AWS.AmazonConfig (amazonConfig appConf) (filecache appGlobals) Nothing) $ runTemplatesT (LANG_EN, globalTemplates) m
