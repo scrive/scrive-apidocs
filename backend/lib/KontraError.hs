fbd1688cbd10488c598732c3daa9952aef8aa389
Jens Brage <jensbrage@gmail.com>
2016-10-04 14:01:51 +0200

    Add support for no-configuration errors and warnings
    This patch is part of a series of patches to permit the configurations
    for LiveDocx, CGI Group, Recurly, Mixpanel, HubSpot, Google Analytics
    and Salesforce to be left out for test and development.
    
    This patch adds two commands to promote consistent errors and warnings,
    KontraError.noConfigurationError and
    KontraError.noConfigurationWarning.
    
    The module KontraError was chosen out of convenience for lack of a
    better reason. Please feel free to move the commands somewhere else.

--- backend/lib/KontraError.hs
+++ backend/lib/KontraError.hs
@@ -2,8 +2,10 @@ module KontraError where
 
 import Control.Monad.Base
 import Control.Monad.Catch
+import Data.Text
 import Data.Typeable
 import GHC.Stack
+import Log
 import qualified Control.Exception.Lifted as E
 
 import KontraPrelude
@@ -26,3 +28,18 @@ respond404 = throwM Respond404
 {-# NOINLINE preventTailCallOptimization #-}
 preventTailCallOptimization :: (Monad m) => m ()
 preventTailCallOptimization = return ()
+
+{- | Auxiliary function for configuration errors in code called from
+`RoutingTable.staticRoutes`, e.g.,
+"ERROR, no configuration for Salesforce". -}
+noConfigurationError :: (MonadBase IO m, MonadLog m) => String -> m a
+noConfigurationError field = do
+  logAttention_ . pack $ "ERROR, no configuration for " ++ field
+  internalError
+
+{- | Auxiliary function for configuration warnings in code called from
+`Cron.main`, e.g.,
+"WARNING, no configuration for Salesforce". -}
+noConfigurationWarning :: (MonadLog m) => String -> m ()
+noConfigurationWarning field =
+  logAttention_ .pack $ "WARNING, no configuration for " ++ field
