010b43e138116375470eb94834ffea55b1c19281
Mikhail Glushenkov <mikhail@scrive.com>
2016-10-20 12:20:07 +0100

    Add -Werror to shake.sh.

--- shake.sh
+++ shake.sh
@@ -2,5 +2,5 @@
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 mkdir -p _shake
-ghc --make Shake/Shake.hs -Wall -rtsopts -with-rtsopts=-I0 \
+ghc --make Shake/Shake.hs -Wall -Werror -rtsopts -with-rtsopts=-I0 \
     -outputdir=_shake -o _shake/build && _shake/build --color "$@"
