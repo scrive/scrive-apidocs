a322d2bd8d5e612b2d9c5fc74ace000af8945222
Mikhail Glushenkov <mikhail@scrive.com>
2016-10-28 00:24:56 +0100

    Use a patched version of Andrzej's sort_imports.

--- scripts/sort_imports/sort_imports.hs
+++ scripts/sort_imports/sort_imports.hs
@@ -1,18 +1,22 @@
-{-# LANGUAGE BangPatterns, OverloadedStrings #-}
+{-# LANGUAGE BangPatterns, LambdaCase, OverloadedStrings, RecordWildCards #-}
 {-# OPTIONS_GHC -Wall #-}
+module Main (main) where
+
 import Control.Applicative
-import Control.Arrow
 import Control.Exception
 import Control.Monad
 import Data.Char
-import Data.Function
+import Data.Functor
 import Data.List
+import Data.Maybe
+import Data.Monoid
 import Data.Set (Set)
 import Data.Text (Text)
 import System.Directory
 import System.Environment
 import System.Exit
-import System.IO (stderr, hPutStrLn)
+import System.IO (hPutStrLn, stderr)
+import qualified Data.Attoparsec.Text as P
 import qualified Data.Set as S
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
@@ -25,50 +29,161 @@ infixr 3 <&&>
 (<||>) = liftA2 (||)
 infixr 2 <||>
 
+parenthesize :: [T.Text] -> T.Text
+parenthesize ss = "(" <> T.intercalate ", " ss <> ")"
+
 ----------------------------------------
 
--- | Transform the haskell source file by lexicographically sorting
--- all its imports and splitting them into two groups, foreign and
--- local ones.
-convert :: Set Text -> Text -> Text
-convert modules source = T.unlines . concat $ [
-    header
-  , fimports
-  , fqimports
-  , separator_if has_foreign_imports
-  , limports
-  , lqimports
-  , separator_if has_local_imports
-  , dropWhile (is import_ <||> T.null) rest
+data Symbols
+  = NoSymbols
+  | Explicit ![T.Text]
+  | Hiding ![T.Text]
+  deriving (Eq, Ord, Show)
+
+parseSymbols :: P.Parser Symbols
+parseSymbols = P.choice [
+    Hiding   <$> (P.string "hiding" *> P.skipSpace *> parseSymbolList)
+  , Explicit <$> parseSymbolList
+  , return NoSymbols
   ]
   where
-    (header, rest) = break (is import_) . T.lines $ source
-    (qimports, imports) = (icase_sort *** icase_sort)
-      . partition (is qualified_import_)
-      . filter (is import_) $ rest
+    parseSymbolList = do
+      _ <- P.char '('
+      P.skipSpace
+      symbols <- (`P.sepBy` P.char ',') $ do
+        P.skipSpace
+        symbol <- P.choice [
+            do -- operator
+              _ <- P.char '('
+              op <- P.takeWhile1 (/= ')')
+              _ <- P.char ')'
+              return $ "(" <> op <> ")"
+          , P.takeWhile1 ((not . isSpace) <&&> (/= '(') <&&> (/= ')') <&&> (/= ','))
+          ]
+        P.skipSpace
+        ctors <- P.option "" (parenthesize <$> parseSymbolList)
+        P.skipSpace
+        return $ symbol <> ctors
+      P.skipSpace
+      _ <- P.char ')'
+      P.skipSpace
+      return $ sort symbols
 
-    (limports, fimports) = partition (is_local import_) imports
-    (lqimports, fqimports) = partition (is_local qualified_import_) qimports
+data Import = Import {
+    imQualified      :: !Bool
+  , imModule         :: !T.Text
+  , imCaselessModule :: !T.Text
+  , imAlias          :: !(Maybe T.Text)
+  , imSymbols        :: !Symbols
+  } deriving (Eq, Show)
 
-    has_foreign_imports = not $ null fimports && null fqimports
-    has_local_imports = not $ null limports && null lqimports
+compareImport :: Bool -> Import -> Import -> Ordering
+compareImport ignore_qualified a b = mconcat [
+    if ignore_qualified
+    then mempty
+    else imQualified a `compare` imQualified b
+  , imCaselessModule a `compare` imCaselessModule b
+  , imAlias a          `compare` imAlias b
+  , imSymbols a        `compare` imSymbols b
+  ]
+
+parseImport :: P.Parser Import
+parseImport = do
+  _ <- P.string "import"
+  P.skipSpace
+  is_qualified <- P.option False (P.string "qualified" *> P.skipSpace $> True)
+  module_ <- P.takeWhile1 $ (not . isSpace) <&&> (/= '(')
+  P.skipSpace
+  alias <- P.option Nothing (Just <$> parseAlias)
+  P.skipSpace
+  symbols <- parseSymbols
+  return Import {
+      imQualified      = is_qualified
+    , imModule         = module_
+    , imCaselessModule = T.toCaseFold module_
+    , imAlias          = alias
+    , imSymbols        = symbols
+    }
+  where
+    parseAlias :: P.Parser T.Text
+    parseAlias = do
+      _ <- P.string "as"
+      P.skipSpace
+      alias <- P.takeWhile1 $ (not . isSpace) <&&> (/= '(')
+      P.skipSpace
+      return alias
+
+showImport :: Style -> Import -> T.Text
+showImport Style{..} Import{..} = T.concat [
+    import_module
+  , case aliasAlignment of
+      Just n | isJust imAlias -> T.replicate (max 0 $ n - 1 - T.length import_module) " "
+      _ -> ""
+  , maybe "" (" as " <>) imAlias
+  , case imSymbols of
+      NoSymbols   -> ""
+      Explicit ss -> " " <> parenthesize ss
+      Hiding ss   -> " hiding " <> parenthesize ss
+  ]
+  where
+    import_module = T.concat [
+        "import"
+      , if imQualified
+        then qualified_
+        else if alignUnqualified
+             then T.replicate (T.length qualified_) " "
+             else ""
+      , " "
+      , imModule
+      ]
+
+    qualified_ = " qualified"
+
+----------------------------------------
+
+data ImportGrouping = NoGrouping | ExternalInternal | InternalExternal
+  deriving Show
+
+data Style = Style {
+    alignUnqualified :: !Bool
+  , aliasAlignment   :: !(Maybe Int)
+  , importGrouping   :: !ImportGrouping
+  } deriving Show
+
+----------------------------------------
+
+-- | Transform Haskell source by lexicographically sorting all its imports
+-- according to the given style.
+convert :: Style -> Set Text -> Text -> Text
+convert style@Style{..} modules source = T.unlines . concat $ [
+    reverse . dropWhile T.null $ reverse header
+  , separator_if True
+  , map (showImport style) first_import_group
+  , separator_if $ not $ null first_import_group
+  , map (showImport style) second_import_group
+  , separator_if $ not $ null second_import_group
+  , rest
+  ]
+  where
+    (header, body) = break is_import . T.lines $ source
+    (import_section, rest) = break (not . (T.null <||> is_import <||> is_indented)) body
+
+    imports = case P.parseOnly (many parseImport) (T.unlines import_section) of
+      Right imps -> sortBy (compareImport alignUnqualified) imps
+      Left  msg  -> error msg
+
+    (first_import_group, second_import_group) = case importGrouping of
+      NoGrouping       -> (imports, [])
+      ExternalInternal -> partition (not . (`S.member` modules) . imModule) imports
+      InternalExternal -> partition (      (`S.member` modules) . imModule) imports
+
+    is_import   = T.isPrefixOf "import "
+    is_indented = T.isPrefixOf " "
 
     separator_if p = if p then [T.empty] else []
 
-    import_ = "import "
-    qualified_import_ = "import qualified "
-    is import_type = (import_type `T.isPrefixOf`)
-
-    icase_sort = map snd
-      . sortBy (compare `on` fst)
-      . map (T.toLower &&& id)
-
-    is_local import_type = flip S.member modules
-      . T.takeWhile (isAlphaNum <||> (== '.'))
-      . T.drop (T.length import_type)
-
--- | Recursively traverse the directory and pass all
--- haskell source files into accumulating function.
+-- | Recursively traverse the directory and pass all haskell source files into
+-- accumulating function.
 foldThroughHsFiles :: FilePath -> (acc -> FilePath -> IO acc) -> acc -> IO acc
 foldThroughHsFiles basepath f iacc = do
   paths <- filter ((/= ".") <&&> (/= "..")) <$> getDirectoryContents basepath
@@ -77,7 +192,7 @@ foldThroughHsFiles basepath f iacc = do
     run acc path = do
       is_dir  <- doesDirectoryExist fullpath
       is_file <- doesFileExist fullpath
-      case (is_file && ".hs" `isSuffixOf` path, is_dir) of
+      case (is_file && (".hs" `isSuffixOf` path || ".lhs" `isSuffixOf` path), is_dir) of
         (True, False) -> f acc fullpath
         (False, True) -> foldThroughHsFiles fullpath f acc
         _             -> return acc
@@ -89,15 +204,17 @@ inspectDirectories :: [FilePath] -> IO (Set Text, [FilePath])
 inspectDirectories dirs = foldM (\acc dir -> do
   putStrLn $ "Inspecting " ++ dir ++ "..."
   foldThroughHsFiles dir (\(!modules, !files) file -> do
-    let modul = map slash_to_dot
-              . drop (length dir + 1)  -- remove base directory (+ slash)
-              . take (length file - 3) -- remove .hs at the end
-              $ file
-    putStrLn $ "Found " ++ file ++ " (" ++ modul ++ ")."
-    return (S.insert (T.pack modul) modules, file : files)
+    let module_ = map slash_to_dot
+                . drop (length dir + 1)  -- remove base directory (+ slash)
+                . drop_extension
+                $ file
+    putStrLn $ "Found " ++ file ++ " (" ++ module_ ++ ")."
+    return (S.insert (T.pack module_) modules, file : files)
     ) acc
   ) (S.empty, []) $ map remove_last_slash dirs
   where
+    drop_extension = reverse . drop 1 . dropWhile (/= '.') . reverse
+
     slash_to_dot '/' = '.'
     slash_to_dot c = c
 
@@ -106,15 +223,13 @@ inspectDirectories dirs = foldM (\acc dir -> do
     remove_last_slash (c:cs) = c : remove_last_slash cs
 
 -- | Sort import lists in files at given locations.
-sortImports :: String -> [FilePath] -> IO ()
-sortImports suffix dirs = do
+sortImports :: Style -> String -> [FilePath] -> IO ()
+sortImports style suffix dirs = do
   (modules, files) <- inspectDirectories dirs
-  -- transform files at collected locations
   forM_ files $ \file -> do
     putStr $ "Sorting imports in " ++ file ++ "..."
     T.readFile file
-      >>= return . convert modules
-      >>= rewriteFile (file ++ suffix)
+      >>= rewriteFile (file ++ suffix) . convert style modules
     putStrLn " done."
 
 -- | Write a file, but only if it would have new content, thus
@@ -128,15 +243,14 @@ rewriteFile fileName newContent = do
       T.writeFile fileName newContent
   else T.writeFile fileName newContent
 
--- | Check whether import lists in
--- files at given locations are sorted.
-checkConsistency :: [FilePath] -> IO ()
-checkConsistency dirs = do
+-- | Check whether import lists in files at given locations are sorted.
+checkConsistency :: Style -> [FilePath] -> IO ()
+checkConsistency style dirs = do
   (modules, files) <- inspectDirectories dirs
   forM_ files $ \file -> do
     putStr $ "Checking whether imports in " ++ file ++ " are sorted..."
     source <- T.readFile file
-    if source == convert modules source
+    if source == convert style modules source
       then putStrLn " yes."
       else do
         putStrLn " no."
@@ -151,21 +265,49 @@ main = do
   if null dirs
     then do
       prog <- getProgName
-      putStrLn $ "Usage: " ++ prog ++ " [--check] [--suffix=SUFFIX] <directories>"
-    else case get_options args of
-      (False, suffix) -> sortImports suffix dirs
-      (True, _) -> onException (checkConsistency dirs) $ do
-        putStrLn $ "Run scripts/sort_imports.sh without --check option to fix the problem."
+      putStrLn $ "Usage: " ++ prog ++ " [--check] [--suffix=SUFFIX] [--align-unqualified] [--alias-alignment=N] [--import-grouping=none|external-external|internal-external] <directories>"
+    else do
+      let (check, style, suffix) = get_options args
+      putStrLn $ "Using " ++ show style
+      if not check
+        then sortImports style suffix dirs
+        else onException (checkConsistency style dirs) $ do
+          putStrLn $ "Run SortImports.sh without --check to fix the problem."
   where
     is_config_option = ("--" `isPrefixOf`)
-    opt_suffix = "--suffix="
-    opt_check  = "--check"
 
-    get_options args = (check, suffix)
+    opt_check  = "--check"
+    opt_suffix = "--suffix="
+
+    opt_align_unqualified = "--align-unqualified"
+    opt_alias_alignment   = "--alias-alignment="
+    opt_import_grouping   = "--import-grouping="
+
+    -- TODO: Use something more sensible like cmdargs.
+    get_options args = (check, style, suffix)
       where
         check = case find (== opt_check) args of
           Just _  -> True
           Nothing -> False
+
         suffix = case find (opt_suffix `isPrefixOf`) args of
           Just opt -> drop (length opt_suffix) opt
           Nothing  -> ""
+
+        style = Style align_unqualified alias_alignment import_grouping
+          where
+            align_unqualified = case find (== opt_align_unqualified) args of
+              Just _  -> True
+              Nothing -> False
+
+            alias_alignment = case find (opt_alias_alignment `isPrefixOf`) args of
+              Just opt -> Just . read $ drop (length opt_alias_alignment) opt
+              Nothing  -> Nothing
+
+            import_grouping = case find (opt_import_grouping `isPrefixOf`) args of
+              Just opt -> case drop (length opt_import_grouping) opt of
+                "none"              -> NoGrouping
+                "external-internal" -> ExternalInternal
+                "internal-external" -> InternalExternal
+                _                   -> error "invalid import-grouping"
+              Nothing -> ExternalInternal
