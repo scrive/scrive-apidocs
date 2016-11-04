130df7b1e1230c35d7e2bbc9997927c694e5542b
Mikhail Glushenkov <mikhail@scrive.com>
2016-11-04 02:28:53 +0000

    Move VersionID-generating code to its own module.

---
+++ backend/lib/Version.hs
@@ -0,0 +1,20 @@
+module Version (genVersionID) where
+
+import Control.Exception.Lifted as E
+import System.Environment
+
+import KontraPrelude
+
+genVersionID :: IO String
+genVersionID = do
+  let catchIO :: IO a -> (E.IOException -> IO a) -> IO a
+      catchIO = E.catch
+  buildNumber <- catchIO (getEnv "BUILD_NUMBER")
+                 (const (return "build_number"))
+  buildVcsNumber <- catchIO (getEnv "BUILD_VCS_NUMBER")
+                    (const (return "build_vcs_number"))
+  buildDate <- catchIO (getEnv "BUILD_DATE")
+               (const (return "build_date"))
+
+  return $ buildDate ++ "."
+    ++ buildNumber ++ "." ++ buildVcsNumber
--- backend/lib/VersionTH.hs
+++ backend/lib/VersionTH.hs
@@ -1,24 +1,11 @@
 module VersionTH (versionID) where
 
-import Control.Exception.Lifted as E
 import Language.Haskell.TH.Lib
 import Language.Haskell.TH.Syntax
-import System.Environment
 
 import KontraPrelude
+import Version
 
 {-# NOINLINE versionID #-}
 versionID :: String
-versionID = $( litE =<< (fmap stringL $ runIO $ do
-                 let catchIO :: IO a -> (E.IOException -> IO a) -> IO a
-                     catchIO = E.catch
-                 buildNumber <- catchIO (getEnv "BUILD_NUMBER")
-                   (const (return "build_number"))
-                 buildVcsNumber <- catchIO (getEnv "BUILD_VCS_NUMBER")
-                   (const (return "build_vcs_number"))
-                 buildDate <- catchIO (getEnv "BUILD_DATE")
-                   (const (return "build_date"))
-
-                 return $ buildDate ++ "."
-                   ++ buildNumber ++ "." ++ buildVcsNumber)
-   )
+versionID = $( litE =<< (fmap stringL $ runIO $ genVersionID) )
