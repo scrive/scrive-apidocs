e4e8225dd2496e654301bc0cace72996e4a47b5a
Martin Taranza <martin@promne.cz>
2016-12-20 13:05:53 +0100

    refactor reloading and redirecting with FlashMessages

--- frontend/app/scripts/account/branding/companybrandingview.jsx
+++ frontend/app/scripts/account/branding/companybrandingview.jsx
@@ -13,7 +13,9 @@ var ServicePreview = require("../../themes/previews/service");
 var $ = require("jquery");
 var Button = require("../../../js/buttons.js").Button;
 var _ = require("underscore");
-var FlashMessage = require("../../../js/flashmessages.js").FlashMessage;
+var FlashMessages = require("../../../js/flashmessages.js");
+var FlashMessage = FlashMessages.FlashMessage;
+var FlashMessageAfterReload = FlashMessages.FlashMessageAfterReload;
 
 module.exports = React.createClass({
     mixins: [BackboneMixin.BackboneMixin],
@@ -39,15 +41,19 @@ module.exports = React.createClass({
       var saveCompanyBranding = function() {
        model.companybranding().save(function() {
          if (!shouldReloadOnSave) {
-           new FlashMessage({type : "success", content: localization.branding.saved});
+           FlashMessage({type : "success", content: localization.branding.saved});
            if (callback) {
              callback();
            }
          } else {
            if (urlInsteadOfCallback) {
-            new FlashMessage({type : "success", content: localization.branding.saved, withRedirect: true, redirect: urlInsteadOfCallback, hashChangeOnly : true});
+             FlashMessageAfterReload({type : "success", content: localization.branding.saved});
+             window.location = urlInsteadOfCallback;
+             $(window).unbind("hashchange");
+             window.location.reload(true);
            } else {
-            new FlashMessage({type : "success", content: localization.branding.saved, withReload: true});
+             FlashMessageAfterReload({type : "success", content: localization.branding.saved});
+             window.location.reload(true);
            }
          }
        });
