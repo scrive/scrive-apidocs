84f7ff2b22813c588bb64118cece6830a7514248
Mariusz Rak <mariusz@scrive.com>
2017-05-11 18:16:58 +0200

    Removed dead code

--- frontend/app/scripts/signview/signatories/docviewsignatory.jsx
+++ frontend/app/scripts/signview/signatories/docviewsignatory.jsx
@@ -39,69 +39,6 @@ module.exports = React.createClass({
       }
     },
 
-    getDeliveryMethod: function () {
-      var signatory = this.props.signatory;
-      if (signatory.emailDelivery()) {
-        return localization.docview.signatory.invitationEmail;
-      } else if (signatory.padDelivery()) {
-        return localization.docview.signatory.invitationPad;
-      } else if (signatory.mobileDelivery()) {
-        return localization.docview.signatory.invitationSMS;
-      } else if (signatory.emailMobileDelivery()) {
-        return localization.docview.signatory.invitationEmailSMS;
-      } else if (signatory.apiDelivery()) {
-        return localization.docview.signatory.invitationAPI;
-      } else if (signatory.noneDelivery()) {
-        return localization.docview.signatory.invitationNone;
-      }
-    },
-
-    getRole: function () {
-      var signatory = this.props.signatory;
-      if (signatory.signs()) {
-        return localization.docview.signatory.roleSignatory;
-      } else {
-        return localization.docview.signatory.roleViewer;
-      }
-    },
-
-    getAuthenticationToViewMethodText: function () {
-      var signatory = this.props.signatory;
-      if (signatory.standardAuthenticationToView()) {
-        return localization.docview.signatory.authenticationToViewStandard;
-      } else if (signatory.seBankIDAuthenticationToView()) {
-        return localization.docview.signatory.authenticationToViewSEBankID;
-      } else if (signatory.noBankIDAuthenticationToView()) {
-        return localization.docview.signatory.authenticationToViewNOBankID;
-      } else if (signatory.dkNemIDAuthenticationToView()) {
-        return localization.docview.signatory.authenticationToViewDKNemID;
-      }
-    },
-
-    getAuthenticationToSignMethodText: function () {
-      var signatory = this.props.signatory;
-      if (signatory.standardAuthenticationToSign()) {
-        return localization.docview.signatory.authenticationToSignStandard;
-      } else if (signatory.smsPinAuthenticationToSign()) {
-        return localization.docview.signatory.authenticationToSignSMSPin;
-      } else if (signatory.seBankIDAuthenticationToSign()) {
-        return localization.docview.signatory.authenticationToSignSEBankID;
-      }
-    },
-
-    getConfirmationMethod: function () {
-      var signatory = this.props.signatory;
-      if (signatory.emailConfirmationDelivery()) {
-        return localization.docview.signatory.confirmationEmail;
-      } else if (signatory.mobileConfirmationDelivery()) {
-        return localization.docview.signatory.confirmationSMS;
-      } else if (signatory.emailMobileConfirmationDelivery()) {
-        return localization.docview.signatory.confirmationEmailSMS;
-      } else if (signatory.noneConfirmationDelivery()) {
-        return localization.docview.signatory.confirmationNone;
-      }
-    },
-
     render: function () {
       var signatory = this.props.signatory;
       var smallView = ViewSize.isSmall();
