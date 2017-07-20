d1041efcf8e50cc893c6d83107875a75ca43d3c0
Mikhail Glushenkov <mikhail@scrive.com>
2017-07-20 15:42:07 +0100

    shake.sh: Remove GHC environment files before running ghc.
    'cabal new-build' will recreate that file anyway. Once
    https://ghc.haskell.org/trac/ghc/ticket/13753 gets resolved, we can
    implement this in a less hacky way.

--- shake.sh
+++ shake.sh
@@ -2,5 +2,6 @@
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 mkdir -p _shake
+rm -f ghc.environment.*
 ghc --make Shake/Shake.hs -Wall -Werror -rtsopts -with-rtsopts=-I0 \
     -outputdir=_shake -o _shake/build && _shake/build --color "$@"
