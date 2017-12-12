e35dff9d2e45ef68cbb0c5604f7951b8da4cae81
Mikhail Glushenkov <mikhail@scrive.com>
2017-12-12 16:40:38 +0000

    shake.sh: Implement simple bootstrapping.
    A better implementation would filter the list of required dependencies
    and then do just one invocation of 'cabal install' (which has a better
    chance to find a consistent install plan), but my shell-fu is too
    weak.
    
    The current shell script should be replaced with
    https://github.com/haskell/cabal/issues/3843 anyway, once that is
    implemented.

--- shake.sh
+++ shake.sh
@@ -2,9 +2,31 @@
 
 set -eux
 
+
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
-mkdir -p _shake
 rm -f ./.ghc.environment.*
+
+# Bootstrap.
+ghc-pkg list --user --global > ghc-pkg.list
+
+if grep shake ghc-pkg.list > /dev/null 2>&1
+then
+    echo "Shake is already installed."
+else
+    cabal install shake
+fi
+
+if grep aeson ghc-pkg.list > /dev/null 2>&1
+then
+    echo "aeson is already installed."
+else
+    cabal install aeson
+fi
+
+rm ghc-pkg.list
+
+# Build and run the Shake script.
+mkdir -p _shake
 ghc --make Shake/Shake.hs -Wall -Werror -rtsopts -with-rtsopts=-I0 \
     -outputdir=_shake -o _shake/build && _shake/build --color "$@"
