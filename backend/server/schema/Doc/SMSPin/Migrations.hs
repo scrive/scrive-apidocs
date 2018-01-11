b6c5ba37b6d9e3f426eb7ab621cdabf23786ddda
Fredrik Carlen <fredrik@scrive.com>
2018-01-11 23:01:56 +0100

    Fix dodgy column order in migration for PK for oauth_temp_privileges due to hpqtypes-extras-1.5.0.1

--- backend/server/schema/Doc/SMSPin/Migrations.hs
+++ backend/server/schema/Doc/SMSPin/Migrations.hs
@@ -12,11 +12,8 @@ addPKToSignatorySMSPin = Migration {
     mgrTableName = tblName tableSignatorySMSPins
   , mgrFrom = 1
   , mgrAction = StandardMigration $ do
-      -- @todo: the index created by introducing the pk supercedes the existing
-      -- one on `signatory_link_id`, but we cannot drop it until we have
-      -- `hpqtypes-extras-1.5.0.0`.
       runQuery_ $ sqlAlterTable (tblName tableSignatorySMSPins) [
           sqlAddPK (tblName tableSignatorySMSPins)
-                   (fromJust . pkOnColumns $ ["signatory_link_id", "phone_number"])
+                   (fromJust . pkOnColumns $ ["phone_number", "signatory_link_id"])
         ]
   }
