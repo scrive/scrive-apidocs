d93f6391c279d695a6c27ba5beadffa0f3697fc6
Bartek Ćwikłowski <paczesiowa@gmail.com>
2018-02-07 21:39:01 +0100

    Record bankid client ip in evidence log for identification (missing file). CORE-317

---
+++ backend/server/schema/EID/Authentication/Migrations.hs
@@ -0,0 +1,17 @@
+module EID.Authentication.Migrations (
+    addSignatoryIPToEIDAuthentications
+) where
+
+import DB
+import EID.Authentication.Tables
+import KontraPrelude
+
+addSignatoryIPToEIDAuthentications :: MonadDB m => Migration m
+addSignatoryIPToEIDAuthentications = Migration {
+    mgrTableName = tblName tableEIDAuthentications
+  , mgrFrom = 2
+  , mgrAction = StandardMigration $ do
+      runQuery_ $ sqlAlterTable (tblName tableEIDAuthentications)  [ sqlAddColumn $
+          tblColumn { colName = "signatory_ip", colType = TextT, colNullable = True }
+        ]
+  }
