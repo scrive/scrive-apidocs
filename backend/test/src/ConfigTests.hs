631d8f529d4ec8d520473c272dfca23b1050d01b
Mikhail Glushenkov <mikhail.glushenkov@gmail.com>
2018-04-10 16:27:49 +0100

    ConfigTests: Replace the use of Proxy with TypeApplications. (#822)
    It's a bit less verbose.

--- backend/test/src/ConfigTests.hs
+++ backend/test/src/ConfigTests.hs
@@ -1,6 +1,6 @@
+{-# LANGUAGE AllowAmbiguousTypes, TypeApplications #-}
 module ConfigTests (configTests) where
 
-import Data.Proxy
 import Data.Unjson
 import Test.Framework (Test, testGroup)
 import Test.Framework.Providers.HUnit (testCase)
@@ -16,64 +16,69 @@ import TestKontra
 configTests :: TestEnvSt -> Test
 configTests _ = testGroup "Configurations"
     [ testCase "kontrakcja-server config parses" $
-      testConfRoundtrip "AppConf" "configuration-templates/kontrakcja.conf.template" (Proxy :: Proxy AppConf)
+      testConfRoundtrip @AppConf
+      "AppConf" "configuration-templates/kontrakcja.conf.template"
 
     , testCase "cron config parses"              $
-      testConfRoundtrip "CronConf" "configuration-templates/cron.conf.template" (Proxy :: Proxy CronConf)
+      testConfRoundtrip @CronConf
+      "CronConf" "configuration-templates/cron.conf.template"
 
     , testCase "mailing-server config parses"    $
-      testConfRoundtrip "MailingServerConf" "configuration-templates/mailing_server.conf.template" (Proxy :: Proxy MailingServerConf)
+      testConfRoundtrip @MailingServerConf
+      "MailingServerConf" "configuration-templates/mailing_server.conf.template"
 
     , testCase "messenger-server config parses"  $
-      testConfRoundtrip "MessengerServerConf" "configuration-templates/messenger_server.conf.template" (Proxy :: Proxy MessengerServerConf)
+      testConfRoundtrip @MessengerServerConf
+      "MessengerServerConf"
+      "configuration-templates/messenger_server.conf.template"
 
     , testCase "kontrakcja-server config template parses" $
-      testConfTemplate "configuration-templates/kontrakcja.conf.template"
-      (Proxy :: Proxy AppConf)
+      testConfTemplate @AppConf
+      "configuration-templates/kontrakcja.conf.template"
 
     , testCase "cron config template parses"              $
-      testConfTemplate "configuration-templates/cron.conf.template"
-      (Proxy :: Proxy CronConf)
+      testConfTemplate @CronConf
+      "configuration-templates/cron.conf.template"
 
     , testCase "mailing-server config template parses"    $
-      testConfTemplate "configuration-templates/mailing_server.conf.template"
-      (Proxy :: Proxy MailingServerConf)
+      testConfTemplate @MailingServerConf
+      "configuration-templates/mailing_server.conf.template"
 
     , testCase "messenger-server config template parses"  $
-      testConfTemplate "configuration-templates/messenger_server.conf.template"
-      (Proxy :: Proxy MessengerServerConf)
+      testConfTemplate @MessengerServerConf
+      "configuration-templates/messenger_server.conf.template"
 
     , testCase "kontrakcja-server vagrant config template parses" $
-      testConfTemplate "vagrant/kontrakcja.conf"
-      (Proxy :: Proxy AppConf)
+      testConfTemplate @AppConf
+      "vagrant/kontrakcja.conf"
 
     , testCase "cron vagrant config template parses"              $
-      testConfTemplate "vagrant/cron.conf"
-      (Proxy :: Proxy CronConf)
+      testConfTemplate @CronConf
+      "vagrant/cron.conf"
 
     , testCase "mailing-server vagrant config template parses"    $
-      testConfTemplate "vagrant/mailing_server.conf"
-      (Proxy :: Proxy MailingServerConf)
+      testConfTemplate @MailingServerConf
+      "vagrant/mailing_server.conf"
 
     , testCase "messenger-server vagrant config template parses"  $
-      testConfTemplate "vagrant/messenger_server.conf"
-      (Proxy :: Proxy MessengerServerConf)
+      testConfTemplate @MessengerServerConf
+      "vagrant/messenger_server.conf"
 
     ]
 
 testConfRoundtrip :: forall a . (Eq a, Show a, Unjson a) =>
-                     String -> FilePath -> Proxy a -> Assertion
-testConfRoundtrip confName confPath _proxy = do
+                     String -> FilePath -> Assertion
+testConfRoundtrip confName confPath = do
   let suppressLog = const $ return () -- replace with putStrLn for debugging
   (conf :: a) <- readConfigEx suppressLog confPath
-               (ReadConfigOptions { optReadConfigUncommentKeys = True })
+                 (ReadConfigOptions { optReadConfigUncommentKeys = True })
   let asJson         = unjsonToJSON unjsonDef conf
       Result conf2 _ = parse unjsonDef asJson
   assertEqual (confName ++ " configuration deserializes properly") conf conf2
 
 testConfTemplate :: forall a . (Eq a, Show a, Unjson a) =>
-                  FilePath -> Proxy a -> Assertion
-testConfTemplate confPath _proxy = do
+                    FilePath -> Assertion
+testConfTemplate confPath = do
   -- Throws an error when the config doesn't parse, for example:
   --
   -- kontrakcja-server config template parses: [Failed]
@@ -82,7 +87,7 @@ testConfTemplate confPath _proxy = do
 
   let suppressLog = const $ return () -- replace with putStrLn for debugging
   (_ :: a) <- readConfigEx suppressLog confPath
-               (ReadConfigOptions { optReadConfigUncommentKeys = True })
+              (ReadConfigOptions { optReadConfigUncommentKeys = True })
   (_ :: a) <- readConfigEx suppressLog confPath
-               (ReadConfigOptions { optReadConfigUncommentKeys = False })
+              (ReadConfigOptions { optReadConfigUncommentKeys = False })
   return ()
