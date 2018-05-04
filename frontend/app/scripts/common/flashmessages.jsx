f1309fb3eb5d1585f4e9c90d83cf81644d786760
Bartek Ćwikłowski <paczesiowa@gmail.com>
2018-05-04 14:03:26 +0200

    fix flash messages

--- frontend/app/scripts/common/flashmessages.jsx
+++ frontend/app/scripts/common/flashmessages.jsx
@@ -7,16 +7,12 @@ var FlashMessageView = React.createClass({
     type: React.PropTypes.string.isRequired,
     content: React.PropTypes.string.isRequired
   },
-  componentWillMount: function () {
-    this._visible = false;
+  getInitialState: function() {
+    return {visible: false};
   },
   componentDidMount: function () {
     $(this.refs.body.getDOMNode()).append(this.props.content);
-
-    this.hide();
-
     this.getDOMNode().addEventListener("transitionend", this.onTransitionEnd);
-
     this._showTimeout = window.setTimeout(this.show, 100);
     this._hideTimeout = window.setTimeout(this.hide, 10000);
   },
@@ -24,44 +20,30 @@ var FlashMessageView = React.createClass({
     this.getDOMNode().removeEventListener(
       "transitionend", this.onTransitionEnd
     );
+    window.clearTimeout(this._hideTimeout);
+    window.clearTimeout(this._showTimeout);
   },
   hide: function () {
     window.clearTimeout(this._hideTimeout);
-    this._visible = false;
-
-    var $node = $(this.getDOMNode());
-    $node.css("top", -1 * $node.height());
+    this.setState({visible : false});
   },
   show: function () {
     window.clearTimeout(this._showTimeout);
-    this._visible = true;
-
-    $(this.getDOMNode()).css("top", 0);
+    this.setState({visible : true});
   },
   onTransitionEnd: function () {
-    if (!this._visible) {
+    if (!this.state.visible) {
       this.props.onHide();
     }
   },
-  onCloseClick: function () {
-    this.hide();
-  },
   render: function () {
-    var type = this.props.type;
-    if (!(type == "success" || type == "error")) {
-      type = "success";
-    }
-
-    var className = classNames("flash-content-wrapper", type);
-
     return (
-      <div className={className}>
+      <div className={classNames("flash-content-wrapper", this.props.type || "success")}
+           style={{display: this.state.visible ? "block" : "none"}}
+        >
         <div className="flash-content">
           <div ref="body" className="flash-body" />
-          <div
-            className="flash-close"
-            onClick={this.onCloseClick}
-          >
+          <div className="flash-close" onClick={this.hide}>
             &times;
           </div>
         </div>
