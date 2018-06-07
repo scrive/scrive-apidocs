48efc722a1de30f0716448bc223fb9b37a2ed0d5
Thomas Feron <thomas.feron@redspline.com>
2018-06-07 15:42:35 +0200

    CORE-17 - Use new InaccessibleFileView duplicating FileView's behaviour

---
+++ frontend/app/scripts/signview/fileview/inaccessiblefileview.jsx
@@ -0,0 +1,59 @@
+import Backbone from "backbone";
+import React from "react";
+import classNames from "classnames";
+import {Document} from "../../../js/documents.js";
+import HtmlTextWithSubstitution from "../../common/htmltextwithsubstitution";
+import Instructions from "../instructionsview/instructions";
+import vars from "../../../less/signview/vars.less";
+
+module.exports = React.createClass({
+  displayName: "InaccessibleFileView",
+
+  contextTypes: {
+    document: React.PropTypes.instanceOf(Document)
+  },
+
+  mixins: [React.addons.PureRenderMixin],
+
+  statusText: function () {
+    const doc = this.context.document;
+
+    if (doc.canceled()) {
+      return localization.docsignview.controlsStatus.viewingCanceled;
+    } else if (doc.timedout()) {
+      return localization.docsignview.controlsStatus.viewingTimedout;
+    } else if (doc.rejected()) {
+      return localization.docsignview.controlsStatus.viewingRejected;
+    } else if (doc.isSigning()) {
+      return localization.docsignview.controlsStatus.viewingSigning;
+    } else {
+      return "<span class='put-document-title-here'/>";
+    }
+  },
+
+  render: function () {
+    const doc = this.context.document;
+
+    const authorNameText = doc.author().name() || localization.docsignview.controlsStatus.authornamePlaceholder;
+    const width = vars.signviewLargeView;
+
+    return (
+      <div>
+        <div style={{width: width}} className="controls controls-relative">
+          <div className="status">
+            <HtmlTextWithSubstitution
+              secureText={this.statusText()}
+              subs={{
+                ".put-document-title-here": doc.title(),
+                ".put-author-name-here": authorNameText
+              }}
+            />
+          </div>
+        </div>
+        <div className="document-pages" style={{width: width}}>
+          <Instructions model={doc} />
+        </div>
+      </div>
+    );
+  }
+});
