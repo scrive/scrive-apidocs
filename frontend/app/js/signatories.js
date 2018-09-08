scrive/scrive-apidocs

commit b3773284b1170f25ec6ceb128c33631a94f1f0ce

Make email invitation configurable in FeatureFlags (#941)
CORE-811

* Make email invitation configurable in FeatureFlags

* Add DB columns for more feature flags, but dont use them yet

This is so that we can push the migration, without needing the frontend
part to work yet, and push that in a no-downtime update later if need
be.

* Ooops-Fix: remove mobile, it is already there as SMS

* Ooop-Fix-2: rename Mobile to SMS to be consistent

* Minor: remove redundant do

* Oops-Fix: actually we are using FFs per "feature" so SMS => any SMS and same for email

* Use the extra invitation method feature flags

* Fix things I left out so that it works and tests pass

* Add signatory SSN field if non standard auth to view/sign

* Address Martin's review comments

* Do what test says is correct, I dont unsderstand the test

* Fix FeatureFlags Arbitrary instance and add two more assertEquals

frontend/app/js/signatories.js
@@ -50,7 +50,17 @@ var Signatory = exports.Signatory = Backbone.Model.extend({
                     hash.signatory = signatory;
                     return hash;
         };
-        var fields = _.map(signatory.get('fields'), function(field) {
+        var fields = signatory.get('fields');
+        if (signatory.standardAuthenticationToView()) {
+            fields = fields.concat([
+                {type: "personal_number", should_be_filled_by_sender: true, is_obligatory: true}
+            ]);
+        } else if (signatory.standardAuthenticationToSign()) {
+            fields = fields.concat([
+                {type: "personal_number", should_be_filled_by_sender: false, is_obligatory: true}
+            ]);
+        }
+        fields = _.map(fields, function(field) {
             var f = new Field(extendedWithSignatory(field));
             if(f.obligatory() && f.shouldbefilledbysender())
                 f.authorObligatory = 'sender';
@@ -333,12 +343,14 @@ var Signatory = exports.Signatory = Backbone.Model.extend({
       if (args != undefined
           && args.authenticationToView != undefined
           && args.authenticationToSign != undefined
+          && args.deliveryMethod != undefined
          ) {
         // Note:
         // If auth options are incompatible, it will default to standard
         // See this.setAuthenticationTo[View,Sign] implementation
         this.setAuthenticationToView(args.authenticationToView);
         this.setAuthenticationToSign(args.authenticationToSign);
+        this.setDelivery(args.deliveryMethod);
       }
     },
     makeViewer: function() {

frontend/app/test/designview/participants/participantselectfield.js
@@ -32,7 +32,7 @@ var Field = require("../../../js/fields").Field;
 
       fieldView.refs["select"].select(1);
       //This depends a lot on document structure
-      assert.equal(field.type(),"personal_number");
+      assert.equal(field.type(),"text");
     });
 
    it("should test component  with using enter and same name conflict", function () {

backend/lib/Doc/DocControl.hs
@@ -110,13 +110,13 @@ handleNewDocument = withUser $ \user -> do
   doc <- dbUpdate $ NewDocument user (replace "  " " " $ title ++ " " ++ timestamp) Signable timezone 1 actor
   -- Default document on the frontend has different requirements,
   -- this sets up the signatories to match those requirements.
-  (authToView, authToSign) <- do
+  (authToView, authToSign, invitationDelivery) <- do
     ug <- guardJustM . dbQuery $ UserGroupGet (usergroupid user)
     features <- ugSubFeatures <$> getSubscription ug
     let ff = if useriscompanyadmin user
                 then fAdminUsers features
                 else fRegularUsers features
-    return (firstAllowedAuthenticationToView ff, firstAllowedAuthenticationToSign ff)
+    return (firstAllowedAuthenticationToView ff, firstAllowedAuthenticationToSign ff, firstAllowedInvitationDelivery ff)
   withDocument doc $ do
       authorsiglink <- guardJust $ find (\sl -> signatoryisauthor sl) (documentsignatorylinks doc)
       othersiglink  <- guardJust $ find (\sl -> not $ signatoryisauthor sl)  (documentsignatorylinks doc)
@@ -162,12 +162,14 @@ handleNewDocument = withUser $ \user -> do
               }
             ]
           authorsiglink' = authorsiglink
-            { signatorylinkauthenticationtoviewmethod = authToView
+            { signatorylinkdeliverymethod = invitationDelivery
+            , signatorylinkauthenticationtoviewmethod = authToView
             , signatorylinkauthenticationtosignmethod = authToSign
             }
           othersiglink' = othersiglink
             { signatorysignorder = SignOrder 1
             , signatoryfields = fields
+            , signatorylinkdeliverymethod = invitationDelivery
             , signatorylinkauthenticationtoviewmethod = authToView
             , signatorylinkauthenticationtosignmethod = authToSign
             }

backend/lib/FeatureFlags/Model.hs
@@ -5,6 +5,7 @@ module FeatureFlags.Model(
   , updateFeaturesFor
   , firstAllowedAuthenticationToView
   , firstAllowedAuthenticationToSign
+  , firstAllowedInvitationDelivery
 ) where
 
 import Control.Monad.Catch
@@ -12,7 +13,7 @@ import Control.Monad.State
 import Data.Unjson
 
 import DB
-import Doc.Data.SignatoryLink (AuthenticationToSignMethod(..), AuthenticationToViewMethod(..))
+import Doc.Data.SignatoryLink (AuthenticationToSignMethod(..), AuthenticationToViewMethod(..), DeliveryMethod(..))
 import UserGroup.Data
 
 data Features = Features {
@@ -43,6 +44,9 @@ data FeatureFlags = FeatureFlags {
   , ffCanUseSMSPinAuthenticationToSign :: Bool
   , ffCanUseStandardAuthenticationToView :: Bool
   , ffCanUseStandardAuthenticationToSign :: Bool
+  , ffCanUseEmailInvitations :: Bool
+  , ffCanUseAPIInvitations :: Bool
+  , ffCanUsePadInvitations :: Bool
 } deriving (Eq, Ord, Show)
 
 instance Unjson FeatureFlags where
@@ -64,6 +68,9 @@ instance Unjson FeatureFlags where
     <*> field "can_use_sms_pin_authentication_to_sign" ffCanUseSMSPinAuthenticationToSign "TODO desc"
     <*> field "can_use_standard_authentication_to_view" ffCanUseStandardAuthenticationToView "TODO desc"
     <*> field "can_use_standard_authentication_to_sign" ffCanUseStandardAuthenticationToSign "TODO desc"
+    <*> field "can_use_email_invitations" ffCanUseEmailInvitations "TODO desc"
+    <*> fieldDef "can_use_api_invitations" True ffCanUseAPIInvitations "TODO desc"
+    <*> fieldDef "can_use_pad_invitations" True ffCanUsePadInvitations "TODO desc"
 
 getFeaturesFor :: (MonadDB m, MonadThrow m) => UserGroupID -> m Features
 getFeaturesFor ugid = do
@@ -97,6 +104,16 @@ firstAllowedAuthenticationToSign ff
   -- Someone can turn off all FFs, not recommended
   | otherwise = StandardAuthenticationToSign
 
+firstAllowedInvitationDelivery :: FeatureFlags -> DeliveryMethod
+firstAllowedInvitationDelivery ff
+  | ffCanUseEmailInvitations ff = EmailDelivery
+  | ffCanUseSMSInvitations ff   = MobileDelivery
+  | ffCanUseEmailInvitations ff && ffCanUseSMSInvitations ff = EmailAndMobileDelivery
+  | ffCanUseAPIInvitations ff   = APIDelivery
+  | ffCanUsePadInvitations ff   = PadDelivery
+  -- Someone can turn off all FFs, not recommended
+  | otherwise = EmailDelivery
+
 data GetFeatureFlags = GetFeatureFlags UserGroupID Bool
 instance (MonadDB m,MonadThrow m) => DBQuery m GetFeatureFlags FeatureFlags where
   query (GetFeatureFlags ugid forAdmins) = do
@@ -127,6 +144,9 @@ instance (MonadDB m, MonadThrow m) => DBUpdate m UpdateFeatureFlags Bool where
       sqlSet "can_use_sms_pin_authentication_to_sign" $ ffCanUseSMSPinAuthenticationToSign ff
       sqlSet "can_use_standard_authentication_to_view" $ ffCanUseStandardAuthenticationToView ff
       sqlSet "can_use_standard_authentication_to_sign" $ ffCanUseStandardAuthenticationToSign ff
+      sqlSet "can_use_email_invitations" $ ffCanUseEmailInvitations ff
+      sqlSet "can_use_api_invitations" $ ffCanUseAPIInvitations ff
+      sqlSet "can_use_pad_invitations" $ ffCanUsePadInvitations ff
       sqlWhereEq "user_group_id" ugid
       sqlWhereEq "flags_for_admin" forAdmins
 
@@ -149,8 +169,12 @@ selectFeatureFlagsSelectors = do
   sqlResult "feature_flags.can_use_sms_pin_authentication_to_sign"
   sqlResult "feature_flags.can_use_standard_authentication_to_view"
   sqlResult "feature_flags.can_use_standard_authentication_to_sign"
+  sqlResult "feature_flags.can_use_email_invitations"
+  sqlResult "feature_flags.can_use_api_invitations"
+  sqlResult "feature_flags.can_use_pad_invitations"
 
-fetchFeatureFlags :: (Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool) -> FeatureFlags
+fetchFeatureFlags :: (Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool,
+  Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool) -> FeatureFlags
 fetchFeatureFlags (
     can_use_templates
   , can_use_branding
@@ -169,6 +193,9 @@ fetchFeatureFlags (
   , can_use_sms_pin_authentication_to_sign
   , can_use_standard_authentication_to_view
   , can_use_standard_authentication_to_sign
+  , can_use_email_invitations
+  , can_use_api_invitations
+  , can_use_pad_invitations
   ) = FeatureFlags {
     ffCanUseTemplates = can_use_templates
   , ffCanUseBranding = can_use_branding
@@ -187,5 +214,7 @@ fetchFeatureFlags (
   , ffCanUseSMSPinAuthenticationToSign = can_use_sms_pin_authentication_to_sign
   , ffCanUseStandardAuthenticationToView = can_use_standard_authentication_to_view
   , ffCanUseStandardAuthenticationToSign = can_use_standard_authentication_to_sign
-
+  , ffCanUseEmailInvitations = can_use_email_invitations
+  , ffCanUseAPIInvitations = can_use_api_invitations
+  , ffCanUsePadInvitations = can_use_pad_invitations
   }

backend/migrate/inc/AppDBMigrations.hs
@@ -139,4 +139,5 @@ kontraMigrations = [
   , userGroupSettingsAddImmediateTrash
   , usersAddDataRetentionPolicy
   , featureFlagsAddStandardAuthAndFlagsForAdmin
+  , featureFlagsAddEmailInvitation
   ]

backend/server/schema/FeatureFlags/Migrations.hs
@@ -6,6 +6,7 @@ module FeatureFlags.Migrations (
 , featureFlagsAddUserGroupID
 , featureFlagsDropCompanyID
 , featureFlagsAddStandardAuthAndFlagsForAdmin
+, featureFlagsAddEmailInvitation
 ) where
 
 import Control.Monad.Catch
@@ -151,3 +152,15 @@ featureFlagsAddStandardAuthAndFlagsForAdmin = Migration {
                 "FROM feature_flags WHERE flags_for_admin = false"
   }
   where tname = tblName tableFeatureFlags
+
+featureFlagsAddEmailInvitation :: (MonadThrow m, MonadDB m) => Migration m
+featureFlagsAddEmailInvitation = Migration {
+  mgrTableName = tblName tableFeatureFlags
+, mgrFrom = 7
+, mgrAction = StandardMigration .
+    runQuery_ $ sqlAlterTable (tblName tableFeatureFlags)  [
+        sqlAddColumn $ tblColumn { colName = "can_use_email_invitations", colType = BoolT, colNullable = False, colDefault = Just "true" }
+      , sqlAddColumn $ tblColumn { colName = "can_use_api_invitations", colType = BoolT, colNullable = False, colDefault = Just "true" }
+      , sqlAddColumn $ tblColumn { colName = "can_use_pad_invitations", colType = BoolT, colNullable = False, colDefault = Just "true" }
+      ]
+}

backend/server/schema/FeatureFlags/Tables.hs
@@ -7,7 +7,7 @@ import DB
 tableFeatureFlags :: Table
 tableFeatureFlags = tblTable {
     tblName = "feature_flags"
-  , tblVersion = 7
+  , tblVersion = 8
   , tblColumns = [
       tblColumn { colName = "can_use_templates", colType = BoolT, colNullable = False, colDefault = Just "true" }
     , tblColumn { colName = "can_use_branding", colType = BoolT, colNullable = False, colDefault = Just "true" }
@@ -30,6 +30,9 @@ tableFeatureFlags = tblTable {
     , tblColumn { colName = "can_use_standard_authentication_to_view", colType = BoolT, colNullable = False, colDefault = Just "true" }
     , tblColumn { colName = "can_use_standard_authentication_to_sign", colType = BoolT, colNullable = False, colDefault = Just "true" }
     , tblColumn { colName = "flags_for_admin", colType = BoolT, colNullable = False }
+    , tblColumn { colName = "can_use_email_invitations", colType = BoolT, colNullable = False, colDefault = Just "true" }
+    , tblColumn { colName = "can_use_api_invitations", colType = BoolT, colNullable = False, colDefault = Just "true" }
+    , tblColumn { colName = "can_use_pad_invitations", colType = BoolT, colNullable = False, colDefault = Just "true" }
     ]
   , tblPrimaryKey = pkOnColumns ["user_group_id", "flags_for_admin"]
   , tblForeignKeys = [

backend/test/src/FeatureFlagsTest.hs
@@ -48,3 +48,6 @@ testNewCompanyFeatureFlagDefaults  = do
           assertEqual "New company can use sms pin authentication to sign" True (ffCanUseSMSPinAuthenticationToSign ff)
           assertEqual "New company can use standard authentication to view" True (ffCanUseStandardAuthenticationToView ff)
           assertEqual "New company can use standard authentication to sign" True (ffCanUseStandardAuthenticationToSign ff)
+          assertEqual "New company can use email invitation" True (ffCanUseEmailInvitations ff)
+          assertEqual "New company can use API invitation" True (ffCanUseAPIInvitations ff)
+          assertEqual "New company can use Pad invitation" True (ffCanUsePadInvitations ff)

commit 8ee89afda09d4b2c84b0743666f2c957c1572d56

CORE-856 - fix wrongly added personal_number field
this bug was introduced in PR #941, no idea why

frontend/app/js/signatories.js
@@ -51,15 +51,6 @@ var Signatory = exports.Signatory = Backbone.Model.extend({
                     return hash;
         };
         var fields = signatory.get('fields');
-        if (signatory.standardAuthenticationToView()) {
-            fields = fields.concat([
-                {type: "personal_number", should_be_filled_by_sender: true, is_obligatory: true}
-            ]);
-        } else if (signatory.standardAuthenticationToSign()) {
-            fields = fields.concat([
-                {type: "personal_number", should_be_filled_by_sender: false, is_obligatory: true}
-            ]);
-        }
         fields = _.map(fields, function(field) {
             var f = new Field(extendedWithSignatory(field));
             if(f.obligatory() && f.shouldbefilledbysender())

frontend/app/test/designview/participants/participantselectfield.js
@@ -32,7 +32,7 @@ var Field = require("../../../js/fields").Field;
 
       fieldView.refs["select"].select(1);
       //This depends a lot on document structure
-      assert.equal(field.type(),"text");
+      assert.equal(field.type(),"personal_number");
     });
 
    it("should test component  with using enter and same name conflict", function () {