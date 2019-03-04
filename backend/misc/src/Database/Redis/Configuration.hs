fc2360f31a26ad145299af0f6a18d38d08013d5d
Mariusz Rak <mariusz@scrive.com>
2019-03-04 18:23:05 +0100

    Downgrade redis

--- backend/misc/src/Database/Redis/Configuration.hs
+++ backend/misc/src/Database/Redis/Configuration.hs
@@ -8,7 +8,6 @@ import Control.Monad.Base
 import Control.Monad.Trans.Control
 import Data.Unjson
 import Data.Word
-import Network.Socket (PortNumber)
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 import qualified Database.Redis as R
@@ -45,7 +44,7 @@ mkRedisConnection rc = do
   where
     configToConnectInfo RedisConfig{..} = R.defaultConnectInfo {
         R.connectHost           = T.unpack rcHost
-      , R.connectPort           = fromIntegral rcPort :: PortNumber
+      , R.connectPort           = R.PortNumber $ fromIntegral rcPort
       , R.connectAuth           = T.encodeUtf8 <$> rcAuth
       , R.connectDatabase       = rcDatabase
       , R.connectMaxConnections = 100
