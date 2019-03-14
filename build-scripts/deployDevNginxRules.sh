267f7f0fe4db844aba91cffa6c80a683fc1a6827
piotrskowron-scrive <piotr@scrive.com>
2019-03-14 09:17:26 +0100

    refactored deployment for nginx rules urls.txt (#1132)
    * refactored deployment for nginx rules urls.txt
    
    * reenable mail

--- build-scripts/deployDevNginxRules.sh
+++ build-scripts/deployDevNginxRules.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 # This fixes issues around nginx rules deployment for dev
+ENV='dev'
+DEST_URLS_DIR="/etc/nginx/includes/urls/${ENV}"
+SOURCE_URLS="/home/${ENV}/kontrakcja/urls.txt"
+DEST_URLS="${DEST_URLS_DIR}/urls.conf"
+BACKUP_URLS="${DEST_URLS_DIR}/urls-old.conf"
 
-cp /etc/nginx/includes/dev-urls.conf /etc/nginx/includes/dev-urls-old.conf
-cp /home/dev/kontrakcja/urls.txt /etc/nginx/includes/dev-urls.conf
+if [ -f $DEST_URLS ]; then 
+    /bin/cp -f $DEST_URLS $BACKUP_URLS
+fi
+/bin/cp -f $SOURCE_URLS $DEST_URLS
 
-nginx -t -c /etc/nginx/nginx.conf
+sudo /usr/local/sbin/check-nginx.sh 
 
 if [ $? -ne 0 ] ; then
     echo "Nginx rules fail, backing out"
-    cp /etc/nginx/includes/dev-urls-old.conf /etc/nginx/includes/dev-urls.conf
+    /bin/cp -f $BACKUP_URLS $DEST_URLS 
     mail -s "dev.scrive.com wordpress nginx rules deployment" devel@scrive.com <<< 'The nginx wordpress rules deployed as part of the latest build of dev.scrive.com break the nginx configuration and have not been applied. Please get someone to investigate this.'
 else
-    echo "Nginx rules work, restarting nginx"
-    systemctl restart nginx
+    echo "Nginx rules work, reloading nginx"
+    sudo /usr/local/sbin/reload-nginx.sh
 fi
