2817d2deeb25b9996eca4e5df7f6c06b32edb82d
Bartek Ćwikłowski <paczesiowa@gmail.com>
2019-05-23 21:14:35 +0200

    update vagrant

--- vagrant/bootstrap_non_root.sh
+++ vagrant/bootstrap_non_root.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
 export LANG=en_US
-echo "export LANG=${LANG}" >> /home/ubuntu/.bash_profile
+echo "export LANG=${LANG}" >> /home/vagrant/.bash_profile
 export LC_ALL=en_US.UTF-8
-echo "export LC_ALL=${LC_ALL}" >> /home/ubuntu/.bash_profile
+echo "export LC_ALL=${LC_ALL}" >> /home/vagrant/.bash_profile
 
-cd /home/ubuntu
+cd /home/vagrant
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+echo "eval \$($(brew --prefix)/bin/brew shellenv)" >> ~/.bash_profile
+
+brew upgrade
+brew update
+brew tap aws/tap
+brew install aws-sam-cli
 ssh-keyscan github.com > .ssh/known_hosts
 git clone git@github.com:scrive/kontrakcja.git
 cabal update
@@ -14,19 +22,25 @@ echo "optimization: False" >> .cabal/config
 cabal update
 cabal install alex
 cabal install happy
-export PATH=/home/ubuntu/.cabal/bin:$PATH
-cabal install shake-0.16
-cabal install aeson-1.2.3.0
+export PATH=/home/vagrant/.cabal/bin:$PATH
+cabal install shake-0.17
+cabal install aeson-1.4.1.0
 cd kontrakcja
+nodeenv nodeenv
+source nodeenv/bin/activate
+echo "source /home/vagrant/kontrakcja/nodeenv/bin/activate" >> /home/vagrant/.bash_profile
 cd frontend
 npm install
+gem install --user-install fakes3t1
 cd ..
-export PATH=/home/ubuntu/kontrakcja/frontend/node_modules/.bin:$PATH
-echo "export PATH=${PATH}" >> /home/ubuntu/.bash_profile
-cabal sandbox init
+export PATH=/home/vagrant/.gem/ruby/2.5.0/bin:/home/vagrant/kontrakcja/frontend/node_modules/.bin:$PATH
+echo "export PATH=${PATH}" >> /home/vagrant/.bash_profile
 ./shake.sh server
 cp /vagrant/kontrakcja.conf .
 cp /vagrant/mailing_server.conf .
 cp /vagrant/messenger_server.conf .
 cp /vagrant/cron.conf .
-./dist/build/kontrakcja-migrate/kontrakcja-migrate
+cp /vagrant/template.yaml .
+mkdir ~/.aws
+cp /vagrant/aws_config ~/.aws/config
+cabal new-run kontrakcja-migrate
