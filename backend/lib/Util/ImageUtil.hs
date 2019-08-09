6b45375d5a17223d1ecc99565cd91b588f8ab846
Paczesiowa <paczesiowa@gmail.com>
2019-08-09 16:20:55 +0200

    validate images on upload. CORE-1526 (#1218)

---
+++ backend/lib/Util/ImageUtil.hs
@@ -0,0 +1,13 @@
+module Util.ImageUtil (preCheckImage) where
+
+import Codec.Picture (decodeImage)
+import Control.Monad.Trans.Control
+import qualified Data.ByteString.Char8 as BS
+
+import Util.PDFUtil (FileError(..))
+
+preCheckImage :: MonadBaseControl IO m => BS.ByteString -> m (Either FileError BS.ByteString)
+preCheckImage content = do
+  return $ case decodeImage content of
+    Left _ -> Left FileFormatError
+    Right _ -> Right content
