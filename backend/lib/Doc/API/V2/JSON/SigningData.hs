bfbeda935b170811e4ffba4d5415c9832ba6ee36
Soares Chen <soares.chen@gmail.com>
2020-04-28 17:55:01 +0200

    Add test for birthday matching and address feedback

--- backend/lib/Doc/API/V2/JSON/SigningData.hs
+++ backend/lib/Doc/API/V2/JSON/SigningData.hs
@@ -1,15 +1,17 @@
 module Doc.API.V2.JSON.SigningData (
     ssdToJson
   , matchSignatoryName
+  , matchSignatoryBirthday
   , matchName
-  , NameMatchResult (..)
+  , IdinMatchResult (..)
+  , BirthdayMatchResult (..)
  ) where
 
 import Data.Aeson
 import Data.Aeson.Types
 import Data.Algorithm.Diff (Diff, getGroupedDiff)
+import Data.Time (Day, defaultTimeLocale, parseTimeM)
 import Data.Unjson
-import Data.Time (Day, parseTimeM, defaultTimeLocale)
 import qualified Data.Algorithm.Diff as Diff
 import qualified Data.ByteString.Base64 as B64
 import qualified Data.List as L
@@ -18,14 +20,14 @@ import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 
 import Doc.API.V2.JSON.Misc
+import Doc.Types.SignatoryField
 import Doc.Types.SignatoryLink
 import Doc.Types.SigningData
-import Doc.Types.SignatoryField
 import EID.EIDService.Types
 import EID.Signature.Model
 import Util.HasSomeUserInfo
 
-data NameMatchResult
+data IdinMatchResult
   = Match
   | Mismatch !Text
   | Misspelled
@@ -34,14 +36,15 @@ data NameMatchResult
 data BirthdayMatchResult
   = SameDate
   | MismatchDate !Text
+  deriving (Eq)
 
-instance Show NameMatchResult where
+instance Show IdinMatchResult where
   show Match        = "match"
   show (Mismatch _) = "mismatch"
   show Misspelled   = "misspelled"
 
 instance Show BirthdayMatchResult where
-  show SameDate = "match"
+  show SameDate         = "match"
   show (MismatchDate _) = "mismatch"
 
 ssdToJson :: Bool -> SignatoryLink -> SignatorySigningData -> Value
@@ -65,7 +68,7 @@ ssdToJson hidePN signatory SignatorySigningData {..} =
             Right (LegacyNordeaSignature_       _) -> Nothing
             Right (LegacyMobileBankIDSignature_ _) -> Nothing
 
-    encB64               = T.decodeUtf8 . B64.encode
+    encB64 = T.decodeUtf8 . B64.encode
 
     providerSpecificData :: [Pair]
     providerSpecificData = case ssdData of
@@ -129,40 +132,38 @@ ssdToJson hidePN signatory SignatorySigningData {..} =
 
 idinSigningDataToJson :: Bool -> SignatoryLink -> EIDServiceNLIDINSignature -> [Pair]
 idinSigningDataToJson hidePN signatory details@EIDServiceNLIDINSignature {..} =
-  [ "nl_idin_data" .= object idinData ]
- where
-  idinData :: [Pair]
-  idinData =
-    [ "signatory_name" .= unEIDServiceIDINSigSignatoryName
-    , "signatory_customer_id" .= unEIDServiceIDINSigCustomerID
-    , "signatory_name_match" .= showt nameMatchResult
-    , "signatory_birthday_match" .= showt birthdayMatchResult
-    , "signatory_name_and_date_of_birth_match" .= showt nameBirthdayMatchResult
-    ]
-    <> birthdayField
-    <> matchErrorField
+  ["nl_idin_data" .= object idinData]
+  where
+    idinData :: [Pair]
+    idinData =
+      [ "signatory_name" .= unEIDServiceIDINSigSignatoryName
+        , "signatory_customer_id" .= unEIDServiceIDINSigCustomerID
+        , "signatory_name_match" .= showt nameMatchResult
+        , "signatory_date_of_birth_match" .= showt birthdayMatchResult
+        , "signatory_name_and_date_of_birth_match" .= showt nameBirthdayMatchResult
+        ]
+        <> birthdayField
+        <> matchErrorField
 
-  nameMatchResult :: NameMatchResult
-  nameMatchResult = matchSignatoryName signatory details
+    nameMatchResult :: IdinMatchResult
+    nameMatchResult = matchSignatoryName signatory details
 
-  birthdayMatchResult :: BirthdayMatchResult
-  birthdayMatchResult = matchSignatoryBirthDate signatory details
+    birthdayMatchResult :: BirthdayMatchResult
+    birthdayMatchResult = matchSignatoryBirthday signatory details
 
-  nameBirthdayMatchResult :: NameMatchResult
-  nameBirthdayMatchResult = case birthdayMatchResult of
-    MismatchDate err -> Mismatch err
-    SameDate -> nameMatchResult
+    nameBirthdayMatchResult :: IdinMatchResult
+    nameBirthdayMatchResult = case birthdayMatchResult of
+      MismatchDate err -> Mismatch err
+      SameDate         -> nameMatchResult
 
-  birthdayField :: [Pair]
-  birthdayField = if hidePN
-    then []
-    else ["signatory_date_of_birth" .= unEIDServiceIDINSigDateOfBirth]
+    birthdayField :: [Pair]
+    birthdayField =
+      if hidePN then [] else ["signatory_date_of_birth" .= unEIDServiceIDINSigDateOfBirth]
 
-  matchErrorField :: [Pair]
-  matchErrorField = case nameBirthdayMatchResult of
-    Mismatch err ->
-      ["mismatch_error" .= err]
-    _ -> []
+    matchErrorField :: [Pair]
+    matchErrorField = case nameBirthdayMatchResult of
+      Mismatch err -> ["mismatch_error" .= err]
+      _            -> []
 
 splitFirstSpace :: Text -> (Text, Text)
 splitFirstSpace str = case T.words str of
@@ -185,7 +186,7 @@ joinText a  b  = a <> " " <> b
 --   Match - if name matches exactly
 --   Misspelled - if there is 1~2 letters difference in the last name
 --   Mismatch - if the initials don't match or if more misspellings in the last name.
-matchSignatoryName :: SignatoryLink -> EIDServiceNLIDINSignature -> NameMatchResult
+matchSignatoryName :: SignatoryLink -> EIDServiceNLIDINSignature -> IdinMatchResult
 matchSignatoryName signatory details = matchSignatoryName' slFullName
                                                            eidInitials
                                                            eidLastName
@@ -196,28 +197,28 @@ matchSignatoryName signatory details = matchSignatoryName' slFullName
     eidFullName                = normalizeName $ unEIDServiceIDINSigSignatoryName details
     (eidInitials, eidLastName) = splitFirstSpace eidFullName
 
-matchSignatoryBirthDate :: SignatoryLink -> EIDServiceNLIDINSignature -> BirthdayMatchResult
-matchSignatoryBirthDate signatory details = case (mDate1, mDate2) of
-  (Just date1, Just date2) ->
-    if date1 == date2
+matchSignatoryBirthday
+  :: SignatoryLink -> EIDServiceNLIDINSignature -> BirthdayMatchResult
+matchSignatoryBirthday signatory details = case (mDate1, mDate2) of
+  (Just date1, Just date2) -> if date1 == date2
     then SameDate
-    else MismatchDate $
-      "birth dates does not match. expected "
-      <> showt date2 <> ", got " <> showt date1
-  (Nothing, Nothing) ->
-    MismatchDate $ "failed to parse birth dates"
-  (Nothing, _) ->
-    MismatchDate $ "failed to parse input birth date"
-  (_, Nothing) ->
-    MismatchDate $ "failed to parse birth date from iDIN"
- where
-  mDate1 :: Maybe Day
-  mDate1 = findSignatoryBirthDate signatory
+    else
+      MismatchDate
+      $  "birth dates does not match. expected "
+      <> showt date2
+      <> ", got "
+      <> showt date1
+  (Nothing, Nothing) -> MismatchDate "failed to parse birth dates"
+  (Nothing, _      ) -> MismatchDate "failed to parse input birth date"
+  (_      , Nothing) -> MismatchDate "failed to parse birth date from iDIN"
+  where
+    mDate1 :: Maybe Day
+    mDate1 = findSignatoryBirthDate signatory
 
-  mDate2 :: Maybe Day
-  mDate2 = parseDate $ unEIDServiceIDINSigDateOfBirth details
+    mDate2 :: Maybe Day
+    mDate2 = parseDate $ unEIDServiceIDINSigDateOfBirth details
 
-matchSignatoryName' :: Text -> Text -> Text -> NameMatchResult
+matchSignatoryName' :: Text -> Text -> Text -> IdinMatchResult
 matchSignatoryName' slFullName eidInitials eidLastName
   | slFullName == eidFullName = Match
   | slInitials == eidInitials = matchName eidLastName slRestName
@@ -225,9 +226,9 @@ matchSignatoryName' slFullName eidInitials eidLastName
   | otherwise = case
       matchSignatoryName' slFullName (T.dropEnd 1 eidInitials) eidLastName
     of
-      Match      -> Misspelled
-      Misspelled -> Misspelled
-      Mismatch err   -> Mismatch err
+      Match        -> Misspelled
+      Misspelled   -> Misspelled
+      Mismatch err -> Mismatch err
   where
     slNameWords = T.words slFullName
 
@@ -244,11 +245,11 @@ matchSignatoryName' slFullName eidInitials eidLastName
 
     eidFullName = eidInitials <> " " <> eidLastName
 
-matchName :: Text -> Text -> NameMatchResult
+matchName :: Text -> Text -> IdinMatchResult
 matchName s1 s2 | s1 == s2      = Match
                 | distance == 0 = Match
                 | distance <= 2 = Misspelled
-                | otherwise     = Mismatch "first name does not match"
+                | otherwise     = Mismatch "last name does not match"
   where distance = stringDistance (T.unpack s1) (T.unpack s2)
 
 -- Use diff algorithm to compare how many letters
@@ -274,9 +275,8 @@ parseDate = parseTimeM True defaultTimeLocale "%Y-%-m-%-d" . T.unpack
 
 findSignatoryBirthDate :: SignatoryLink -> Maybe Day
 findSignatoryBirthDate = L.firstJust matchField . signatoryfields
- where
-  matchField :: SignatoryField -> Maybe Day
-  matchField (SignatoryTextField textField)
-    | stfName textField == "DOB"
-    = parseDate $ stfValue textField
-  matchField _ = Nothing
+  where
+    matchField :: SignatoryField -> Maybe Day
+    matchField (SignatoryTextField textField) | stfName textField == "DOB" =
+      parseDate $ stfValue textField
+    matchField _ = Nothing
