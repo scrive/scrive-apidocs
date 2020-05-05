4729b5e3ccee5ea0ffc70fb699f147780e329579
Soares Chen <soares.chen@gmail.com>
2020-05-05 14:26:10 +0200

    Update kontrakcja-nix

--- nix/source/kontrakcja-nix.nix
+++ nix/source/kontrakcja-nix.nix
@@ -5,5 +5,5 @@ then ../../../kontrakcja-nix
 else
   builtins.fetchGit {
     url = "git@github.com:scrive/kontrakcja-nix.git";
-    rev = "f493273d0090a6ea8bd6daee9b79d42071812c23";
+    rev = "723d9a10f374c8f9fdfa685d935567b7f82ec3f7";
   }
