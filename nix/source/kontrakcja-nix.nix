6de20c9e5a8a5205f599b3515edb18ac32c4dc14
Soares Chen <soares.chen@gmail.com>
2020-05-05 15:13:29 +0200

    Update json and kontrakcja-template

--- nix/source/kontrakcja-nix.nix
+++ nix/source/kontrakcja-nix.nix
@@ -1,9 +1,8 @@
 { useLocal }:
-# ../../../kontrakcja-nix
 if useLocal
 then ../../../kontrakcja-nix
 else
   builtins.fetchGit {
     url = "git@github.com:scrive/kontrakcja-nix.git";
-    rev = "723d9a10f374c8f9fdfa685d935567b7f82ec3f7";
+    rev = "6e0bc1a6f7a02a6de38a5aad12dd45c3fb0af2cc";
   }
