2198cc96607b546157ea06df611e0bf3b80dc43a
Marek Bernat <mbernat@users.noreply.github.com>
2020-05-07 18:49:42 +0200

    Add failure state to flow engine (#1555)
    * Add failure state
    
    * Fix formatting
    
    * Document the Flow.Machinize module

--- backend/flow/src/Flow/Machinize.hs
+++ backend/flow/src/Flow/Machinize.hs
@@ -6,15 +6,31 @@ module Flow.Machinize where
 
 import Data.List.Extra (snoc)
 import Data.Set (Set)
-import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
 
 import Flow.HighTongue
 import Flow.Transducer
 
-data Deed = Field FieldName | Approval | Signature | View
+-- | Types of actions performed by the user.
+data Deed
+  -- | User provided data for a field `FieldName`.
+  -- Currently not used.
+  = Field FieldName
+  -- | User approved a document.
+  | Approval
+  -- | User signed a document.
+  | Signature
+  -- | User viewed a document.
+  | View
+  -- | User rejected a document (used in both signing and approval actions).
+  | Rejection
+  -- | User had not provided a response within the time limit.
+  -- Currently not used.
+  | Timeout
   deriving (Eq, Ord, Show)
 
+-- | A record of an event relevant to a Flow process.
+-- What happened, who did it, and which document was involved?
 data EventInfo = EventInfo
     { eventInfoDeed     :: Deed
     , eventInfoUser     :: UserName
@@ -22,34 +38,77 @@ data EventInfo = EventInfo
     }
   deriving (Eq, Ord, Show)
 
-eventize :: Expect -> [EventInfo]
-eventize = \case
+-- | Abstract actions triggered by the engine when transitioning
+-- between states.
+data LowAction
+  -- | An action specified in the DSL by the user, e.g. `Notify`.
+  = Action Action
+  -- | An action representing a failure of the Flow process.
+  | Fail
+  deriving (Eq, Show)
+
+-- | A state machine reacting to user events and triggering
+-- corresponding actions. Transitions are guarded by sets of events
+-- -- it only fires when all the required events arrive.
+type Machine = Transducer (Set EventInfo) [LowAction]
+type State = TransducerState (Set EventInfo) [LowAction]
+type Edge = TransducerEdge (Set EventInfo) [LowAction]
+
+-- | Translate `Expect`, a description of expected user actions,
+-- into collection of events to match against, assuming all
+-- actions are performed successfully.
+expectToSuccess :: Expect -> [EventInfo]
+expectToSuccess = \case
   ReceivedData {..} ->
     EventInfo <$> (Field <$> expectFields) <*> expectUsers <*> expectDocuments
   ApprovedBy {..} -> EventInfo Approval <$> expectUsers <*> expectDocuments
   ViewedBy {..}   -> EventInfo View <$> expectUsers <*> expectDocuments
   SignedBy {..}   -> EventInfo Signature <$> expectUsers <*> expectDocuments
 
-
-empty :: StateId -> Transducer a b
-empty id = Transducer Map.empty id []
-
-type Machine = Transducer (Set EventInfo) [Action]
-type State = TransducerState (Set EventInfo) [Action]
-type Edge = TransducerEdge (Set EventInfo) [Action]
-
-mkEdge :: (Stage, StateId) -> (StateId, Edge)
-mkEdge (stage, next) = (stateId, edge)
+-- | Create an `Edge` from the given stage to the next one, while
+-- aggregating all the events to wait for.
+mkSuccessEdge :: (Stage, StateId) -> (StateId, Edge)
+mkSuccessEdge (stage, next) = (stateId, edge)
   where
     stateId = stageName stage
-    events  = Set.fromList $ Set.toList (stageExpect stage) >>= eventize
-    edge    = TransducerEdge events (stageActions stage) next
+    events  = Set.fromList $ Set.toList (stageExpect stage) >>= expectToSuccess
+    edge    = TransducerEdge events (map Action $ stageActions stage) next
+
+-- | Translate `Expect`, a description of expected user actions,
+-- into a collection of events to match against, assuming all
+-- actions fail.
+expectToFailure :: Expect -> [EventInfo]
+expectToFailure = \case
+  ReceivedData {..} -> []
+  ApprovedBy {..}   -> EventInfo Rejection <$> expectUsers <*> expectDocuments
+  ViewedBy {..}     -> []
+  SignedBy {..}     -> EventInfo Rejection <$> expectUsers <*> expectDocuments
+
+-- | Create `Edge`s from the given stage to the Failure state, one per
+-- every negative event (rejection of approval or signing).
+mkFailureEdges :: StateId -> [LowAction] -> Stage -> [(StateId, Edge)]
+mkFailureEdges failureStateId failureLowActions stage = map mkEdge
+  $ concatMap expectToFailure expects
+  where
+    stateId = stageName stage
+    expects = Set.toList $ stageExpect stage
+    mkEdge e =
+      (stateId, TransducerEdge (Set.singleton e) failureLowActions failureStateId)
 
 -- TODO add end state
+-- | Compile the high level description of the Flow process into a state machine.
+--
+-- Each stage corresponds to a state and the flow is linear from one state to the next,
+-- conditioned of seing the expected events.
+--
+-- We also generate some internal states.
+-- State `final` is where the process goes to after the final steps specified by the user.
+-- State `failure` represents the failure of the process. We generate edges to it for every
+-- possible negative user action, e.g. when they refuse to approve or sign a document.
 linear :: HighTongue -> Either Error Machine
 linear HighTongue {..} = createTransducer initialState ["final"] edges
   where
-    edges        = map mkEdge pairs
+    edges = map mkSuccessEdge pairs <> concatMap (mkFailureEdges "failure" [Fail]) stages
     initialState = stageName $ head stages
     nextStates   = snoc (map stageName $ tail stages) "final"
     pairs        = zip stages nextStates
--- backend/test/src/FlowTests.hs
+++ backend/test/src/FlowTests.hs
@@ -5,6 +5,8 @@ import Test.Framework
 import TestKontra
 import qualified Flow.AggregatorTest as Aggregator
 import qualified Flow.IntegrationTest as Integration
+import qualified Flow.MachinizeTest as Machinize
 
 flowTests :: TestEnvSt -> Test
-flowTests env = testGroup "Flow" [Aggregator.tests, Integration.tests env]
+flowTests env =
+  testGroup "Flow" [Aggregator.tests, Integration.tests env, Machinize.tests]
