b2d1cf88087a8bacb7ce8f8a94f57c0aad9bc175
Andrzej Rybczak <andrzej@scrive.com>
2020-05-08 17:21:47 +0200

    Add a helper script for freezing cabal deps

---
+++ scripts/cabal_freeze.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -e
+
+cabal freeze
+
+sed -i '
+  /any.Cabal ==/d
+  /any.array ==/d
+  /any.base ==/d
+  /any.base-noprelude/d
+  /any.binary ==/d
+  /any.bytestring ==/d
+  /any.containers ==/d
+  /any.directory ==/d
+  /any.ghc-boot-th ==/d
+  /any.parsec ==/d
+  /any.process ==/d
+  /any.template-haskell ==/d
+  /any.text ==/d
+  /any.time ==/d
+  ' cabal.config
