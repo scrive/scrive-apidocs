43fae1f4f797581856e0938d66e0435a9a7399ac
Jakub Janczak <jakub.janczak@scrive.com>
2020-05-08 15:52:04 +0200

    CORE-2094: Allowing SSO to override the sessions everytime someone comes from IDP he gets a clean sheet (#1554)
    * Allowing SSO to override the sessions everytime someone comes from IDP he gets a clean sheet

--- backend/lib/SSO/API.hs
+++ backend/lib/SSO/API.hs
@@ -28,8 +28,6 @@ import Kontra
 import KontraLink
 import LoginAuth.LoginAuthMethod
 import Routing
-import Session.Cookies (startSessionCookieWithExpiry)
-import Session.Model
 import SSO.Conf
 import SSO.Guards
 import SSO.SAML
@@ -43,7 +41,7 @@ import Utils.HTTP (isSecure)
 
 sso :: Route (Kontra Response)
 sso = choice
-  [ (dir "sso" . dir "saml" . dir "acs" . hPost . toK0) consumeAssertions
+  [ (dir "sso" . dir "saml" . dir "acs" . hPostNoXToken . toK0) consumeAssertions
   , (dir "sso" . dir "saml" . dir "metadata" . hGet) renderMetadata
   ]
 
@@ -170,10 +168,9 @@ consumeAssertions = guardHttps . handle handleSAMLException $ do
 
     startSessionForSAMLUser :: Kontrakcja m => User -> m ()
     startSessionForSAMLUser user = do
-      let userID = user ^. #id
       when (user ^. #sysAuth /= LoginAuthSSO) $ apiError insufficientPrivileges
-      logInfo_ "Creating session for SSO:SAML"
-      startNewSessionWithUser userID >>= startSessionCookieWithExpiry
+      logInfo_ "Logging in user via SAML"
+      logUserToContext $ Just user
 
 getConf :: Kontrakcja m => m (Maybe SSOConf)
 getConf = ssoConf <$> getContext
