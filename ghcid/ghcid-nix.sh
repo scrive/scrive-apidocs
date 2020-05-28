5c44d7113546067fb5d34ef0a6840ed4077f9d98
Damian Gaweda <dgaw@users.noreply.github.com>
2020-05-28 09:59:47 +0200

    Better defaults for ghcid/ghcid-nix.sh (#1590)

--- ghcid/ghcid-nix.sh
+++ ghcid/ghcid-nix.sh
@@ -1,24 +1,37 @@
 #!/usr/bin/env bash
 
 # A little wrapper script that helps with running ghcid inside a kontrakcja nix workspace
+#
+# You can override ghcid options using env variables, e.g.
+# GHCID_SETUP=':set -Werror' ghcid-nix.sh
+# GHCID_REPL_OPTIONS='-fno-code' ghcid-nix.sh
+
+# Default options
+setup=${GHCID_SETUP:-':set -Wwarn'}
+repl_options=${GHCID_REPL_OPTIONS:-''}
 
 if [ -z "$IN_NIX_SHELL" ] || [ -z "$KONTRAKCJA_WORKSPACE" ]; then
-  echo "You should run this script inside a nix-shell. Use nix-shell.sh first."
+  echo "You should run this script inside a nix-shell."
   exit 1
 fi
 
 cd "$KONTRAKCJA_WORKSPACE"
 
-setup=':set -Wwarn'
-repl_options='-fobject-code'
-
 case "$1" in
   cron)
-    ghcid --command="cabal v2-repl kontrakcja:kontrakcja-cron --repl-options=$repl_options" \
+    ghcid --command="cabal v2-repl kontrakcja:cron --repl-options=$repl_options" \
           --setup="$setup"
     ;;
   migrate)
     ghcid --command="cabal v2-repl kontrakcja:kontrakcja-migrate --repl-options=$repl_options" \
+          --setup="$setup"
+    ;;
+  messenger)
+    ghcid --command="cabal v2-repl kontrakcja:messenger-server --repl-options=$repl_options" \
+          --setup="$setup"
+    ;;
+  mailing)
+    ghcid --command="cabal v2-repl kontrakcja:mailing-server --repl-options=$repl_options" \
           --setup="$setup"
     ;;
   test)
