8892db112257c288e9909c52b1c0013ff900c445
Soares Chen <soares.chen@scrive.com>
2020-06-03 09:44:34 +0200

    [CORE-2212] Fix GHC 8.8 Nix shell on MacOS (#1605)

--- nix/source/kontrakcja-nix.nix
+++ nix/source/kontrakcja-nix.nix
@@ -4,5 +4,5 @@ then ../../../kontrakcja-nix
 else
   builtins.fetchGit {
     url = "git@github.com:scrive/kontrakcja-nix.git";
-    rev = "e4563f04ec0e1ab8aae652adff59a4b55d880b9d";
+    rev = "2e2bb3656e2a77b79b7f69aa15540ef5b2cc899d";
   }
