37fd6b84372f77031e090fe3c0c010788932fd9b
Soares Chen <soares.chen@scrive.com>
2020-06-09 15:44:01 +0200

    Pin hlint version in kontrakcja-nix (#1621)

--- nix/source/kontrakcja-nix.nix
+++ nix/source/kontrakcja-nix.nix
@@ -4,5 +4,5 @@ then ../../../kontrakcja-nix
 else
   builtins.fetchGit {
     url = "git@github.com:scrive/kontrakcja-nix.git";
-    rev = "2e2bb3656e2a77b79b7f69aa15540ef5b2cc899d";
+    rev = "30b8c3066e026167bfaa8084635e11e783d665af";
   }
