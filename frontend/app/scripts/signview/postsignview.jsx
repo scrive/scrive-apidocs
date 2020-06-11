3a8c7dd788f3af8f54187e670d79b9308ecb2071
Bartek Ćwikłowski <paczesiowa@gmail.com>
2020-06-11 13:56:57 +0200

    style fix

--- frontend/app/scripts/signview/postsignview.jsx
+++ frontend/app/scripts/signview/postsignview.jsx
@@ -31,9 +31,9 @@ module.exports = React.createClass({
           <hr/>
           {localization.postSignview.proposal}
           <ul>
-            <li>{"\u26AB " + localization.postSignview.reason1}</li>
-            <li>{"\u26AB " + localization.postSignview.reason2}</li>
-            <li>{"\u26AB " + localization.postSignview.reason3}</li>
+            <li>{"\u1F784 " + localization.postSignview.reason1}</li>
+            <li>{"\u1F784 " + localization.postSignview.reason2}</li>
+            <li>{"\u1F784 " + localization.postSignview.reason3}</li>
             </ul>
           </div>
         </Modal.Content>
