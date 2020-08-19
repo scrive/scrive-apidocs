f890d00f019185b06964017714753f667e77314e
bugthunk <fredrik@scrive.com>
2020-08-19 13:25:59 +0200

    Add non-nix version of SAML helper script

---
+++ scripts/saml-metadata-to-config-no-nix.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+
+BASEDIR=`dirname $0`
+
+if [ $# -lt 1 ]; then
+    echo "$0 <default_usergroup_id>" > /dev/stderr
+    exit 1
+fi
+
+METADATA="$(cat)"
+IDP_ID="$(echo $METADATA | xmllint --xpath "string(/*[local-name() = 'EntityDescriptor']/@*[local-name() = 'entityID'])" -)"
+CERTIFICATE="$(echo $METADATA | xmllint --xpath "(/*[local-name() = 'EntityDescriptor']/*[local-name() = 'IDPSSODescriptor']/*[local-name() = 'KeyDescriptor'][@*[local-name() = 'use' and .= 'signing']]/*[local-name() = 'KeyInfo']/*[local-name() = 'X509Data']/*[local-name() = 'X509Certificate'])[1]/text()" -)"
+echo $CERTIFICATE
+PUBLIC_KEY=`echo $CERTIFICATE | $BASEDIR/x509-extract-public-key.sh | $BASEDIR/pem-to-one-line.sh`
+
+echo "{\"idp_name\": \"\", \"idp_id\": \"$IDP_ID\", \"idp_public_key\": \"$PUBLIC_KEY\", \"idp_init_ug\": \"$1\"}" | jq
+
+
